9fe9d9f80f27b011fcb272b747343ac1254e6f38
algorithmzuo <algorithmzuo@163.com>
2023-08-09 19:40:54 +0800

    modify code

--- src/code/Video_028_2_RadixSort.java
+++ src/code/Video_028_2_RadixSort.java
@@ -19,16 +19,21 @@ public class Video_028_2_RadixSort {
 		if (arr.length > 1) {
 			// 如果会溢出，那么要改用long类型数组来排序
 			int n = arr.length;
+			// 找到数组中的最小值
 			int min = arr[0];
 			for (int i = 1; i < n; i++) {
 				min = Math.min(min, arr[i]);
 			}
 			int max = 0;
 			for (int i = 0; i < n; i++) {
+				// 数组中的每个数字，减去数组中的最小值，就把arr转成了非负数组
 				arr[i] -= min;
+				// 记录数组中的最大值
 				max = Math.max(max, arr[i]);
 			}
+			// 根据最大值在BASE进制下的位数，决定基数排序做多少轮
 			radixSort(arr, n, bits(max));
+			// 数组中所有数都减去了最小值，所以最后不要忘了还原
 			for (int i = 0; i < n; i++) {
 				arr[i] += min;
 			}
@@ -49,18 +54,22 @@ public class Video_028_2_RadixSort {
 	// 基数排序核心代码
 	// arr内要保证没有负数
 	// n是arr的长度
-	// m是arr中最大值在BASE进制下有几位
-	public static void radixSort(int[] arr, int n, int m) {
+	// bits是arr中最大值在BASE进制下有几位
+	public static void radixSort(int[] arr, int n, int bits) {
 		// 理解的时候可以假设BASE = 10
-		for (int offset = 1; m > 0; offset *= BASE, m--) {
+		for (int offset = 1; bits > 0; offset *= BASE, bits--) {
 			Arrays.fill(cnts, 0);
 			for (int i = 0; i < n; i++) {
+				// 数字提取某一位的技巧
 				cnts[(arr[i] / offset) % BASE]++;
 			}
+			// 处理成前缀次数累加的形式
 			for (int i = 1; i < BASE; i++) {
 				cnts[i] = cnts[i] + cnts[i - 1];
 			}
 			for (int i = n - 1; i >= 0; i--) {
+				// 前缀数量分区的技巧
+				// 数字提取某一位的技巧
 				help[--cnts[(arr[i] / offset) % BASE]] = arr[i];
 			}
 			for (int i = 0; i < n; i++) {
