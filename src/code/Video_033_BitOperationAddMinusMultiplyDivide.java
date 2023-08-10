7dbc4e6f098b270473f1f46606efb1d51bfb30b3
algorithmzuo <algorithmzuo@163.com>
2023-08-10 22:45:24 +0800

    modify code

--- src/code/Video_033_BitOperationAddMinusMultiplyDivide.java
+++ src/code/Video_033_BitOperationAddMinusMultiplyDivide.java
@@ -5,6 +5,47 @@ package code;
 // 测试链接 : https://leetcode.cn/problems/divide-two-integers/
 public class Video_033_BitOperationAddMinusMultiplyDivide {
 
+	public static int MIN = Integer.MIN_VALUE;
+
+	public static int divide(int a, int b) {
+		if (a == MIN && b == MIN) {
+			// a和b都是整数最小
+			return 1;
+		}
+		if (a != MIN && b != MIN) {
+			// a和b都不是整数最小，那么正常去除
+			return div(a, b);
+		}
+		if (b == MIN) {
+			// a不是整数最小，b是整数最小
+			return 0;
+		}
+		// a是整数最小，b是-1，返回整数最大，因为题目里明确这么说了
+		if (b == neg(1)) {
+			return Integer.MAX_VALUE;
+		}
+		// a是整数最小，b不是整数最小，b也不是-1
+		a = add(a, b < 0 ? neg(b) : b);
+		int ans = div(a, b);
+		int offset = b < 0 ? 1 : neg(1);
+		return add(ans, offset);
+	}
+
+	// 必须保证a和b都不是整数最小值
+	// 返回a除以b的结果
+	public static int div(int a, int b) {
+		int x = a < 0 ? neg(a) : a;
+		int y = b < 0 ? neg(b) : b;
+		int ans = 0;
+		for (int i = 30; i >= 0; i = minus(i, 1)) {
+			if ((x >> i) >= y) {
+				ans |= (1 << i);
+				x = minus(x, y << i);
+			}
+		}
+		return a < 0 ^ b < 0 ? neg(ans) : ans;
+	}
+
 	public static int add(int a, int b) {
 		int ans = a;
 		while (b != 0) {
@@ -33,44 +74,6 @@ public class Video_033_BitOperationAddMinusMultiplyDivide {
 			b >>>= 1;
 		}
 		return ans;
-	}
-
-	public static int MIN = Integer.MIN_VALUE;
-
-	public static int divide(int a, int b) {
-		if (a == MIN && b == MIN) {
-			return 1;
-		}
-		if (a != MIN && b != MIN) {
-			return div(a, b);
-		}
-		if (b == MIN) {
-			return 0;
-		}
-		// a是整数最小，b是-1，根据题目的要求，返回整数最大
-		if (b == neg(1)) {
-			return Integer.MAX_VALUE;
-		}
-		// a是整数最小，b不是整数最小也不是-1
-		a = add(a, b < 0 ? neg(b) : b);
-		int ans = div(a, b);
-		int offset = a < 0 ^ b < 0 ? neg(1) : 1;
-		return add(ans, offset);
-	}
-
-	// 必须保证a和b都不是整数最小值
-	// 返回a除以b的结果
-	public static int div(int a, int b) {
-		int x = a < 0 ? neg(a) : a;
-		int y = b < 0 ? neg(b) : b;
-		int ans = 0;
-		for (int i = 30; i >= 0; i = minus(i, 1)) {
-			if ((x >> i) >= y) {
-				ans |= (1 << i);
-				x = minus(x, y << i);
-			}
-		}
-		return a < 0 ^ b < 0 ? neg(ans) : ans;
 	}
 
 }
