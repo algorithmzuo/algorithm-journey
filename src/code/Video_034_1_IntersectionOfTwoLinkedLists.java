4d6e29db921ceecb389e7510f2ce12b30abe8c8c
algorithmzuo <algorithmzuo@163.com>
2023-08-14 17:53:50 +0800

    modify code

--- src/code/Video_034_1_IntersectionOfTwoLinkedLists.java
+++ src/code/Video_034_1_IntersectionOfTwoLinkedLists.java
@@ -15,36 +15,36 @@ public class Video_034_1_IntersectionOfTwoLinkedLists {
 		if (h1 == null || h2 == null) {
 			return null;
 		}
-		ListNode x1 = h1, x2 = h2;
+		ListNode a = h1, b = h2;
 		int diff = 0;
-		while (x1.next != null) {
-			x1 = x1.next;
+		while (a.next != null) {
+			a = a.next;
 			diff++;
 		}
-		while (x2.next != null) {
-			x2 = x2.next;
+		while (b.next != null) {
+			b = b.next;
 			diff--;
 		}
-		if (x1 != x2) {
+		if (a != b) {
 			return null;
 		}
 		if (diff >= 0) {
-			x1 = h1;
-			x2 = h2;
+			a = h1;
+			b = h2;
 		} else {
-			x1 = h2;
-			x2 = h1;
+			a = h2;
+			b = h1;
 		}
 		diff = Math.abs(diff);
 		while (diff != 0) {
-			x1 = x1.next;
+			a = a.next;
 			diff--;
 		}
-		while (x1 != x2) {
-			x1 = x1.next;
-			x2 = x2.next;
+		while (a != b) {
+			a = a.next;
+			b = b.next;
 		}
-		return x1;
+		return a;
 	}
 
 }
