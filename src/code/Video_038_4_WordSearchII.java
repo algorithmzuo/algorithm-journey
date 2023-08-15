f503c5fe3d1deac2974a4b83517a5f97b89e8a45
algorithmzuo <algorithmzuo@163.com>
2023-08-15 23:54:54 +0800

    modify code

--- src/code/Video_038_4_WordSearchII.java
+++ src/code/Video_038_4_WordSearchII.java
@@ -12,6 +12,8 @@ public class Video_038_4_WordSearchII {
 
 	public static int[][] tree = new int[MAXN][26];
 
+	public static int[] pass = new int[MAXN];
+
 	public static String[] end = new String[MAXN];
 
 	public static int cnt;
@@ -20,12 +22,14 @@ public class Video_038_4_WordSearchII {
 		cnt = 1;
 		for (String word : words) {
 			int cur = 1;
+			pass[cur]++;
 			for (int i = 0, path; i < word.length(); i++) {
 				path = word.charAt(i) - 'a';
 				if (tree[cur][path] == 0) {
 					tree[cur][path] = ++cnt;
 				}
 				cur = tree[cur][path];
+				pass[cur]++;
 			}
 			end[cur] = word;
 		}
@@ -34,6 +38,7 @@ public class Video_038_4_WordSearchII {
 	public static void clearTrie() {
 		for (int i = 1; i <= cnt; i++) {
 			Arrays.fill(tree[i], 0);
+			pass[i] = 0;
 			end[i] = null;
 		}
 	}
@@ -50,25 +55,30 @@ public class Video_038_4_WordSearchII {
 		return ans;
 	}
 
-	public static void dfs(char[][] board, int i, int j, int t, List<String> ans) {
+	public static int dfs(char[][] board, int i, int j, int t, List<String> ans) {
 		if (i < 0 || i == board.length || j < 0 || j == board[0].length || board[i][j] == 0) {
-			return;
+			return 0;
 		}
 		char tmp = board[i][j];
 		int road = tmp - 'a';
 		t = tree[t][road];
-		if (t != 0) {
-			if (end[t] != null) {
-				ans.add(end[t]);
-				end[t] = null;
-			}
-			board[i][j] = 0;
-			dfs(board, i - 1, j, t, ans);
-			dfs(board, i + 1, j, t, ans);
-			dfs(board, i, j - 1, t, ans);
-			dfs(board, i, j + 1, t, ans);
-			board[i][j] = tmp;
+		if (t == 0 || pass[t] == 0) {
+			return 0;
 		}
+		int fix = 0;
+		if (end[t] != null) {
+			fix++;
+			ans.add(end[t]);
+			end[t] = null;
+		}
+		board[i][j] = 0;
+		fix += dfs(board, i - 1, j, t, ans);
+		fix += dfs(board, i + 1, j, t, ans);
+		fix += dfs(board, i, j - 1, t, ans);
+		fix += dfs(board, i, j + 1, t, ans);
+		pass[t] -= fix;
+		board[i][j] = tmp;
+		return fix;
 	}
 
 }
