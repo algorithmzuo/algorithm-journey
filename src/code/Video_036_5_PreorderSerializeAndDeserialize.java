7d12c08ff88c9d34bbd3468bcf4bb97fb86951ae
algorithmzuo <algorithmzuo@163.com>
2023-08-15 21:00:10 +0800

    modify code

--- src/code/Video_036_5_PreorderSerializeAndDeserialize.java
+++ src/code/Video_036_5_PreorderSerializeAndDeserialize.java
@@ -2,7 +2,7 @@ package code;
 
 // 二叉树先序序列化和反序列化
 // 测试链接 : https://leetcode.cn/problems/serialize-and-deserialize-binary-tree/
-public class Video_036_6_PreorderSerializeAndDeserialize {
+public class Video_036_5_PreorderSerializeAndDeserialize {
 
 	// 不提交这个类
 	public static class TreeNode {
@@ -15,8 +15,19 @@ public class Video_036_6_PreorderSerializeAndDeserialize {
 		}
 	}
 
+    // 二叉树可以通过先序、后序或者按层遍历的方式序列化和反序列化
+    // 但是，二叉树无法通过中序遍历的方式实现序列化和反序列化
+    // 因为不同的两棵树，可能得到同样的中序序列，即便补了空位置也可能一样。
+    // 比如如下两棵树
+    //         __2
+    //        /
+    //       1
+    //       和
+    //       1__
+    //          \
+    //           2
+    // 补足空位置的中序遍历结果都是{ null, 1, null, 2, null}
 	// 提交这个类
-	// 先序序列化
 	public class Codec {
 
 		public String serialize(TreeNode root) {
