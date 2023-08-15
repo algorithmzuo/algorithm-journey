3cda13461a6150157a510c4335bf1115c2a9b2b4
algorithmzuo <algorithmzuo@163.com>
2023-08-15 17:27:11 +0800

    modify code

--- src/code/Video_037_3_LowestCommonAncestorBinarySearch.java
+++ src/code/Video_037_3_LowestCommonAncestorBinarySearch.java
@@ -16,9 +16,9 @@ public class Video_037_3_LowestCommonAncestorBinarySearch {
 		// root从上到下
 		// 如果先遇到了p，说明p是答案
 		// 如果先遇到了q，说明q是答案
-		// 如果root在p和q的值之间，不用管p和q谁大谁小，只要root在中间，那么此时的root就是答案
-		// 如果root在p和q的值的左侧，那么root往右移动
-		// 如果root在p和q的值的右侧，那么root往左移动
+		// 如果root在p~q的值之间，不用管p和q谁大谁小，只要root在中间，那么此时的root就是答案
+		// 如果root在p~q的值的左侧，那么root往右移动
+		// 如果root在p~q的值的右侧，那么root往左移动
 		while (root.val != p.val && root.val != q.val) {
 			if (Math.min(p.val, q.val) < root.val && root.val < Math.max(p.val, q.val)) {
 				break;
