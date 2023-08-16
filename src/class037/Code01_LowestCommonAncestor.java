91db09f0885b944a60f47bf4c68ee3cc997bc31b
algorithmzuo <algorithmzuo@163.com>
2023-08-16 20:14:48 +0800

    modify code

--- src/class037/Code01_LowestCommonAncestor.java
+++ src/class037/Code01_LowestCommonAncestor.java
@@ -13,19 +13,16 @@ public class Code01_LowestCommonAncestor {
 
 	// 提交如下的方法
 	public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		if (root == null) {
-			return null;
-		}
-		if (root == p || root == q) {
+		if (root == null || root == p || root == q) {
 			return root;
 		}
 		TreeNode l = lowestCommonAncestor(root.left, p, q);
 		TreeNode r = lowestCommonAncestor(root.right, p, q);
-		if (l == null && r == null) {
-			return null;
-		}
 		if (l != null && r != null) {
 			return root;
+		}
+		if (l == null && r == null) {
+			return null;
 		}
 		return l != null ? l : r;
 	}
