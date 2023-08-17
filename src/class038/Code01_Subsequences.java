549bce3c503e6085aebad2c8a9db9c87a1daec3d
algorithmzuo <algorithmzuo@163.com>
2023-08-17 22:20:56 +0800

    modify code

--- src/class038/Code01_Subsequences.java
+++ src/class038/Code01_Subsequences.java
@@ -3,6 +3,7 @@ package class038;
 import java.util.HashSet;
 
 // 字符串的全部子序列
+// 子序列本身是可以有重复的，只是这个题目要求去重
 // 测试链接 : https://www.nowcoder.com/practice/92e6247998294f2c933906fdedbc6e6a
 public class Code01_Subsequences {
 
