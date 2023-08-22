9de5eecc3764e022456de0e53dc48012e90cd925
XuJi <xuji@MacBook-Air.local>
2023-08-23 01:55:22 +0800

    t

--- src/class038/Code04_PermutationWithoutRepetition.java
+++ src/class038/Code04_PermutationWithoutRepetition.java
@@ -6,39 +6,42 @@ import java.util.List;
 
 // 有重复项数组的去重全排列
 // 测试链接 : https://leetcode.cn/problems/permutations-ii/
+// 1:03:25 ~ 1:06:38
 public class Code04_PermutationWithoutRepetition {
 
-	public static List<List<Integer>> permuteUnique(int[] nums) {
-		List<List<Integer>> ans = new ArrayList<>();
-		f(nums, 0, ans);
-		return ans;
-	}
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        func(nums, 0, ans);
+        return ans;
+    }
 
-	public static void f(int[] nums, int i, List<List<Integer>> ans) {
-		if (i == nums.length) {
-			List<Integer> cur = new ArrayList<>();
-			for (int num : nums) {
-				cur.add(num);
-			}
-			ans.add(cur);
-		} else {
-			HashSet<Integer> set = new HashSet<>();
-			for (int j = i; j < nums.length; j++) {
-				// nums[j]没有来到过i位置，才会去尝试
-				if (!set.contains(nums[j])) {
-					set.add(nums[j]);
-					swap(nums, i, j);
-					f(nums, i + 1, ans);
-					swap(nums, i, j);
-				}
-			}
-		}
-	}
+    private void func(int[] nums, int i, List<List<Integer>> ans) {
+        if (i == nums.length) {
+            List<Integer> cur = new ArrayList<>();
+            for (int num : nums)
+                cur.add(num);
+            ans.add(cur);
+        } else {
+            for (int j = i; j < nums.length; j++) {
+                if (check(nums, i, j)) continue;
+                swap(nums, i, j);
+                func(nums, i + 1, ans);
+                swap(nums, i, j);
+            }
+        }
+    }
 
-	public static void swap(int[] nums, int i, int j) {
-		int tmp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = tmp;
-	}
+    private void swap(int[] nums, int i, int j) {
+        if (i == j) return;
+        nums[i] ^= nums[j];
+        nums[j] ^= nums[i];
+        nums[i] ^= nums[j];
+    }
+
+    private boolean check(int[] nums, int i, int j) {
+        while (i < j && nums[i] != nums[j])
+            i++;
+        return i < j;
+    }
 
 }
--- src/class039/Code01_BasicCalculatorIII.java
+++ src/class039/Code01_BasicCalculatorIII.java
@@ -1,68 +1,129 @@
 package class039;
 
-import java.util.ArrayList;
+import java.util.Stack;
 
 // 含有嵌套的表达式求值
 // 测试链接 : https://leetcode.cn/problems/basic-calculator-iii/
+// 01:27 ~ 39:00
 public class Code01_BasicCalculatorIII {
 
-	public static int calculate(String str) {
-		where = 0;
-		return f(str.toCharArray(), 0);
-	}
+    public static int start;
 
-	public static int where;
+    public static int calculate(String str) {
+        start = 0;
+        return func1(str.toCharArray(), 0);
+//        return func2(str.toCharArray(), 0);// 我改写的😄
+    }
 
-	// s[i....]开始计算，遇到字符串终止 或者 遇到)停止
-	// 返回 : 自己负责的这一段，计算的结果
-	// 返回之间，更新全局变量where，为了上游函数知道从哪继续！
-	public static int f(char[] s, int i) {
-		int cur = 0;
-		ArrayList<Integer> numbers = new ArrayList<>();
-		ArrayList<Character> ops = new ArrayList<>();
-		while (i < s.length && s[i] != ')') {
-			if (s[i] >= '0' && s[i] <= '9') {
-				cur = cur * 10 + s[i++] - '0';
-			} else if (s[i] != '(') {
-				// 遇到了运算符 + - * /
-				push(numbers, ops, cur, s[i++]);
-				cur = 0;
-			} else {
-				// i (.....)
-				// 遇到了左括号！
-				cur = f(s, i + 1);
-				i = where + 1;
-			}
-		}
-		push(numbers, ops, cur, '+');
-		where = i;
-		return compute(numbers, ops);
-	}
+    // 12:58
+    // str[i....]开始计算，遇到字符串终止 或者 遇到)停止
+    // 返回 : 自己负责的这一段，计算的结果
+    // 返回之间，更新全局变量start，为了上游函数知道从哪继续！
+    // In computer programming, an operand is any object capable of being manipulated.
+    // For example, in "1 + 2" the "1" and "2" are the operands and the plus symbol is the operator.
+    public static int func1(char[] str, int i) {
+        int operand = 0;
+        Stack<Integer> operands = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        while (i < str.length && str[i] != ')') {
+            if (Character.isDigit(str[i])) {
+                operand = operand * 10 + str[i++] - '0';
+            } else if (str[i] != '(') {
+                push1(operands, operators, operand, str[i++]);
+                operand = 0;
+            } else {
+                operand = func1(str, i + 1);
+                i = start + 1;
+            }
+        }
+        push1(operands, operators, operand, '+');
+        start = i;
+        return compute1(operands, operators);
+    }
 
-	public static void push(ArrayList<Integer> numbers, ArrayList<Character> ops, int cur, char op) {
-		int n = numbers.size();
-		if (n == 0 || ops.get(n - 1) == '+' || ops.get(n - 1) == '-') {
-			numbers.add(cur);
-			ops.add(op);
-		} else {
-			int topNumber = numbers.get(n - 1);
-			char topOp = ops.get(n - 1);
-			if (topOp == '*') {
-				numbers.set(n - 1, topNumber * cur);
-			} else {
-				numbers.set(n - 1, topNumber / cur);
-			}
-			ops.set(n - 1, op);
-		}
-	}
+    public static int func2(char[] str, int i) {
+        int operand = 0;
+        char operator = '+';
+        Stack<Integer> operands = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        push2(operands, operators, operand, operator);
+        while (i < str.length && str[i] != ')') {
+            if (Character.isDigit(str[i])) {
+                operand = operand * 10 + str[i++] - '0';
+            } else if (str[i] != '(') {
+                push2(operands, operators, operand, operator);
+                operand = 0;
+                operator = str[i++];
+            } else {
+                operand = func2(str, i + 1);
+                i = start + 1;
+            }
+        }
+        push2(operands, operators, operand, operator);
+        start = i;
+        return compute2(operands, operators);
+    }
 
-	public static int compute(ArrayList<Integer> numbers, ArrayList<Character> ops) {
-		int n = numbers.size();
-		int ans = numbers.get(0);
-		for (int i = 1; i < n; i++) {
-			ans += ops.get(i - 1) == '+' ? numbers.get(i) : -numbers.get(i);
-		}
-		return ans;
-	}
+    public static void push1(Stack<Integer> operands, Stack<Character> operators, int operand, char operator) {
+        int n = operands.size();
+        if (n == 0 || operators.peek() == '+' || operators.peek() == '-') {
+            operands.push(operand);
+            operators.push(operator);
+        } else {
+            int topOperand = operands.pop();
+            char topOperator = operators.pop();
+            if (topOperator == '*') {
+                operands.push(topOperand * operand);
+            } else if (topOperator == '/') {
+                operands.push(topOperand / operand);
+            }
+            operators.push(operator);
+        }
+    }
+
+    public static void push2(Stack<Integer> operands, Stack<Character> operators, int operand, char operator) {
+        if (operator == '+' || operator == '-') {
+            operands.push(operand);
+            operators.push(operator);
+        } else {
+            int topOperand = operands.pop();
+            if (operator == '*')
+                operands.push(topOperand * operand);
+            else if (operator == '/')
+                operands.push(topOperand / operand);
+        }
+    }
+
+    public static int compute1(Stack<Integer> operands, Stack<Character> operators) {
+        int invalid = operators.pop();
+        int ans = operands.pop();
+        while (!operands.isEmpty()) {
+            char operator = operators.pop();
+            if (operator == '+')
+                ans = ans + operands.pop();
+            else if (operator == '-')
+                ans = -ans + operands.pop();
+        }
+        return ans;
+    }
+
+    public static int compute2(Stack<Integer> operands, Stack<Character> operators) {
+        int ans = 0;
+        while (!operands.isEmpty() && !operators.isEmpty()) {
+            char operator = operators.pop();
+            if (operator == '+')
+                ans += operands.pop();
+            else if (operator == '-')
+                ans -= operands.pop();
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+//        String expression = "3+(2*(9-4))";
+        String expression = "33+33";
+        int result = calculate(expression);
+        System.out.println("Result: " + result);
+    }
 
 }
