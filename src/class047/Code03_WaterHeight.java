9095285dbf770d57f9217f4f4e8add5584558446
algorithmzuo <algorithmzuo@163.com>
2023-08-25 13:53:13 +0800

    modify code

--- src/class047/Code03_WaterHeight.java
+++ src/class047/Code03_WaterHeight.java
@@ -14,7 +14,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 
-public class Code02_WaterHeight {
+public class Code03_WaterHeight {
 
 	// 题目说了m <= 10^6，代表湖泊宽度
 	// 这就是MAXN的含义，湖泊最大宽度的极限
@@ -22,7 +22,7 @@ public class Code02_WaterHeight {
 
 	// 数值保护，因为题目中v最大是10000
 	// 所以左侧影响最远的位置到达了x - 3 * v + 1
-	// 所以右侧影响最远的位置到达了x + 3 * v + 1
+	// 所以右侧影响最远的位置到达了x + 3 * v - 1
 	// x如果是正式的位置(1~m)，那么左、右侧可能超过正式位置差不多30000的规模
 	// 这就是OFFSET的含义
 	public static int OFFSET = 30001;
@@ -32,7 +32,7 @@ public class Code02_WaterHeight {
 	// 所以准备一个长度为OFFSET + MAXN + OFFSET的数组
 	// 这样一来，左侧影响最远的位置...右侧影响最远的位置，
 	// 都可以被arr中的下标表示出来，就省去了很多越界讨论
-	// 详细解释看change方法的注释
+	// 详细解释看fall方法的注释
 	public static int[] arr = new int[OFFSET + MAXN + OFFSET];
 
 	public static int n, m;
@@ -51,10 +51,10 @@ public class Code02_WaterHeight {
 				in.nextToken();
 				x = (int) in.nval;
 				// v体积的朋友，在x处落水，修改差分数组
-				change(v, x);
+				fall(v, x);
 			}
 			// 生成水位数组
-			compute();
+			build();
 			// 开始收集答案
 			// 0...OFFSET这些位置是辅助位置，为了防止越界设计的
 			// 从OFFSET+1开始往下数m个，才是正式的位置1...m
@@ -71,24 +71,28 @@ public class Code02_WaterHeight {
 		br.close();
 	}
 
-	public static void change(int v, int x) {
+	public static void fall(int v, int x) {
 		// 为了防止x - 3 * v + 1是个负数，进而有很多、很烦的边界讨论
 		// 所以任何位置，都加上一个较大的数字(OFFSET)
 		// 这样一来，所有下标就都在0以上了，省去了太多的边界问题
 		// 这就是为什么arr在初始化的时候要准备OFFSET + MAXN + OFFSET这么多的空间
-		arr[x - 3 * v + 1 + OFFSET] += 1;
-		arr[x - 2 * v + 1 + OFFSET] -= 2;
-		arr[x + 1 + OFFSET] += 2;
-		arr[x + 2 * v + 1 + OFFSET] -= 2;
-		arr[x + 3 * v + 1 + OFFSET] += 1;
+		set(x - 3 * v + 1 + OFFSET, x - 2 * v + OFFSET, 1, v, 1);
+		set(x - 2 * v + 1 + OFFSET, x + OFFSET, v - 1, -v, -1);
+		set(x + 1 + OFFSET, x + 2 * v + OFFSET, -v + 1, v, 1);
+		set(x + 2 * v + 1 + OFFSET, x + 3 * v - 1 + OFFSET, v - 1, 1, -1);
 	}
 
-	public static void compute() {
-		// 先把arr加工成 "增幅数组"
+	public static void set(int l, int r, int s, int e, int d) {
+		arr[l] += s;
+		arr[l + 1] += d - s;
+		arr[r + 1] -= d + e;
+		arr[r + 2] += e;
+	}
+
+	public static void build() {
 		for (int i = 1; i <= m + OFFSET; i++) {
 			arr[i] += arr[i - 1];
 		}
-		// 最后把arr加工成 "水位数组"
 		for (int i = 1; i <= m + OFFSET; i++) {
 			arr[i] += arr[i - 1];
 		}
