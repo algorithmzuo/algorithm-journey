6d938ad1b982af28c5f194d5d02f7ec7d386e192
algorithmzuo <algorithmzuo@163.com>
2023-08-29 15:11:04 +0800

    modify code

--- src/class054/Code06_MaximalRectangle.java
+++ src/class054/Code06_MaximalRectangle.java
@@ -1,5 +1,7 @@
 package class054;
 
+import java.util.Arrays;
+
 // 最大矩形
 // 给定一个仅包含 0 和 1 、大小为 rows * cols 的二维二进制矩阵
 // 找出只包含 1 的最大矩形，并返回其面积
@@ -9,28 +11,29 @@ public class Code06_MaximalRectangle {
 	public static int maximalRectangle(char[][] map) {
 		int n = map.length;
 		int m = map[0].length;
-		int[] height = new int[m];
+		Arrays.fill(height, 0, m, 0);
 		int ans = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				height[j] = map[i][j] == '0' ? 0 : height[j] + 1;
 			}
-			ans = Math.max(largestRectangleArea(height), ans);
+			ans = Math.max(largestRectangleArea(m), ans);
 		}
 		return ans;
 	}
 
 	public static int MAXN = 201;
 
+	public static int[] height = new int[MAXN];
+
 	public static int[] stack = new int[MAXN];
 
 	public static int r;
 
-	public static int largestRectangleArea(int[] height) {
-		int n = height.length;
+	public static int largestRectangleArea(int m) {
 		r = 0;
 		int ans = 0, cur, left;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < m; i++) {
 			while (r > 0 && height[i] <= height[stack[r - 1]]) {
 				cur = stack[--r];
 				left = r == 0 ? -1 : stack[r - 1];
@@ -41,7 +44,7 @@ public class Code06_MaximalRectangle {
 		while (r > 0) {
 			cur = stack[--r];
 			left = r == 0 ? -1 : stack[r - 1];
-			ans = Math.max(ans, (n - left - 1) * height[cur]);
+			ans = Math.max(ans, (m - left - 1) * height[cur]);
 		}
 		return ans;
 	}
---
+++ src/class056/Code03_CourseScheduleII.java
@@ -0,0 +1,46 @@
+package class056;
+
+import java.util.ArrayList;
+
+// 课程表 II
+// 现在你总共有 numCourses 门课需要选，记为 0 到 numCourses - 1
+// 给你一个数组 prerequisites ，其中 prerequisites[i] = [ai, bi]
+// 表示在选修课程 ai 前 必须 先选修 bi
+// 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示：[0,1]
+// 返回你为了学完所有课程所安排的学习顺序。可能会有多个正确的顺序
+// 你只要返回 任意一种 就可以了。如果不可能完成所有课程，返回 一个空数组
+// 测试链接 : https://leetcode.cn/problems/course-schedule-ii/
+public class Code03_CourseScheduleII {
+
+	public static int[] findOrder(int n, int[][] need) {
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			graph.add(new ArrayList<>());
+		}
+		int[] indegree = new int[n];
+		for (int[] edge : need) {
+			graph.get(edge[1]).add(edge[0]);
+			indegree[edge[0]]++;
+		}
+		int[] queue = new int[n];
+		int l = 0;
+		int r = 0;
+		for (int i = 0; i < n; i++) {
+			if (indegree[i] == 0) {
+				queue[r++] = i;
+			}
+		}
+		int count = 0;
+		while (l < r) {
+			int cur = queue[l++];
+			count++;
+			for (int next : graph.get(cur)) {
+				if (--indegree[next] == 0) {
+					queue[r++] = next;
+				}
+			}
+		}
+		return count == n ? queue : new int[0];
+	}
+
+}
