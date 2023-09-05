fd33a12f97f5c751692b79a6951c27199461c3ed
algorithmzuo <algorithmzuo@163.com>
2023-09-05 16:49:30 +0800

    modify code

--- src/class051/Code05_WaitingTime.java
+++ src/class051/Code05_WaitingTime.java
@@ -8,7 +8,7 @@ import java.util.PriorityQueue;
 // 假设：m远远大于n，比如n <= 1000, m <= 10的9次方，该怎么做？
 // 谷歌的面试，这个题连考了2个月
 // 没有找到测试链接，写了对数器验证
-public class Code05_MinWaitingTime {
+public class Code05_WaitingTime {
 
 	// 堆模拟
 	// 验证方法，不是重点
