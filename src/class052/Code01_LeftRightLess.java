8b47af07b78c051f3d4814faf87ebb1dbb8c24ee
algorithmzuo <algorithmzuo@163.com>
2023-09-08 16:49:50 +0800

    modify code

--- src/class052/Code01_LeftRightLess.java
+++ src/class052/Code01_LeftRightLess.java
@@ -56,6 +56,7 @@ public class Code01_LeftRightLess {
 	public static void compute() {
 		r = 0;
 		int cur;
+		// 遍历阶段
 		for (int i = 0; i < n; i++) {
 			while (r > 0 && arr[stack[r - 1]] >= arr[i]) {
 				cur = stack[--r];
@@ -64,16 +65,15 @@ public class Code01_LeftRightLess {
 			}
 			stack[r++] = i;
 		}
+		// 清算阶段
 		while (r > 0) {
 			cur = stack[--r];
 			ans[cur][0] = r > 0 ? stack[r - 1] : -1;
 			ans[cur][1] = -1;
 		}
-		for (int i = 1; i < n; i++) {
-			if (ans[i][0] != -1 && arr[ans[i][0]] == arr[i]) {
-				ans[i][0] = ans[ans[i][0]][0];
-			}
-		}
+		// 修正阶段
+		// 左侧的答案不需要修正一定是正确的，只有右侧答案需要修正
+		// 从右往左修正，n-1位置的右侧答案一定是-1，不需要修正
 		for (int i = n - 2; i >= 0; i--) {
 			if (ans[i][1] != -1 && arr[ans[i][1]] == arr[i]) {
 				ans[i][1] = ans[ans[i][1]][1];
