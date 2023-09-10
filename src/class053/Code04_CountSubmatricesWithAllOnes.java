f9b1f2de7afa966c5f3e1d283fdb9e3aa3021837
algorithmzuo <algorithmzuo@163.com>
2023-09-10 14:36:54 +0800

    modify code

--- src/class053/Code04_CountSubmatricesWithAllOnes.java
+++ src/class053/Code04_CountSubmatricesWithAllOnes.java
@@ -39,33 +39,42 @@ public class Code04_CountSubmatricesWithAllOnes {
 	//    1         1         1
 	//             
 	//    2  ....   6   ....  9
-	// 如上图，假设在6位置，1的高度为6
-	// 在6位置的左边，离6位置最近、且小于高度6的位置是2，2位置的高度是3
-	// 在6位置的右边，离6位置最近、且小于高度6的位置是9，9位置的高度是4
+	//   left      cur        i
+	// 如上图，假设6位置从栈中弹出，6位置的高度为6(上面6个1)
+	// 6位置的左边、离6位置最近、且小于高度6的是2位置(left)，2位置的高度是3
+	// 6位置的右边、离6位置最近、且小于高度6的是9位置(i)，9位置的高度是4
 	// 此时我们求什么？
-	// 1) 求在3~8范围上，必须以高度6作为高的矩形，有几个？
-	// 2) 求在3~8范围上，必须以高度5作为高的矩形，有几个？
-	// 也就是说，<=4的高度，一律不求
-	// 那么，1) 求必须以位置6的高度6作为高的矩形，有几个？
+	// 1) 求在3~8范围上必须以高度6作为高的矩形有几个？
+	// 2) 求在3~8范围上必须以高度5作为高的矩形有几个？
+	// 也就是说，<=4的高度一律不求，>6的高度一律不求！
+	// 其他位置也会从栈里弹出，等其他位置弹出的时候去求！
+	// 那么在3~8范围上必须以高度6作为高的矩形有几个？如下：
 	// 3..3  3..4  3..5  3..6  3..7  3..8
 	// 4..4  4..5  4..6  4..7  4..8
 	// 5..5  5..6  5..7  5..8
 	// 6..6  6..7  6..8
 	// 7..7  7..8
 	// 8..8
-	// 这么多！= 21 = (9 - 2 - 1) * (9 - 2) / 2
-	// 这就是任何一个数字从栈里弹出的时候，计算矩形数量的方式
+	// 21个！什么公式？
+	// 3...8范围的长度为6，那么数量就是 : 6 * 7 / 2
+	// 同理在3~8范围上，必须以高度5作为高的矩形也是这么多
+	// 所以cur从栈里弹出时产生的数量 : 
+	// (cur位置的高度-Max{left位置的高度,i位置的高度}) * ((i-left-1)*(i-left)/2)
 	public static int countFromBottom(int m) {
 		int ans = 0;
 		r = 0;
-		for (int i = 0, left, len, down; i < m; i++) {
+		for (int i = 0, left, len, bottom; i < m; i++) {
 			while (r > 0 && height[stack[r - 1]] >= height[i]) {
 				int cur = stack[--r];
 				if (height[cur] > height[i]) {
+					// 只有height[cur] > height[i]才结算
+					// 如果是因为height[cur]==height[i]导致cur位置从栈中弹出
+					// 那么不结算！等i位置弹出的时候再说！
+					// 上一节课讲了很多这种相等时候的处理，比如"柱状图中最大的矩形"问题
 					left = r == 0 ? -1 : stack[r - 1];
 					len = i - left - 1;
-					down = Math.max(left == -1 ? 0 : height[left], height[i]);
-					ans += (height[cur] - down) * num(len);
+					bottom = Math.max(left == -1 ? 0 : height[left], height[i]);
+					ans += (height[cur] - bottom) * len * (len + 1) / 2;
 				}
 			}
 			stack[r++] = i;
@@ -75,13 +84,9 @@ public class Code04_CountSubmatricesWithAllOnes {
 			int left = r == 0 ? -1 : stack[r - 1];
 			int len = m - left - 1;
 			int down = left == -1 ? 0 : height[left];
-			ans += (height[cur] - down) * num(len);
+			ans += (height[cur] - down) * len * (len + 1) / 2;
 		}
 		return ans;
-	}
-
-	public static int num(int len) {
-		return ((len * (1 + len)) >> 1);
 	}
 
 }
