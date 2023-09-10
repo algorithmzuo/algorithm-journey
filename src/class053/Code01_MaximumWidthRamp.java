849336f83fa17b03725b1cc3c3a5224675d5a599
algorithmzuo <algorithmzuo@163.com>
2023-09-10 15:17:13 +0800

    modify code

--- src/class053/Code01_MaximumWidthRamp.java
+++ src/class053/Code01_MaximumWidthRamp.java
@@ -13,16 +13,18 @@ public class Code01_MaximumWidthRamp {
 	public static int r;
 
 	public static int maxWidthRamp(int[] arr) {
-		r = 0;
+		// 令r=1相当于0位置进栈了
+		// stack[0] = 0，然后栈的大小变成1
+		r = 1;
 		int n = arr.length;
-		for (int i = 0; i < n; i++) {
-			if (r == 0 || arr[stack[r - 1]] > arr[i]) {
+		for (int i = 1; i < n; i++) {
+			if (arr[stack[r - 1]] > arr[i]) {
 				stack[r++] = i;
 			}
 		}
 		int ans = 0;
 		for (int j = n - 1; j >= 0; j--) {
-			while (r != 0 && arr[stack[r - 1]] <= arr[j]) {
+			while (r > 0 && arr[stack[r - 1]] <= arr[j]) {
 				int i = stack[--r];
 				ans = Math.max(ans, j - i);
 			}
