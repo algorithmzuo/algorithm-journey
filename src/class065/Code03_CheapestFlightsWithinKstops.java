1e063acd70f7ff50c8da91fd2134c62d8bb8ab7a
algorithmzuo <algorithmzuo@163.com>
2023-09-17 17:18:41 +0800

    modify code

---
+++ src/class065/Code03_CheapestFlightsWithinKstops.java
@@ -0,0 +1,31 @@
+package class065;
+
+import java.util.Arrays;
+
+// K 站中转内最便宜的航班
+// 有 n 个城市通过一些航班连接。给你一个数组 flights
+// 其中 flights[i] = [fromi, toi, pricei]
+// 表示该航班都从城市 fromi 开始，以价格 pricei 抵达 toi。
+// 现在给定所有的城市和航班，以及出发城市 src 和目的地 dst，你的任务是找到出一条最多经过 k 站中转的路线
+// 使得从 src 到 dst 的 价格最便宜 ，并返回该价格。 如果不存在这样的路线，则输出 -1。
+// 测试链接 : https://leetcode.cn/problems/cheapest-flights-within-k-stops/
+public class Code03_CheapestFlightsWithinKstops {
+
+	// bellman-ford算法
+	public static int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+		int[] cost = new int[n];
+		Arrays.fill(cost, Integer.MAX_VALUE);
+		cost[src] = 0;
+		for (int i = 0; i <= k; i++) {
+			int[] next = Arrays.copyOf(cost, n);
+			for (int[] f : flights) {
+				if (cost[f[0]] != Integer.MAX_VALUE) {
+					next[f[1]] = Math.min(next[f[1]], cost[f[0]] + f[2]);
+				}
+			}
+			cost = next;
+		}
+		return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
+	}
+
+}
---
+++ src/class065/Code04_SPFA.java
@@ -0,0 +1,108 @@
+package class065;
+
+// bellman-ford算法 + SPFA优化
+// 给定一个 n个点的有向图，请求出图中是否存在从顶点 1 出发能到达的负环
+// 负环的定义是：一条边权之和为负数的回路。
+// 测试链接 : https://www.luogu.com.cn/problem/P3385
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+import java.util.Arrays;
+
+public class Code04_SPFA {
+
+	public static int MAXN = 2001;
+
+	public static int MAXM = 6001;
+
+	// 链式前向星建图
+	public static int[] head = new int[MAXN];
+
+	public static int[] next = new int[MAXM];
+
+	public static int[] to = new int[MAXM];
+
+	public static int[] weight = new int[MAXM];
+
+	public static int cnt;
+
+	// spfa需要
+	public static int[] distance = new int[MAXN];
+
+	public static int[] updateCnt = new int[MAXN];
+
+	public static int MAXQ = 4000001;
+
+	public static int[] queue = new int[MAXQ];
+
+	public static int l, r;
+
+	public static void build(int n) {
+		cnt = 1;
+		l = r = 0;
+		Arrays.fill(head, 1, n + 1, 0);
+		Arrays.fill(distance, 1, n + 1, Integer.MAX_VALUE);
+		Arrays.fill(updateCnt, 1, n + 1, 0);
+	}
+
+	public static void addEdge(int u, int v, int w) {
+		next[cnt] = head[u];
+		to[cnt] = v;
+		weight[cnt] = w;
+		head[u] = cnt++;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		in.nextToken();
+		int cases = (int) in.nval;
+		for (int i = 0, n, m; i < cases; i++) {
+			in.nextToken(); n = (int) in.nval;
+			in.nextToken(); m = (int) in.nval;
+			build(n);
+			for (int j = 0, u, v, w; j < m; j++) {
+				in.nextToken(); u = (int) in.nval;
+				in.nextToken(); v = (int) in.nval;
+				in.nextToken(); w = (int) in.nval;
+				if (w >= 0) {
+					addEdge(u, v, w);
+					addEdge(v, u, w);
+				} else {
+					addEdge(u, v, w);
+				}
+			}
+			out.println(spfa(n) ? "YES" : "NO");
+		}
+		out.flush();
+		out.close();
+		br.close();
+	}
+
+	public static boolean spfa(int n) {
+		distance[1] = 0;
+		updateCnt[1]++;
+		queue[r++] = 1;
+		while (l < r) {
+			int u = queue[l++];
+			for (int edge = head[u], v, w; edge > 0; edge = next[edge]) {
+				v = to[edge];
+				w = weight[edge];
+				if (distance[u] + w < distance[v]) {
+					distance[v] = distance[u] + w;
+					if (updateCnt[v]++ == n) {
+						return true;
+					}
+					queue[r++] = v;
+				}
+			}
+		}
+		return false;
+	}
+
+}
