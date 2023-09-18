3f513f6cb60dd133e750a2113a41ee506ba09e2d
algorithmzuo <algorithmzuo@163.com>
2023-09-18 18:20:48 +0800

    modify code

--- src/class064/Code02_DijkstraLuogu.java
+++ src/class064/Code02_DijkstraLuogu.java
@@ -14,9 +14,10 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
-// 即便是比赛，也能通过的实现
-// 建图用链式前向星、堆也是完全自己全实现一遍，不用任何动态结构
-// 这个实现留给有需要的同学，一般情况下并不需要做到这个程度
+// 以下的实现，即便是比赛也能通过
+// 建图用链式前向星、堆也是用数组结构手写的，不用任何动态结构
+// 这个实现留给有需要的同学，这个测试必须这么写才能通过
+// 但是一般情况下并不需要做到这个程度
 public class Code02_DijkstraLuogu {
 
 	public static int MAXN = 100001;
