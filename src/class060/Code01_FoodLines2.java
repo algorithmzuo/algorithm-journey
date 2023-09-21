d89e6ad5fb8ccaef3d08ff4e1933d3972bd96af7
algorithmzuo <algorithmzuo@163.com>
2023-09-21 20:24:52 +0800

    modify code

--- src/class060/Code01_FoodLines2.java
+++ src/class060/Code01_FoodLines2.java
@@ -17,7 +17,7 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
-public class Code01_FoodLines {
+public class Code01_FoodLines2 {
 
 	public static int MAXN = 5001;
 
@@ -34,6 +34,9 @@ public class Code01_FoodLines {
 
 	public static int cnt;
 
+	// 是不是食物链最顶层
+	public static boolean[] top = new boolean[MAXN];
+
 	// 拓扑排序需要的队列
 	public static int[] queue = new int[MAXN];
 
@@ -48,11 +51,14 @@ public class Code01_FoodLines {
 	public static void build(int n) {
 		cnt = 1;
 		Arrays.fill(indegree, 0, n + 1, 0);
+		Arrays.fill(top, 0, n + 1, true);
 		Arrays.fill(lines, 0, n + 1, 0);
 		Arrays.fill(head, 0, n + 1, 0);
 	}
 
 	public static void addEdge(int u, int v) {
+		top[u] = false;
+		indegree[v]++;
 		next[cnt] = head[u];
 		to[cnt] = v;
 		head[u] = cnt++;
@@ -73,7 +79,6 @@ public class Code01_FoodLines {
 				in.nextToken();
 				v = (int) in.nval;
 				addEdge(u, v);
-				indegree[v]++;
 			}
 			out.println(ways());
 		}
@@ -91,19 +96,19 @@ public class Code01_FoodLines {
 				lines[i] = 1;
 			}
 		}
-		int ans = 0;
 		while (l < r) {
-			int u = queue[l++];
-			if (head[u] == 0) {
-				ans = (ans + lines[u]) % MOD;
-			} else {
-				for (int ei = head[u], v; ei > 0; ei = next[ei]) {
-					v = to[ei];
-					lines[v] = (lines[v] + lines[u]) % MOD;
-					if (--indegree[v] == 0) {
-						queue[r++] = v;
-					}
+			for (int u = queue[l++], v, edge = head[u]; edge != 0; edge = next[edge]) {
+				v = to[edge];
+				lines[v] = (lines[v] + lines[u]) % MOD;
+				if (--indegree[v] == 0) {
+					queue[r++] = v;
 				}
+			}
+		}
+		int ans = 0;
+		for (int i = 1; i <= n; i++) {
+			if (top[i]) {
+				ans = (ans + lines[i]) % MOD;
 			}
 		}
 		return ans;
