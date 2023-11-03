fe9d575769da1d00c098b85bbd09567a973eb1bd
algorithmzuo <algorithmzuo@163.com>
2023-11-03 14:27:02 +0800

    modify code

--- src/class070/Code01_MaximumSubarray.java
+++ src/class070/Code01_MaximumSubarray.java
@@ -6,18 +6,20 @@ package class070;
 // 测试链接 : https://leetcode.cn/problems/maximum-subarray/
 public class Code01_MaximumSubarray {
 
+	// 动态规划
 	public static int maxSubArray1(int[] nums) {
 		int n = nums.length;
 		int[] dp = new int[n];
 		dp[0] = nums[0];
 		int ans = nums[0];
 		for (int i = 1; i < n; i++) {
-			dp[i] = nums[i] + Math.max(0, dp[i - 1]);
+			dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
 			ans = Math.max(ans, dp[i]);
 		}
 		return ans;
 	}
 
+	// 空间压缩
 	public static int maxSubArray2(int[] nums) {
 		int n = nums.length;
 		int ans = nums[0];
@@ -40,20 +42,20 @@ public class Code01_MaximumSubarray {
 
 	public static int sum;
 
-	public static void maxSubArray3(int[] nums) {
+	public static void extra(int[] nums) {
 		left = right = 0;
 		sum = nums[0];
-		for (int i = 1, l = 0, pre = nums[0]; i < nums.length; i++) {
+		for (int l = 0, r = 1, pre = nums[0]; r < nums.length; r++) {
 			if (pre >= 0) {
-				pre += nums[i];
+				pre += nums[r];
 			} else {
-				pre = nums[i];
-				l = i;
+				pre = nums[r];
+				l = r;
 			}
 			if (pre > sum) {
 				sum = pre;
 				left = l;
-				right = i;
+				right = r;
 			}
 		}
 	}
