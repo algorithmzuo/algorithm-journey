c4ec108d1bf8637ee56349b3f6754a12a48a0216
algorithmzuo <algorithmzuo@163.com>
2023-11-03 15:19:41 +0800

    modify code

--- src/class070/Code06_MaximumSubmatrix.java
+++ src/class070/Code06_MaximumSubmatrix.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 // 子矩阵最大累加和问题
 // 给定一个二维数组grid，找到其中子矩阵的最大累加和
 // 返回拥有最大累加和的子矩阵左上角和右下角坐标
-// 如果有多个子矩阵都有最大累加和，返回任何一个都可以
+// 如果有多个子矩阵都有最大累加和，返回哪一个都可以
 // 测试链接 : https://leetcode.cn/problems/max-submatrix-lcci/
 public class Code06_MaximumSubmatrix {
 
