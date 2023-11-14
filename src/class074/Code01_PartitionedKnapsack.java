348b01293132f61924eb47323abe887bd720cfd9
algorithmzuo <algorithmzuo@163.com>
2023-11-14 16:52:56 +0800

    modify code

--- src/class074/Code01_PartitionedKnapsack.java
+++ src/class074/Code01_PartitionedKnapsack.java
@@ -1,9 +1,10 @@
 package class074;
 
 // 分组背包(模版)
-// 自01背包问世之后，小 A 对此深感兴趣
-// 一天，小 A 去远游，却发现他的背包不同于 01 背包，他的物品大致可分为 k 组
-// 每组中的物品只能选择1件，现在他想知道最大的利用价值是多少
+// 给定一个正数m表示背包的容量，有n个货物可供挑选
+// 每个货物有自己的体积(容量消耗)、价值(获得收益)、组号(分组)
+// 同一组内的物品只能挑选1件，所有挑选物品的体积总和不能超过背包容量
+// 怎么挑选货物能达到价值最大，返回最大的价值
 // 测试链接 : https://www.luogu.com.cn/problem/P1757
 // 请同学们务必参考如下代码中关于输入、输出的处理
 // 这是输入输出处理效率很高的写法
@@ -23,9 +24,9 @@ public class Code01_PartitionedKnapsack {
 
 	public static int MAXM = 1001;
 
-	// arr[i][0] 重量
-	// arr[i][1] 价值
-	// arr[i][2] 组号
+	// arr[i][0] i号物品的体积
+	// arr[i][1] i号物品的价值
+	// arr[i][2] i号物品的组号
 	public static int[][] arr = new int[MAXN][3];
 
 	public static int[] dp = new int[MAXM];
