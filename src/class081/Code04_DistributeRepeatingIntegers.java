79ee9edd08ddce107abdf923002eb4c5051106ff
algorithmzuo <algorithmzuo@163.com>
2023-12-01 15:04:57 +0800

    modify code

--- src/class081/Code04_DistributeRepeatingIntegers.java
+++ src/class081/Code04_DistributeRepeatingIntegers.java
@@ -11,15 +11,8 @@ import java.util.Arrays;
 // 测试链接 : https://leetcode.cn/problems/distribute-repeating-integers/
 public class Code04_DistributeRepeatingIntegers {
 
-	// 不能用贪心的例子 :
-	// nums = [1,1,2,2,1]
-	// quantity = [2,2,1]
 	// 时间复杂度O(n * 3的m次方)，空间复杂度O(n * 2的m次方)
 	// ppt上有时间复杂度解析
-	// 纯暴力的递归（不做任何动态规划）的解法速度更快，但是并不推荐，不再讲述
-	// 原因上节课说了（讲解080-状压dp-上，题目3）
-	// 同样的数据量，如果出题人刻意构造严苛的数据状况，
-	// 纯暴力的递归（不做任何动态规划）就无法通过了，但是状压dp一定可以通过
 	public static boolean canDistribute(int[] nums, int[] quantity) {
 		Arrays.sort(nums);
 		int n = 1;
@@ -39,8 +32,8 @@ public class Code04_DistributeRepeatingIntegers {
 			}
 		}
 		cnt[n - 1] = c;
-		int m = 1 << quantity.length;
-		int[] sum = new int[m];
+		int m = quantity.length;
+		int[] sum = new int[1 << m];
 		// 下面这个枚举是生成quantity中的每个子集，所需要数字的个数
 		for (int i = 0, v, h; i < quantity.length; i++) {
 			v = quantity[i];
@@ -49,35 +42,35 @@ public class Code04_DistributeRepeatingIntegers {
 				sum[h | j] = sum[j] + v;
 			}
 		}
-		int[][] dp = new int[n][m];
-		return f(cnt, sum, 0, m - 1, dp);
+		int[][] dp = new int[1 << m][n];
+		return f(cnt, sum, (1 << m) - 1, 0, dp);
 	}
 
-	public static boolean f(int[] cnt, int[] sum, int i, int j, int[][] dp) {
-		if (j == 0) {
+	public static boolean f(int[] cnt, int[] sum, int status, int index, int[][] dp) {
+		if (status == 0) {
 			return true;
 		}
-		if (i == cnt.length) {
+		if (index == cnt.length) {
 			return false;
 		}
-		if (dp[i][j] != 0) {
-			return dp[i][j] == 1;
+		if (dp[status][index] != 0) {
+			return dp[status][index] == 1;
 		}
 		boolean ans = false;
-		int c = cnt[i];
+		int c = cnt[index];
 		// 这是整个实现最核心的枚举
-		// s枚举了j的所有子集状态
+		// s枚举了status的所有子集状态
 		// 建议记住
-		for (int s = j; s > 0; s = (s - 1) & j) {
-			if (sum[s] <= c && f(cnt, sum, i + 1, j ^ s, dp)) {
+		for (int s = status; s > 0; s = (s - 1) & status) {
+			if (sum[s] <= c && f(cnt, sum, status ^ s, index + 1, dp)) {
 				ans = true;
 				break;
 			}
 		}
 		if (!ans) {
-			ans = f(cnt, sum, i + 1, j, dp);
+			ans = f(cnt, sum, status, index + 1, dp);
 		}
-		dp[i][j] = ans ? 1 : -1;
+		dp[status][index] = ans ? 1 : -1;
 		return ans;
 	}
 
