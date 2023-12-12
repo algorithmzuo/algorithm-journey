575435cb668ecf0fc617bf35911fb19b2d86faa0
algorithmzuo <algorithmzuo@163.com>
2023-12-12 18:44:51 +0800

    modify code

--- src/class084/Code04_CountSpecialIntegers.java
+++ src/class084/Code04_CountSpecialIntegers.java
@@ -3,7 +3,7 @@ package class084;
 // 统计特殊整数
 // 如果一个正整数每一个数位都是 互不相同 的，我们称它是 特殊整数
 // 给你一个正整数n，请你返回区间 [1, n] 之间特殊整数的数目
-//  测试链接 : https://leetcode.cn/problems/count-special-integers/
+// 测试链接 : https://leetcode.cn/problems/count-special-integers/
 public class Code04_CountSpecialIntegers {
 
 	public static int[] offset = {
