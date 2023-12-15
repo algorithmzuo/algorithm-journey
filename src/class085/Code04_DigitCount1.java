88079ef1b235bec244056bdeaa3e054f920f5044
algorithmzuo <algorithmzuo@163.com>
2023-12-15 13:07:57 +0800

    modify code

--- src/class085/Code04_DigitCount1.java
+++ src/class085/Code04_DigitCount1.java
@@ -1,47 +1,18 @@
 package class085;
 
 // 范围内的数字计数
-// 给定两个正整数a和b，求在[a,b] 中的所有整数中
-// 每个数码(digit)各出现了多少次
-// 测试链接 : https://www.luogu.com.cn/problem/P2602
-// 请同学们务必参考如下代码中关于输入、输出的处理
-// 这是输入输出处理效率很高的写法
-// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.StreamTokenizer;
-
+// 给定一个在 0 到 9 之间的整数 d，和两个正整数 low 和 high 分别作为上下界
+// 返回 d 在 low 和 high 之间的整数中出现的次数，包括边界 low 和 high
+// 测试链接 : https://leetcode.cn/problems/digit-count-in-range/
 public class Code04_DigitCount1 {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StreamTokenizer in = new StreamTokenizer(br);
-		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-		while (in.nextToken() != StreamTokenizer.TT_EOF) {
-			long a = (long) in.nval;
-			in.nextToken();
-			long b = (long) in.nval;
-			for (int i = 0; i < 9; i++) {
-				out.print(digitsCount(i, a, b) + " ");
-			}
-			out.println(digitsCount(9, a, b));
-		}
-		out.flush();
-		out.close();
-		br.close();
-	}
-
-	public static long digitsCount(int d, long low, long high) {
+	public static int digitsCount(int d, int low, int high) {
 		return count(high, d) - count(low - 1, d);
 	}
 
-	public static long count(long n, int d) {
-		long ans = 0;
-		for (long i = 1, tmp = n, high, cur; tmp != 0; i *= 10, tmp /= 10) {
+	public static int count(int n, int d) {
+		int ans = 0;
+		for (int i = 1, tmp = n, high, cur; tmp != 0; i *= 10, tmp /= 10) {
 			high = tmp / 10;
 			if (d == 0) {
 				if (high == 0) {
