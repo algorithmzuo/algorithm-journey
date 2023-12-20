93dc5e6747e7c30d1030e98df4a03e4fd62b9aad
algorithmzuo <algorithmzuo@163.com>
2023-12-20 20:48:30 +0800

    modify code

--- src/class086/Code04_Diving1.java
+++ src/class086/Code04_Diving1.java
@@ -21,8 +21,12 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 
-// 讲解069 - 多维费用背包 + 空间压缩
-public class Code04_Diving {
+// 讲解069 - 多维费用背包
+// 不做空间压缩的版本
+// 无法通过全部测试用例
+// 这个题必须做空间压缩
+// 空间压缩的实现在Code04_Diving2
+public class Code04_Diving1 {
 
 	public static int MAXN = 101;
 
@@ -34,17 +38,19 @@ public class Code04_Diving {
 
 	public static int[] c = new int[MAXN];
 
-	public static int[][] dp = new int[MAXM][MAXM];
+	public static int[][][] dp = new int[MAXN][MAXM][MAXM];
 
-	public static String[][] path = new String[MAXM][MAXM];
+	public static String[][][] path = new String[MAXN][MAXM][MAXM];
 
 	public static int m, v, n;
 
 	public static void build() {
-		for (int i = 0; i <= m; i++) {
-			for (int j = 0; j <= v; j++) {
-				dp[i][j] = 0;
-				path[i][j] = null;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j <= m; j++) {
+				for (int k = 0; k <= v; k++) {
+					dp[i][j][k] = 0;
+					path[i][j][k] = null;
+				}
 			}
 		}
 	}
@@ -69,33 +75,36 @@ public class Code04_Diving {
 				c[i] = (int) in.nval;
 			}
 			compute();
-			out.println(dp[m][v]);
-			out.println(path[m][v]);
+			out.println(dp[n][m][v]);
+			out.println(path[n][m][v]);
 		}
 		out.flush();
 		out.close();
 		br.close();
 	}
 
-	// 这个方法就是多维费用背包的空间压缩版本
-	// 请务必掌握空间压缩技巧
-	// 之前的课讲了很多遍了
+	// 不做空间压缩
+	// 最普通版本的多维费用背包
 	public static void compute() {
-		String cur;
+		String p2;
 		for (int i = 1; i <= n; i++) {
-			for (int j = m; j >= a[i]; j--) {
-				for (int k = v; k >= b[i]; k--) {
-					if (path[j - a[i]][k - b[i]] == null) {
-						cur = String.valueOf(i);
-					} else {
-						cur = path[j - a[i]][k - b[i]] + " " + String.valueOf(i);
-					}
-					if (dp[j][k] < dp[j - a[i]][k - b[i]] + c[i]) {
-						dp[j][k] = dp[j - a[i]][k - b[i]] + c[i];
-						path[j][k] = cur;
-					} else if (dp[j][k] == dp[j - a[i]][k - b[i]] + c[i]) {
-						if (cur.compareTo(path[j][k]) < 0) {
-							path[j][k] = cur;
+			for (int j = 0; j <= m; j++) {
+				for (int k = 0; k <= v; k++) {
+					dp[i][j][k] = dp[i - 1][j][k];
+					path[i][j][k] = path[i - 1][j][k];
+					if (j - a[i] >= 0 && k - b[i] >= 0) {
+						if (path[i - 1][j - a[i]][k - b[i]] == null) {
+							p2 = String.valueOf(i);
+						} else {
+							p2 = path[i - 1][j - a[i]][k - b[i]] + " " + String.valueOf(i);
+						}
+						if (dp[i][j][k] < dp[i - 1][j - a[i]][k - b[i]] + c[i]) {
+							dp[i][j][k] = dp[i - 1][j - a[i]][k - b[i]] + c[i];
+							path[i][j][k] = p2;
+						} else if (dp[i][j][k] == dp[i - 1][j - a[i]][k - b[i]] + c[i]) {
+							if (path[i][j][k] == null || p2.compareTo(path[i][j][k]) < 0) {
+								path[i][j][k] = p2;
+							}
 						}
 					}
 				}
