f18390e5c770515874688224e13114d0d0214dcc
algorithmzuo <algorithmzuo@163.com>
2023-12-29 17:49:23 +0800

    modify code

--- src/class091/Code02_SmallestRange.java
+++ src/class091/Code02_SmallestRange.java
@@ -9,29 +9,38 @@ import java.util.TreeSet;
 // 测试链接 : https://leetcode.cn/problems/smallest-range-covering-elements-from-k-lists/
 public class Code02_SmallestRange {
 
+	public static class Node {
+		public int v;
+		public int i;
+		public int j;
+
+		public Node(int a, int b, int c) {
+			v = a;
+			i = b;
+			j = c;
+		}
+	}
+
 	public static int[] smallestRange(List<List<Integer>> nums) {
 		int n = nums.size();
-		// 0 : 值
-		// 1 : 哪个数组
-		// 2 : 哪个下标
-		TreeSet<int[]> set = new TreeSet<>((a, b) -> a[0] != b[0] ? (a[0] - b[0]) : (a[1] - b[1]));
+		TreeSet<Node> set = new TreeSet<>((a, b) -> a.v != b.v ? (a.v - b.v) : (a.i - b.i));
 		for (int i = 0; i < n; i++) {
-			set.add(new int[] { nums.get(i).get(0), i, 0 });
+			set.add(new Node(nums.get(i).get(0), i, 0));
 		}
 		int r = Integer.MAX_VALUE;
 		int a = 0;
 		int b = 0;
-		int[] max, min;
+		Node max, min;
 		while (set.size() == n) {
 			max = set.last();
 			min = set.pollFirst();
-			if (max[0] - min[0] < r) {
-				r = max[0] - min[0];
-				a = min[0];
-				b = max[0];
+			if (max.v - min.v < r) {
+				r = max.v - min.v;
+				a = min.v;
+				b = max.v;
 			}
-			if (min[2] < nums.get(min[1]).size() - 1) {
-				set.add(new int[] { nums.get(min[1]).get(min[2] + 1), min[1], min[2] + 1 });
+			if (min.j + 1 < nums.get(min.i).size()) {
+				set.add(new Node(nums.get(min.i).get(min.j + 1), min.i, min.j + 1));
 			}
 		}
 		return new int[] { a, b };
