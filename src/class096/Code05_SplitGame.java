45c2fef9b1ceca7f68b8ee24bcbf03167650f671
algorithmzuo <algorithmzuo@163.com>
2024-01-04 00:01:34 +0800

    modify code

--- src/class096/Code05_SplitGame.java
+++ src/class096/Code05_SplitGame.java
@@ -75,7 +75,7 @@ public class Code05_SplitGame {
 
 	public static String compute() {
 		int eor = 0;
-		for (int i = n - 1; i >= 0; i--) {
+		for (int i = n - 1; i >= 1; i--) {
 			if (nums[i] % 2 == 1) {
 				eor ^= sg[i];
 			}
