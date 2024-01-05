deae9b995e95ac4b49b6c3cf84f8247be1cee236
algorithmzuo <algorithmzuo@163.com>
2024-01-05 16:20:56 +0800

    modify code

--- src/class094/Code05_CuttingTree.java
+++ src/class094/Code05_CuttingTree.java
@@ -1,10 +1,11 @@
 package class094;
 
 // 砍树
-// 给定n棵树，和两个长度为n的数组a和b
-// i号棵树的初始重量为a[i]，i号树每天的增长重量为b[i]
-// 你每天最多能砍1棵树，这天收益 = 砍的树初始重量 + 砍的树增长到这天的总增重
-// 给定m，表示你有m天，返回m天内你获得的最大收益
+// 一共有n棵树，每棵树都有两个信息：
+// 第一天这棵树的初始重量、这棵树每天的增长重量
+// 你每天最多能砍1棵树，砍下这棵树的收益为：
+// 这棵树的初始重量 + 这棵树增长到这一天的总增重
+// 从第1天开始，你一共有m天可以砍树，返回m天内你获得的最大收益
 // 测试链接 : https://pintia.cn/problem-sets/91827364500/exam/problems/91827367873
 // 请同学们务必参考如下代码中关于输入、输出的处理
 // 这是输入输出处理效率很高的写法
@@ -20,9 +21,14 @@ import java.util.Arrays;
 
 public class Code05_CuttingTree {
 
-	public static int[][] tree = new int[250][2];
+	public static int MAXN = 251;
 
-	public static int[][] dp = new int[250][250];
+	// tree[i][0] : 第i棵树第一天的初始重量
+	// tree[i][1] : 第i棵树每天的增长重量
+	public static int[][] tree = new int[MAXN][2];
+
+	// dp[i][j] : 前i棵树中选若干棵树，一定要在j天内砍完，最大收益是多少
+	public static int[][] dp = new int[MAXN][MAXN];
 
 	public static int t, n, m;
 
@@ -32,16 +38,16 @@ public class Code05_CuttingTree {
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		in.nextToken();
 		t = (int) in.nval;
-		for (int i = 0; i < t; i++) {
+		for (int i = 1; i <= t; i++) {
 			in.nextToken();
 			n = (int) in.nval;
 			in.nextToken();
 			m = (int) in.nval;
-			for (int j = 0; j < n; j++) {
+			for (int j = 1; j <= n; j++) {
 				in.nextToken();
 				tree[j][0] = (int) in.nval;
 			}
-			for (int j = 0; j < n; j++) {
+			for (int j = 1; j <= n; j++) {
 				in.nextToken();
 				tree[j][1] = (int) in.nval;
 			}
@@ -52,23 +58,19 @@ public class Code05_CuttingTree {
 		br.close();
 	}
 
-	// 第i棵树初始重量，tree[i][0]
-	// 第i棵树每天的增长重量，tree[i][1]
 	public static int compute() {
-		Arrays.sort(tree, 0, n, (o1, o2) -> o1[1] - o2[1]);
-		dp[0][0] = tree[0][0];
-		for (int i = 1; i < n; i++) {
-			dp[i][0] = Math.max(dp[i - 1][0], tree[i][0]);
-		}
-		for (int j = 1; j < m; j++) {
-			dp[0][j] = dp[0][j - 1] + tree[0][1];
-		}
-		for (int i = 1; i < n; i++) {
-			for (int j = 1; j < m; j++) {
-				dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1] + tree[i][0] + tree[i][1] * j);
+		// 树的初始量不重要，因为只要砍了，就一定获得固定值
+		// 根据增长速度排序，增长量小的在前，增长量大的在后
+		// 认为越靠后的树，越要尽量晚的砍伐，课上重点讲的点
+		Arrays.sort(tree, 1, n + 1, (o1, o2) -> o1[1] - o2[1]);
+		// dp[0][...] = 0 : 表示如果没有树，不管过去多少天，收益都是0
+		// dp[...][0] = 0 : 表示不管有几棵树，没有时间砍树，收益都是0
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1] + tree[i][0] + tree[i][1] * (j - 1));
 			}
 		}
-		return dp[n - 1][m - 1];
+		return dp[n][m];
 	}
 
 }
