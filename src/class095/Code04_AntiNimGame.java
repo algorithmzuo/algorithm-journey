29299cd59036fbd0587d8b605e144f536dbd2a17
algorithmzuo <algorithmzuo@163.com>
2024-01-10 10:01:19 +0800

    modify code

--- src/class095/Code04_AntiNimGame.java
+++ src/class095/Code04_AntiNimGame.java
@@ -6,7 +6,7 @@ package class095;
 // 谁先拿走最后的石头就失败，返回最终谁会获胜
 // 先手获胜，打印John
 // 后手获胜，打印Brother
-// 测试链接 : https://www.luogu.com.cn/problem/P2197
+// 测试链接 : https://www.luogu.com.cn/problem/P4279
 // 请同学们务必参考如下代码中关于输入、输出的处理
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
