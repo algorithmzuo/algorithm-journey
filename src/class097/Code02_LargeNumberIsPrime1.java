2b9f17371d9e40db53532f01a10bce2dea570a19
algorithmzuo <algorithmzuo@163.com>
2024-01-11 19:59:12 +0800

    modify code

--- src/class097/Code02_LargeNumberIsPrime1.java
+++ src/class097/Code02_LargeNumberIsPrime1.java
@@ -1,37 +1,87 @@
 package class097;
 
-// 判断较大的数字是否为质数(Miller-Rabin模版)
+// 判断较大的数字是否为质数(Miller-Rabin测试)
 // 测试链接 : https://www.luogu.com.cn/problem/U148828
-// 请同学们务必参考如下代码中关于输入、输出的处理
-// 这是输入输出处理效率很高的写法
-// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
+// 如下代码无法通过所有测试用例
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.math.BigInteger;
+import java.util.Random;
 
-// 为什么不自己写，而要用BigInteger中的isProbablePrime方法
-// 课上有说明为什么
+// 本文件可以解决10^9范围内数字的质数检查
+// 时间复杂度O(s * (logn)的三次方)，很快
+// 为什么不能搞定所有long类型的数字检查
+// 课上已经做了说明
 public class Code02_LargeNumberIsPrime1 {
 
-	// 测试次数，次数越多失误率越低，但速度也越慢
-	// 检查n这个数字是否为质数的时间复杂度O(s * (logn)的三次方)
-	public static int s = 10;
+	// 测试次数
+	public static int s = 30;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		int t = Integer.valueOf(br.readLine());
 		for (int i = 0; i < t; i++) {
-			BigInteger n = new BigInteger(br.readLine());
-			out.println(n.isProbablePrime(s) ? "Yes" : "No");
+			long n = Long.valueOf(br.readLine());
+			out.println(millerRabin(n, s) ? "Yes" : "No");
 		}
 		out.flush();
 		out.close();
 		br.close();
+	}
+
+	public static boolean millerRabin(long n, int s) {
+		if (n <= 2) {
+			return n == 2;
+		}
+		if ((n & 1) == 0) {
+			return false;
+		}
+		for (int i = 0; i < s && i < n; i++) {
+			Random rand = new Random();
+			long a = Math.abs(rand.nextLong()) % (n - 1) + 1;
+			if (witness(a, n)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean witness(long a, long n) {
+		long u = n - 1;
+		int t = 0;
+		while ((u & 1) == 0) {
+			t++;
+			u >>= 1;
+		}
+		long x1 = power(a, u, n), x2;
+		for (int i = 1; i <= t; i++) {
+			x2 = power(x1, 2, n);
+			if (x2 == 1 && x1 != 1 && x1 != n - 1) {
+				return true;
+			}
+			x1 = x2;
+		}
+		if (x1 != 1) {
+			return true;
+		}
+		return false;
+	}
+
+	// 快速幂
+	public static long power(long n, long p, long mod) {
+		long ans = 1;
+		while (p > 0) {
+			if ((p & 1) == 1) {
+				ans = (ans * n) % mod;
+			}
+			n = (n * n) % mod;
+			p >>= 1;
+		}
+		return ans;
 	}
 
 }
