9a210ed36a8df27caa44a94a4cbf12618266588a
algorithmzuo <algorithmzuo@163.com>
2024-01-11 20:02:58 +0800

    modify code

--- src/class097/Code02_LargeNumberIsPrime1.java
+++ src/class097/Code02_LargeNumberIsPrime1.java
@@ -3,6 +3,10 @@ package class097;
 // 判断较大的数字是否为质数(Miller-Rabin测试)
 // 测试链接 : https://www.luogu.com.cn/problem/U148828
 // 如下代码无法通过所有测试用例
+// 本文件可以解决10^9范围内数字的质数检查
+// 时间复杂度O(s * (logn)的三次方)，很快
+// 为什么不能搞定所有long类型的数字检查
+// 课上已经做了说明
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,10 +15,6 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.Random;
 
-// 本文件可以解决10^9范围内数字的质数检查
-// 时间复杂度O(s * (logn)的三次方)，很快
-// 为什么不能搞定所有long类型的数字检查
-// 课上已经做了说明
 public class Code02_LargeNumberIsPrime1 {
 
 	// 测试次数
--- src/class097/Code02_LargeNumberIsPrime2.java
+++ src/class097/Code02_LargeNumberIsPrime2.java
@@ -4,7 +4,10 @@ package class097;
 // 测试链接 : https://www.luogu.com.cn/problem/U148828
 // 请同学们务必参考如下代码中关于输入、输出的处理
 // 这是输入输出处理效率很高的写法
-// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+// 本文件可以搞定任意范围数字的质数检查，时间复杂度O(s * (logn)的三次方)
+// 为什么不自己写，为什么要用BigInteger中的isProbablePrime方法
+// 课上已经做了说明
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,14 +16,9 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 
-// 可以通过所有测试用例
-// 本文件可以搞定任意范围数字的质数检查
-// 为什么不自己写，为什么要用BigInteger中的isProbablePrime方法
-// 课上已经做了说明
 public class Code02_LargeNumberIsPrime2 {
 
 	// 测试次数，次数越多失误率越低，但速度也越慢
-	// 检查n这个数字是否为质数的时间复杂度O(s * (logn)的三次方)
 	public static int s = 10;
 
 	public static void main(String[] args) throws IOException {
