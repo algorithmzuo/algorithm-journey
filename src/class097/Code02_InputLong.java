ea15ff3a121b429b7769bebece97f10cfb4cb99d
algorithmzuo <algorithmzuo@163.com>
2024-01-16 17:12:41 +0800

    modify code

--- src/class097/Code02_InputLong.java
+++ src/class097/Code02_InputLong.java
@@ -8,7 +8,7 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 
 // 读入long类型数字的注意点
-// 讲解019的扩展，没有看过讲解019去看一下，很重要
+// 讲解019的扩展，没有看过讲解019的同学去看一下
 public class Code02_InputLong {
 
 	public static void main(String[] args) throws IOException {
@@ -19,17 +19,17 @@ public class Code02_InputLong {
 	public static void f1() throws IOException {
 		System.out.println("f1函数测试读入");
 		// 尝试读入 : 131237128371723187
+		// in.nval读出的是double类型
+		// double类型64位
+		// long类型也是64位
+		// double的64位会分配若干位去表达小数部分
+		// long类型的64位全用来表达整数部分
+		// 所以读入是long范围的数，如果用以下的写法
+		// in.nval会先变成double类型，如果再转成long类型，就可能有精度损耗
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer in = new StreamTokenizer(br);
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		in.nextToken();
-		// in.nval读出的是double类型
-		// double类型64位
-		// long类型也是64位
-		// 但是double类型的64位会分配若干位给小数
-		// 而long类型的64位全用来做整数
-		// 所以当读入数字在long的范围内
-		// in.nval会先变成double类型，如果再转成long类型，就可能有精度损耗
 		long num = (long) in.nval;
 		out.println(num);
 		out.flush();
