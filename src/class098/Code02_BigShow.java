4aef683319ec532e6a2ebdae4a5f2237accf59de
algorithmzuo <algorithmzuo@163.com>
2024-01-17 18:06:27 +0800

    modify code

--- src/class098/Code02_BigShow.java
+++ src/class098/Code02_BigShow.java
@@ -6,45 +6,19 @@ package class098;
 public class Code02_BigShow {
 
 	public static void main(String[] args) {
+		System.out.println("f1() : ");
 		System.out.println("矩阵乘法展示开始");
-		int[][] a = { { 1, 3 }, { 4, 2 } };
-		int[][] b = { { 2, 3 }, { 3, 2 } };
-		//        2  3
-		//        3  2
-		//
-		// 1  3   ?  ?
-		// 4  2   ?  ?
-		int[][] ans1 = multiply(a, b);
-		print(ans1);
-		System.out.println("======");
-		int[][] c = { { 2, 4, 1 }, { 3, 2, 2 } };
-		int[][] d = { { 2, 3, 2 }, { 3, 2, 3 }, { 1, 1, 4 } };
-		//           2  3  2
-		//           3  2  3
-		//           1  1  4
-		//
-		// 2  4  1   ?  ?  ?
-		// 3  2  2   ?  ?  ?
-		int[][] ans2 = multiply(c, d);
-		print(ans2);
-		System.out.println("======");
-		int[][] e = { { 3, 1, 2 } };
-		int[][] f = { { 1, 2, 1 }, { 3, 2, 1 }, { 4, 2, -2 } };
-		//           1  2  1
-		//           3  2  1
-		//           4  2 -2
-		//
-		// 3  1  2   ?  ?  ?
-		int[][] ans3 = multiply(e, f);
-		print(ans3);
+		f1();
 		System.out.println("矩阵乘法展示结束");
 		System.out.println();
+		System.out.println("f2() : ");
 		System.out.println("求斐波那契数列第n项");
 		System.out.println("用矩阵乘法解决");
 		System.out.println("展示开始");
-		f1();
+		f2();
 		System.out.println("展示结束");
 		System.out.println();
+		System.out.println("f3() : ");
 		System.out.println("求斐波那契数列第n项");
 		System.out.println("用矩阵快速幂解决");
 		System.out.println("展示开始");
@@ -86,8 +60,68 @@ public class Code02_BigShow {
 		return ans;
 	}
 
-	// 用矩阵乘法解决斐波那契第n项的问题
+	// 打印二维矩阵
+	public static void print(int[][] m) {
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length; j++) {
+				if (m[i][j] < 10) {
+					System.out.print(m[i][j] + "   ");
+				} else if (m[i][j] < 100) {
+					System.out.print(m[i][j] + "  ");
+				} else {
+					System.out.print(m[i][j] + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	// 矩阵乘法的展示
 	public static void f1() {
+		int[][] a = { { 1, 3 }, { 4, 2 } };
+		int[][] b = { { 2, 3 }, { 3, 2 } };
+		//        2  3
+		//        3  2
+		//
+		// 1  3   ?  ?
+		// 4  2   ?  ?
+		int[][] ans1 = multiply(a, b);
+		print(ans1);
+		System.out.println("======");
+		int[][] c = { { 2, 4 }, { 3, 2 } };
+		int[][] d = { { 2, 3, 2 }, { 3, 2, 3 } };
+		//         2  3  2
+		//         3  2  3
+		//
+		// 2  4    ?  ?  ?
+		// 3  2    ?  ?  ?
+		int[][] ans2 = multiply(c, d);
+		print(ans2);
+		System.out.println("======");
+		int[][] e = { { 2, 4 }, { 1, 2 }, { 3, 1 } };
+		int[][] f = { { 2, 3 }, { 4, 1 } };
+		//          2  3
+		//          4  1
+		//
+		// 2  4     ?  ?
+		// 1  2     ?  ?
+		// 3  1     ?  ?
+		int[][] ans3 = multiply(e, f);
+		print(ans3);
+		System.out.println("======");
+		int[][] g = { { 3, 1, 2 } };
+		int[][] h = { { 1, 2, 1 }, { 3, 2, 1 }, { 4, 2, -2 } };
+		//           1  2  1
+		//           3  2  1
+		//           4  2 -2
+		//
+		// 3  1  2   ?  ?  ?
+		int[][] ans4 = multiply(g, h);
+		print(ans4);
+	}
+
+	// 用矩阵乘法解决斐波那契第n项的问题
+	public static void f2() {
 		// 0  1  1  2  3  5  8 13 21 34...
 		// 0  1  2  3  4  5  6  7  8  9
 		int[][] start = { { 1, 0 } };
@@ -125,7 +159,7 @@ public class Code02_BigShow {
 	}
 
 	// 用矩阵快速幂解决斐波那契第n项的问题
-	public static void f2() {
+	public static void f3() {
 		// 0  1  1  2  3  5  8 13 21 34...
 		// 0  1  2  3  4  5  6  7  8  9
 		int[][] start = { { 1, 0 } };
@@ -144,22 +178,6 @@ public class Code02_BigShow {
 		System.out.println("======");
 		ans = multiply(start, power(m, 4));
 		print(ans);
-	}
-
-	// 打印二维矩阵
-	public static void print(int[][] m) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[0].length; j++) {
-				if (m[i][j] < 10) {
-					System.out.print(m[i][j] + "   ");
-				} else if (m[i][j] < 100) {
-					System.out.print(m[i][j] + "  ");
-				} else {
-					System.out.print(m[i][j] + " ");
-				}
-			}
-			System.out.println();
-		}
 	}
 
 }
