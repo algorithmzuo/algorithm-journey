c730b9c8dadc6eebdfd6f45e96a74e613522600c
algorithmzuo <algorithmzuo@163.com>
2024-01-19 16:27:47 +0800

    modify code

--- src/class099/Code01_Inverse1.java
+++ src/class099/Code01_Inverse1.java
@@ -7,7 +7,8 @@ package class099;
 // 测试链接 : https://www.luogu.com.cn/problem/P3811
 // 请同学们务必参考如下代码中关于输入、输出的处理
 // 这是输入输出处理效率很高的写法
-// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
+// 提交以下的code，提交时请把类名改成"Main"，如下代码不能通过全部测试用例
+// 仅是展示了单个逆元如何计算
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,8 +17,6 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 
-// 如下代码不能通过全部测试用例
-// 仅是展示了单个逆元如何计算
 public class Code01_Inverse1 {
 
 	public static int n, p;
