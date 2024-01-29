bb3e051117054eb3526734ae8d0be4b01d976f0c
algorithmzuo <algorithmzuo@163.com>
2024-01-29 12:57:34 +0800

    modify code

--- src/class101/Code05_ExpandKMP.java
+++ src/class101/Code05_ExpandKMP.java
@@ -20,24 +20,73 @@ import java.io.PrintWriter;
 
 public class Code05_ExpandKMP {
 
-	public static char[] a;
+	public static int MAXN = 20000001;
 
-	public static char[] b;
+	public static int[] zxt = new int[MAXN];
+
+	public static int[] ext = new int[MAXN];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-		a = br.readLine().toCharArray();
-		b = br.readLine().toCharArray();
-		compute();
-		out.println();
+		char[] a = br.readLine().toCharArray();
+		char[] b = br.readLine().toCharArray();
+		znext(b, b.length);
+		exkmp(a, b, a.length, b.length);
+		out.println(eor(zxt, b.length));
+		out.println(eor(ext, a.length));
 		out.flush();
 		out.close();
 		br.close();
 	}
 
-	public static void compute() {
+	public static void znext(char[] s, int n) {
+		zxt[0] = n;
+		for (int i = 0, j = 1; j < n && s[i] == s[j]; i++, j++) {
+			zxt[1]++;
+		}
+		for (int i = 2, p = zxt[1], k = 1, l, j; i < n; i++) {
+			p = k + zxt[k] - 1;
+			l = zxt[i - k];
+			if (i + l <= p) {
+				zxt[i] = l;
+			} else {
+				j = Math.max(0, p - i + 1);
+				while (i + j < n && s[i + j] == s[j]) {
+					j++;
+				}
+				zxt[i] = j;
+				k = i;
+			}
+		}
+	}
 
+	public static void exkmp(char[] a, char[] b, int n, int m) {
+		for (int i = 0; i < n && i < m && a[i] == b[i]; i++) {
+			ext[0]++;
+		}
+		for (int i = 1, p = ext[0], k = 0, l, j; i < n; i++) {
+			p = k + ext[k] - 1;
+			l = zxt[i - k];
+			if (i + l <= p) {
+				ext[i] = l;
+			} else {
+				j = Math.max(0, p - i + 1);
+				while (i + j < n && j < m && a[i + j] == b[j]) {
+					j++;
+				}
+				ext[i] = j;
+				k = i;
+			}
+		}
+	}
+
+	public static long eor(int[] arr, int n) {
+		long ans = 0;
+		for (int i = 0; i < n; i++) {
+			ans ^= (long) (i + 1) * (arr[i] + 1);
+		}
+		return ans;
 	}
 
 }
