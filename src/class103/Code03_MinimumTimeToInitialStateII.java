e5c00dee341610eb0da69ef175397e7446c7f8f1
algorithmzuo <algorithmzuo@163.com>
2024-02-05 11:57:13 +0800

    modify code

---
+++ src/class103/Code03_MinimumTimeToInitialStateII.java
@@ -0,0 +1,45 @@
+package class103;
+
+// 将单词恢复初始状态所需的最短时间II
+// 给你一个下标从0开始的字符串word和一个整数k
+// 在每一秒，必须执行以下操作
+// 移除word的前k个字符
+// 在word的末尾添加k个任意字符
+// 添加的字符不必和移除的字符相同
+// 返回将word恢复到初始状态所需的最短时间
+// 该时间必须大于零
+// 测试链接 : https://leetcode.cn/problems/minimum-time-to-revert-word-to-initial-state-ii/
+public class Code03_MinimumTimeToInitialStateII {
+
+	public static int minimumTimeToInitialState(String word, int k) {
+		char[] s = word.toCharArray();
+		int n = s.length;
+		zArray(s, n);
+		for (int i = 1; i < n; i++) {
+			if (i % k == 0 && i + z[i] == n) {
+				return i / k;
+			}
+		}
+		return (n + k - 1) / k;
+	}
+
+	public static int MAXN = 100001;
+
+	public static int[] z = new int[MAXN];
+
+	public static void zArray(char[] s, int n) {
+		z[0] = n;
+		for (int i = 1, c = 0, r = 0, len; i < n; i++) {
+			len = r > i ? Math.min(r - i, z[i - c]) : 0;
+			while (i + len < n && s[i + len] == s[len]) {
+				len++;
+			}
+			if (i + len > r) {
+				r = i + len;
+				c = i;
+			}
+			z[i] = len;
+		}
+	}
+
+}
