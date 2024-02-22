5f79dce94ae57273d2a578b36e648ce6b26b11cc
algorithmzuo <algorithmzuo@163.com>
2024-02-22 17:34:54 +0800

    modify code

--- src/class105/Code06_LikeLotusRoot.java
+++ src/class105/Code06_LikeLotusRoot.java
@@ -1,9 +1,9 @@
 package class105;
 
 // 根据匹配定义求匹配子串的数量
-// 给定长为n的源串s，以及长度为m的模式串p，还有一个正数k
+// 给定长为n的字符串s，以及长度为m的字符串p，还有一个正数k
 // s'与s匹配的定义为，s'与s长度相同，且最多有k个位置字符不同
-// 要求查找源串中有多少子串与模式串匹配
+// 要求查找字符串s中有多少子串与字符串p匹配
 // 测试链接 : https://www.luogu.com.cn/problem/P3763
 // 请同学们务必参考如下代码中关于输入、输出的处理
 // 这是输入输出处理效率很高的写法
@@ -84,8 +84,8 @@ public class Code06_LikeLotusRoot {
 		return ans;
 	}
 
-	// s[l1......r1] 和 p[l2......r2]，这两段一定等长
-	// 返回这两段上字符不一样的位置是不是<=k
+	// s[l1......r1] 和 p[l2......r2] 这两段一定等长
+	// 返回这两段上字符不一样的位置是不是<=k个
 	public static boolean check(int l1, int r1, int l2, int r2, int k) {
 		int diff = 0;
 		while (l1 <= r1 && diff <= k) {
