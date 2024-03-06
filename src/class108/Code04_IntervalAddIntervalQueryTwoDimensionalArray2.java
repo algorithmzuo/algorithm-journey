4bf0956852e33a9bbb95ddc16ff28fb82576a241
algorithmzuo <algorithmzuo@163.com>
2024-03-06 23:25:33 +0800

    modify code

--- src/class108/Code04_IntervalAddIntervalQueryTwoDimensionalArray2.java
+++ src/class108/Code04_IntervalAddIntervalQueryTwoDimensionalArray2.java
@@ -1,112 +1,69 @@
 package class108;
 
-// 二维数组上范围增加、范围查询，使用树状数组的模版(java)
+// 二维数组上范围增加、范围查询，使用树状数组的模版(C++)
 // 测试链接 : https://www.luogu.com.cn/problem/P4514
-// 如下实现完全正确，但是洛谷平台对空间卡的很严，只有使用C++能全部通过
-// 甚至C++的实现稍微不注意读写效率也无法通过
-// java的版本就没有能完全通过的，这是洛谷平台根本没有照顾各种语言的实现所导致的
-// 在真正笔试、比赛时，一定是兼顾各种语言的，该实现一定能通过
-// 该实现所有测试用例的答案都正确
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一致
+// 提交如下代码，可以通过所有测试用例
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.StreamTokenizer;
-
-public class Code04_IntervalAddIntervalQueryTwoDimensionalArray2 {
-
-	public static int MAXN = 2050;
-
-	public static int MAXM = 2050;
-
-	public static int[][] tree1 = new int[MAXN][MAXM];
-
-	public static int[][] tree2 = new int[MAXN][MAXM];
-
-	public static int[][] tree3 = new int[MAXN][MAXM];
-
-	public static int[][] tree4 = new int[MAXN][MAXM];
-
-	public static int n, m;
-
-	public static int lowbit(int i) {
-		return i & -i;
-	}
-
-	public static void add(int x, int y, int v) {
-		for (int i = x; i <= n; i += lowbit(i)) {
-			for (int j = y; j <= m; j += lowbit(j)) {
-				tree1[i][j] += v;
-				tree2[i][j] += x * v;
-				tree3[i][j] += y * v;
-				tree4[i][j] += x * y * v;
-			}
-		}
-	}
-
-	public static int sum(int x, int y) {
-		int ans = 0;
-		for (int i = x; i > 0; i -= lowbit(i)) {
-			for (int j = y; j > 0; j -= lowbit(j)) {
-				ans += (x + 1) * (y + 1) * tree1[i][j] - (y + 1) * tree2[i][j] - (x + 1) * tree3[i][j] + tree4[i][j];
-			}
-		}
-		return ans;
-	}
-
-	public static void add(int a, int b, int c, int d, int v) {
-		add(a, b, v);
-		add(a, d + 1, -v);
-		add(c + 1, b, -v);
-		add(c + 1, d + 1, v);
-	}
-
-	public static int range(int a, int b, int c, int d) {
-		return sum(c, d) - sum(a - 1, d) - sum(c, b - 1) + sum(a - 1, b - 1);
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StreamTokenizer in = new StreamTokenizer(br);
-		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-		String op;
-		int a, b, c, d, v;
-		while (in.nextToken() != StreamTokenizer.TT_EOF) {
-			op = in.sval;
-			if (op.equals("X")) {
-				in.nextToken();
-				n = (int) in.nval;
-				in.nextToken();
-				m = (int) in.nval;
-			} else if (op.equals("L")) {
-				in.nextToken();
-				a = (int) in.nval;
-				in.nextToken();
-				b = (int) in.nval;
-				in.nextToken();
-				c = (int) in.nval;
-				in.nextToken();
-				d = (int) in.nval;
-				in.nextToken();
-				v = (int) in.nval;
-				add(a, b, c, d, v);
-			} else {
-				in.nextToken();
-				a = (int) in.nval;
-				in.nextToken();
-				b = (int) in.nval;
-				in.nextToken();
-				c = (int) in.nval;
-				in.nextToken();
-				d = (int) in.nval;
-				out.println(range(a, b, c, d));
-			}
-		}
-		out.flush();
-		out.close();
-		br.close();
-	}
-
-}
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//const int MAXN = 2050;
+//const int MAXM = 2050;
+//int tree1[MAXN][MAXM], tree2[MAXN][MAXM], tree3[MAXN][MAXM], tree4[MAXN][MAXM];
+//int n, m;
+//
+//int lowbit(int i) {
+//    return i & -i;
+//}
+//
+//void add(int x, int y, int v) {
+//    for (int i = x; i <= n; i += lowbit(i)) {
+//        for (int j = y; j <= m; j += lowbit(j)) {
+//            tree1[i][j] += v;
+//            tree2[i][j] += x * v;
+//            tree3[i][j] += y * v;
+//            tree4[i][j] += x * y * v;
+//        }
+//    }
+//}
+//
+//int sum(int x, int y) {
+//    int ans = 0;
+//    for (int i = x; i > 0; i -= lowbit(i)) {
+//        for (int j = y; j > 0; j -= lowbit(j)) {
+//            ans += (x + 1) * (y + 1) * tree1[i][j] - (y + 1) * tree2[i][j] - (x + 1) * tree3[i][j] + tree4[i][j];
+//        }
+//    }
+//    return ans;
+//}
+//
+//void add(int a, int b, int c, int d, int v) {
+//    add(a, b, v);
+//    add(c + 1, d + 1, v);
+//    add(a, d + 1, -v);
+//    add(c + 1, b, -v);
+//}
+//
+//int range(int a, int b, int c, int d) {
+//    return sum(c, d) - sum(a - 1, d) - sum(c, b - 1) + sum(a - 1, b - 1);
+//}
+//
+//int main() {
+//    char op;
+//    int a, b, c, d, v;
+//    scanf("%s", &op);
+//    scanf("%d%d", &n, &m);
+//    while (scanf("%s", &op) != EOF) {
+//        if (op == 'X') {
+//            scanf("%d%d", &n, &m);
+//        } else if (op == 'L') {
+//            scanf("%d%d%d%d%d", &a, &b, &c, &d, &v);
+//            add(a, b, c, d, v);
+//        } else {
+//            scanf("%d%d%d%d", &a, &b, &c, &d);
+//            printf("%d\n", range(a, b, c, d));
+//        }
+//    }
+//    return 0;
+//}
