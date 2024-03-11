12fe6682b4e63de29cba433923705556ff20a2d1
algorithmzuo <algorithmzuo@163.com>
2024-03-11 16:51:33 +0800

    modify code

--- src/class112/Code01_.java
+++ src/class112/Code01_.java
@@ -1,5 +1,6 @@
 package class112;
 
+// 测试链接 : https://www.luogu.com.cn/problem/P4145
 public class Code01_ {
 
 }
--- src/class112/Code03_.java
+++ src/class112/Code03_.java
@@ -1,5 +1,6 @@
 package class112;
 
+// 测试链接 : http://poj.org/problem?id=2528
 public class Code03_ {
 
 }
