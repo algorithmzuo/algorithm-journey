69af13b1aaa70c0b4ab9dc26132e2903f5c6ef6d
algorithmzuo <algorithmzuo@163.com>
2024-03-11 21:57:35 +0800

    modify code

--- src/class112/Code01_SquareRoot.java
+++ src/class112/Code01_SquareRoot.java
@@ -1,16 +1,118 @@
 package class112;
 
-// 开平方
-// 给定一个长度为n的数组arr，有两种类型的操作
+// 范围上开平方并求累加和
+// 给定一个长度为n的数组arr，实现以下两种类型的操作
 // 操作0 : arr[l..r]范围上的每个数开平方，结果向下取整
 // 操作1 : 返回arr[l..r]范围上所有数字的累加和
-// 两种操作一共发生m次
+// 两种操作一共发生m次，数据中有可能l>r，遇到这种情况请交换l和r
 // 1 <= n, m <= 10^5
 // 1 <= arr[i] <= 10^12
 // 测试链接 : https://www.luogu.com.cn/problem/P4145
 // 请同学们务必参考如下代码中关于输入、输出的处理
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
 public class Code01_SquareRoot {
+
+	public static int MAXN = 100001;
+
+	public static long[] arr = new long[MAXN];
+
+	public static long[] sum = new long[MAXN << 2];
+
+	public static long[] max = new long[MAXN << 2];
+
+	public static void build(int l, int r, int rt) {
+		if (l == r) {
+			sum[rt] = arr[l];
+			max[rt] = arr[l];
+		} else {
+			int mid = (l + r) / 2;
+			build(l, mid, rt << 1);
+			build(mid + 1, r, rt << 1 | 1);
+			up(rt);
+		}
+	}
+
+	public static void up(int rt) {
+		sum[rt] = sum[rt << 1] + sum[rt << 1 | 1];
+		max[rt] = Math.max(max[rt << 1], max[rt << 1 | 1]);
+	}
+
+	public static void change(int jobl, int jobr, int l, int r, int rt) {
+		if (l == r) {
+			long sqrt = (long) Math.sqrt(sum[rt]);
+			sum[rt] = sqrt;
+			max[rt] = sqrt;
+		} else {
+			int mid = (l + r) / 2;
+			if (jobl <= mid && max[rt << 1] > 1) {
+				change(jobl, jobr, l, mid, rt << 1);
+			}
+			if (jobr > mid && max[rt << 1 | 1] > 1) {
+				change(jobl, jobr, mid + 1, r, rt << 1 | 1);
+			}
+			up(rt);
+		}
+	}
+
+	public static long query(int jobl, int jobr, int l, int r, int rt) {
+		if (jobl <= l && r <= jobr) {
+			return sum[rt];
+		}
+		int mid = (l + r) / 2;
+		long ans = 0;
+		if (jobl <= mid) {
+			ans += query(jobl, jobr, l, mid, rt << 1);
+		}
+		if (jobr > mid) {
+			ans += query(jobl, jobr, mid + 1, r, rt << 1 | 1);
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		while (in.nextToken() != StreamTokenizer.TT_EOF) {
+			int n = (int) in.nval;
+			for (int i = 1; i <= n; i++) {
+				in.nextToken();
+				arr[i] = (long) in.nval;
+			}
+			build(1, n, 1);
+			in.nextToken();
+			int m = (int) in.nval;
+			for (int i = 1, op, jobl, jobr, tmp; i <= m; i++) {
+				in.nextToken();
+				op = (int) in.nval;
+				in.nextToken();
+				jobl = (int) in.nval;
+				in.nextToken();
+				jobr = (int) in.nval;
+				if (jobl > jobr) {
+					tmp = jobl;
+					jobl = jobr;
+					jobr = tmp;
+				}
+				if (op == 0) {
+					change(jobl, jobr, 1, n, 1);
+				} else {
+					out.println(query(jobl, jobr, 1, n, 1));
+				}
+			}
+		}
+		out.flush();
+		out.close();
+		br.close();
+	}
 
 }
