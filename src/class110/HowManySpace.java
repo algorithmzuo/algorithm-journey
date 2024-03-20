2dffcbb59fbfb4ec8579ddaa444149715a01f67b
algorithmzuo <algorithmzuo@163.com>
2024-03-20 15:44:59 +0800

    modify code

---
+++ src/class110/HowManySpace.java
@@ -0,0 +1,30 @@
+package class110;
+
+// 线段树范围为1 ~ n时，需要几倍空间才够用？
+public class HowManySpace {
+
+	public static int maxi(int l, int r, int i) {
+		if (l == r) {
+			return i;
+		} else {
+			int mid = (l + r) / 2;
+			return Math.max(maxi(l, mid, i << 1), maxi(mid + 1, r, i << 1 | 1));
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 999;
+		double max = 0;
+		int r = 0;
+		for (int i = 1; i <= n; i++) {
+			double cur = (double) maxi(1, i, 1) / (double) i;
+			System.out.println("范围[1 ~ " + i + "]时，需要开辟空间 / 范围的倍数 : " + cur);
+			if (cur > max) {
+				max = cur;
+				r = i;
+			}
+		}
+		System.out.println("其中最大的倍数，范围[1 ~ " + r + "]时，需要开辟空间 / 范围的倍数 : " + max);
+	}
+
+}
