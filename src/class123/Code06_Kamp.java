aacffcc059e110cb4a7c583aaba76cb47fc0f7d0
algorithmzuo <algorithmzuo@163.com>
2024-05-15 11:47:18 +0800

    modify code

--- src/class123/Code06_Kamp.java
+++ src/class123/Code06_Kamp.java
@@ -12,7 +12,7 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
-public class Code05_Kamp {
+public class Code06_Kamp {
 
 	public static int MAXN = 500001;
 
