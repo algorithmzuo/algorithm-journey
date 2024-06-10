3cf28b5859545942017c8ad8befdf5d8df79db92
algorithmzuo <algorithmzuo@163.com>
2024-06-10 22:03:03 +0800

    modify code

---
+++ src/class052/Code06_MonotonicStackLuogu.java
@@ -0,0 +1,68 @@
+package class052;
+
+// 单调栈(洛谷测试)
+// 给定一个长度为n的数组，打印每个位置的右侧，<=该位置数字的位置
+// 测试链接 : https://www.luogu.com.cn/problem/P5788
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有用例
+// 注意 : 洛谷这道题对java特别不友好，不这么写通过不了，注意看注释，非常极限
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+public class Code06_MonotonicStackLuogu {
+
+	public static void main(String[] args) throws IOException {
+		int n = nextInt();
+		int[] arr = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = nextInt();
+		}
+		// 单调栈中保证 : 左 >= 右
+		int[] stack = new int[n + 1];
+		int r = 0;
+		// 注意，这里为了省空间，直接复用了arr
+		// 比如一个位置x从栈中弹出，是当前的i位置让其弹出的
+		// 那么令arr[x] = i
+		// 此时arr[x]不再表示原始数组x位置的值
+		// 而去表示，原始数组中，x的右边，>=arr[x]，最近的位置
+		// 也就是说，重新复用arr，让其变成答案数组
+		// 为啥这么节省？没办法，不这么节省通过不了测试，空间卡的非常极限
+		for (int i = 1; i <= n; i++) {
+			while (r > 0 && arr[stack[r - 1]] < arr[i]) {
+				arr[stack[--r]] = i;
+			}
+			stack[r++] = i;
+		}
+		while (r > 0) {
+			arr[stack[--r]] = 0;
+		}
+		out.print(arr[1]);
+		for (int i = 2; i <= n; i++) {
+			out.print(" " + arr[i]);
+		}
+		out.println();
+		out.flush();
+	}
+
+	// 用如下的方式读数据，其实并不推荐
+	// 但是这道题特别卡空间，需要这么读数据让内存开销最小
+	// 一般笔试、比赛时，不需要这么写
+	public static InputStream in = new BufferedInputStream(System.in);
+
+	public static PrintWriter out = new PrintWriter(System.out);
+
+	public static int nextInt() throws IOException {
+		int ch, sign = 1, ans = 0;
+		while (!Character.isDigit(ch = in.read())) {
+			if (ch == '-')
+				sign = -1;
+		}
+		do {
+			ans = ans * 10 + ch - '0';
+		} while (Character.isDigit(ch = in.read()));
+		return (ans * sign);
+	}
+
+}
