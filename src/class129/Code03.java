d7ad8b9f1e0624ee09415c59b4f541aa8468c799
algorithmzuo <algorithmzuo@163.com>
2024-06-11 16:09:02 +0800

    modify code

--- src/class129/Code03.java
+++
@@ -1,6 +0,0 @@
-package class129;
-
-// 测试链接 : https://www.luogu.com.cn/problem/P1081
-public class Code03 {
-
-}
---
+++ src/class129/Code03_RoadTrip.java
@@ -0,0 +1,8 @@
+package class129;
+
+// 开车旅行
+// 测试链接 : https://www.luogu.com.cn/problem/P1081
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有用例
+public class Code03_RoadTrip {
+
+}
