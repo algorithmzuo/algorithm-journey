826009225c6ea54fbe9a2aaa5c017ec235bceeed
algorithmzuo <algorithmzuo@163.com>
2024-06-26 18:12:14 +0800

    modify code

--- src/class131/Code01_CountOfRangeSum2.java
+++ src/class131/Code01_CountOfRangeSum2.java
@@ -15,13 +15,15 @@ public class Code01_CountOfRangeSum2 {
 	// 树状数组 + 离散化的解法，理解难度较低
 	public static int countRangeSum(int[] nums, int lower, int upper) {
 		build(nums);
-		add(0, 1);
 		long sum = 0;
 		int ans = 0;
 		for (int i = 0; i < n; i++) {
 			sum += nums[i];
-			ans += sum(sum - lower) - sum(sum - upper - 1);
-			add(sum, 1);
+			ans += sum(rank(sum - lower)) - sum(rank(sum - upper - 1));
+			if (lower <= sum && sum <= upper) {
+				ans++;
+			}
+			add(rank(sum), 1);
 		}
 		return ans;
 	}
@@ -35,21 +37,24 @@ public class Code01_CountOfRangeSum2 {
 	public static int[] tree = new int[MAXN];
 
 	public static void build(int[] nums) {
+		// 生成前缀和数组
 		n = nums.length;
 		for (int i = 1, j = 0; i <= n; i++, j++) {
 			sort[i] = sort[i - 1] + nums[j];
 		}
-		sort[n + 1] = 0;
-		Arrays.sort(sort, 1, n + 2);
+		// 前缀和数组排序和去重，最终有m个不同的前缀和
+		Arrays.sort(sort, 1, n + 1);
 		m = 1;
-		for (int i = 2; i <= n + 1; i++) {
+		for (int i = 2; i <= n; i++) {
 			if (sort[m] != sort[i]) {
 				sort[++m] = sort[i];
 			}
 		}
+		// 初始化树状数组，下标1~m
 		Arrays.fill(tree, 1, m + 1, 0);
 	}
 
+	// 返回 <=v 并且尽量大的前缀和是第几号前缀和
 	public static int rank(long v) {
 		int left = 1, right = m, mid;
 		int ans = 0;
@@ -65,24 +70,22 @@ public class Code01_CountOfRangeSum2 {
 		return ans;
 	}
 
-	public static int lowbit(int i) {
-		return i & -i;
-	}
-
-	public static void add(long v, int c) {
-		int i = rank(v);
+	// 树状数组模版代码，没有任何修改
+	// i号前缀和，个数增加c个
+	public static void add(int i, int c) {
 		while (i <= m) {
 			tree[i] += c;
-			i += lowbit(i);
+			i += i & -i;
 		}
 	}
 
-	public static int sum(long v) {
-		int i = rank(v);
+	// 树状数组模版代码，没有任何修改
+	// 查询1~i号前缀和一共有几个
+	public static int sum(int i) {
 		int ans = 0;
 		while (i > 0) {
 			ans += tree[i];
-			i -= lowbit(i);
+			i -= i & -i;
 		}
 		return ans;
 	}
