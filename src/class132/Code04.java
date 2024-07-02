5374a30e52aa5ab142306d5c7dfc3f4f5d555b6c
algorithmzuo <algorithmzuo@163.com>
2024-07-02 22:30:24 +0800

    modify code

---
+++ src/class132/Code04.java
@@ -0,0 +1,6 @@
+package class132;
+
+// 测试链接 : https://www.luogu.com.cn/problem/P1437
+public class Code04 {
+
+}
---
+++ src/class132/Code05.java
@@ -0,0 +1,6 @@
+package class132;
+
+// 测试链接 : https://acm.hdu.edu.cn/showproblem.php?pid=4539
+public class Code05 {
+
+}
