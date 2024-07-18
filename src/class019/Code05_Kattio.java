2b093d938e8fa7c397a8924975d8de1e98112496
algorithmzuo <algorithmzuo@163.com>
2024-07-18 12:16:35 +0800

    modify code

--- src/class019/Code05_Kattio.java
+++ src/class019/Code05_Kattio.java
@@ -1,16 +1,16 @@
 package class019;
 
 // 本文件课上没有讲，介绍一下Kattio类的使用
-// 如果题目输入就是无法一个一个的读出数字，使用StreamTokenizer就是怎么都无法正确读入
+// 某些题目的输入，使用StreamTokenizer就是无法正确读入
 // 那么可以使用本文件提供的Kattio类
 // 比如，就是需要依次读取一个一个的字符串进行处理
 // 再比如，StreamTokenizer读取不溢出、但是很大的long类型数字时，可能会读入错误
 // 再比如，StreamTokenizer读取科学计数法表达的double类型数字时，可能会读入错误
 // 会发现读取的结果不对
-// 但是用Kattio进行读取，就没有这个问题
+// 使用Kattio进行读取，就没有这些问题
 // 可以直接运行本文件的main函数，根据提示输入给定的数字，能清晰的看到这一点
 // 那么可不可以放弃StreamTokenizer，以后都用Kattio呢？
-// 不行！因为StreamTokenizer的效率还是比Kattio好的
+// 不行！因为StreamTokenizer的效率还是比Kattio好！
 // 只有在StreamTokenizer无法正确读取的情况下，才考虑使用Kattio类
 // 其他语言中一定有对等的概念，不是java的同学，请自行搞定
 
