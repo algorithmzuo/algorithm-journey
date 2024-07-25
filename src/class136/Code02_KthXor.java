08afe424ed3a2f65b7b6c015a27c55e1b06bf8d9
algorithmzuo <algorithmzuo@163.com>
2024-07-25 17:05:57 +0800

    modify code

--- src/class136/Code02_KthXor.java
+++ src/class136/Code02_KthXor.java
@@ -6,17 +6,16 @@ package class136;
 // 返回第k小的异或和
 // 1 <= n <= 10^5
 // 0 <= arr[i] <= 2^50
+// 1 <= k <= 2^50
 // 测试链接 : https://loj.ac/p/114
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.io.StreamTokenizer;
 
 public class Code02_KthXor {
 
@@ -83,67 +82,26 @@ public class Code02_KthXor {
 	}
 
 	public static void main(String[] args) throws IOException {
-		// 题目会读取10^18范围内的long类型数字
-		// 用StreamTokenizer可能无法正确读取，因为先变成double再转成long
-		// 这里用Kattio类，具体看讲解019的代码中，Code05_Kattio文件
-		// 有详细的说明
-		Kattio io = new Kattio();
-		n = io.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		in.nextToken();
+		n = (int) in.nval;
 		for (int i = 1; i <= n; i++) {
-			arr[i] = io.nextLong();
+			in.nextToken();
+			arr[i] = (long) in.nval;
 		}
 		compute();
-		int q = io.nextInt();
+		in.nextToken();
+		int q = (int) in.nval;
 		for (int i = 1; i <= q; i++) {
-			long k = io.nextLong();
-			io.println(query(k));
+			in.nextToken();
+			long k = (long) in.nval;
+			out.println(query(k));
 		}
-		io.flush();
-		io.close();
-	}
-
-	// Kattio类IO效率很好，但还是不如StreamTokenizer
-	// 只有StreamTokenizer无法正确处理时，才考虑使用这个类
-	// 参考链接 : https://oi-wiki.org/lang/java-pro/
-	public static class Kattio extends PrintWriter {
-		private BufferedReader r;
-		private StringTokenizer st;
-
-		public Kattio() {
-			this(System.in, System.out);
-		}
-
-		public Kattio(InputStream i, OutputStream o) {
-			super(o);
-			r = new BufferedReader(new InputStreamReader(i));
-		}
-
-		public Kattio(String intput, String output) throws IOException {
-			super(output);
-			r = new BufferedReader(new FileReader(intput));
-		}
-
-		public String next() {
-			try {
-				while (st == null || !st.hasMoreTokens())
-					st = new StringTokenizer(r.readLine());
-				return st.nextToken();
-			} catch (Exception e) {
-			}
-			return null;
-		}
-
-		public int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		public double nextDouble() {
-			return Double.parseDouble(next());
-		}
-
-		public long nextLong() {
-			return Long.parseLong(next());
-		}
+		out.flush();
+		out.close();
+		br.close();
 	}
 
 }
