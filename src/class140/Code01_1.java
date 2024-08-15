e7fbf1c59a33a5f831fe71cb69cb252390a96ade
algorithmzuo <algorithmzuo@163.com>
2024-08-15 15:47:20 +0800

    modify code

--- src/class140/Code01_1.java
+++ src/class140/Code01_1.java
@@ -1,6 +1,12 @@
 package class140;
 
 // 测试链接 : https://www.luogu.com.cn/problem/P5656
+// 如下实现是正确的，但是洛谷平台对空间卡的很严，只有使用C++能全部通过
+// java的版本就是无法完全通过的，空间会超过限制，主要是IO空间占用大
+// 这是洛谷平台没有照顾各种语言的实现所导致的
+// 在真正笔试、比赛时，一定是兼顾各种语言的，该实现是一定正确的
+// C++版本就是Code01_2文件
+// C++版本和java版本逻辑完全一样，但只有C++版本可以通过所有测试用例
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -47,15 +53,15 @@ public class Code01_1 {
 				long xd = b / d;
 				long yd = a / d;
 				if (x < 0) {
-					// x要想增长到>=1且最小的值，差几个xd，算出来，应该是(1 - x) / xd，向上取整
-					// a/b 向上取整可以写成，(a + b - 1) / b
-					// 所以(1 - x) / xd，向上取整为，(1 - x + xd - 1) / xd
-					// 化简为(xd - x) / xd
+					// x要想增长到>=1且最小的值，差几个xd，算出来就是k的值
+					// 那应该是(1-x)/xd，结果向上取整
+					// (a/b)向上取整可以写成(a+b-1)/b
+					// 所以(1-x)/xd，向上取整为(1-x+xd-1)/xd，化简为(xd-x)/xd
 					long k = (xd - x) / xd;
 					x += xd * k;
 					y -= yd * k;
 				} else {
-					// x要想减少到>=1且最小的值，差几个xd，算出来，自然向下取整
+					// x要想减少到>=1且最小的值，差几个xd，算出来就是k的值，向下取整
 					long k = (x - 1) / xd;
 					x -= xd * k;
 					y += yd * k;
@@ -70,8 +76,7 @@ public class Code01_1 {
 				} else {
 					// 无正整数解
 					out.print(x + " ");
-					// (1 - y) / yd，向上取整为
-					// ((1 - y) + yd - 1) / yd，化简成，(yd - y) / yd
+					// (1-y)/yd，向上取整为(1-y+yd-1)/yd，化简为(yd-y)/yd
 					out.println(y + yd * ((yd - y) / yd));
 				}
 			} else {
--- src/class140/Code01_2.java
+++ src/class140/Code01_2.java
@@ -1,6 +1,8 @@
 package class140;
 
 // 测试链接 : https://www.luogu.com.cn/problem/P5656
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
 
 //#include <iostream>
 //#include <cstdio>
