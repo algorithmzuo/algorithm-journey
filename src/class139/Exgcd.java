abafd1058569d278fa4f3aa8f36f97af52229bff
algorithmzuo <algorithmzuo@163.com>
2024-08-15 12:26:45 +0800

    modify code

--- src/class139/Exgcd.java
+++ src/class139/Exgcd.java
@@ -5,7 +5,7 @@ public class Exgcd {
 	// 扩展欧几里得算法
 	// 对于方程ax + by = gcd(a,b)
 	// 当a和b确定，那么gcd(a,b)也确定
-	// 扩展欧几里得算法可以给出a和b的最大公约数d、以及其中一个特解x, y
+	// 扩展欧几里得算法可以给出a和b的最大公约数d、以及其中一个特解x、y
 	public static long d, x, y;
 
 	public static void exgcd(long a, long b) {
@@ -39,9 +39,9 @@ public class Exgcd {
 	}
 
 	public static void main(String[] args) {
-		// 扩展欧几里得算法展示
-		int a = 60;
-		int b = 100;
+		// 扩展欧几里得算法例子
+		int a = 6;
+		int b = 10;
 		exgcd(a, b);
 		System.out.println("gcd(" + a + ", " + b + ")" + " = " + d);
 		System.out.println("x = " + x + ", " + " y = " + y);
