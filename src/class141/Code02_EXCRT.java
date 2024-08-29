4a1483657611d0bf1dbf57639502817b09ead522
algorithmzuo <algorithmzuo@163.com>
2024-08-29 12:10:23 +0800

    modify code

--- src/class141/Code02_EXCRT.java
+++ src/class141/Code02_EXCRT.java
@@ -31,8 +31,7 @@ public class Code02_EXCRT {
 				return -1;
 			}
 			// 通解 x = x0 * (c/d) + (b/d) * n
-			// 想得到最小非负数的特解
-			// 最小非负数的特解x = (x0 * (c/d)) % (b/d) 取余数
+			// 最小非负数的特解x = (x0 * (c/d)) % (b/d) 取非负余数
 			x = multiply(x, c / d, b / d);
 			tmp = lcm * (b / d);
 			ans = (ans + multiply(x, lcm, tmp)) % tmp;
