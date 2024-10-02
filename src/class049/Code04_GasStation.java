5e015903f821b81a9055566d4e3a807d7b58d68a
algorithmzuo <algorithmzuo@163.com>
2024-10-02 18:29:34 +0800

    modify code

--- src/class049/Code04_GasStation.java
+++ src/class049/Code04_GasStation.java
@@ -12,16 +12,18 @@ public class Code04_GasStation {
 
 	public static int canCompleteCircuit(int[] gas, int[] cost) {
 		int n = gas.length;
-		// 本来下标是0..n-1，概念上扩展到0...2*n-1
-		// 那么从任何点出发的情况都可以包括了，i位置的信息在(i%n)位置
-		// 窗口范围[l..r)，左闭右开，r是窗口的下一个位置(到不了的位置)
+		// 本来下标是0..n-1，但是扩充到0..2*n-1，i位置的余量信息在(r%n)位置
+		// 窗口范围是[l, r)，左闭右开，也就是说窗口是[l..r-1]，r是到不了的位置
 		for (int l = 0, r = 0, sum; l < n; l = r + 1, r = l) {
 			sum = 0;
 			while (sum + gas[r % n] - cost[r % n] >= 0) {
-				if (r - l == n) {
+				// r位置即将右扩，窗口会变大
+				if (r - l + 1 == n) { // 此时检查是否已经转了一圈
 					return l;
 				}
+				// r位置进入窗口，累加和加上r位置的余量
 				sum += gas[r % n] - cost[r % n];
+				// r右扩，窗口变大了
 				r++;
 			}
 		}
