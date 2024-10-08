7a68307ccf9162988490b826b413a6e2b24b318b
algorithmzuo <algorithmzuo@163.com>
2024-10-08 21:32:30 +0800

    modify code

--- src/class146/Code03_AlienPlus.java
+++ src/class146/Code03_AlienPlus.java
@@ -2,6 +2,7 @@ package class146;
 
 // 火星人plus
 // 测试链接 : https://www.luogu.com.cn/problem/U72177
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
 import java.io.BufferedReader;
 import java.io.IOException;
