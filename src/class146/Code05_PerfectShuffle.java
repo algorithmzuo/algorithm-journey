04c71b6eef364cec989a233b9e64429581e95d5e
algorithmzuo <algorithmzuo@163.com>
2024-10-10 16:24:33 +0800

    modify code

--- src/class146/Code05_PerfectShuffle.java
+++ src/class146/Code05_PerfectShuffle.java
@@ -1,7 +1,150 @@
 package class146;
 
 // 完美洗牌算法
+// 给定数组arr，和其中的某个范围arr[l..r]，该范围长度为n，n是偶数
+// 所以arr[l..r]范围上一定可以分成左右两个部分，arr[l1, l2, ...lk, r1, r2, ...rk]
+// 请把arr[l..r]范围上的数字调整成arr[r1, l1, r2, l2, ... rk, lk]，其他位置的数字不变
+// 要求时间复杂度O(n)，额外空间复杂度O(1)
 // 对数器验证
 public class Code05_PerfectShuffle {
+
+	// 申请额外空间的做法
+	// 保证调整的范围是偶数长度
+	// 为了测试
+	public static void shuffle1(int[] arr, int l, int r) {
+		int n = r - l + 1;
+		int[] help = new int[n];
+		for (int l1 = l, r1 = (l + r) / 2 + 1, j = 0; j < n; l1++, r1++) {
+			help[j++] = arr[r1];
+			help[j++] = arr[l1];
+		}
+		for (int i = l, j = 0; j < n; i++, j++) {
+			arr[i] = help[j];
+		}
+	}
+
+	// 正式方法
+	// 完美洗牌算法
+	public static int MAXN = 20;
+
+	public static int[] start = new int[MAXN];
+
+	public static int[] split = new int[MAXN];
+
+	public static int size;
+
+	// 保证调整的范围是偶数长度
+	public static void shuffle2(int[] arr, int l, int r) {
+		int n = r - l + 1;
+		build(n);
+		for (int i = size, m; n > 0;) {
+			if (split[i] <= n) {
+				m = (l + r) / 2;
+				rotate(arr, l + split[i] / 2, m, m + split[i] / 2);
+				circle(arr, l, l + split[i] - 1, i);
+				l += split[i];
+				n -= split[i];
+			} else {
+				i--;
+			}
+		}
+	}
+
+	public static void build(int n) {
+		size = 0;
+		for (int s = 1, p = 2; p <= n; s *= 3, p = s * 3 - 1) {
+			start[++size] = s;
+			split[size] = p;
+		}
+	}
+
+	public static void rotate(int[] arr, int l, int m, int r) {
+		reverse(arr, l, m);
+		reverse(arr, m + 1, r);
+		reverse(arr, l, r);
+	}
+
+	public static void reverse(int[] arr, int l, int r) {
+		while (l < r) {
+			swap(arr, l++, r--);
+		}
+	}
+
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	public static void circle(int[] arr, int l, int r, int t) {
+		int m = (l + r) / 2;
+		for (int i = 1, init, cur, next, curv, nextv; i <= t; i++) {
+			init = cur = l + start[i] - 1;
+			next = to(cur, l, m, r);
+			curv = arr[cur];
+			while (next != init) {
+				nextv = arr[next];
+				arr[next] = curv;
+				curv = nextv;
+				cur = next;
+				next = to(cur, l, m, r);
+			}
+			arr[init] = curv;
+		}
+	}
+
+	public static int to(int i, int l, int m, int r) {
+		if (i <= m) {
+			return i + (i - l + 1);
+		} else {
+			return i - (r - i + 1);
+		}
+	}
+
+	// 生成随机数组
+	// 为了测试
+	public static int[] randomArray(int n, int v) {
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[i] = (int) (Math.random() * v);
+		}
+		return ans;
+	}
+
+	// 拷贝数组
+	// 为了测试
+	public static int[] copyArray(int[] arr) {
+		int n = arr.length;
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[i] = arr[i];
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		int n = 10000;
+		int v = 100000;
+		int[] arr1 = randomArray(n, v);
+		int[] arr2 = copyArray(arr1);
+		int test = 50000;
+		System.out.println("测试开始");
+		for (int i = 1, a, b, l, r; i <= test; i++) {
+			a = (int) (Math.random() * n);
+			b = (int) (Math.random() * n);
+			l = Math.min(a, b);
+			r = Math.max(a, b);
+			if (((r - l + 1) & 1) == 0) {
+				shuffle1(arr1, l, r);
+				shuffle2(arr2, l, r);
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			if (arr1[i] != arr2[i]) {
+				System.out.println("出错了!");
+			}
+		}
+		System.out.println("测试结束");
+	}
 
 }
--- src/class146/Code06_WiggleSortII.java
+++ src/class146/Code06_WiggleSortII.java
@@ -1,7 +1,136 @@
 package class146;
 
-// 摇摆排序ii
+// 摇摆排序ii(进阶要求)
+// 给定一个数组arr，重新排列数组，确保满足：arr[0] < arr[1] > arr[2] < arr[3] > ...
+// 题目保证输入的数组一定有解，要求时间复杂度O(n)，额外空间复杂度O(1)
 // 测试链接 : https://leetcode.cn/problems/wiggle-sort-ii/
 public class Code06_WiggleSortII {
+
+	// 最优解
+	// 时间复杂度O(n)，额外空间复杂度O(1)
+	public static void wiggleSort(int[] arr) {
+		int n = arr.length;
+		randomizedSelect(arr, n, n / 2);
+		if ((n & 1) == 0) {
+			shuffle(arr, 0, n - 1);
+			reverse(arr, 0, n - 1);
+		} else {
+			shuffle(arr, 1, n - 1);
+		}
+	}
+
+	// 随机选择算法，不会去看讲解024
+	// 无序数组中找到第i小的数x，顺便把数组调整为
+	// 小于x的部分 等于x的部分 大于x的部分
+	// 时间复杂度O(n), 额外空间复杂度O(1)
+	public static int randomizedSelect(int[] arr, int n, int i) {
+		int ans = 0;
+		for (int l = 0, r = n - 1; l <= r;) {
+			partition(arr, l, r, arr[l + (int) (Math.random() * (r - l + 1))]);
+			if (i < first) {
+				r = first - 1;
+			} else if (i > last) {
+				l = last + 1;
+			} else {
+				ans = arr[i];
+				break;
+			}
+		}
+		return ans;
+	}
+
+	public static int first, last;
+
+	public static void partition(int[] arr, int l, int r, int x) {
+		first = l;
+		last = r;
+		int i = l;
+		while (i <= last) {
+			if (arr[i] == x) {
+				i++;
+			} else if (arr[i] < x) {
+				swap(arr, first++, i++);
+			} else {
+				swap(arr, i, last--);
+			}
+		}
+	}
+
+	// 完美洗牌算法
+	// 时间复杂度O(n), 额外空间复杂度O(1)
+	public static int MAXN = 20;
+
+	public static int[] start = new int[MAXN];
+
+	public static int[] split = new int[MAXN];
+
+	public static int size;
+
+	public static void shuffle(int[] arr, int l, int r) {
+		int n = r - l + 1;
+		build(n);
+		for (int i = size, m; n > 0;) {
+			if (split[i] <= n) {
+				m = (l + r) / 2;
+				rotate(arr, l + split[i] / 2, m, m + split[i] / 2);
+				circle(arr, l, l + split[i] - 1, i);
+				l += split[i];
+				n -= split[i];
+			} else {
+				i--;
+			}
+		}
+	}
+
+	public static void build(int n) {
+		size = 0;
+		for (int s = 1, p = 2; p <= n; s *= 3, p = s * 3 - 1) {
+			start[++size] = s;
+			split[size] = p;
+		}
+	}
+
+	public static void rotate(int[] arr, int l, int m, int r) {
+		reverse(arr, l, m);
+		reverse(arr, m + 1, r);
+		reverse(arr, l, r);
+	}
+
+	public static void reverse(int[] arr, int l, int r) {
+		while (l < r) {
+			swap(arr, l++, r--);
+		}
+	}
+
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	public static void circle(int[] arr, int l, int r, int t) {
+		int m = (l + r) / 2;
+		for (int i = 1, init, cur, next, curv, nextv; i <= t; i++) {
+			init = cur = l + start[i] - 1;
+			next = to(cur, l, m, r);
+			curv = arr[cur];
+			while (next != init) {
+				nextv = arr[next];
+				arr[next] = curv;
+				curv = nextv;
+				cur = next;
+				next = to(cur, l, m, r);
+			}
+			arr[init] = curv;
+		}
+	}
+
+	public static int to(int i, int l, int m, int r) {
+		if (i <= m) {
+			return i + (i - l + 1);
+		} else {
+			return i - (r - i + 1);
+		}
+	}
 
 }
