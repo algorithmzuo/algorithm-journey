b27506a3df9dd54defc7d2eaca23b40551a2d61c
algorithmzuo <algorithmzuo@163.com>
2024-10-14 21:23:51 +0800

    modify code

---
+++ src/class147/Code07_LeafExpectation.java
@@ -0,0 +1,28 @@
+package class147;
+
+// 叶子节点数的期望
+// 测试链接 : https://www.luogu.com.cn/problem/P3978
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
+public class Code07_LeafExpectation {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		in.nextToken();
+		double n = in.nval;
+		out.printf("%.12f", n * (n + 1) / (2 * (2 * n - 1)));
+		out.flush();
+		out.close();
+		br.close();
+	}
+
+}
