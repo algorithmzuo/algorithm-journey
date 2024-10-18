af06e14cf9d2373751e44ac98d9efacfa5370e4a
algorithmzuo <algorithmzuo@163.com>
2024-10-18 18:55:43 +0800

    modify code

--- src/class147/Code09_LeafExpectation.java
+++ src/class147/Code09_LeafExpectation.java
@@ -2,7 +2,7 @@ package class147;
 
 // 叶子节点数的期望
 // 一共有n个节点，认为节点之间无差别，能形成很多不同结构的二叉树
-// 假设不同结构的二叉树，等概率出现一棵，返回叶子节点的期望是多少
+// 假设所有不同结构的二叉树，等概率出现一棵，返回叶子节点的期望
 // 1 <= n <= 10^9
 // 答案误差小于10的-9次方
 // 测试链接 : https://www.luogu.com.cn/problem/P3978
@@ -23,7 +23,7 @@ public class Code09_LeafExpectation {
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		in.nextToken();
 		double n = in.nval;
-		out.printf("%.9f", n * (n + 1) / (2 * (2 * n - 1)));
+		out.printf("%.9f", n * (n + 1) / ((2 * n - 1) * 2));
 		out.flush();
 		out.close();
 		br.close();
