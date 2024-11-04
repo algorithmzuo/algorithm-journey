6ef1d37415c98b9ab90285f093f496f44931ee6c
algorithmzuo <algorithmzuo@163.com>
2024-11-04 17:05:38 +0800

    modify code

---
+++ src/class150/ScapeGoat1.java
@@ -0,0 +1,225 @@
+package class150;
+
+// 替罪羊树的实现(java版)
+// 实现一种结构，支持如下操作，要求单次调用的时间复杂度O(log n)
+// 1，增加x，重复加入算多个词频
+// 2，删除x，如果有多个，只删掉一个
+// 3，查询x的排名，x的排名为，比x小的数的个数+1
+// 4，查询数据中排名为x的数
+// 5，查询x的前驱，x的前驱为，小于x的数中最大的数，不存在返回整数最小值
+// 6，查询x的后继，x的后继为，大于x的数中最小的数，不存在返回整数最大值
+// 所有操作的次数 <= 10^5
+// -10^7 <= x <= +10^7
+// 测试链接 : https://www.luogu.com.cn/problem/P3369
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
+public class ScapeGoat1 {
+
+	public static int MAXN = 100001;
+
+	public static double ALPHA = 0.7;
+
+	// 整棵树的头节点编号
+	public static int head = 0;
+
+	// 节点的key值
+	public static int[] key = new int[MAXN];
+
+	// 节点key的计数
+	public static int[] count = new int[MAXN];
+
+	// 左孩子
+	public static int[] left = new int[MAXN];
+
+	// 右孩子
+	public static int[] right = new int[MAXN];
+
+	// 数字总数
+	public static int[] size = new int[MAXN];
+
+	// 节点总数
+	public static int[] diff = new int[MAXN];
+
+	public static int[] order = new int[MAXN];
+
+	public static int[] stack = new int[MAXN];
+
+	public static int orderSize, stackSize;
+
+	public static void prepare(int n) {
+		stackSize = 0;
+		for (int i = n; i >= 1; i--) {
+			stack[++stackSize] = i;
+		}
+	}
+
+	public static void inorder(int i) {
+		if (i != 0) {
+			inorder(left[i]);
+			if (count[i] > 0) {
+				order[++orderSize] = i;
+			} else {
+				stack[++stackSize] = i;
+			}
+			inorder(right[i]);
+		}
+	}
+
+	public static void up(int i) {
+		size[i] = size[left[i]] + size[right[i]] + count[i];
+		diff[i] = diff[left[i]] + diff[right[i]] + (count[i] > 0 ? 1 : 0);
+	}
+
+	public static int build(int l, int r) {
+		if (l > r) {
+			return 0;
+		}
+		int m = (l + r) / 2;
+		int h = order[m];
+		left[h] = build(l, m - 1);
+		right[h] = build(m + 1, r);
+		up(h);
+		return h;
+	}
+
+	public static int rebuild(int i) {
+		orderSize = 0;
+		inorder(i);
+		if (orderSize > 0) {
+			return build(1, orderSize);
+		} else {
+			return 0;
+		}
+	}
+
+	public static boolean balance(int i) {
+		return ALPHA * diff[i] > Math.max(diff[left[i]], diff[right[i]]);
+	}
+
+	public static void add(int num) {
+		head = add(head, num);
+	}
+
+	public static int add(int i, int num) {
+		if (i == 0) {
+			i = stack[stackSize--];
+			key[i] = num;
+			left[i] = right[i] = 0;
+			count[i] = size[i] = diff[i] = 1;
+			return i;
+		} else {
+			if (key[i] == num) {
+				count[i]++;
+			} else if (key[i] > num) {
+				left[i] = add(left[i], num);
+			} else {
+				right[i] = add(right[i], num);
+			}
+			up(i);
+			return balance(i) ? i : rebuild(i);
+		}
+	}
+
+	public static int rank(int num) {
+		return small(head, num) + 1;
+	}
+
+	public static int small(int i, int num) {
+		if (i == 0) {
+			return 0;
+		}
+		if (key[i] >= num) {
+			return small(left[i], num);
+		} else {
+			return size[left[i]] + count[i] + small(right[i], num);
+		}
+	}
+
+	public static int index(int x) {
+		return index(head, x);
+	}
+
+	public static int index(int i, int x) {
+		if (size[left[i]] >= x) {
+			return index(left[i], x);
+		} else if (size[left[i]] + count[i] < x) {
+			return index(right[i], x - size[left[i]] - count[i]);
+		}
+		return key[i];
+	}
+
+	public static int pre(int num) {
+		int kth = rank(num);
+		if (kth == 1) {
+			return Integer.MIN_VALUE;
+		} else {
+			return index(kth - 1);
+		}
+	}
+
+	public static int post(int num) {
+		int kth = rank(num + 1);
+		if (kth == size[head] + 1) {
+			return Integer.MAX_VALUE;
+		} else {
+			return index(kth);
+		}
+	}
+
+	public static void remove(int num) {
+		if (rank(num) != rank(num + 1)) {
+			head = remove(head, num);
+		}
+	}
+
+	public static int remove(int i, int num) {
+		if (key[i] == num) {
+			count[i]--;
+		} else if (key[i] > num) {
+			left[i] = remove(left[i], num);
+		} else {
+			right[i] = remove(right[i], num);
+		}
+		up(i);
+		return balance(i) ? i : rebuild(i);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		in.nextToken();
+		int n = (int) in.nval;
+		prepare(n);
+		for (int i = 1, op, x; i <= n; i++) {
+			in.nextToken();
+			op = (int) in.nval;
+			in.nextToken();
+			x = (int) in.nval;
+			if (op == 1) {
+				add(x);
+			} else if (op == 2) {
+				remove(x);
+			} else if (op == 3) {
+				out.println(rank(x));
+			} else if (op == 4) {
+				out.println(index(x));
+			} else if (op == 5) {
+				out.println(pre(x));
+			} else {
+				out.println(post(x));
+			}
+		}
+		out.flush();
+		out.close();
+		br.close();
+	}
+
+}
---
+++ src/class150/ScapeGoat2.java
@@ -0,0 +1,204 @@
+package class150;
+
+// 替罪羊树的实现(C++版)
+// 实现一种结构，支持如下操作，要求单次调用的时间复杂度O(log n)
+// 1，增加x，重复加入算多个词频
+// 2，删除x，如果有多个，只删掉一个
+// 3，查询x的排名，x的排名为，比x小的数的个数+1
+// 4，查询数据中排名为x的数
+// 5，查询x的前驱，x的前驱为，小于x的数中最大的数，不存在返回整数最小值
+// 6，查询x的后继，x的后继为，大于x的数中最小的数，不存在返回整数最大值
+// 所有操作的次数 <= 10^5
+// -10^7 <= x <= +10^7
+// 测试链接 : https://www.luogu.com.cn/problem/P3369
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <cmath>
+//#include <climits>
+//
+//using namespace std;
+//
+//const int MAXN = 100001;
+//
+//const double ALPHA = 0.7;
+//
+//int head = 0;
+//int key[MAXN];
+//int key_count[MAXN];
+//int ls[MAXN];
+//int rs[MAXN];
+//int size[MAXN];
+//int diff[MAXN];
+//int order[MAXN];
+//int stack[MAXN];
+//int orderSize, stackSize;
+//
+//void prepare(int n) {
+//    stackSize = 0;
+//    for (int i = n; i >= 1; --i) {
+//        stack[++stackSize] = i;
+//    }
+//}
+//
+//void inorder(int i) {
+//    if (i != 0) {
+//        inorder(ls[i]);
+//        if (key_count[i] > 0) {
+//            order[++orderSize] = i;
+//        } else {
+//            stack[++stackSize] = i;
+//        }
+//        inorder(rs[i]);
+//    }
+//}
+//
+//void up(int i) {
+//    size[i] = size[ls[i]] + size[rs[i]] + key_count[i];
+//    diff[i] = diff[ls[i]] + diff[rs[i]] + (key_count[i] > 0 ? 1 : 0);
+//}
+//
+//int build(int l, int r) {
+//    if (l > r) {
+//        return 0;
+//    }
+//    int m = (l + r) / 2;
+//    int h = order[m];
+//    ls[h] = build(l, m - 1);
+//    rs[h] = build(m + 1, r);
+//    up(h);
+//    return h;
+//}
+//
+//int rebuild(int i) {
+//    orderSize = 0;
+//    inorder(i);
+//    if (orderSize > 0) {
+//        return build(1, orderSize);
+//    } else {
+//        return 0;
+//    }
+//}
+//
+//bool balance(int i) {
+//    return ALPHA * diff[i] > max(diff[ls[i]], diff[rs[i]]);
+//}
+//
+//int add(int i, int num) {
+//    if (i == 0) {
+//        i = stack[stackSize--];
+//        key[i] = num;
+//        ls[i] = rs[i] = 0;
+//        key_count[i] = size[i] = diff[i] = 1;
+//        return i;
+//    } else {
+//        if (key[i] == num) {
+//            key_count[i]++;
+//        } else if (key[i] > num) {
+//            ls[i] = add(ls[i], num);
+//        } else {
+//            rs[i] = add(rs[i], num);
+//        }
+//        up(i);
+//        return balance(i) ? i : rebuild(i);
+//    }
+//}
+//
+//int add(int num) {
+//    head = add(head, num);
+//    return head;
+//}
+//
+//int small(int i, int num) {
+//    if (i == 0) {
+//        return 0;
+//    }
+//    if (key[i] >= num) {
+//        return small(ls[i], num);
+//    } else {
+//        return size[ls[i]] + key_count[i] + small(rs[i], num); // 使用 key_count
+//    }
+//}
+//
+//int getRank(int num) {
+//    return small(head, num) + 1;
+//}
+//
+//int index(int i, int x) {
+//    if (size[ls[i]] >= x) {
+//        return index(ls[i], x);
+//    } else if (size[ls[i]] + key_count[i] < x) {
+//        return index(rs[i], x - size[ls[i]] - key_count[i]);
+//    }
+//    return key[i];
+//}
+//
+//int index(int x) {
+//    return index(head, x);
+//}
+//
+//int pre(int num) {
+//    int kth = getRank(num);
+//    if (kth == 1) {
+//        return INT_MIN;
+//    } else {
+//        return index(kth - 1);
+//    }
+//}
+//
+//int post(int num) {
+//    int kth = getRank(num + 1);
+//    if (kth == size[head] + 1) {
+//        return INT_MAX;
+//    } else {
+//        return index(kth);
+//    }
+//}
+//
+//int remove(int i, int num) {
+//    if (key[i] == num) {
+//        key_count[i]--;
+//    } else if (key[i] > num) {
+//        ls[i] = remove(ls[i], num);
+//    } else {
+//        rs[i] = remove(rs[i], num);
+//    }
+//    up(i);
+//    return balance(i) ? i : rebuild(i);
+//}
+//
+//int remove(int num) {
+//    if (getRank(num) != getRank(num + 1)) {
+//        head = remove(head, num);
+//    }
+//    return head;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    int n;
+//    cin >> n;
+//    prepare(n);
+//    for (int i = 0; i < n; ++i) {
+//        int op, x;
+//        cin >> op >> x;
+//        if (op == 1) {
+//            add(x);
+//        } else if (op == 2) {
+//            remove(x);
+//        } else if (op == 3) {
+//            cout << getRank(x) << "\n";
+//        } else if (op == 4) {
+//            cout << index(x) << "\n";
+//        } else if (op == 5) {
+//            cout << pre(x) << "\n";
+//        } else {
+//            cout << post(x) << "\n";
+//        }
+//    }
+//    return 0;
+//}
