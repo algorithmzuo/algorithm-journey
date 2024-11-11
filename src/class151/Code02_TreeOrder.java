2f7a431140c54c468550c52d46534871634a49fa
algorithmzuo <algorithmzuo@163.com>
2024-11-11 16:02:44 +0800

    modify code

---
+++ src/class151/Code02_TreeOrder.java
@@ -0,0 +1,80 @@
+package class151;
+
+// 树的序
+// 测试链接 : https://www.luogu.com.cn/problem/P1377
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
+public class Code02_TreeOrder {
+
+	public static int MAXN = 100001;
+
+	public static int[] left = new int[MAXN];
+
+	public static int[] right = new int[MAXN];
+
+	public static int[] arr = new int[MAXN];
+
+	public static int[] stack = new int[MAXN];
+
+	public static int n;
+
+	public static void build() {
+		for (int i = 1, top = 0, pos = 0; i <= n; i++) {
+			pos = top;
+			while (pos > 0 && arr[stack[pos]] > arr[i]) {
+				pos--;
+			}
+			if (pos > 0) {
+				right[stack[pos]] = i;
+			}
+			if (pos < top) {
+				left[i] = stack[pos + 1];
+			}
+			stack[++pos] = i;
+			top = pos;
+		}
+	}
+
+	// 防止递归爆栈用迭代的方式实现先序遍历
+	public static void pre() {
+		int size = 1, i = 0, cur;
+		while (size > 0) {
+			cur = stack[size--];
+			arr[++i] = cur;
+			if (right[cur] != 0) {
+				stack[++size] = right[cur];
+			}
+			if (left[cur] != 0) {
+				stack[++size] = left[cur];
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		in.nextToken();
+		n = (int) in.nval;
+		for (int i = 1; i <= n; i++) {
+			in.nextToken();
+			arr[(int) in.nval] = i;
+		}
+		build();
+		pre();
+		for (int i = 1; i <= n; i++) {
+			out.print(arr[i] + " ");
+		}
+		out.flush();
+		out.close();
+		br.close();
+	}
+
+}
