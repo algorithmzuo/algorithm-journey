50d5e870c3e699e7fafaf1782903bae3e6e03352
algorithmzuo <algorithmzuo@163.com>
2024-11-19 15:58:24 +0800

    modify code

---
+++ src/class152/Code03_LiteraryBalancedTree1.java
@@ -0,0 +1,149 @@
+package class152;
+
+// 文艺平衡树(java版)
+// 测试链接 : https://www.luogu.com.cn/problem/P3391
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+import java.util.Arrays;
+
+public class Code03_LiteraryBalancedTree1 {
+
+	public static int MAXN = 100001;
+
+	public static int head = 0;
+
+	public static int cnt = 0;
+
+	public static int[] key = new int[MAXN];
+
+	public static int[] left = new int[MAXN];
+
+	public static int[] right = new int[MAXN];
+
+	public static int[] size = new int[MAXN];
+
+	public static double[] priority = new double[MAXN];
+
+	public static boolean[] reverse = new boolean[MAXN];
+
+	public static int[] ans = new int[MAXN];
+
+	public static int ansi;
+
+	public static void up(int i) {
+		size[i] = size[left[i]] + size[right[i]] + 1;
+	}
+
+	public static void down(int i) {
+		if (reverse[i]) {
+			int tmp = left[i];
+			left[i] = right[i];
+			right[i] = tmp;
+			reverse[left[i]] = !reverse[left[i]];
+			reverse[right[i]] = !reverse[right[i]];
+			reverse[i] = false;
+		}
+	}
+
+	public static void split(int l, int r, int i, int rank) {
+		if (i == 0) {
+			right[l] = left[r] = 0;
+		} else {
+			down(i);
+			if (size[left[i]] + 1 <= rank) {
+				right[l] = i;
+				split(i, r, right[i], rank - size[left[i]] - 1);
+			} else {
+				left[r] = i;
+				split(l, i, left[i], rank);
+			}
+			up(i);
+		}
+	}
+
+	public static int merge(int l, int r) {
+		if (l == 0 || r == 0) {
+			return l + r;
+		}
+		if (priority[l] >= priority[r]) {
+			down(l);
+			right[l] = merge(right[l], r);
+			up(l);
+			return l;
+		} else {
+			down(r);
+			left[r] = merge(l, left[r]);
+			up(r);
+			return r;
+		}
+	}
+
+	public static void inorder(int i) {
+		if (i != 0) {
+			down(i);
+			inorder(left[i]);
+			ans[++ansi] = key[i];
+			inorder(right[i]);
+		}
+	}
+
+	public static void clear() {
+		Arrays.fill(key, 1, cnt + 1, 0);
+		Arrays.fill(left, 1, cnt + 1, 0);
+		Arrays.fill(right, 1, cnt + 1, 0);
+		Arrays.fill(size, 1, cnt + 1, 0);
+		Arrays.fill(priority, 1, cnt + 1, 0);
+		Arrays.fill(reverse, 1, cnt + 1, false);
+		cnt = 0;
+		head = 0;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		in.nextToken();
+		int n = (int) in.nval;
+		in.nextToken();
+		int k = (int) in.nval;
+		for (int i = 1; i <= n; i++) {
+			key[++cnt] = i;
+			size[cnt] = 1;
+			priority[cnt] = Math.random();
+			head = merge(head, cnt);
+		}
+		for (int i = 1, x, y, l, m, lm, r; i <= k; i++) {
+			in.nextToken();
+			x = (int) in.nval;
+			in.nextToken();
+			y = (int) in.nval;
+			split(0, 0, head, y);
+			r = left[0];
+			lm = right[0];
+			left[0] = right[0] = 0;
+			split(0, 0, lm, x - 1);
+			l = right[0];
+			m = left[0];
+			left[0] = right[0] = 0;
+			reverse[m] = !reverse[m];
+			head = merge(merge(l, m), r);
+		}
+		ansi = 0;
+		inorder(head);
+		for (int i = 1; i <= ansi; i++) {
+			out.print(ans[i] + " ");
+		}
+		out.println();
+		clear();
+		out.flush();
+		out.close();
+		br.close();
+	}
+
+}
---
+++ src/class152/Code03_LiteraryBalancedTree2.java
@@ -0,0 +1,127 @@
+package class152;
+
+// 文艺平衡树(C++版)
+// 测试链接 : https://www.luogu.com.cn/problem/P3391
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <iostream>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <cmath>
+//#include <ctime>
+//#include <cstring>
+//#include <algorithm>
+//using namespace std;
+//
+//const int MAXN = 100001;
+//
+//int head = 0;
+//int cnt = 0;
+//int key[MAXN];
+//int ls[MAXN];
+//int rs[MAXN];
+//int size[MAXN];
+//double priority[MAXN];
+//bool rev[MAXN];
+//int ans[MAXN];
+//int ansi;
+//
+//void up(int i) {
+//    size[i] = size[ls[i]] + size[rs[i]] + 1;
+//}
+//
+//void down(int i) {
+//    if (rev[i]) {
+//        swap(ls[i], rs[i]);
+//        rev[ls[i]] ^= 1;
+//        rev[rs[i]] ^= 1;
+//        rev[i] = false;
+//    }
+//}
+//
+//void split(int l, int r, int i, int rank) {
+//    if (i == 0) {
+//        rs[l] = ls[r] = 0;
+//    } else {
+//        down(i);
+//        if (size[ls[i]] + 1 <= rank) {
+//            rs[l] = i;
+//            split(i, r, rs[i], rank - size[ls[i]] - 1);
+//        } else {
+//            ls[r] = i;
+//            split(l, i, ls[i], rank);
+//        }
+//        up(i);
+//    }
+//}
+//
+//int merge(int l, int r) {
+//    if (l == 0 || r == 0) {
+//        return l + r;
+//    }
+//    if (priority[l] >= priority[r]) {
+//        down(l);
+//        rs[l] = merge(rs[l], r);
+//        up(l);
+//        return l;
+//    } else {
+//        down(r);
+//        ls[r] = merge(l, ls[r]);
+//        up(r);
+//        return r;
+//    }
+//}
+//
+//void inorder(int i) {
+//    if (i != 0) {
+//        down(i);
+//        inorder(ls[i]);
+//        ans[++ansi] = key[i];
+//        inorder(rs[i]);
+//    }
+//}
+//
+//void clear() {
+//    memset(key, 0, sizeof(key));
+//    memset(ls, 0, sizeof(ls));
+//    memset(rs, 0, sizeof(rs));
+//    memset(size, 0, sizeof(size));
+//    memset(priority, 0, sizeof(priority));
+//    memset(rev, 0, sizeof(rev));
+//    cnt = 0;
+//    head = 0;
+//}
+//
+//int main() {
+//	  srand(time(0));
+//    int n, k;
+//    cin >> n >> k;
+//    for (int i = 1; i <= n; i++) {
+//        key[++cnt] = i;
+//        size[cnt] = 1;
+//        priority[cnt] = (double)rand() / RAND_MAX;
+//        head = merge(head, cnt);
+//    }
+//    for (int i = 1, x, y, l, m, lm, r; i <= k; i++) {
+//        cin >> x >> y;
+//        split(0, 0, head, y);
+//        r = ls[0];
+//        lm = rs[0];
+//        ls[0] = rs[0] = 0;
+//        split(0, 0, lm, x - 1);
+//        l = rs[0];
+//        m = ls[0];
+//        ls[0] = rs[0] = 0;
+//        rev[m] ^= 1;
+//        head = merge(merge(l, m), r);
+//    }
+//    ansi = 0;
+//    inorder(head);
+//    for (int i = 1; i <= ansi; i++) {
+//        cout << ans[i] << " ";
+//    }
+//    cout << endl;
+//    clear();
+//    return 0;
+//}
