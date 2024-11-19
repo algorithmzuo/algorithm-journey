aa216495507c0d2b149ace3758d33639eaba94de
algorithmzuo <algorithmzuo@163.com>
2024-11-19 19:05:42 +0800

    modify code

--- src/class152/Code04_FHQTreapPersistent2.java
+++ src/class152/Code04_FHQTreapPersistent2.java
@@ -1,6 +1,6 @@
 package class152;
 
-// 可持久化有序表，FHQ-Treap实现，不用词频统计，C++版
+// 可持久化平衡树，FHQ-Treap实现，不用词频统计，C++版
 // 测试链接 : https://www.luogu.com.cn/problem/P3835
 // 如下实现是C++的版本，C++版本和java版本逻辑完全一样
 // 提交如下代码，可以通过所有测试用例
