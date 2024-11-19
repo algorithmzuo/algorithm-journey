aed65dd798250963563922e7a1a70566e8dec21b
algorithmzuo <algorithmzuo@163.com>
2024-11-19 16:02:12 +0800

    modify code

--- src/class152/Code03_LiteraryTree1.java
+++ src/class152/Code03_LiteraryTree1.java
@@ -12,7 +12,7 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
-public class Code03_LiteraryBalancedTree1 {
+public class Code03_LiteraryTree1 {
 
 	public static int MAXN = 100001;
 
