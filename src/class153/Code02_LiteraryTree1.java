0873df497bf215467dbeac0542a8611cd83ca558
algorithmzuo <algorithmzuo@163.com>
2024-11-27 14:30:40 +0800

    modify code

---
+++ src/class153/Code02_LiteraryTree1.java
@@ -0,0 +1,225 @@
+package class153;
+
+// 文艺平衡树，Splay实现范围翻转，java版本
+// 长度为n的序列，下标从1开始，一开始序列为1, 2, ..., n
+// 接下来会有k个操作，每个操作给定l，r，表示从l到r范围上的所有数字翻转
+// 做完k次操作后，从左到右打印所有数字
+// 1 <= n, k <= 10^5
+// 测试链接 : https://www.luogu.com.cn/problem/P3391
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
+public class Code02_LiteraryTree1 {
+
+	public static int MAXN = 100005;
+
+	public static int head = 0;
+
+	public static int cnt = 0;
+
+	public static int[] key = new int[MAXN];
+
+	public static int[] father = new int[MAXN];
+
+	public static int[] left = new int[MAXN];
+
+	public static int[] right = new int[MAXN];
+
+	public static int[] size = new int[MAXN];
+
+	public static boolean[] reverse = new boolean[MAXN];
+
+	public static int[] stack = new int[MAXN];
+
+	public static int si;
+
+	public static int[] ans = new int[MAXN];
+
+	public static int ai;
+
+	public static void up(int i) {
+		size[i] = size[left[i]] + size[right[i]] + 1;
+	}
+
+	public static int lr(int i) {
+		return right[father[i]] == i ? 1 : 0;
+	}
+
+	public static void upRotate(int i) {
+		int f = father[i], g = father[f], soni = lr(i), sonf = lr(f);
+		if (soni == 1) {
+			right[f] = left[i];
+			if (right[f] != 0) {
+				father[right[f]] = f;
+			}
+			left[i] = f;
+		} else {
+			left[f] = right[i];
+			if (left[f] != 0) {
+				father[left[f]] = f;
+			}
+			right[i] = f;
+		}
+		if (g != 0) {
+			if (sonf == 1) {
+				right[g] = i;
+			} else {
+				left[g] = i;
+			}
+		}
+		father[f] = i;
+		father[i] = g;
+		up(f);
+		up(i);
+	}
+
+	public static void splay(int i, int goal) {
+		int f = father[i], g = father[f];
+		while (f != goal) {
+			if (g != goal) {
+				if (lr(i) == lr(f)) {
+					upRotate(f);
+				} else {
+					upRotate(i);
+				}
+			}
+			upRotate(i);
+			f = father[i];
+			g = father[f];
+		}
+		if (goal == 0) {
+			head = i;
+		}
+	}
+
+	public static void down(int i) {
+		if (reverse[i]) {
+			reverse[left[i]] = !reverse[left[i]];
+			reverse[right[i]] = !reverse[right[i]];
+			int tmp = left[i];
+			left[i] = right[i];
+			right[i] = tmp;
+			reverse[i] = false;
+		}
+	}
+
+	public static int find(int i, int rank) {
+		while (i != 0) {
+			down(i);
+			if (size[left[i]] + 1 == rank) {
+				return i;
+			} else if (size[left[i]] >= rank) {
+				i = left[i];
+			} else {
+				rank -= size[left[i]] + 1;
+				i = right[i];
+			}
+		}
+		return 0;
+	}
+
+	public static void add(int num) {
+		key[++cnt] = num;
+		size[cnt] = 1;
+		if (head == 0) {
+			head = cnt;
+		} else {
+			int f = 0, i = head, son = 0;
+			while (i != 0) {
+				size[i]++;
+				f = i;
+				if (key[i] <= num) {
+					son = 1;
+					i = right[i];
+				} else {
+					son = 0;
+					i = left[i];
+				}
+			}
+			if (son == 1) {
+				right[f] = cnt;
+			} else {
+				left[f] = cnt;
+			}
+			father[cnt] = f;
+			splay(cnt, 0);
+		}
+	}
+
+	public static void reverse(int l, int r) {
+		int i = find(head, l - 1);
+		int j = find(head, r + 1);
+		splay(i, 0);
+		splay(j, i);
+		reverse[left[right[head]]] = !reverse[left[right[head]]];
+	}
+
+	// 递归方式实现二叉树中序遍历
+	// 对本题来说，递归不会爆栈，但其实是有风险的
+	public static void inorder(int i) {
+		if (i != 0) {
+			down(i);
+			inorder(left[i]);
+			ans[++ai] = key[i];
+			inorder(right[i]);
+		}
+	}
+
+	// 迭代方式实现二叉树中序遍历，防止递归爆栈
+	// 不懂的话，看讲解018，迭代方式实现二叉树中序遍历
+	// 遍历时候懒更新任务也要下发
+	public static void inorder() {
+		si = 0;
+		int i = head;
+		while (si != 0 || i != 0) {
+			if (i != 0) {
+				down(i);
+				stack[++si] = i;
+				i = left[i];
+			} else {
+				i = stack[si--];
+				ans[++ai] = key[i];
+				i = right[i];
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		in.nextToken();
+		int n = (int) in.nval;
+		in.nextToken();
+		int m = (int) in.nval;
+		add(Integer.MIN_VALUE);
+		add(Integer.MAX_VALUE);
+		for (int i = 1; i <= n; i++) {
+			add(i);
+		}
+		for (int i = 1, x, y; i <= m; i++) {
+			in.nextToken();
+			x = (int) in.nval;
+			in.nextToken();
+			y = (int) in.nval;
+			reverse(x + 1, y + 1);
+		}
+		ai = 0;
+//		inorder(head);
+		inorder();
+		for (int i = 2; i < ai; i++) {
+			out.print(ans[i] + " ");
+		}
+		out.println();
+		out.flush();
+		out.close();
+		br.close();
+	}
+
+}
---
+++ src/class153/Code02_LiteraryTree2.java
@@ -0,0 +1,197 @@
+package class153;
+
+// 文艺平衡树，Splay实现范围翻转，C++版本
+// 长度为n的序列，下标从1开始，一开始序列为1, 2, ..., n
+// 接下来会有k个操作，每个操作给定l，r，表示从l到r范围上的所有数字翻转
+// 做完k次操作后，从左到右打印所有数字
+// 1 <= n, k <= 10^5
+// 测试链接 : https://www.luogu.com.cn/problem/P3391
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <iostream>
+//#include <vector>
+//#include <stack>
+//#include <climits>
+//using namespace std;
+//
+//const int MAXN = 100005;
+//
+//int head = 0;
+//int cnt = 0;
+//int key[MAXN];
+//int fa[MAXN];
+//int ls[MAXN];
+//int rs[MAXN];
+//int size[MAXN];
+//bool rev[MAXN];
+//int sta[MAXN];
+//int si;
+//int ans[MAXN];
+//int ai;
+//
+//void up(int i) {
+//    size[i] = size[ls[i]] + size[rs[i]] + 1;
+//}
+//
+//int lr(int i) {
+//    return rs[fa[i]] == i ? 1 : 0;
+//}
+//
+//void upRotate(int i) {
+//    int f = fa[i], g = fa[f], soni = lr(i), sonf = lr(f);
+//    if (soni == 1) {
+//        rs[f] = ls[i];
+//        if (rs[f] != 0) {
+//            fa[rs[f]] = f;
+//        }
+//        ls[i] = f;
+//    } else {
+//        ls[f] = rs[i];
+//        if (ls[f] != 0) {
+//            fa[ls[f]] = f;
+//        }
+//        rs[i] = f;
+//    }
+//    if (g != 0) {
+//        if (sonf == 1) {
+//            rs[g] = i;
+//        } else {
+//            ls[g] = i;
+//        }
+//    }
+//    fa[f] = i;
+//    fa[i] = g;
+//    up(f);
+//    up(i);
+//}
+//
+//void splay(int i, int goal) {
+//    int f = fa[i], g = fa[f];
+//    while (f != goal) {
+//        if (g != goal) {
+//            if (lr(i) == lr(f)) {
+//                upRotate(f);
+//            } else {
+//                upRotate(i);
+//            }
+//        }
+//        upRotate(i);
+//        f = fa[i];
+//        g = fa[f];
+//    }
+//    if (goal == 0) {
+//        head = i;
+//    }
+//}
+//
+//void down(int i) {
+//    if (rev[i]) {
+//        rev[ls[i]] = !rev[ls[i]];
+//        rev[rs[i]] = !rev[rs[i]];
+//        int tmp = ls[i];
+//        ls[i] = rs[i];
+//        rs[i] = tmp;
+//        rev[i] = false;
+//    }
+//}
+//
+//int find(int i, int rank) {
+//    while (i != 0) {
+//        down(i);
+//        if (size[ls[i]] + 1 == rank) {
+//            return i;
+//        } else if (size[ls[i]] >= rank) {
+//            i = ls[i];
+//        } else {
+//            rank -= size[ls[i]] + 1;
+//            i = rs[i];
+//        }
+//    }
+//    return 0;
+//}
+//
+//void add(int num) {
+//    key[++cnt] = num;
+//    size[cnt] = 1;
+//    if (head == 0) {
+//        head = cnt;
+//    } else {
+//        int f = 0, i = head, son = 0;
+//        while (i != 0) {
+//            size[i]++;
+//            f = i;
+//            if (key[i] <= num) {
+//                son = 1;
+//                i = rs[i];
+//            } else {
+//                son = 0;
+//                i = ls[i];
+//            }
+//        }
+//        if (son == 1) {
+//            rs[f] = cnt;
+//        } else {
+//            ls[f] = cnt;
+//        }
+//        fa[cnt] = f;
+//        splay(cnt, 0);
+//    }
+//}
+//
+//void reverse(int l, int r) {
+//    int i = find(head, l - 1);
+//    int j = find(head, r + 1);
+//    splay(i, 0);
+//    splay(j, i);
+//    rev[ls[rs[head]]] = !rev[ls[rs[head]]];
+//}
+//
+//void inorder(int i) {
+//    if (i != 0) {
+//        down(i);
+//        inorder(ls[i]);
+//        ans[++ai] = key[i];
+//        inorder(rs[i]);
+//    }
+//}
+//
+//void inorder() {
+//    si = 0;
+//    int i = head;
+//    while (si != 0 || i != 0) {
+//        if (i != 0) {
+//            down(i);
+//            sta[++si] = i;
+//            i = ls[i];
+//        } else {
+//            i = sta[si--];
+//            ans[++ai] = key[i];
+//            i = rs[i];
+//        }
+//    }
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    int n, m;
+//    cin >> n >> m;
+//    add(INT_MIN);
+//    add(INT_MAX);
+//    for (int i = 1; i <= n; i++) {
+//        add(i);
+//    }
+//    for (int i = 1, x, y; i <= m; i++) {
+//        cin >> x >> y;
+//        reverse(x + 1, y + 1);
+//    }
+//    ai = 0;
+//    // inorder(head);
+//    inorder();
+//    for (int i = 2; i < ai; i++) {
+//        cout << ans[i] << " ";
+//    }
+//    cout << endl;
+//    return 0;
+//}
