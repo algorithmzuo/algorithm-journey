62f4fee4c4521e864c62c6e00ce3905a6fb12a68
algorithmzuo <algorithmzuo@163.com>
2024-12-09 19:27:35 +0800

    modify code

---
+++ src/class154/Code02_RomanGame1.java
@@ -0,0 +1,344 @@
+package class154;
+
+// 罗马游戏，左偏树模版，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P2713
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.util.InputMismatchException;
+
+public class Code02_RomanGame1 {
+
+	public static int MAXN = 1000001;
+
+	public static int INF = 1000000001;
+
+	public static int[] num = new int[MAXN];
+
+	public static int[] father = new int[MAXN];
+
+	public static int[] left = new int[MAXN];
+
+	public static int[] right = new int[MAXN];
+
+	public static int[] dist = new int[MAXN];
+
+	public static int find(int i) {
+		if (father[i] != i) {
+			father[i] = find(father[i]);
+		}
+		return father[i];
+	}
+
+	public static int merge(int i, int j) {
+		if (i == 0 || j == 0) {
+			return i + j;
+		}
+		int tmp;
+		if (num[i] > num[j]) {
+			tmp = i;
+			i = j;
+			j = tmp;
+		}
+		right[i] = merge(right[i], j);
+		if (dist[left[i]] < dist[right[i]]) {
+			tmp = left[i];
+			left[i] = right[i];
+			right[i] = tmp;
+		}
+		dist[i] = dist[right[i]] + 1;
+		father[i] = father[left[i]] = father[right[i]] = i;
+		return i;
+	}
+
+	public static void pop(int i) {
+		num[i] = -INF;
+		father[left[i]] = left[i];
+		father[right[i]] = right[i];
+		// 下面这一句的功能
+		// 因为有路径压缩，所以i下方的某个节点x，可能有father[x] = i
+		// 但是现在堆要去掉i了，所以x一直往上找到i是无效的
+		// 为了i能再往上找到正确的头，所以有下面这句
+		father[i] = merge(left[i], right[i]);
+	}
+
+	public static void main(String[] args) {
+		FastReader in = new FastReader(System.in);
+		FastWriter out = new FastWriter(System.out);
+		int n = in.readInt();
+		for (int i = 1; i <= n; i++) {
+			father[i] = i;
+			num[i] = in.readInt();
+		}
+		int m = in.readInt();
+		String op;
+		for (int i = 1, x, y; i <= m; i++) {
+			op = in.readString();
+			if (op.equals("M")) {
+				x = in.readInt();
+				y = in.readInt();
+				if (num[x] != -INF && num[y] != -INF) {
+					int l = find(x);
+					int r = find(y);
+					if (l != r) {
+						father[l] = father[r] = merge(l, r);
+					}
+				}
+			} else {
+				x = in.readInt();
+				if (num[x] == -INF) {
+					out.println(0);
+				} else {
+					int ans = find(x);
+					out.println(num[ans]);
+					pop(ans);
+				}
+			}
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 快读
+	public static class FastReader {
+		InputStream is;
+		private byte[] inbuf = new byte[1024];
+		public int lenbuf = 0;
+		public int ptrbuf = 0;
+
+		public FastReader(final InputStream is) {
+			this.is = is;
+		}
+
+		public String readString() {
+			char cur;
+			do {
+				cur = (char) readByte();
+			} while (cur == ' ' || cur == '\n');
+			StringBuilder builder = new StringBuilder();
+			while (cur != ' ' && cur != '\n') {
+				builder.append(cur);
+				cur = (char) readByte();
+			}
+			return builder.toString();
+		}
+
+		public int readByte() {
+			if (lenbuf == -1) {
+				throw new InputMismatchException();
+			}
+			if (ptrbuf >= lenbuf) {
+				ptrbuf = 0;
+				try {
+					lenbuf = is.read(inbuf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (lenbuf <= 0) {
+					return -1;
+				}
+			}
+			return inbuf[ptrbuf++];
+		}
+
+		public int readInt() {
+			return (int) readLong();
+		}
+
+		public long readLong() {
+			long num = 0;
+			int b;
+			boolean minus = false;
+			while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+				;
+			if (b == '-') {
+				minus = true;
+				b = readByte();
+			}
+
+			while (true) {
+				if (b >= '0' && b <= '9') {
+					num = num * 10 + (b - '0');
+				} else {
+					return minus ? -num : num;
+				}
+				b = readByte();
+			}
+		}
+	}
+
+	// 快写
+	public static class FastWriter {
+		private static final int BUF_SIZE = 1 << 13;
+		private final byte[] buf = new byte[BUF_SIZE];
+		private OutputStream out;
+		private Writer writer;
+		private int ptr = 0;
+
+		public FastWriter(Writer writer) {
+			this.writer = new BufferedWriter(writer);
+			out = new ByteArrayOutputStream();
+		}
+
+		public FastWriter(OutputStream os) {
+			this.out = os;
+		}
+
+		public FastWriter(String path) {
+			try {
+				this.out = new FileOutputStream(path);
+			} catch (FileNotFoundException e) {
+				throw new RuntimeException("FastWriter");
+			}
+		}
+
+		public FastWriter write(byte b) {
+			buf[ptr++] = b;
+			if (ptr == BUF_SIZE) {
+				innerflush();
+			}
+			return this;
+		}
+
+		public FastWriter write(String s) {
+			s.chars().forEach(c -> {
+				buf[ptr++] = (byte) c;
+				if (ptr == BUF_SIZE) {
+					innerflush();
+				}
+			});
+			return this;
+		}
+
+		private static int countDigits(long l) {
+			if (l >= 1000000000000000000L) {
+				return 19;
+			}
+			if (l >= 100000000000000000L) {
+				return 18;
+			}
+			if (l >= 10000000000000000L) {
+				return 17;
+			}
+			if (l >= 1000000000000000L) {
+				return 16;
+			}
+			if (l >= 100000000000000L) {
+				return 15;
+			}
+			if (l >= 10000000000000L) {
+				return 14;
+			}
+			if (l >= 1000000000000L) {
+				return 13;
+			}
+			if (l >= 100000000000L) {
+				return 12;
+			}
+			if (l >= 10000000000L) {
+				return 11;
+			}
+			if (l >= 1000000000L) {
+				return 10;
+			}
+			if (l >= 100000000L) {
+				return 9;
+			}
+			if (l >= 10000000L) {
+				return 8;
+			}
+			if (l >= 1000000L) {
+				return 7;
+			}
+			if (l >= 100000L) {
+				return 6;
+			}
+			if (l >= 10000L) {
+				return 5;
+			}
+			if (l >= 1000L) {
+				return 4;
+			}
+			if (l >= 100L) {
+				return 3;
+			}
+			if (l >= 10L) {
+				return 2;
+			}
+			return 1;
+		}
+
+		public FastWriter write(long x) {
+			if (x == Long.MIN_VALUE) {
+				return write("" + x);
+			}
+			if (ptr + 21 >= BUF_SIZE) {
+				innerflush();
+			}
+			if (x < 0) {
+				write((byte) '-');
+				x = -x;
+			}
+			int d = countDigits(x);
+			for (int i = ptr + d - 1; i >= ptr; i--) {
+				buf[i] = (byte) ('0' + x % 10);
+				x /= 10;
+			}
+			ptr += d;
+			return this;
+		}
+
+		public FastWriter writeln(long x) {
+			return write(x).writeln();
+		}
+
+		public FastWriter writeln() {
+			return write((byte) '\n');
+		}
+
+		private void innerflush() {
+			try {
+				out.write(buf, 0, ptr);
+				ptr = 0;
+			} catch (IOException e) {
+				throw new RuntimeException("innerflush");
+			}
+		}
+
+		public void flush() {
+			innerflush();
+			try {
+				if (writer != null) {
+					writer.write(((ByteArrayOutputStream) out).toString());
+					out = new ByteArrayOutputStream();
+					writer.flush();
+				} else {
+					out.flush();
+				}
+			} catch (IOException e) {
+				throw new RuntimeException("flush");
+			}
+		}
+
+		public FastWriter println(long x) {
+			return writeln(x);
+		}
+
+		public void close() {
+			flush();
+			try {
+				out.close();
+			} catch (Exception e) {
+			}
+		}
+
+	}
+
+}
---
+++ src/class154/Code02_RomanGame2.java
@@ -0,0 +1,87 @@
+package class154;
+
+// 罗马游戏，左偏树模版，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P2713
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//const int MAXN = 1000001;
+//const int INF = 1000000001;
+//int num[MAXN];
+//int fa[MAXN];
+//int ls[MAXN];
+//int rs[MAXN];
+//int dist[MAXN];
+//
+//int find(int i) {
+//    if (fa[i] != i) {
+//        fa[i] = find(fa[i]);
+//    }
+//    return fa[i];
+//}
+//
+//int merge(int i, int j) {
+//    if (i == 0 || j == 0) {
+//        return i + j;
+//    }
+//    if (num[i] > num[j]) {
+//        int tmp = i;
+//        i = j;
+//        j = tmp;
+//    }
+//    rs[i] = merge(rs[i], j);
+//    if (dist[ls[i]] < dist[rs[i]]) {
+//        int tmp = ls[i];
+//        ls[i] = rs[i];
+//        rs[i] = tmp;
+//    }
+//    dist[i] = dist[rs[i]] + 1;
+//    fa[i] = fa[ls[i]] = fa[rs[i]] = i;
+//    return i;
+//}
+//
+//void pop(int i) {
+//    num[i] = -INF;
+//    fa[ls[i]] = ls[i];
+//    fa[rs[i]] = rs[i];
+//    fa[i] = merge(ls[i], rs[i]);
+//}
+//
+//int main() {
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    int n; cin >> n;
+//    for (int i = 1; i <= n; i++) {
+//        fa[i] = i;
+//        cin >> num[i];
+//    }
+//    int m; cin >> m;
+//    for (int i = 1; i <= m; i++) {
+//        string op; cin >> op;
+//        if (op == "M") {
+//            int x, y; cin >> x >> y;
+//            if (num[x] != -INF && num[y] != -INF) {
+//                int l = find(x);
+//                int r = find(y);
+//                if (l != r) {
+//                    int rt = merge(l, r);
+//                    fa[l] = fa[r] = rt;
+//                }
+//            }
+//        } else {
+//            int x; cin >> x;
+//            if (num[x] == -INF) {
+//                cout << 0 << endl;
+//            } else {
+//                int ans = find(x);
+//                cout << num[ans] << endl;
+//                pop(ans);
+//            }
+//        }
+//    }
+//    return 0;
+//}
