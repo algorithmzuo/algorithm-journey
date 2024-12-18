51b2e919c335af3a0ae1700829f0871d16c55222
algorithmzuo <algorithmzuo@163.com>
2024-12-18 23:53:38 +0800

    modify code

--- src/class155/Code02_TrickyOperation1.java
+++ src/class155/Code02_TrickyOperation1.java
@@ -2,6 +2,7 @@ package class155;
 
 // 棘手的操作，java版
 // 测试链接 : https://www.luogu.com.cn/problem/P3273
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
 import java.io.IOException;
 import java.io.PrintWriter;
--- src/class155/Code03_KShortestPath1.java
+++ src/class155/Code03_KShortestPath1.java
@@ -2,6 +2,7 @@ package class155;
 
 // 可持久化左偏树 + k短路，java版
 // 测试链接 : https://www.luogu.com.cn/problem/P2483
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
 import java.io.BufferedWriter;
 import java.io.IOException;
