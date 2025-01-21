5aaf935f620442a9c8f89c5d36d199aa2a06e8b1
algorithmzuo <algorithmzuo@163.com>
2025-01-21 17:59:01 +0800

    modify code

--- src/class159/Code06_XorOperation1.java
+++ src/class159/Code06_XorOperation1.java
@@ -11,7 +11,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 
-public class Code05_XorOperation1 {
+public class Code06_XorOperation1 {
 
 	public static int MAXN = 300001;
 
