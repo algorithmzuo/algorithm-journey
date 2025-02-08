2545d82d2953cc5fe33d32b152349d0ca0d51207
algorithmzuo <algorithmzuo@163.com>
2025-02-08 17:58:01 +0800

    modify code

--- src/class160/Code01_NestedTree1.java
+++ src/class160/Code01_NestedTree1.java
@@ -1,6 +1,6 @@
 package class160;
 
-// 树状数组套线段树，java版
+// 树套树，java版
 // 测试链接 : https://www.luogu.com.cn/problem/P3380
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
@@ -12,7 +12,7 @@ import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 import java.util.Arrays;
 
-public class Code01_IndextreeWithSegmenttree1 {
+public class Code01_NestedTree1 {
 
 	public static int MAXN = 50001;
 
