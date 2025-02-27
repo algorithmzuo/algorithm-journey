43f11bb2d74c48779836b9a95f22c6f4fbb5bbe2
algorithmzuo <algorithmzuo@163.com>
2025-02-27 16:33:55 +0800

    modify code

--- src/class161/Code06_Tourism1.java
+++ src/class161/Code06_Tourism1.java
@@ -11,7 +11,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.StreamTokenizer;
 
-public class Code06_Travel1 {
+public class Code06_Tourism1 {
 
 	public static int MAXN = 50001;
 
