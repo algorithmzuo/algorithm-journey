36b34d520d2839dfad3f245ae51ca08294a7ec44
algorithmzuo <algorithmzuo@163.com>
2025-03-10 16:55:41 +0800

    modify code

--- src/class162/Code05_DominantIndices1.java
+++ src/class162/Code05_DominantIndices1.java
@@ -1,6 +1,6 @@
 package class162;
 
-// 哪个距离的点最多，java递归版
+// 哪个距离的点最多，java版
 // 一共有n个节点，给定n-1条边，所有节点连成一棵树，规定1号节点是头
 // 规定任何点到自己的距离为0
 // 定义d(u, x)，以u为头的子树中，到u的距离为x的节点数
@@ -9,8 +9,7 @@ package class162;
 // 1 <= n <= 10^6
 // 测试链接 : https://www.luogu.com.cn/problem/CF1009F
 // 测试链接 : https://codeforces.com/problemset/problem/1009/F
-// 提交以下的code，提交时请把类名改成"Main"，因为递归爆栈所以无法通过
-// 迭代版的实现就是本节课Code05_DominantIndices2文件
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -53,7 +52,7 @@ public class Code05_DominantIndices1 {
 		head[u] = cnt;
 	}
 
-	// 递归版，C++可以通过，java会爆栈
+	// 递归版，C++可以通过，java会爆栈，迭代版是dfs3方法
 	public static void dfs1(int u, int fa) {
 		for (int e = head[u], v; e > 0; e = next[e]) {
 			v = to[e];
@@ -72,7 +71,7 @@ public class Code05_DominantIndices1 {
 		len[u] = len[son[u]] + 1;
 	}
 
-	// 递归版，C++可以通过，java会爆栈
+	// 递归版，C++可以通过，java会爆栈，迭代版是dfs4方法
 	public static void dfs2(int u, int fa) {
 		// 第一部分，给所有儿子设置start位置
 		set(u, 0, 1);
@@ -116,6 +115,113 @@ public class Code05_DominantIndices1 {
 		}
 	}
 
+	// 不会改迭代版，去看讲解118，详解了从递归版改迭代版
+	public static int[][] ufe = new int[MAXN][3];
+
+	public static int stacksize, u, f, e;
+
+	public static void push(int a, int b, int c) {
+		ufe[stacksize][0] = a;
+		ufe[stacksize][1] = b;
+		ufe[stacksize][2] = c;
+		stacksize++;
+	}
+
+	public static void pop() {
+		--stacksize;
+		u = ufe[stacksize][0];
+		f = ufe[stacksize][1];
+		e = ufe[stacksize][2];
+	}
+
+	// 迭代版
+	public static void dfs3() {
+		stacksize = 0;
+		push(1, 0, -1);
+		int v;
+		while (stacksize > 0) {
+			pop();
+			if (e == -1) {
+				e = head[u];
+			} else {
+				e = next[e];
+			}
+			if (e != 0) {
+				push(u, f, e);
+				v = to[e];
+				if (v != f) {
+					push(v, u, -1);
+				}
+			} else {
+				for (int ei = head[u]; ei > 0; ei = next[ei]) {
+					v = to[ei];
+					if (v != f) {
+						if (son[u] == 0 || len[son[u]] < len[v]) {
+							son[u] = v;
+						}
+					}
+				}
+				len[u] = len[son[u]] + 1;
+			}
+		}
+	}
+
+	// 迭代版
+	public static void dfs4() {
+		stacksize = 0;
+		push(1, 0, -1);
+		int v;
+		while (stacksize > 0) {
+			pop();
+			if (e == -1) { // e == -1，表示第一次来到当前节点，对应第一部分
+				set(u, 0, 1);
+				if (son[u] == 0) {
+					ansx[u] = 0;
+					continue;
+				}
+				start[son[u]] = start[u] + 1;
+				int startSum = start[u] + len[u];
+				for (int e = head[u]; e > 0; e = next[e]) {
+					v = to[e];
+					if (v != f && v != son[u]) {
+						start[v] = startSum;
+						startSum += len[v];
+					}
+				}
+				push(u, f, -2); // 设置e = -2，再弹出就表示处理完长儿子了
+				push(son[u], u, -1); // 长儿子的任务进入栈，对应第二部分
+				continue;
+			} else if (e == -2) { // e == -2，表示长儿子已经处理完，该处理其他儿子了
+				e = head[u];
+			} else { // 每个儿子都处理
+				e = next[e];
+			}
+			if (e != 0) { // 对应第三部分
+				push(u, f, e);
+				v = to[e];
+				if (v != f && v != son[u]) {
+					push(v, u, -1);
+				}
+			} else { // 对应第四部分，计算答案
+				ansx[u] = ansx[son[u]] + 1;
+				for (int e = head[u]; e > 0; e = next[e]) {
+					v = to[e];
+					if (v != f && v != son[u]) {
+						for (int i = 1; i <= len[v]; i++) {
+							set(u, i, get(u, i) + get(v, i - 1));
+							if (get(u, i) > get(u, ansx[u]) || (get(u, i) == get(u, ansx[u]) && i < ansx[u])) {
+								ansx[u] = i;
+							}
+						}
+					}
+				}
+				if (get(u, ansx[u]) == 1) {
+					ansx[u] = 0;
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer in = new StreamTokenizer(br);
@@ -130,9 +236,9 @@ public class Code05_DominantIndices1 {
 			addEdge(u, v);
 			addEdge(v, u);
 		}
-		dfs1(1, 0);
+		dfs3();
 		start[1] = 1;
-		dfs2(1, 0);
+		dfs4();
 		for (int i = 1; i <= n; i++) {
 			out.println(ansx[i]);
 		}
