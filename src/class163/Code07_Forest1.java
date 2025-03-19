b0fa4d32920d12ebf7d5b602ae62d8d6c75be308
algorithmzuo <algorithmzuo@163.com>
2025-03-19 20:11:14 +0800

    modify code

---
+++ src/class163/Code07_Forest1.java
@@ -0,0 +1,344 @@
+package class163;
+
+// 森林，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P3302
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class Code07_Forest1 {
+
+	public static int MAXN = 80001;
+	public static int MAXT = MAXN * 110;
+	public static int MAXH = 20;
+	public static int testcase;
+	public static int n, m, t;
+
+	public static int[] arr = new int[MAXN];
+	public static int[] sorted = new int[MAXN];
+	public static int diff;
+
+	public static int[] head = new int[MAXN];
+	public static int[] next = new int[MAXN << 1];
+	public static int[] to = new int[MAXN << 1];
+	public static int cntg = 0;
+
+	public static int[] root = new int[MAXN];
+	public static int[] left = new int[MAXT];
+	public static int[] right = new int[MAXT];
+	public static int[] siz = new int[MAXT];
+	public static int cntt = 0;
+
+	public static int[] dep = new int[MAXN];
+	public static int[] treeHead = new int[MAXN];
+	public static int[] headSiz = new int[MAXN];
+	public static int[][] stjump = new int[MAXN][MAXH];
+
+	public static int kth(int num) {
+		int left = 1, right = diff, mid;
+		while (left <= right) {
+			mid = (left + right) / 2;
+			if (sorted[mid] == num) {
+				return mid;
+			} else if (sorted[mid] < num) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return -1;
+	}
+
+	public static void addEdge(int u, int v) {
+		next[++cntg] = head[u];
+		to[cntg] = v;
+		head[u] = cntg;
+	}
+
+	public static int insert(int jobi, int l, int r, int i) {
+		int rt = ++cntt;
+		left[rt] = left[i];
+		right[rt] = right[i];
+		siz[rt] = siz[i] + 1;
+		if (l < r) {
+			int mid = (l + r) / 2;
+			if (jobi <= mid) {
+				left[rt] = insert(jobi, l, mid, left[rt]);
+			} else {
+				right[rt] = insert(jobi, mid + 1, r, right[rt]);
+			}
+		}
+		return rt;
+	}
+
+	public static int query(int jobk, int l, int r, int u, int v, int lca, int lcafa) {
+		if (l == r) {
+			return l;
+		}
+		int lsize = siz[left[u]] + siz[left[v]] - siz[left[lca]] - siz[left[lcafa]];
+		int mid = (l + r) / 2;
+		if (lsize >= jobk) {
+			return query(jobk, l, mid, left[u], left[v], left[lca], left[lcafa]);
+		} else {
+			return query(jobk - lsize, mid + 1, r, right[u], right[v], right[lca], right[lcafa]);
+		}
+	}
+
+	// 递归版，C++可以通过，java无法通过，递归会爆栈
+	public static void dfs1(int u, int fa, int treeh) {
+		root[u] = insert(arr[u], 1, diff, root[fa]);
+		dep[u] = dep[fa] + 1;
+		treeHead[u] = treeh;
+		headSiz[treeh]++;
+		stjump[u][0] = fa;
+		for (int p = 1; p < MAXH; p++) {
+			stjump[u][p] = stjump[stjump[u][p - 1]][p - 1];
+		}
+		for (int e = head[u]; e > 0; e = next[e]) {
+			if (to[e] != fa) {
+				dfs1(to[e], u, treeh);
+			}
+		}
+	}
+
+	// 迭代版，都可以通过
+	// 讲解118，详解了从递归版改迭代版
+	public static int[][] stack = new int[MAXN][4];
+
+	public static int stackSize, cur, father, treehead, edge;
+
+	public static void push(int cur, int father, int treehead, int edge) {
+		stack[stackSize][0] = cur;
+		stack[stackSize][1] = father;
+		stack[stackSize][2] = treehead;
+		stack[stackSize][3] = edge;
+		stackSize++;
+	}
+
+	public static void pop() {
+		--stackSize;
+		cur = stack[stackSize][0];
+		father = stack[stackSize][1];
+		treehead = stack[stackSize][2];
+		edge = stack[stackSize][3];
+	}
+
+	// dfs1的迭代版
+	public static void dfs2(int i, int fa, int treeh) {
+		stackSize = 0;
+		push(i, fa, treeh, -1);
+		while (stackSize > 0) {
+			pop();
+			if (edge == -1) {
+				root[cur] = insert(arr[cur], 1, diff, root[father]);
+				dep[cur] = dep[father] + 1;
+				treeHead[cur] = treehead;
+				headSiz[treehead]++;
+				stjump[cur][0] = father;
+				for (int p = 1; p < MAXH; p++) {
+					stjump[cur][p] = stjump[stjump[cur][p - 1]][p - 1];
+				}
+				edge = head[cur];
+			} else {
+				edge = next[edge];
+			}
+			if (edge != 0) {
+				push(cur, father, treehead, edge);
+				if (to[edge] != father) {
+					push(to[edge], cur, treehead, -1);
+				}
+			}
+		}
+	}
+
+	public static int lca(int a, int b) {
+		if (dep[a] < dep[b]) {
+			int tmp = a;
+			a = b;
+			b = tmp;
+		}
+		for (int p = MAXH - 1; p >= 0; p--) {
+			if (dep[stjump[a][p]] >= dep[b]) {
+				a = stjump[a][p];
+			}
+		}
+		if (a == b) {
+			return a;
+		}
+		for (int p = MAXH - 1; p >= 0; p--) {
+			if (stjump[a][p] != stjump[b][p]) {
+				a = stjump[a][p];
+				b = stjump[b][p];
+			}
+		}
+		return stjump[a][0];
+	}
+
+	public static int queryKth(int x, int y, int k) {
+		int xylca = lca(x, y);
+		int lcafa = stjump[xylca][0];
+		int i = query(k, 1, diff, root[x], root[y], root[xylca], root[lcafa]);
+		return sorted[i];
+	}
+
+	public static void connect(int x, int y) {
+		addEdge(x, y);
+		addEdge(y, x);
+		int fx = treeHead[x];
+		int fy = treeHead[y];
+		if (headSiz[fx] >= headSiz[fy]) {
+			dfs2(y, x, fx); // 调用dfs1的迭代版
+		} else {
+			dfs2(x, y, fy); // 调用dfs1的迭代版
+		}
+	}
+
+	public static void prepare() {
+		for (int i = 1; i <= n; i++) {
+			sorted[i] = arr[i];
+		}
+		Arrays.sort(sorted, 1, n + 1);
+		diff = 1;
+		for (int i = 2; i <= n; i++) {
+			if (sorted[diff] != sorted[i]) {
+				sorted[++diff] = sorted[i];
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			arr[i] = kth(arr[i]);
+		}
+		for (int i = 1; i <= n; i++) {
+			if (treeHead[i] == 0) {
+				dfs2(i, 0, i); // 调用dfs1的迭代版
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		FastReader in = new FastReader();
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		testcase = in.nextInt();
+		n = in.nextInt();
+		m = in.nextInt();
+		t = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			arr[i] = in.nextInt();
+		}
+		for (int i = 1, u, v; i <= m; i++) {
+			u = in.nextInt();
+			v = in.nextInt();
+			addEdge(u, v);
+			addEdge(v, u);
+		}
+		prepare();
+		char op;
+		int x, y, k, lastAns = 0;
+		for (int i = 1; i <= t; i++) {
+			op = in.nextChar();
+			x = in.nextInt() ^ lastAns;
+			y = in.nextInt() ^ lastAns;
+			if (op == 'Q') {
+				k = in.nextInt() ^ lastAns;
+				lastAns = queryKth(x, y, k);
+				out.println(lastAns);
+			} else {
+				connect(x, y);
+			}
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private final InputStream in;
+		private final byte[] buffer;
+		private int ptr, len;
+
+		public FastReader() {
+			in = System.in;
+			buffer = new byte[BUFFER_SIZE];
+			ptr = len = 0;
+		}
+
+		private boolean hasNextByte() throws IOException {
+			if (ptr < len)
+				return true;
+			ptr = 0;
+			len = in.read(buffer);
+			return len > 0;
+		}
+
+		private byte readByte() throws IOException {
+			if (!hasNextByte())
+				return -1;
+			return buffer[ptr++];
+		}
+
+		public char nextChar() throws IOException {
+			byte c;
+			do {
+				c = readByte();
+				if (c == -1)
+					return 0;
+			} while (c <= ' ');
+			char ans = 0;
+			while (c > ' ') {
+				ans = (char) c;
+				c = readByte();
+			}
+			return ans;
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+			byte b = readByte();
+			while (isWhitespace(b))
+				b = readByte();
+			boolean minus = false;
+			if (b == '-') {
+				minus = true;
+				b = readByte();
+			}
+			while (!isWhitespace(b) && b != -1) {
+				num = num * 10 + (b - '0');
+				b = readByte();
+			}
+			return minus ? -num : num;
+		}
+
+		public double nextDouble() throws IOException {
+			double num = 0, div = 1;
+			byte b = readByte();
+			while (isWhitespace(b))
+				b = readByte();
+			boolean minus = false;
+			if (b == '-') {
+				minus = true;
+				b = readByte();
+			}
+			while (!isWhitespace(b) && b != '.' && b != -1) {
+				num = num * 10 + (b - '0');
+				b = readByte();
+			}
+			if (b == '.') {
+				b = readByte();
+				while (!isWhitespace(b) && b != -1) {
+					num += (b - '0') / (div *= 10);
+					b = readByte();
+				}
+			}
+			return minus ? -num : num;
+		}
+
+		private boolean isWhitespace(byte b) {
+			return b == ' ' || b == '\n' || b == '\r' || b == '\t';
+		}
+	}
+
+}
---
+++ src/class163/Code07_Forest2.java
@@ -0,0 +1,199 @@
+package class163;
+
+// 森林，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P3302
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//const int MAXN = 80001;
+//const int MAXT = MAXN * 110;
+//const int MAXH = 20;
+//int testcase;
+//int n, m, t;
+//
+//int arr[MAXN];
+//int sorted[MAXN];
+//int diff;
+//
+//int head[MAXN];
+//int nxt[MAXN << 1];
+//int to[MAXN << 1];
+//int cntg = 0;
+//
+//int root[MAXN];
+//int ls[MAXT];
+//int rs[MAXT];
+//int siz[MAXT];
+//int cntt = 0;
+//
+//int dep[MAXN];
+//int treeHead[MAXN];
+//int headSiz[MAXN];
+//int stjump[MAXN][MAXH];
+//
+//int kth(int num) {
+//    int left = 1, right = diff, mid;
+//    while (left <= right) {
+//        mid = (left + right) / 2;
+//        if (sorted[mid] == num) {
+//            return mid;
+//        } else if (sorted[mid] < num) {
+//            left = mid + 1;
+//        } else {
+//            right = mid - 1;
+//        }
+//    }
+//    return -1;
+//}
+//
+//void addEdge(int u, int v) {
+//    nxt[++cntg] = head[u];
+//    to[cntg] = v;
+//    head[u] = cntg;
+//}
+//
+//int insert(int jobi, int l, int r, int i) {
+//    int rt = ++cntt;
+//    ls[rt] = ls[i];
+//    rs[rt] = rs[i];
+//    siz[rt] = siz[i] + 1;
+//    if (l < r) {
+//        int mid = (l + r) / 2;
+//        if (jobi <= mid) {
+//            ls[rt] = insert(jobi, l, mid, ls[rt]);
+//        } else {
+//            rs[rt] = insert(jobi, mid + 1, r, rs[rt]);
+//        }
+//    }
+//    return rt;
+//}
+//
+//int query(int jobk, int l, int r, int u, int v, int lca_, int lcafa) {
+//    if (l == r) {
+//        return l;
+//    }
+//    int lsize = siz[ls[u]] + siz[ls[v]] - siz[ls[lca_]] - siz[ls[lcafa]];
+//    int mid = (l + r) / 2;
+//    if (lsize >= jobk) {
+//        return query(jobk, l, mid, ls[u], ls[v], ls[lca_], ls[lcafa]);
+//    } else {
+//        return query(jobk - lsize, mid + 1, r, rs[u], rs[v], rs[lca_], rs[lcafa]);
+//    }
+//}
+//
+//void dfs(int u, int fa, int treeh) {
+//    root[u] = insert(arr[u], 1, diff, root[fa]);
+//    dep[u] = dep[fa] + 1;
+//    treeHead[u] = treeh;
+//    headSiz[treeh]++;
+//    stjump[u][0] = fa;
+//    for (int p = 1; p < MAXH; p++) {
+//        stjump[u][p] = stjump[ stjump[u][p - 1] ][p - 1];
+//    }
+//    for (int e = head[u]; e > 0; e = nxt[e]) {
+//        if (to[e] != fa) {
+//            dfs(to[e], u, treeh);
+//        }
+//    }
+//}
+//
+//int lca(int a, int b) {
+//    if (dep[a] < dep[b]) {
+//        int tmp = a;
+//        a = b;
+//        b = tmp;
+//    }
+//    for (int p = MAXH - 1; p >= 0; p--) {
+//        if (dep[stjump[a][p]] >= dep[b]) {
+//            a = stjump[a][p];
+//        }
+//    }
+//    if (a == b) {
+//        return a;
+//    }
+//    for (int p = MAXH - 1; p >= 0; p--) {
+//        if (stjump[a][p] != stjump[b][p]) {
+//            a = stjump[a][p];
+//            b = stjump[b][p];
+//        }
+//    }
+//    return stjump[a][0];
+//}
+//
+//int queryKth(int x, int y, int k) {
+//    int xylca = lca(x, y);
+//    int lcafa = stjump[xylca][0];
+//    int i = query(k, 1, diff, root[x], root[y], root[xylca], root[lcafa]);
+//    return sorted[i];
+//}
+//
+//void connect(int x, int y) {
+//    addEdge(x, y);
+//    addEdge(y, x);
+//    int fx = treeHead[x];
+//    int fy = treeHead[y];
+//    if (headSiz[fx] >= headSiz[fy]) {
+//        dfs(y, x, fx);
+//    } else {
+//        dfs(x, y, fy);
+//    }
+//}
+//
+//void prepare() {
+//    for (int i = 1; i <= n; i++) {
+//        sorted[i] = arr[i];
+//    }
+//    sort(sorted + 1, sorted + n + 1);
+//    diff = 1;
+//    for (int i = 2; i <= n; i++) {
+//        if (sorted[diff] != sorted[i]) {
+//            sorted[++diff] = sorted[i];
+//        }
+//    }
+//    for (int i = 1; i <= n; i++) {
+//        arr[i] = kth(arr[i]);
+//    }
+//    for (int i = 1; i <= n; i++) {
+//        if (treeHead[i] == 0) {
+//            dfs(i, 0, i);
+//        }
+//    }
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> testcase >> n >> m >> t;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i];
+//    }
+//    for (int i = 1; i <= m; i++) {
+//        int u, v;
+//        cin >> u >> v;
+//        addEdge(u, v);
+//        addEdge(v, u);
+//    }
+//    prepare();
+//    char op;
+//    int x, y, k, lastAns = 0;
+//    for (int i = 1; i <= t; i++) {
+//        cin >> op;
+//        cin >> x;
+//        cin >> y;
+//        x ^= lastAns;
+//        y ^= lastAns;
+//        if (op == 'Q') {
+//            cin >> k;
+//            k ^= lastAns;
+//            lastAns = queryKth(x, y, k);
+//            cout << lastAns << "\n";
+//        } else {
+//            connect(x, y);
+//        }
+//    }
+//    return 0;
+//}
