d5d27fa4c7515930b5b22997fe1ea80dcb10e316
algorithmzuo <algorithmzuo@163.com>
2025-03-27 20:37:49 +0800

    modify code

--- src/class164/Code01_KruskalRebuild1.java
+++ src/class164/Code01_KruskalRebuild1.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
 
-public class Code01_KruskalRebuildTree1 {
+public class Code01_KruskalRebuild1 {
 
 	public static int MAXK = 200001;
 	public static int MAXM = 300001;
