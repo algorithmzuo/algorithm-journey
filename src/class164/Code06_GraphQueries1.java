a5130113f1be6fb223de40662675468c64c7162b
algorithmzuo <algorithmzuo@163.com>
2025-04-01 15:05:33 +0800

    modify code

--- src/class164/Code06_GraphQueries1.java
+++ src/class164/Code06_GraphQueries1.java
@@ -25,11 +25,11 @@ public class Code06_GraphQueries1 {
 	public static int MAXH = 20;
 	public static int n, m, q;
 
-	// 所有节点的值，需要记录，线段树也要使用
+	// 节点值的数组，需要记录，线段树也要使用
 	public static int[] node = new int[MAXN];
-	// 所有边，根据删除操作，设置每条边的权值
+	// 所有边的数组，逆序处理删除操作，设置每条边的权值
 	public static int[][] edge = new int[MAXM][3];
-	// 所有操作
+	// 记录所有操作
 	public static int[][] ques = new int[MAXQ][2];
 
 	// 并查集
