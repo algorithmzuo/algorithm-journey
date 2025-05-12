e5b1a898621904f0060f231fa06911c1b1cd81a7
algorithmzuo <algorithmzuo@163.com>
2025-05-12 18:59:10 +0800

    modify code

---
+++ src/class168/Code05_PastoralOddities1.java
@@ -0,0 +1,9 @@
+package class168;
+
+// 度为奇数的最大边权，java版
+// 测试链接 : https://www.luogu.com.cn/problem/CF603E
+// 测试链接 : https://codeforces.com/problemset/problem/603/E
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+public class Code05_PastoralOddities1 {
+
+}
