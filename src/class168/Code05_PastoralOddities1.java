10dcbbf9fb58448e256ebd53e50c4debf913ac1f
algorithmzuo <algorithmzuo@163.com>
2025-05-12 22:34:37 +0800

    modify code

--- src/class168/Code05_PastoralOddities1.java
+++ src/class168/Code05_PastoralOddities1.java
@@ -4,6 +4,197 @@ package class168;
 // 测试链接 : https://www.luogu.com.cn/problem/CF603E
 // 测试链接 : https://codeforces.com/problemset/problem/603/E
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 public class Code05_PastoralOddities1 {
+
+	public static int MAXN = 100001;
+	public static int MAXM = 300002;
+	public static int n, m;
+
+	// 边的编号i、端点x、端点y、权值w
+	public static int[][] edge = new int[MAXM][4];
+	public static int[][] sorted = new int[MAXM][4];
+
+	// 节点数为奇数的联通区数量
+	public static int oddnum;
+	// 可撤销并查集
+	public static int[] father = new int[MAXN];
+	public static int[] siz = new int[MAXN];
+	public static int[][] rollback = new int[MAXN][2];
+	public static int opsize = 0;
+
+	public static int[] ans = new int[MAXM];
+
+	public static int find(int i) {
+		while (i != father[i]) {
+			i = father[i];
+		}
+		return i;
+	}
+
+	public static boolean union(int x, int y) {
+		int fx = find(x);
+		int fy = find(y);
+		if (fx == fy) {
+			return false;
+		}
+		if ((siz[fx] & 1) == 1 && (siz[fy] & 1) == 1) {
+			oddnum -= 2;
+		}
+		if (siz[fx] < siz[fy]) {
+			int tmp = fx;
+			fx = fy;
+			fy = tmp;
+		}
+		father[fy] = fx;
+		siz[fx] += siz[fy];
+		rollback[++opsize][0] = fx;
+		rollback[opsize][1] = fy;
+		return true;
+	}
+
+	public static void undo() {
+		int fx = rollback[opsize][0];
+		int fy = rollback[opsize--][1];
+		father[fy] = fy;
+		siz[fx] -= siz[fy];
+		if ((siz[fx] & 1) == 1 && (siz[fy] & 1) == 1) {
+			oddnum += 2;
+		}
+	}
+
+	public static void compute(int ql, int qr, int vl, int vr) {
+		if (ql > qr) {
+			return;
+		}
+		if (vl == vr) {
+			for (int i = ql; i <= qr; i++) {
+				ans[i] = sorted[vl][3];
+			}
+		} else {
+			int mid = (vl + vr) >> 1;
+			int unionCnt1 = 0;
+			for (int i = vl; i <= mid; i++) {
+				if (sorted[i][0] < ql) {
+					if (union(sorted[i][1], sorted[i][2])) {
+						unionCnt1++;
+					}
+				}
+			}
+			int unionCnt2 = 0;
+			int split = qr + 1;
+			for (int i = ql; i <= qr; i++) {
+				if (edge[i][3] <= sorted[mid][3]) {
+					if (union(edge[i][1], edge[i][2])) {
+						unionCnt2++;
+					}
+				}
+				if (oddnum == 0) {
+					split = i;
+					break;
+				}
+			}
+			for (int i = 1; i <= unionCnt2; i++) {
+				undo();
+			}
+			compute(ql, split - 1, mid + 1, vr);
+			for (int i = 1; i <= unionCnt1; i++) {
+				undo();
+			}
+			int unionCnt3 = 0;
+			for (int i = ql; i <= split - 1; i++) {
+				if (edge[i][3] <= sorted[vl][3]) {
+					if (union(edge[i][1], edge[i][2])) {
+						unionCnt3++;
+					}
+				}
+			}
+			compute(split, qr, vl, mid);
+			for (int i = 1; i <= unionCnt3; i++) {
+				undo();
+			}
+		}
+	}
+
+	public static void prepare() {
+		oddnum = n;
+		for (int i = 1; i <= n; i++) {
+			father[i] = i;
+			siz[i] = 1;
+		}
+		for (int i = 1; i <= m; i++) {
+			sorted[i][0] = edge[i][0];
+			sorted[i][1] = edge[i][1];
+			sorted[i][2] = edge[i][2];
+			sorted[i][3] = edge[i][3];
+		}
+		Arrays.sort(sorted, 1, m + 1, (a, b) -> a[3] - b[3]);
+		sorted[m + 1][3] = -1;
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= m; i++) {
+			edge[i][0] = i;
+			edge[i][1] = in.nextInt();
+			edge[i][2] = in.nextInt();
+			edge[i][3] = in.nextInt();
+		}
+		prepare();
+		compute(1, m, 1, m + 1);
+		for (int i = 1; i <= m; i++) {
+			out.println(ans[i]);
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
 
 }
---
+++ src/class168/Code05_PastoralOddities2.java
@@ -0,0 +1,154 @@
+package class168;
+
+// 度为奇数的最大边权，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/CF603E
+// 测试链接 : https://codeforces.com/problemset/problem/603/E
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct Edge {
+//    int i, x, y, w;
+//};
+//
+//bool EdgeCmp(Edge a, Edge b) {
+//    return a.w < b.w;
+//}
+//
+//const int MAXN = 100001;
+//const int MAXM = 300002;
+//int n, m;
+//
+//Edge edge[MAXM];
+//Edge sorted[MAXM];
+//
+//int oddnum;
+//int father[MAXN];
+//int siz[MAXN];
+//int rollback[MAXN][2];
+//int opsize = 0;
+//
+//int ans[MAXM];
+//
+//int find(int i) {
+//    while (i != father[i]) {
+//        i = father[i];
+//    }
+//    return i;
+//}
+//
+//bool Union(int x, int y) {
+//    int fx = find(x);
+//    int fy = find(y);
+//    if (fx == fy) {
+//        return false;
+//    }
+//    if ((siz[fx] & 1) == 1 && (siz[fy] & 1) == 1) {
+//        oddnum -= 2;
+//    }
+//    if (siz[fx] < siz[fy]) {
+//        int tmp = fx;
+//        fx = fy;
+//        fy = tmp;
+//    }
+//    father[fy] = fx;
+//    siz[fx] += siz[fy];
+//    rollback[++opsize][0] = fx;
+//    rollback[opsize][1] = fy;
+//    return true;
+//}
+//
+//void undo() {
+//    int fx = rollback[opsize][0];
+//    int fy = rollback[opsize--][1];
+//    father[fy] = fy;
+//    siz[fx] -= siz[fy];
+//    if ((siz[fx] & 1) == 1 && (siz[fy] & 1) == 1) {
+//        oddnum += 2;
+//    }
+//}
+//
+//void compute(int ql, int qr, int vl, int vr) {
+//    if (ql > qr) {
+//        return;
+//    }
+//    if (vl == vr) {
+//        for (int i = ql; i <= qr; i++) {
+//            ans[i] = sorted[vl].w;
+//        }
+//    } else {
+//        int mid = (vl + vr) >> 1;
+//        int unionCnt1 = 0;
+//        for (int i = vl; i <= mid; i++) {
+//            if (sorted[i].i < ql) {
+//                if (Union(sorted[i].x, sorted[i].y)) {
+//                    unionCnt1++;
+//                }
+//            }
+//        }
+//        int unionCnt2 = 0;
+//        int split = qr + 1;
+//        for (int i = ql; i <= qr; i++) {
+//            if (edge[i].w <= sorted[mid].w) {
+//                if (Union(edge[i].x, edge[i].y)) {
+//                    unionCnt2++;
+//                }
+//            }
+//            if (oddnum == 0) {
+//                split = i;
+//                break;
+//            }
+//        }
+//        for (int i = 1; i <= unionCnt2; i++) {
+//            undo();
+//        }
+//        compute(ql, split - 1, mid + 1, vr);
+//        for (int i = 1; i <= unionCnt1; i++) {
+//            undo();
+//        }
+//        int unionCnt3 = 0;
+//        for (int i = ql; i <= split - 1; i++) {
+//            if (edge[i].w <= sorted[vl].w) {
+//                if (Union(edge[i].x, edge[i].y)) {
+//                    unionCnt3++;
+//                }
+//            }
+//        }
+//        compute(split, qr, vl, mid);
+//        for (int i = 1; i <= unionCnt3; i++) {
+//            undo();
+//        }
+//    }
+//}
+//
+//void prepare() {
+//    oddnum = n;
+//    for (int i = 1; i <= n; i++) {
+//        father[i] = i;
+//        siz[i] = 1;
+//    }
+//    for (int i = 1; i <= m; i++) {
+//        sorted[i] = edge[i];
+//    }
+//    sort(sorted + 1, sorted + m + 1, EdgeCmp);
+//    sorted[m + 1].w = -1;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1; i <= m; i++) {
+//        edge[i].i = i;
+//        cin >> edge[i].x >> edge[i].y >> edge[i].w;
+//    }
+//    prepare();
+//    compute(1, m, 1, m + 1);
+//    for (int i = 1; i <= m; i++) {
+//        cout << ans[i] << '\n';
+//    }
+//    return 0;
+//}
