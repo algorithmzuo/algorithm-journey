2250e4e49b35f0e6c6726f2c27d4e129f019710f
algorithmzuo <algorithmzuo@163.com>
2025-05-16 16:47:47 +0800

    modify code

--- src/class169/Code01_Juice1.java
+++ src/class169/Code01_Juice1.java
@@ -113,8 +113,9 @@ public class Code01_Juice1 {
 			for (int i = 1; i <= rsiz; i++) {
 				qid[ql + lsiz + i - 1] = rset[i];
 			}
-			compute(ql, ql + lsiz - 1, vl, mid);
+			// 先右后左
 			compute(ql + lsiz, qr, mid + 1, vr);
+			compute(ql, ql + lsiz - 1, vl, mid);
 		}
 	}
 
