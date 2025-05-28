1b0555c6fac37992aab70f48efe6cdb7ea214c3e
algorithmzuo <algorithmzuo@163.com>
2025-05-28 14:20:08 +0800

    modify code

--- src/class170/Code02_DynamicInversion1.java
+++ src/class170/Code02_DynamicInversion1.java
@@ -4,6 +4,161 @@ package class170;
 // 测试链接 : https://www.luogu.com.cn/problem/P3157
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 public class Code02_DynamicInversion1 {
+
+	public static int MAXN = 100001;
+	public static int MAXM = 50001;
+	public static int n, m;
+
+	public static int[] num = new int[MAXN];
+	public static int[] pos = new int[MAXN];
+	public static int[] del = new int[MAXM];
+
+	// 位置i、数值v、效果d、问题q
+	public static int[][] arr = new int[MAXN + MAXM][4];
+	public static int cnt = 0;
+
+	public static int[] tree = new int[MAXN];
+
+	public static long[] ans = new long[MAXM];
+
+	public static int lowbit(int i) {
+		return i & -i;
+	}
+
+	public static void add(int i, int v) {
+		while (i <= n) {
+			tree[i] += v;
+			i += lowbit(i);
+		}
+	}
+
+	public static int query(int i) {
+		int ret = 0;
+		while (i > 0) {
+			ret += tree[i];
+			i -= lowbit(i);
+		}
+		return ret;
+	}
+
+	public static void merge(int l, int m, int r) {
+		int p1, p2;
+		for (p1 = l - 1, p2 = m + 1; p2 <= r; p2++) {
+			while (p1 + 1 <= m && arr[p1 + 1][0] <= arr[p2][0]) {
+				p1++;
+				add(arr[p1][1], arr[p1][2]);
+			}
+			ans[arr[p2][3]] += arr[p2][2] * (query(n) - query(arr[p2][1]));
+		}
+		for (int i = l; i <= p1; i++) {
+			add(arr[i][1], -arr[i][2]);
+		}
+		for (p1 = m + 1, p2 = r; p2 > m; p2--) {
+			while (p1 - 1 >= l && arr[p1 - 1][0] >= arr[p2][0]) {
+				p1--;
+				add(arr[p1][1], arr[p1][2]);
+			}
+			ans[arr[p2][3]] += arr[p2][2] * query(arr[p2][1] - 1);
+		}
+		for (int i = m; i >= p1; i--) {
+			add(arr[i][1], -arr[i][2]);
+		}
+		Arrays.sort(arr, l, r + 1, (a, b) -> a[0] - b[0]);
+	}
+
+	public static void cdq(int l, int r) {
+		if (l == r) {
+			return;
+		}
+		int mid = (l + r) / 2;
+		cdq(l, mid);
+		cdq(mid + 1, r);
+		merge(l, mid, r);
+	}
+
+	public static void prepare() {
+		for (int i = 1; i <= n; i++) {
+			arr[++cnt][0] = i;
+			arr[cnt][1] = num[i];
+			arr[cnt][2] = 1;
+			arr[cnt][3] = 0;
+		}
+		for (int i = 1; i <= m; i++) {
+			arr[++cnt][0] = pos[del[i]];
+			arr[cnt][1] = del[i];
+			arr[cnt][2] = -1;
+			arr[cnt][3] = i;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			num[i] = in.nextInt();
+			pos[num[i]] = i;
+		}
+		for (int i = 1; i <= m; i++) {
+			del[i] = in.nextInt();
+		}
+		prepare();
+		cdq(1, cnt);
+		for (int i = 1; i < m; i++) {
+			ans[i] += ans[i - 1];
+		}
+		for (int i = 0; i < m; i++) {
+			out.println(ans[i]);
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 20];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
 
 }
---
+++ src/class170/Code02_DynamicInversion2.java
@@ -0,0 +1,125 @@
+package class170;
+
+// 动态逆序对，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P3157
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct Number {
+//    int i, v, d, q;
+//};
+//
+//bool CmpIndex(Number x, Number y) {
+//    return x.i < y.i;
+//}
+//
+//const int MAXN = 100001;
+//const int MAXM = 50001;
+//int n, m;
+//
+//int num[MAXN];
+//int pos[MAXN];
+//int del[MAXM];
+//
+//Number arr[MAXN + MAXM];
+//int cnt = 0;
+//
+//int tree[MAXN];
+//
+//long long ans[MAXM];
+//
+//int lowbit(int i) {
+//    return i & -i;
+//}
+//
+//void add(int i, int v) {
+//    while (i <= n) {
+//        tree[i] += v;
+//        i += lowbit(i);
+//    }
+//}
+//
+//int query(int i) {
+//    int ret = 0;
+//    while (i > 0) {
+//        ret += tree[i];
+//        i -= lowbit(i);
+//    }
+//    return ret;
+//}
+//
+//void merge(int l, int m, int r) {
+//    int p1, p2;
+//    for (p1 = l - 1, p2 = m + 1; p2 <= r; p2++) {
+//        while (p1 + 1 <= m && arr[p1 + 1].i <= arr[p2].i) {
+//            p1++;
+//            add(arr[p1].v, arr[p1].d);
+//        }
+//        ans[arr[p2].q] += arr[p2].d * (query(n) - query(arr[p2].v));
+//    }
+//    for (int i = l; i <= p1; i++) {
+//        add(arr[i].v, -arr[i].d);
+//    }
+//    for (p1 = m + 1, p2 = r; p2 > m; p2--) {
+//        while (p1 - 1 >= l && arr[p1 - 1].i >= arr[p2].i) {
+//            p1--;
+//            add(arr[p1].v, arr[p1].d);
+//        }
+//        ans[arr[p2].q] += arr[p2].d * query(arr[p2].v - 1);
+//    }
+//    for (int i = m; i >= p1; i--) {
+//        add(arr[i].v, -arr[i].d);
+//    }
+//    sort(arr + l, arr + r + 1, CmpIndex);
+//}
+//
+//void cdq(int l, int r) {
+//    if (l == r) {
+//        return;
+//    }
+//    int mid = (l + r) / 2;
+//    cdq(l, mid);
+//    cdq(mid + 1, r);
+//    merge(l, mid, r);
+//}
+//
+//void prepare() {
+//    for (int i = 1; i <= n; i++) {
+//        arr[++cnt].i = i;
+//        arr[cnt].v = num[i];
+//        arr[cnt].d = 1;
+//        arr[cnt].q = 0;
+//    }
+//    for (int i = 1; i <= m; i++) {
+//        arr[++cnt].i = pos[del[i]];
+//        arr[cnt].v = del[i];
+//        arr[cnt].d = -1;
+//        arr[cnt].q = i;
+//    }
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> num[i];
+//        pos[num[i]] = i;
+//    }
+//    for (int i = 1; i <= m; i++) {
+//        cin >> del[i];
+//    }
+//    prepare();
+//    cdq(1, cnt);
+//    for (int i = 1; i < m; i++) {
+//        ans[i] += ans[i - 1];
+//    }
+//    for (int i = 0; i < m; i++) {
+//        cout << ans[i] << '\n';
+//    }
+//    return 0;
+//}
