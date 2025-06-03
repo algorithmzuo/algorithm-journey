44e981f9c7da2252f73510d4d1eaac7c89ed04cb
algorithmzuo <algorithmzuo@163.com>
2025-06-03 14:39:35 +0800

    modify code

---
+++ src/class171/Code03_Sequence1.java
@@ -0,0 +1,146 @@
+package class171;
+
+// 序列，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P4093
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class Code03_Sequence1 {
+
+	public static int MAXN = 100001;
+	public static int n, m;
+	// 位置i、数值v、最小值minv、最大值maxv
+	public static int[][] arr = new int[MAXN][4];
+	public static int[] tree = new int[MAXN];
+	public static int[] dp = new int[MAXN];
+
+	public static int lowbit(int i) {
+		return i & -i;
+	}
+
+	public static void more(int i, int num) {
+		while (i <= n) {
+			tree[i] = Math.max(tree[i], num);
+			i += lowbit(i);
+		}
+	}
+
+	public static int query(int i) {
+		int ret = 0;
+		while (i > 0) {
+			ret = Math.max(ret, tree[i]);
+			i -= lowbit(i);
+		}
+		return ret;
+	}
+
+	public static void clear(int i) {
+		while (i <= n) {
+			tree[i] = 0;
+			i += lowbit(i);
+		}
+	}
+
+	public static void merge(int l, int m, int r) {
+		Arrays.sort(arr, l, m + 1, (a, b) -> a[1] - b[1]);
+		Arrays.sort(arr, m + 1, r + 1, (a, b) -> a[2] - b[2]);
+		int p1, p2;
+		for (p1 = l - 1, p2 = m + 1; p2 <= r; p2++) {
+			while (p1 + 1 <= m && arr[p1 + 1][1] <= arr[p2][2]) {
+				p1++;
+				more(arr[p1][3], dp[arr[p1][0]]);
+			}
+			dp[arr[p2][0]] = Math.max(dp[arr[p2][0]], query(arr[p2][1]) + 1);
+		}
+		for (int i = l; i <= p1; i++) {
+			clear(arr[i][3]);
+		}
+		Arrays.sort(arr, l, r + 1, (a, b) -> a[0] - b[0]);
+	}
+
+	public static void cdq(int l, int r) {
+		if (l == r) {
+			return;
+		}
+		int mid = (l + r) / 2;
+		// 为什么不是经典顺序，左、右、整合左右
+		// 因为右侧dp需要先获得左侧dp的值，获得部分的可能性
+		// 然后才能在右侧范围上把剩下的可能性算完
+		cdq(l, mid);
+		merge(l, mid, r);
+		cdq(mid + 1, r);
+	}
+
+	public static void main(String[] args) throws IOException {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			arr[i][0] = i;
+			arr[i][1] = in.nextInt();
+			arr[i][2] = arr[i][1];
+			arr[i][3] = arr[i][1];
+			dp[i] = 1;
+		}
+		for (int i = 1, idx, num; i <= m; i++) {
+			idx = in.nextInt();
+			num = in.nextInt();
+			arr[idx][2] = Math.min(arr[idx][2], num);
+			arr[idx][3] = Math.max(arr[idx][3], num);
+		}
+		cdq(1, n);
+		int ans = 0;
+		for (int i = 1; i <= n; i++) {
+			ans = Math.max(ans, dp[i]);
+		}
+		out.println(ans);
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 20];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
+
+}
---
+++ src/class171/Code03_Sequence2.java
@@ -0,0 +1,111 @@
+package class171;
+
+// 序列，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P4093
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct Node {
+//    int i, v, minv, maxv;
+//};
+//
+//bool CmpI(Node a, Node b) {
+//    return a.i < b.i;
+//}
+//
+//bool CmpV(Node a, Node b) {
+//    return a.v < b.v;
+//}
+//
+//bool CmpMinv(Node a, Node b) {
+//    return a.minv < b.minv;
+//}
+//
+//const int MAXN = 100001;
+//int n, m;
+//Node arr[MAXN];
+//int tree[MAXN];
+//int dp[MAXN];
+//
+//int lowbit(int i) {
+//    return i & -i;
+//}
+//
+//void more(int i, int num) {
+//    while (i <= n) {
+//        tree[i] = max(tree[i], num);
+//        i += lowbit(i);
+//    }
+//}
+//
+//int query(int i) {
+//    int ret = 0;
+//    while (i > 0) {
+//        ret = max(ret, tree[i]);
+//        i -= lowbit(i);
+//    }
+//    return ret;
+//}
+//
+//void clear(int i) {
+//    while (i <= n) {
+//        tree[i] = 0;
+//        i += lowbit(i);
+//    }
+//}
+//
+//void merge(int l, int m, int r) {
+//    sort(arr + l, arr + m + 1, CmpV);
+//    sort(arr + m + 1, arr + r + 1, CmpMinv);
+//    int p1 = l - 1, p2 = m + 1;
+//    for (; p2 <= r; p2++) {
+//        while (p1 + 1 <= m && arr[p1 + 1].v <= arr[p2].minv) {
+//            p1++;
+//            more(arr[p1].maxv, dp[arr[p1].i]);
+//        }
+//        dp[arr[p2].i] = max(dp[arr[p2].i], query(arr[p2].v) + 1);
+//    }
+//    for (int i = l; i <= p1; i++) {
+//        clear(arr[i].maxv);
+//    }
+//    sort(arr + l, arr + r + 1, CmpI);
+//}
+//
+//void cdq(int l, int r) {
+//    if (l == r) {
+//        return;
+//    }
+//    int mid = (l + r) / 2;
+//    cdq(l, mid);
+//    merge(l, mid, r);
+//    cdq(mid + 1, r);
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1; i <= n; i++) {
+//        arr[i].i = i;
+//        cin >> arr[i].v;
+//        arr[i].minv = arr[i].v;
+//        arr[i].maxv = arr[i].v;
+//        dp[i] = 1;
+//    }
+//    for (int i = 1, idx, num; i <= m; i++) {
+//        cin >> idx >> num;
+//        arr[idx].minv = min(arr[idx].minv, num);
+//        arr[idx].maxv = max(arr[idx].maxv, num);
+//    }
+//    cdq(1, n);
+//    int ans = 0;
+//    for (int i = 1; i <= n; i++) {
+//        ans = max(ans, dp[i]);
+//    }
+//    cout << ans << '\n';
+//    return 0;
+//}
