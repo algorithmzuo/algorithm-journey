0946188fcc9cd28d06222efcac2ec7aa9ada372b
algorithmzuo <algorithmzuo@163.com>
2025-06-05 17:58:45 +0800

    modify code

---
+++ src/class171/Code05_Theresa1.java
@@ -0,0 +1,284 @@
+package class171;
+
+// 德丽莎世界第一可爱，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P5621
+// 提交以下的code，提交时请把类名改成"Main"
+// java实现的逻辑一定是正确的，但是有两个测试用例超时
+// 因为这道题只考虑C++能通过的时间标准，根本没考虑java的用户
+// 想通过用C++实现，本节课Code05_Theresa2文件就是C++的实现
+// 两个版本的逻辑完全一样，C++版本可以通过所有测试
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Code05_Theresa1 {
+
+	public static class Node {
+		int a, b, c, d;
+		int i;
+		long v;
+		boolean left;
+
+		public Node(int a_, int b_, int c_, int d_, long v_) {
+			a = a_;
+			b = b_;
+			c = c_;
+			d = d_;
+			v = v_;
+		}
+	}
+
+	public static class CmpAbcdv implements Comparator<Node> {
+		@Override
+		public int compare(Node x, Node y) {
+			if (x.a != y.a) {
+				return x.a - y.a;
+			}
+			if (x.b != y.b) {
+				return x.b - y.b;
+			}
+			if (x.c != y.c) {
+				return x.c - y.c;
+			}
+			if (x.d != y.d) {
+				return x.d - y.d;
+			}
+			return Long.compare(y.v, x.v);
+		}
+	}
+
+	public static class CmpB implements Comparator<Node> {
+		@Override
+		public int compare(Node x, Node y) {
+			return x.b - y.b;
+		}
+	}
+
+	public static class CmpC implements Comparator<Node> {
+		@Override
+		public int compare(Node x, Node y) {
+			return x.c - y.c;
+		}
+	}
+
+	public static CmpAbcdv cmpAbcdv = new CmpAbcdv();
+	public static CmpB cmpB = new CmpB();
+	public static CmpC cmpC = new CmpC();
+
+	public static int MAXN = 100001;
+	public static int n, s;
+	public static long INF = 1000000000000000001L;
+
+	public static Node[] arr = new Node[MAXN];
+	public static int[] abcd = new int[MAXN << 2];
+
+	public static Node[] tmp1 = new Node[MAXN];
+	public static Node[] tmp2 = new Node[MAXN];
+
+	public static long[] tree = new long[MAXN << 2];
+	public static long[] dp = new long[MAXN];
+
+	public static int lowbit(int i) {
+		return i & -i;
+	}
+
+	public static void more(int i, long num) {
+		while (i <= s) {
+			tree[i] = Math.max(tree[i], num);
+			i += lowbit(i);
+		}
+	}
+
+	public static long query(int i) {
+		long ret = -INF;
+		while (i > 0) {
+			ret = Math.max(ret, tree[i]);
+			i -= lowbit(i);
+		}
+		return ret;
+	}
+
+	public static void clear(int i) {
+		while (i <= s) {
+			tree[i] = -INF;
+			i += lowbit(i);
+		}
+	}
+
+	public static void merge(int l, int m, int r) {
+		for (int i = l; i <= r; i++) {
+			tmp2[i] = tmp1[i];
+		}
+		Arrays.sort(tmp2, l, m + 1, cmpC);
+		Arrays.sort(tmp2, m + 1, r + 1, cmpC);
+		int p1, p2;
+		for (p1 = l - 1, p2 = m + 1; p2 <= r; p2++) {
+			while (p1 + 1 <= m && tmp2[p1 + 1].c <= tmp2[p2].c) {
+				p1++;
+				if (tmp2[p1].left) {
+					more(tmp2[p1].d, dp[tmp2[p1].i]);
+				}
+			}
+			if (!tmp2[p2].left) {
+				dp[tmp2[p2].i] = Math.max(dp[tmp2[p2].i], query(tmp2[p2].d) + tmp2[p2].v);
+			}
+		}
+		for (int i = l; i <= p1; i++) {
+			if (tmp2[i].left) {
+				clear(tmp2[i].d);
+			}
+		}
+	}
+
+	public static void cdq2(int l, int r) {
+		if (l == r) {
+			return;
+		}
+		int mid = (l + r) / 2;
+		cdq2(l, mid);
+		merge(l, mid, r);
+		cdq2(mid + 1, r);
+	}
+
+	public static void cdq1(int l, int r) {
+		if (l == r) {
+			return;
+		}
+		int mid = (l + r) / 2;
+		cdq1(l, mid);
+		for (int i = l; i <= r; i++) {
+			tmp1[i] = arr[i];
+			tmp1[i].left = i <= mid;
+		}
+		Arrays.sort(tmp1, l, r + 1, cmpB);
+		cdq2(l, r);
+		cdq1(mid + 1, r);
+	}
+
+	public static int lower(long num) {
+		int l = 1, r = s, m, ans = 1;
+		while (l <= r) {
+			m = (l + r) / 2;
+			if (abcd[m] >= num) {
+				ans = m;
+				r = m - 1;
+			} else {
+				l = m + 1;
+			}
+		}
+		return ans;
+	}
+
+	public static void prepare() {
+		// a、b、c、d离散化
+		for (int i = 1; i <= n; i++) {
+			abcd[++s] = arr[i].a;
+			abcd[++s] = arr[i].b;
+			abcd[++s] = arr[i].c;
+			abcd[++s] = arr[i].d;
+		}
+		Arrays.sort(abcd, 1, s + 1);
+		int m = 1;
+		for (int i = 2; i <= s; i++) {
+			if (abcd[m] != abcd[i]) {
+				abcd[++m] = abcd[i];
+			}
+		}
+		s = m;
+		for (int i = 1; i <= n; i++) {
+			arr[i].a = lower(arr[i].a);
+			arr[i].b = lower(arr[i].b);
+			arr[i].c = lower(arr[i].c);
+			arr[i].d = lower(arr[i].d);
+		}
+		// 合并a、b、c、d都相等的对象成为一组对象
+		Arrays.sort(arr, 1, n + 1, cmpAbcdv);
+		m = 1;
+		for (int i = 2; i <= n; i++) {
+			if (arr[m].a == arr[i].a && arr[m].b == arr[i].b && arr[m].c == arr[i].c && arr[m].d == arr[i].d) {
+				if (arr[i].v > 0) {
+					arr[m].v += arr[i].v;
+				}
+			} else {
+				arr[++m] = arr[i];
+			}
+		}
+		n = m;
+		// 设置组号i、dp初始值
+		for (int i = 1; i <= n; i++) {
+			arr[i].i = i;
+			dp[i] = arr[i].v;
+		}
+		// 初始化树状数组
+		for (int i = 1; i <= s; i++) {
+			tree[i] = -INF;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		for (int i = 1, a, b, c, d, v; i <= n; i++) {
+			a = in.nextInt();
+			b = in.nextInt();
+			c = in.nextInt();
+			d = in.nextInt();
+			v = in.nextInt();
+			arr[i] = new Node(a, b, c, d, v);
+		}
+		prepare();
+		cdq1(1, n);
+		long ans = -INF;
+		for (int i = 1; i <= n; i++) {
+			ans = Math.max(ans, dp[i]);
+		}
+		out.println(ans);
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 20];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
+
+}
---
+++ src/class171/Code05_Theresa2.java
@@ -0,0 +1,192 @@
+package class171;
+
+// 德丽莎世界第一可爱，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P5621
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct Node {
+//    int a, b, c, d;
+//    long long v;
+//    int i;
+//    bool left;
+//};
+//
+//bool CmpAbcdv(Node x, Node y) {
+//    if (x.a != y.a) return x.a < y.a;
+//    if (x.b != y.b) return x.b < y.b;
+//    if (x.c != y.c) return x.c < y.c;
+//    if (x.d != y.d) return x.d < y.d;
+//    return x.v > y.v;
+//}
+//
+//bool CmpB(Node x, Node y) {
+//    return x.b < y.b;
+//}
+//
+//bool CmpC(Node x, Node y) {
+//    return x.c < y.c;
+//}
+//
+//const int MAXN = 100001;
+//const long long INF = 1e18 + 1;
+//int n, s;
+//
+//Node arr[MAXN];
+//int abcd[MAXN << 2];
+//
+//Node tmp1[MAXN];
+//Node tmp2[MAXN];
+//long long tree[MAXN << 2];
+//long long dp[MAXN];
+//
+//int lowbit(int i) {
+//    return i & -i;
+//}
+//
+//void more(int i, long long num) {
+//    while (i <= s) {
+//        tree[i] = max(tree[i], num);
+//        i += lowbit(i);
+//    }
+//}
+//
+//long long query(int i) {
+//    long long ret = -INF;
+//    while (i > 0) {
+//        ret = max(ret, tree[i]);
+//        i -= lowbit(i);
+//    }
+//    return ret;
+//}
+//
+//void clear(int i) {
+//    while (i <= s) {
+//        tree[i] = -INF;
+//        i += lowbit(i);
+//    }
+//}
+//
+//void merge(int l, int m, int r) {
+//    for (int i = l; i <= r; i++) {
+//        tmp2[i] = tmp1[i];
+//    }
+//    sort(tmp2 + l, tmp2 + m + 1, CmpC);
+//    sort(tmp2 + m + 1, tmp2 + r + 1, CmpC);
+//    int p1, p2;
+//    for (p1 = l - 1, p2 = m + 1; p2 <= r; p2++) {
+//        while (p1 + 1 <= m && tmp2[p1 + 1].c <= tmp2[p2].c) {
+//            p1++;
+//            if (tmp2[p1].left) {
+//                more(tmp2[p1].d, dp[tmp2[p1].i]);
+//            }
+//        }
+//        if (!tmp2[p2].left) {
+//            dp[tmp2[p2].i] = max(dp[tmp2[p2].i], query(tmp2[p2].d) + tmp2[p2].v);
+//        }
+//    }
+//    for (int i = l; i <= p1; i++) {
+//        if (tmp2[i].left) {
+//            clear(tmp2[i].d);
+//        }
+//    }
+//}
+//
+//void cdq2(int l, int r) {
+//    if (l == r) return;
+//    int mid = (l + r) / 2;
+//    cdq2(l, mid);
+//    merge(l, mid, r);
+//    cdq2(mid + 1, r);
+//}
+//
+//void cdq1(int l, int r) {
+//    if (l == r) return;
+//    int mid = (l + r) / 2;
+//    cdq1(l, mid);
+//    for (int i = l; i <= r; i++) {
+//        tmp1[i] = arr[i];
+//        tmp1[i].left = i <= mid;
+//    }
+//    sort(tmp1 + l, tmp1 + r + 1, CmpB);
+//    cdq2(l, r);
+//    cdq1(mid + 1, r);
+//}
+//
+//int lower(long long num) {
+//    int l = 1, r = s, ans = 1;
+//    while (l <= r) {
+//        int m = (l + r) / 2;
+//        if (abcd[m] >= num) {
+//            ans = m;
+//            r = m - 1;
+//        } else {
+//            l = m + 1;
+//        }
+//    }
+//    return ans;
+//}
+//
+//void prepare() {
+//    for (int i = 1; i <= n; i++) {
+//        abcd[++s] = arr[i].a;
+//        abcd[++s] = arr[i].b;
+//        abcd[++s] = arr[i].c;
+//        abcd[++s] = arr[i].d;
+//    }
+//    sort(abcd + 1, abcd + s + 1);
+//    int m = 1;
+//    for (int i = 2; i <= s; i++) {
+//        if (abcd[m] != abcd[i]) {
+//            abcd[++m] = abcd[i];
+//        }
+//    }
+//    s = m;
+//    for (int i = 1; i <= n; i++) {
+//        arr[i].a = lower(arr[i].a);
+//        arr[i].b = lower(arr[i].b);
+//        arr[i].c = lower(arr[i].c);
+//        arr[i].d = lower(arr[i].d);
+//    }
+//    sort(arr + 1, arr + n + 1, CmpAbcdv);
+//    m = 1;
+//    for (int i = 2; i <= n; i++) {
+//        if (arr[m].a == arr[i].a && arr[m].b == arr[i].b &&
+//            arr[m].c == arr[i].c && arr[m].d == arr[i].d) {
+//            if (arr[i].v > 0) {
+//                arr[m].v += arr[i].v;
+//            }
+//        } else {
+//            arr[++m] = arr[i];
+//        }
+//    }
+//    n = m;
+//    for (int i = 1; i <= n; i++) {
+//        arr[i].i = i;
+//        dp[i] = arr[i].v;
+//    }
+//    for (int i = 1; i <= s; i++) {
+//        tree[i] = -INF;
+//    }
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i].a >> arr[i].b >> arr[i].c >> arr[i].d >> arr[i].v;
+//    }
+//    prepare();
+//    cdq1(1, n);
+//    long long ans = -INF;
+//    for (int i = 1; i <= n; i++) {
+//        ans = max(ans, dp[i]);
+//    }
+//    cout << ans << '\n';
+//    return 0;
+//}
