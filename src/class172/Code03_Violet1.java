5c0c9a5108fd5d26a868c2d5cffca50031291b7b
algorithmzuo <algorithmzuo@163.com>
2025-06-13 21:11:15 +0800

    modify code

--- src/class172/Code03_Violet1.java
+++ src/class172/Code03_Violet1.java
@@ -4,6 +4,209 @@ package class172;
 // 测试链接 : https://www.luogu.com.cn/problem/P4168
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 public class Code03_Violet1 {
+
+	public static int MAXN = 40001;
+	public static int MAXB = 201;
+	public static int n, m, s;
+	public static int[] arr = new int[MAXN];
+	public static int[] sortv = new int[MAXN];
+
+	public static int blen, bnum;
+	public static int[] bi = new int[MAXN];
+	public static int[] bl = new int[MAXB];
+	public static int[] br = new int[MAXB];
+
+	// freq[i][j]表示前i块中j出现的次数
+	public static int[][] freq = new int[MAXB][MAXN];
+
+	// mode[i][j]表示从i块到j块中的众数
+	public static int[][] mode = new int[MAXB][MAXB];
+
+	// 数字词频统计
+	public static int[] numCnt = new int[MAXN];
+
+	public static int lower(int num) {
+		int l = 1, r = s, m, ans = 0;
+		while (l <= r) {
+			m = (l + r) >> 1;
+			if (sortv[m] >= num) {
+				ans = m;
+				r = m - 1;
+			} else {
+				l = m + 1;
+			}
+		}
+		return ans;
+	}
+
+	public static int getCnt(int l, int r, int v) {
+		return freq[r][v] - freq[l - 1][v];
+	}
+
+	public static void prepare() {
+		// 离散化
+		for (int i = 1; i <= n; i++) {
+			sortv[i] = arr[i];
+		}
+		Arrays.sort(sortv, 1, n + 1);
+		s = 1;
+		for (int i = 2; i <= n; i++) {
+			if (sortv[s] != sortv[i]) {
+				sortv[++s] = sortv[i];
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			arr[i] = lower(arr[i]);
+		}
+		// 建块
+		blen = (int) Math.sqrt(n);
+		bnum = (n + blen - 1) / blen;
+		for (int i = 1; i <= n; i++) {
+			bi[i] = (i - 1) / blen + 1;
+		}
+		for (int i = 1; i <= bnum; i++) {
+			bl[i] = (i - 1) * blen + 1;
+			br[i] = Math.min(i * blen, n);
+		}
+		// 填好freq
+		for (int i = 1; i <= bnum; i++) {
+			for (int j = bl[i]; j <= br[i]; j++) {
+				freq[i][arr[j]]++;
+			}
+			for (int j = 1; j <= s; j++) {
+				freq[i][j] += freq[i - 1][j];
+			}
+		}
+		// 填好mode
+		for (int i = 1; i <= bnum; i++) {
+			for (int j = i; j <= bnum; j++) {
+				int most = mode[i][j - 1];
+				for (int k = bl[j]; k <= br[j]; k++) {
+					int cur = arr[k];
+					int curCnt = getCnt(i, j, cur);
+					int mostCnt = getCnt(i, j, most);
+					if (curCnt > mostCnt || (curCnt == mostCnt && cur < most)) {
+						most = cur;
+					}
+				}
+				mode[i][j] = most;
+			}
+		}
+	}
+
+	public static int query(int l, int r) {
+		int most = 0;
+		if (bi[l] == bi[r]) {
+			for (int i = l; i <= r; i++) {
+				numCnt[arr[i]]++;
+			}
+			for (int i = l; i <= r; i++) {
+				if (numCnt[arr[i]] > numCnt[most] || (numCnt[arr[i]] == numCnt[most] && arr[i] < most)) {
+					most = arr[i];
+				}
+			}
+			for (int i = l; i <= r; i++) {
+				numCnt[arr[i]] = 0;
+			}
+		} else {
+			for (int i = l; i <= br[bi[l]]; i++) {
+				numCnt[arr[i]]++;
+			}
+			for (int i = bl[bi[r]]; i <= r; i++) {
+				numCnt[arr[i]]++;
+			}
+			most = mode[bi[l] + 1][bi[r] - 1];
+			for (int i = l; i <= br[bi[l]]; i++) {
+				int cur = arr[i];
+				int curCnt = getCnt(bi[l] + 1, bi[r] - 1, cur) + numCnt[cur];
+				int mostCnt = getCnt(bi[l] + 1, bi[r] - 1, most) + numCnt[most];
+				if (curCnt > mostCnt || (curCnt == mostCnt && cur < most)) {
+					most = cur;
+				}
+			}
+			for (int i = bl[bi[r]]; i <= r; i++) {
+				int cur = arr[i];
+				int curCnt = getCnt(bi[l] + 1, bi[r] - 1, cur) + numCnt[cur];
+				int mostCnt = getCnt(bi[l] + 1, bi[r] - 1, most) + numCnt[most];
+				if (curCnt > mostCnt || (curCnt == mostCnt && cur < most)) {
+					most = cur;
+				}
+			}
+			for (int i = l; i <= br[bi[l]]; i++) {
+				numCnt[arr[i]] = 0;
+			}
+			for (int i = bl[bi[r]]; i <= r; i++) {
+				numCnt[arr[i]] = 0;
+			}
+		}
+		return sortv[most];
+	}
+
+	public static void main(String[] args) throws IOException {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			arr[i] = in.nextInt();
+		}
+		prepare();
+		for (int i = 1, a, b, l, r, lastAns = 0; i <= m; i++) {
+			a = (in.nextInt() + lastAns - 1) % n + 1;
+			b = (in.nextInt() + lastAns - 1) % n + 1;
+			l = Math.min(a, b);
+			r = Math.max(a, b);
+			lastAns = query(l, r);
+			out.println(lastAns);
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 20];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
 
 }
---
+++ src/class172/Code03_Violet2.java
@@ -0,0 +1,159 @@
+package class172;
+
+// 蒲公英，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P4168
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//const int MAXN = 40001;
+//const int MAXB = 201;
+//int n, m, s;
+//int arr[MAXN];
+//int sortv[MAXN];
+//
+//int blen, bnum;
+//int bi[MAXN];
+//int bl[MAXB];
+//int br[MAXB];
+//
+//int freq[MAXB][MAXN];
+//int mode[MAXB][MAXB];
+//int numCnt[MAXN];
+//
+//int lower(int num) {
+//    int l = 1, r = s, m, ans = 0;
+//    while (l <= r) {
+//        m = (l + r) >> 1;
+//        if (sortv[m] >= num) {
+//            ans = m;
+//            r = m - 1;
+//        } else {
+//            l = m + 1;
+//        }
+//    }
+//    return ans;
+//}
+//
+//int getCnt(int l, int r, int v) {
+//    return freq[r][v] - freq[l - 1][v];
+//}
+//
+//void prepare() {
+//    for (int i = 1; i <= n; i++) {
+//        sortv[i] = arr[i];
+//    }
+//    sort(sortv + 1, sortv + n + 1);
+//    s = 1;
+//    for (int i = 2; i <= n; i++) {
+//        if (sortv[s] != sortv[i]) {
+//            sortv[++s] = sortv[i];
+//        }
+//    }
+//    for (int i = 1; i <= n; i++) {
+//        arr[i] = lower(arr[i]);
+//    }
+//    blen = (int)sqrt(n);
+//    bnum = (n + blen - 1) / blen;
+//    for (int i = 1; i <= n; i++) {
+//        bi[i] = (i - 1) / blen + 1;
+//    }
+//    for (int i = 1; i <= bnum; i++) {
+//        bl[i] = (i - 1) * blen + 1;
+//        br[i] = min(i * blen, n);
+//    }
+//    for (int i = 1; i <= bnum; i++) {
+//        for (int j = bl[i]; j <= br[i]; j++) {
+//            freq[i][arr[j]]++;
+//        }
+//        for (int j = 1; j <= s; j++) {
+//            freq[i][j] += freq[i - 1][j];
+//        }
+//    }
+//    for (int i = 1; i <= bnum; i++) {
+//        for (int j = i; j <= bnum; j++) {
+//            int most = mode[i][j - 1];
+//            for (int k = bl[j]; k <= br[j]; k++) {
+//                int cur = arr[k];
+//                int curCnt = getCnt(i, j, cur);
+//                int mostCnt = getCnt(i, j, most);
+//                if (curCnt > mostCnt || (curCnt == mostCnt && cur < most)) {
+//                    most = cur;
+//                }
+//            }
+//            mode[i][j] = most;
+//        }
+//    }
+//}
+//
+//int query(int l, int r) {
+//    int most = 0;
+//    if (bi[l] == bi[r]) {
+//        for (int i = l; i <= r; i++) {
+//            numCnt[arr[i]]++;
+//        }
+//        for (int i = l; i <= r; i++) {
+//            if (numCnt[arr[i]] > numCnt[most] || (numCnt[arr[i]] == numCnt[most] && arr[i] < most)) {
+//                most = arr[i];
+//            }
+//        }
+//        for (int i = l; i <= r; i++) {
+//            numCnt[arr[i]] = 0;
+//        }
+//    } else {
+//        for (int i = l; i <= br[bi[l]]; i++) {
+//            numCnt[arr[i]]++;
+//        }
+//        for (int i = bl[bi[r]]; i <= r; i++) {
+//            numCnt[arr[i]]++;
+//        }
+//        most = mode[bi[l] + 1][bi[r] - 1];
+//        for (int i = l; i <= br[bi[l]]; i++) {
+//            int cur = arr[i];
+//            int curCnt = getCnt(bi[l] + 1, bi[r] - 1, cur) + numCnt[cur];
+//            int mostCnt = getCnt(bi[l] + 1, bi[r] - 1, most) + numCnt[most];
+//            if (curCnt > mostCnt || (curCnt == mostCnt && cur < most)) {
+//                most = cur;
+//            }
+//        }
+//        for (int i = bl[bi[r]]; i <= r; i++) {
+//            int cur = arr[i];
+//            int curCnt = getCnt(bi[l] + 1, bi[r] - 1, cur) + numCnt[cur];
+//            int mostCnt = getCnt(bi[l] + 1, bi[r] - 1, most) + numCnt[most];
+//            if (curCnt > mostCnt || (curCnt == mostCnt && cur < most)) {
+//                most = cur;
+//            }
+//        }
+//        for (int i = l; i <= br[bi[l]]; i++) {
+//            numCnt[arr[i]] = 0;
+//        }
+//        for (int i = bl[bi[r]]; i <= r; i++) {
+//            numCnt[arr[i]] = 0;
+//        }
+//    }
+//    return sortv[most];
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i];
+//    }
+//    prepare();
+//    int lastAns = 0, a, b, l, r;
+//    for (int i = 0; i < m; i++) {
+//        cin >> a >> b;
+//        a = (a + lastAns - 1) % n + 1;
+//        b = (b + lastAns - 1) % n + 1;
+//        l = min(a, b);
+//        r = max(a, b);
+//        lastAns = query(l, r);
+//        cout << lastAns << '\n';
+//    }
+//    return 0;
+//}
