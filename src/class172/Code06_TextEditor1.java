f9560a41a3d88dc209d6304db8ff354a374a5368
algorithmzuo <algorithmzuo@163.com>
2025-06-15 17:57:21 +0800

    modify code

--- src/class172/Code06_TextEditor1.java
+++ src/class172/Code06_TextEditor1.java
@@ -4,17 +4,172 @@ package class172;
 // 测试链接 : https://www.luogu.com.cn/problem/P4008
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class Code06_TextEditor1 {
 
-	public static void main(String[] args) throws IOException {
-		FastReader in = new FastReader();
-		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	public static int MAXN = 3000001;
+	public static int BLEN = 4001;
+	public static int BNUM = (MAXN / BLEN) << 1;
 
+	public static char[][] blocks = new char[BNUM][BLEN];
+	public static int[] nxt = new int[BNUM];
+	public static int[] siz = new int[BNUM];
+	public static int[] pool = new int[BNUM];
+	public static int psiz = 0;
+
+	public static char[] str = new char[MAXN];
+
+	public static void prepare() {
+		for (int i = 1; i < BNUM; i++) {
+			pool[i] = i;
+		}
+		psiz = BNUM - 1;
+		siz[0] = 0;
+		nxt[0] = -1;
+	}
+
+	public static int assign() {
+		return pool[psiz--];
+	}
+
+	public static void recycle(int i) {
+		pool[++psiz] = i;
+	}
+
+	public static int bi, pi;
+
+	public static void find(int pos) {
+		int cur = 0;
+		while (cur != -1 && pos > siz[cur]) {
+			pos -= siz[cur];
+			cur = nxt[cur];
+		}
+		bi = cur;
+		pi = pos;
+	}
+
+	public static void flush(int cur, int next, int endLen, char[] src, int srcPos) {
+		nxt[next] = nxt[cur];
+		nxt[cur] = next;
+		siz[next] = endLen;
+		System.arraycopy(src, srcPos, blocks[next], 0, endLen);
+	}
+
+	public static void merge(int cur, int next) {
+		System.arraycopy(blocks[next], 0, blocks[cur], siz[cur], siz[next]);
+		siz[cur] += siz[next];
+		nxt[cur] = nxt[next];
+		recycle(next);
+	}
+
+	public static void split(int cur, int pos) {
+		if (cur == -1 || pos == siz[cur]) {
+			return;
+		}
+		int next = assign();
+		flush(cur, next, siz[cur] - pos, blocks[cur], pos);
+		siz[cur] = pos;
+	}
+
+	public static void maintain() {
+		for (int cur = 0, next; cur != -1; cur = nxt[cur]) {
+			next = nxt[cur];
+			while (next != -1 && siz[cur] + siz[next] <= BLEN) {
+				merge(cur, next);
+				next = nxt[cur];
+			}
+		}
+	}
+
+	public static void insert(int pos, int len) {
+		find(pos);
+		split(bi, pi);
+		int cur = bi, newb, done = 0;
+		while (done + BLEN <= len) {
+			newb = assign();
+			flush(cur, newb, BLEN, str, done);
+			done += BLEN;
+			cur = newb;
+		}
+		if (len > done) {
+			newb = assign();
+			flush(cur, newb, len - done, str, done);
+		}
+		maintain();
+	}
+
+	public static void erase(int pos, int len) {
+		find(pos);
+		split(bi, pi);
+		int cur = bi, next = nxt[cur];
+		while (next != -1 && len > siz[next]) {
+			len -= siz[next];
+			recycle(next);
+			next = nxt[next];
+		}
+		if (next != -1) {
+			split(next, len);
+			recycle(next);
+			nxt[cur] = nxt[next];
+		} else {
+			nxt[cur] = -1;
+		}
+		maintain();
+	}
+
+	public static void get(int pos, int len) {
+		find(pos);
+		int cur = bi;
+		pos = pi;
+		int got = (len < siz[cur] - pos) ? len : (siz[cur] - pos);
+		System.arraycopy(blocks[cur], pos, str, 0, got);
+		cur = nxt[cur];
+		while (cur != -1 && got + siz[cur] <= len) {
+			System.arraycopy(blocks[cur], 0, str, got, siz[cur]);
+			got += siz[cur];
+			cur = nxt[cur];
+		}
+		if (cur != -1 && got < len) {
+			System.arraycopy(blocks[cur], 0, str, got, len - got);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader();
+		PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+		int n = in.nextInt();
+		int pos = 0;
+		int len;
+		String op;
+		prepare();
+		for (int i = 1; i <= n; i++) {
+			op = in.nextString();
+			if (op.equals("Prev")) {
+				pos--;
+			} else if (op.equals("Next")) {
+				pos++;
+			} else if (op.equals("Move")) {
+				pos = in.nextInt();
+			} else if (op.equals("Insert")) {
+				len = in.nextInt();
+				for (int j = 0; j < len; j++) {
+					str[j] = in.nextChar();
+				}
+				insert(pos, len);
+			} else if (op.equals("Delete")) {
+				len = in.nextInt();
+				erase(pos, len);
+			} else {
+				len = in.nextInt();
+				get(pos, len);
+				out.write(str, 0, len);
+				out.write('\n');
+			}
+		}
 		out.flush();
 		out.close();
 	}
@@ -26,7 +181,7 @@ public class Code06_TextEditor1 {
 		private final byte[] buffer;
 		private int ptr, len;
 
-		public FastReader() {
+		FastReader() {
 			in = System.in;
 			buffer = new byte[BUFFER_SIZE];
 			ptr = len = 0;
@@ -46,8 +201,8 @@ public class Code06_TextEditor1 {
 			return buffer[ptr++];
 		}
 
-		// 读取下一个ASCII码在闭区间[32, 126]内的字符
-		public char nextChar() throws IOException {
+		// 读取下一个ASCII码范围在[32,126]的字符
+		char nextChar() throws IOException {
 			byte c;
 			do {
 				c = readByte();
@@ -57,12 +212,11 @@ public class Code06_TextEditor1 {
 			return (char) c;
 		}
 
-		public String nextString() throws IOException {
+		String nextString() throws IOException {
 			byte b = readByte();
-			while (isWhitespace(b)) {
+			while (isWhitespace(b))
 				b = readByte();
-			}
-			StringBuilder sb = new StringBuilder(1000);
+			StringBuilder sb = new StringBuilder(16);
 			while (!isWhitespace(b) && b != -1) {
 				sb.append((char) b);
 				b = readByte();
@@ -70,21 +224,20 @@ public class Code06_TextEditor1 {
 			return sb.toString();
 		}
 
-		public int nextInt() throws IOException {
-			int num = 0;
+		int nextInt() throws IOException {
+			int num = 0, sign = 1;
 			byte b = readByte();
 			while (isWhitespace(b))
 				b = readByte();
-			boolean minus = false;
 			if (b == '-') {
-				minus = true;
+				sign = -1;
 				b = readByte();
 			}
 			while (!isWhitespace(b) && b != -1) {
 				num = num * 10 + (b - '0');
 				b = readByte();
 			}
-			return minus ? -num : num;
+			return sign * num;
 		}
 
 		private boolean isWhitespace(byte b) {
---
+++ src/class172/Code06_TextEditor2.java
@@ -0,0 +1,172 @@
+package class172;
+
+// 文本编辑器，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P4008
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//const int MAXN = 3000001;
+//const int BLEN = 4001;
+//const int BNUM = (MAXN / BLEN) << 1;
+//
+//char blocks[BNUM][BLEN];
+//int nxt[BNUM];
+//int siz[BNUM];
+//int pool[BNUM];
+//int psiz = 0;
+//
+//char str[MAXN];
+//
+//void prepare() {
+//    for (int i = 1; i < BNUM; i++) pool[i] = i;
+//    psiz = BNUM - 1;
+//    siz[0] = 0;
+//    nxt[0] = -1;
+//}
+//
+//int assign() {
+//    return pool[psiz--];
+//}
+//
+//void recycle(int i) {
+//    pool[++psiz] = i;
+//}
+//
+//int bi, pi;
+//
+//void find(int pos) {
+//    int cur = 0;
+//    while (cur != -1 && pos > siz[cur]) {
+//        pos -= siz[cur];
+//        cur = nxt[cur];
+//    }
+//    bi = cur;
+//    pi = pos;
+//}
+//
+//void flush(int cur, int nextb, int endLen, char* src, int srcPos) {
+//    nxt[nextb] = nxt[cur];
+//    nxt[cur] = nextb;
+//    siz[nextb] = endLen;
+//    memcpy(blocks[nextb], src + srcPos, endLen);
+//}
+//
+//void merge(int cur, int nextb) {
+//    memcpy(blocks[cur] + siz[cur], blocks[nextb], siz[nextb]);
+//    siz[cur] += siz[nextb];
+//    nxt[cur] = nxt[nextb];
+//    recycle(nextb);
+//}
+//
+//void split(int cur, int pos) {
+//    if (cur == -1 || pos == siz[cur]) return;
+//    int nextb = assign();
+//    flush(cur, nextb, siz[cur] - pos, blocks[cur], pos);
+//    siz[cur] = pos;
+//}
+//
+//void maintain() {
+//    for (int cur = 0, nextb; cur != -1; cur = nxt[cur]) {
+//        nextb = nxt[cur];
+//        while (nextb != -1 && siz[cur] + siz[nextb] <= BLEN) {
+//            merge(cur, nextb);
+//            nextb = nxt[cur];
+//        }
+//    }
+//}
+//
+//void insert(int pos, int len) {
+//    find(pos);
+//    split(bi, pi);
+//    int cur = bi, newb, done = 0;
+//    while (done + BLEN <= len) {
+//        newb = assign();
+//        flush(cur, newb, BLEN, str, done);
+//        done += BLEN;
+//        cur = newb;
+//    }
+//    if (len > done) {
+//        newb = assign();
+//        flush(cur, newb, len - done, str, done);
+//    }
+//    maintain();
+//}
+//
+//void erase(int pos, int len) {
+//    find(pos);
+//    split(bi, pi);
+//    int cur = bi;
+//    int nextb = nxt[cur];
+//    while (nextb != -1 && len > siz[nextb]) {
+//        len -= siz[nextb];
+//        recycle(nextb);
+//        nextb = nxt[nextb];
+//    }
+//    if (nextb != -1) {
+//        split(nextb, len);
+//        recycle(nextb);
+//        nxt[cur] = nxt[nextb];
+//    } else {
+//        nxt[cur] = -1;
+//    }
+//    maintain();
+//}
+//
+//void get(int pos, int len) {
+//    find(pos);
+//    int cur = bi;
+//    pos = pi;
+//    int got = (len < siz[cur] - pos) ? len : (siz[cur] - pos);
+//    memcpy(str, blocks[cur] + pos, got);
+//    cur = nxt[cur];
+//    while (cur != -1 && got + siz[cur] <= len) {
+//        memcpy(str + got, blocks[cur], siz[cur]);
+//        got += siz[cur];
+//        cur = nxt[cur];
+//    }
+//    if (cur != -1 && got < len) {
+//        memcpy(str + got, blocks[cur], len - got);
+//    }
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    int n;
+//    cin >> n;
+//    int pos = 0, len;
+//    char op[10];
+//    prepare();
+//    for (int i = 1; i <= n; i++) {
+//        cin >> op;
+//        if (op[0] == 'P') {
+//            pos--;
+//        } else if (op[0] == 'N') {
+//            pos++;
+//        } else if (op[0] == 'M') {
+//            cin >> pos;
+//        } else if (op[0] == 'I') {
+//            cin >> len;
+//            for (int j = 0; j < len; ) {
+//                char ch = cin.get();
+//                if (32 <= ch && ch <= 126) {
+//                    str[j++] = ch;
+//                }
+//            }
+//            insert(pos, len);
+//        } else if (op[0] == 'D') {
+//            cin >> len;
+//            erase(pos, len);
+//        } else {
+//            cin >> len;
+//            get(pos, len);
+//            cout.write(str, len);
+//            cout.put('\n');
+//        }
+//    }
+//    return 0;
+//}
