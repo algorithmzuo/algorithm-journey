b5452b47a36a85c0b7d987669c2cac191ab3720b
algorithmzuo <algorithmzuo@163.com>
2025-06-19 18:22:38 +0800

    modify code

---
+++ src/class173/Code02_Poker1.java
@@ -0,0 +1,240 @@
+package class173;
+
+// 由乃打扑克，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P5356
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class Code02_Poker1 {
+
+	public static int MAXN = 100001;
+	public static int MAXB = 1001;
+	public static int INF = Integer.MAX_VALUE;
+	public static int n, m;
+
+	public static int[] arr = new int[MAXN];
+	public static int[] sort = new int[MAXN];
+
+	public static int blen, bnum;
+	public static int[] bi = new int[MAXN];
+	public static int[] bl = new int[MAXB];
+	public static int[] br = new int[MAXB];
+	public static int[] lazy = new int[MAXB];
+
+	public static void innerAdd(int l, int r, int v) {
+		for (int i = l; i <= r; i++) {
+			arr[i] += v;
+		}
+		for (int i = bl[bi[l]]; i <= br[bi[l]]; i++) {
+			sort[i] = arr[i];
+		}
+		Arrays.sort(sort, bl[bi[l]], br[bi[l]] + 1);
+	}
+
+	public static void add(int l, int r, int v) {
+		if (bi[l] == bi[r]) {
+			innerAdd(l, r, v);
+		} else {
+			innerAdd(l, br[bi[l]], v);
+			innerAdd(bl[bi[r]], r, v);
+			for (int i = bi[l] + 1; i <= bi[r] - 1; i++) {
+				lazy[i] += v;
+			}
+		}
+	}
+
+	public static int getMin(int l, int r) {
+		int lb = bi[l], rb = bi[r], ans = INF;
+		if (lb == rb) {
+			for (int i = l; i <= r; i++) {
+				ans = Math.min(ans, arr[i] + lazy[lb]);
+			}
+		} else {
+			for (int i = l; i <= br[lb]; i++) {
+				ans = Math.min(ans, arr[i] + lazy[lb]);
+			}
+			for (int i = bl[rb]; i <= r; i++) {
+				ans = Math.min(ans, arr[i] + lazy[rb]);
+			}
+			for (int i = lb + 1; i <= rb - 1; i++) {
+				ans = Math.min(ans, sort[bl[i]] + lazy[i]);
+			}
+		}
+		return ans;
+	}
+
+	public static int getMax(int l, int r) {
+		int lb = bi[l], rb = bi[r], ans = -INF;
+		if (lb == rb) {
+			for (int i = l; i <= r; i++) {
+				ans = Math.max(ans, arr[i] + lazy[lb]);
+			}
+		} else {
+			for (int i = l; i <= br[lb]; i++) {
+				ans = Math.max(ans, arr[i] + lazy[lb]);
+			}
+			for (int i = bl[rb]; i <= r; i++) {
+				ans = Math.max(ans, arr[i] + lazy[rb]);
+			}
+			for (int i = lb + 1; i <= rb - 1; i++) {
+				ans = Math.max(ans, sort[br[i]] + lazy[i]);
+			}
+		}
+		return ans;
+	}
+
+	public static int getCnt(int i, int v) {
+		v -= lazy[i];
+		int l = bl[i], r = br[i];
+		if (sort[l] > v) {
+			return 0;
+		}
+		if (sort[r] <= v) {
+			return r - l + 1;
+		}
+		int m, find = l;
+		while (l <= r) {
+			m = (l + r) / 2;
+			if (sort[m] <= v) {
+				find = m;
+				l = m + 1;
+			} else {
+				r = m - 1;
+			}
+		}
+		return find - bl[i] + 1;
+	}
+
+	public static int num(int l, int r, int v) {
+		int lb = bi[l], rb = bi[r], ans = 0;
+		if (lb == rb) {
+			for (int i = l; i <= r; i++) {
+				if (arr[i] + lazy[lb] <= v) {
+					ans++;
+				}
+			}
+		} else {
+			for (int i = l; i <= br[lb]; i++) {
+				if (arr[i] + lazy[lb] <= v) {
+					ans++;
+				}
+			}
+			for (int i = bl[rb]; i <= r; i++) {
+				if (arr[i] + lazy[rb] <= v) {
+					ans++;
+				}
+			}
+			for (int i = lb + 1; i <= rb - 1; i++) {
+				ans += getCnt(i, v);
+			}
+		}
+		return ans;
+	}
+
+	public static int query(int l, int r, int v) {
+		if (v < 1 || v > r - l + 1) {
+			return -1;
+		}
+		int minv = getMin(l, r);
+		int maxv = getMax(l, r);
+		int midv;
+		int ans = -1;
+		while (minv <= maxv) {
+			midv = minv + (maxv - minv) / 2;
+			if (num(l, r, midv) >= v) {
+				ans = midv;
+				maxv = midv - 1;
+			} else {
+				minv = midv + 1;
+			}
+		}
+		return ans;
+	}
+
+	public static void prepare() {
+		blen = (int) Math.sqrt(n);
+		bnum = (n + blen - 1) / blen;
+		for (int i = 1; i <= n; i++) {
+			bi[i] = (i - 1) / blen + 1;
+		}
+		for (int i = 1; i <= bnum; i++) {
+			bl[i] = (i - 1) * blen + 1;
+			br[i] = Math.min(i * blen, n);
+		}
+		for (int i = 1; i <= n; i++) {
+			sort[i] = arr[i];
+		}
+		for (int i = 1; i <= bnum; i++) {
+			Arrays.sort(sort, bl[i], br[i] + 1);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			arr[i] = in.nextInt();
+		}
+		prepare();
+		for (int i = 1, op, l, r, v; i <= m; i++) {
+			op = in.nextInt();
+			l = in.nextInt();
+			r = in.nextInt();
+			v = in.nextInt();
+			if (op == 1) {
+				out.println(query(l, r, v));
+			} else {
+				add(l, r, v);
+			}
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 20];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
+
+}
---
+++ src/class173/Code02_Poker2.java
@@ -0,0 +1,190 @@
+package class173;
+
+// 由乃打扑克，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P5356
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//const int MAXN = 100001;
+//const int MAXB = 1001;
+//const int INF = 2147483647;
+//
+//int n, m;
+//int arr[MAXN];
+//int sortv[MAXN];
+//
+//int blen, bnum;
+//int bi[MAXN];
+//int bl[MAXB];
+//int br[MAXB];
+//int lazy[MAXB];
+//
+//void innerAdd(int l, int r, int v) {
+//    for (int i = l; i <= r; i++) {
+//        arr[i] += v;
+//    }
+//    for (int i = bl[bi[l]]; i <= br[bi[l]]; i++) {
+//        sortv[i] = arr[i];
+//    }
+//    sort(sortv + bl[bi[l]], sortv + br[bi[l]] + 1);
+//}
+//
+//void add(int l, int r, int v) {
+//    if (bi[l] == bi[r]) {
+//        innerAdd(l, r, v);
+//    } else {
+//        innerAdd(l, br[bi[l]], v);
+//        innerAdd(bl[bi[r]], r, v);
+//        for (int i = bi[l] + 1; i <= bi[r] - 1; i++) {
+//            lazy[i] += v;
+//        }
+//    }
+//}
+//
+//int getMin(int l, int r) {
+//    int lb = bi[l], rb = bi[r], ans = INF;
+//    if (lb == rb) {
+//        for (int i = l; i <= r; i++) {
+//            ans = min(ans, arr[i] + lazy[lb]);
+//        }
+//    } else {
+//        for (int i = l; i <= br[lb]; i++) {
+//            ans = min(ans, arr[i] + lazy[lb]);
+//        }
+//        for (int i = bl[rb]; i <= r; i++) {
+//            ans = min(ans, arr[i] + lazy[rb]);
+//        }
+//        for (int i = lb + 1; i <= rb - 1; i++) {
+//            ans = min(ans, sortv[bl[i]] + lazy[i]);
+//        }
+//    }
+//    return ans;
+//}
+//
+//int getMax(int l, int r) {
+//    int lb = bi[l], rb = bi[r], ans = -INF;
+//    if (lb == rb) {
+//        for (int i = l; i <= r; i++) {
+//            ans = max(ans, arr[i] + lazy[lb]);
+//        }
+//    } else {
+//        for (int i = l; i <= br[lb]; i++) {
+//            ans = max(ans, arr[i] + lazy[lb]);
+//        }
+//        for (int i = bl[rb]; i <= r; i++) {
+//            ans = max(ans, arr[i] + lazy[rb]);
+//        }
+//        for (int i = lb + 1; i <= rb - 1; i++) {
+//            ans = max(ans, sortv[br[i]] + lazy[i]);
+//        }
+//    }
+//    return ans;
+//}
+//
+//int getCnt(int i, int v) {
+//    v -= lazy[i];
+//    int l = bl[i], r = br[i];
+//    if (sortv[l] > v) {
+//        return 0;
+//    }
+//    if (sortv[r] <= v) {
+//        return r - l + 1;
+//    }
+//    int find = l;
+//    while (l <= r) {
+//        int m = (l + r) >> 1;
+//        if (sortv[m] <= v) {
+//            find = m;
+//            l = m + 1;
+//        } else {
+//            r = m - 1;
+//        }
+//    }
+//    return find - bl[i] + 1;
+//}
+//
+//int num(int l, int r, int v) {
+//    int lb = bi[l], rb = bi[r], ans = 0;
+//    if (lb == rb) {
+//        for (int i = l; i <= r; i++) {
+//            if (arr[i] + lazy[lb] <= v) {
+//                ans++;
+//            }
+//        }
+//    } else {
+//        for (int i = l; i <= br[lb]; i++) {
+//            if (arr[i] + lazy[lb] <= v) {
+//                ans++;
+//            }
+//        }
+//        for (int i = bl[rb]; i <= r; i++) {
+//            if (arr[i] + lazy[rb] <= v) {
+//                ans++;
+//            }
+//        }
+//        for (int i = lb + 1; i <= rb - 1; i++) {
+//            ans += getCnt(i, v);
+//        }
+//    }
+//    return ans;
+//}
+//
+//int query(int l, int r, int v) {
+//    if (v < 1 || v > r - l + 1) {
+//        return -1;
+//    }
+//    int minv = getMin(l, r);
+//    int maxv = getMax(l, r);
+//    int ans = -1;
+//    while (minv <= maxv) {
+//        int midv = minv + (maxv - minv) / 2;
+//        if (num(l, r, midv) >= v) {
+//            ans = midv;
+//            maxv = midv - 1;
+//        } else {
+//            minv = midv + 1;
+//        }
+//    }
+//    return ans;
+//}
+//
+//void prepare() {
+//    blen = (int)sqrt(m);
+//    bnum = (n + blen - 1) / blen;
+//    for (int i = 1; i <= n; i++) {
+//        bi[i] = (i - 1) / blen + 1;
+//    }
+//    for (int i = 1; i <= bnum; i++) {
+//        bl[i] = (i - 1) * blen + 1;
+//        br[i] = min(i * blen, n);
+//    }
+//    for (int i = 1; i <= n; i++) {
+//        sortv[i] = arr[i];
+//    }
+//    for (int i = 1; i <= bnum; i++) {
+//        sort(sortv + bl[i], sortv + br[i] + 1);
+//    }
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i];
+//    }
+//    prepare();
+//    for (int i = 1, op, l, r, v; i <= m; i++) {
+//        cin >> op >> l >> r >> v;
+//        if (op == 1) {
+//            cout << query(l, r, v) << '\n';
+//        } else {
+//            add(l, r, v);
+//        }
+//    }
+//    return 0;
+//}
