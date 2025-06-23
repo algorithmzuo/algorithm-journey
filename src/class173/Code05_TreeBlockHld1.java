2d00be49ec404bf9b9213171b308d3772f668462
algorithmzuo <algorithmzuo@163.com>
2025-06-23 14:38:43 +0800

    modify code

--- src/class173/Code05_TreeBlockHld1.java
+++ src/class173/Code05_TreeBlockHld1.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-public class Code05_TreeHld1 {
+public class Code05_TreeBlockHld1 {
 
 	public static int MAXN = 100001;
 	public static int MAXB = 1001;
---
+++ src/class173/Code06_TreeBlockRandom1.java
@@ -0,0 +1,9 @@
+package class173;
+
+// 树上分块模版题，随机撒点分块，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P3603
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+public class Code06_TreeBlockRandom1 {
+
+}
