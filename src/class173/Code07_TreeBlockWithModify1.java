83ef40caf4763a8bd06df43b1e41e549c46744de
algorithmzuo <algorithmzuo@163.com>
2025-06-23 21:33:24 +0800

    modify code

---
+++ src/class173/Code07_TreeBlockWithModify1.java
@@ -0,0 +1,10 @@
+package class173;
+
+// 带修改的树上分块，java版
+// 测试链接 : https://www.luogu.com.cn/problem/CF1491H
+// 测试链接 : https://codeforces.com/problemset/problem/1491/H
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+public class Code07_TreeBlockWithModify1 {
+
+}
