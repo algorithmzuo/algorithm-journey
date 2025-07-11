0bc2fff73cb73693f59d255af591d5eb92c5de8e
algorithmzuo <algorithmzuo@163.com>
2025-07-11 15:35:31 +0800

    modify code

--- src/class094/Code05_CuttingTree.java
+++ src/class094/Code05_CuttingTree.java
@@ -6,9 +6,8 @@ package class094;
 // 你每天最多能砍1棵树，砍下这棵树的收益为：
 // 这棵树的初始重量 + 这棵树增长到这一天的总增重
 // 从第1天开始，你一共有m天可以砍树，返回m天内你获得的最大收益
-// 测试链接 : https://pintia.cn/problem-sets/91827364500/exam/problems/91827367873
-// 请同学们务必参考如下代码中关于输入、输出的处理
-// 这是输入输出处理效率很高的写法
+// 测试链接 : https://pintia.cn/problem-sets/91827364500/exam/problems/type/7?problemSetProblemId=91827367873
+// 如果测试链接失效，去往 ZOJ Problem Set 网站上找第3211题，本题的名字叫 "Dream City"
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 
 import java.io.BufferedReader;
