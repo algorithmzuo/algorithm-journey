38091a52a3667326b42506cba164147b0aab1a94
algorithmzuo <algorithmzuo@163.com>
2025-07-31 21:04:16 +0800

    modify code

---
+++ src/class175/Code04_Initialization1.java
@@ -0,0 +1,162 @@
+package class175;
+
+// 初始化，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P5309
+// 提交以下的code，提交时请把类名改成"Main"
+// java实现的逻辑一定是正确的，但是本题卡常，无法通过所有测试用例
+// 想通过用C++实现，本节课Code04_Initialization2文件就是C++的实现
+// 两个版本的逻辑完全一样，C++版本可以通过所有测试
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class Code04_Initialization1 {
+
+	public static int MAXN = 200001;
+	public static int MAXB = 2001;
+	public static int MOD = 1000000007;
+	public static int n, m;
+
+	public static long[] arr = new long[MAXN];
+	public static long[] sum = new long[MAXN];
+	public static long[][] pre = new long[MAXB][MAXB];
+	public static long[][] suf = new long[MAXB][MAXB];
+
+	public static int blen, bnum;
+	public static int[] bi = new int[MAXN];
+	public static int[] bl = new int[MAXB];
+	public static int[] br = new int[MAXB];
+
+	public static void add(int x, int y, int z) {
+		if (x >= blen) {
+			for (int i = y; i <= n; i += x) {
+				arr[i] += z;
+				sum[bi[i]] += z;
+			}
+		} else {
+			for (int i = y; i <= x; i++) {
+				pre[x][i] += z;
+			}
+			for (int i = 1; i <= y; i++) {
+				suf[x][i] += z;
+			}
+		}
+	}
+
+	public static long querySum(int l, int r) {
+		int lb = bi[l], rb = bi[r];
+		long ans = 0;
+		if (lb == rb) {
+			for (int i = l; i <= r; i++) {
+				ans += arr[i];
+			}
+		} else {
+			for (int i = l; i <= br[lb]; i++) {
+				ans += arr[i];
+			}
+			for (int i = bl[rb]; i <= r; i++) {
+				ans += arr[i];
+			}
+			for (int b = lb + 1; b <= rb - 1; b++) {
+				ans += sum[b];
+			}
+		}
+		return ans;
+	}
+
+	public static long query(int l, int r) {
+		long ans = querySum(l, r);
+		for (int x = 1, lb, rb, num; x < blen; x++) {
+			lb = (l - 1) / x + 1;
+			rb = (r - 1) / x + 1;
+			num = rb - lb - 1;
+			if (lb == rb) {
+				ans = ans - pre[x][(l - 1) % x] + pre[x][(r - 1) % x + 1];
+			} else {
+				ans = ans + pre[x][x] * num + pre[x][(r - 1) % x + 1] + suf[x][(l - 1) % x + 1];
+			}
+		}
+		return ans;
+	}
+
+	public static void prepare() {
+		blen = 128;
+		bnum = (n + blen - 1) / blen;
+		for (int i = 1; i <= n; i++) {
+			bi[i] = (i - 1) / blen + 1;
+		}
+		for (int b = 1; b <= bnum; b++) {
+			bl[b] = (b - 1) * blen + 1;
+			br[b] = Math.min(b * blen, n);
+			for (int i = bl[b]; i <= br[b]; i++) {
+				sum[b] += arr[i];
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			arr[i] = in.nextInt();
+		}
+		prepare();
+		for (int i = 1, op, x, y, z; i <= m; i++) {
+			op = in.nextInt();
+			x = in.nextInt();
+			y = in.nextInt();
+			if (op == 1) {
+				z = in.nextInt();
+				add(x, y, z);
+			} else {
+				out.println((query(x, y) % MOD + MOD) % MOD);
+			}
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
+
+}
---
+++ src/class175/Code04_Initialization2.java
@@ -0,0 +1,112 @@
+package class175;
+
+// 初始化，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P5309
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//const int MAXN = 200001;
+//const int MAXB = 2001;
+//const int MOD  = 1000000007;
+//int n, m;
+//
+//long long arr[MAXN];
+//long long sum[MAXN];
+//long long pre[MAXB][MAXB];
+//long long suf[MAXB][MAXB];
+//
+//int blen, bnum;
+//int bi[MAXN];
+//int bl[MAXB];
+//int br[MAXB];
+//
+//void add(int x, int y, long long z) {
+//    if (x >= blen) {
+//        for (int i = y; i <= n; i += x) {
+//            arr[i] += z;
+//            sum[bi[i]] += z;
+//        }
+//    } else {
+//        for (int i = y; i <= x; i++) {
+//            pre[x][i] += z;
+//        }
+//        for (int i = 1; i <= y; i++) {
+//            suf[x][i] += z;
+//        }
+//    }
+//}
+//
+//long long querySum(int l, int r) {
+//    int lb = bi[l], rb = bi[r];
+//    long long ans = 0;
+//    if (lb == rb) {
+//        for (int i = l; i <= r; i++) {
+//            ans += arr[i];
+//        }
+//    } else {
+//        for (int i = l; i <= br[lb]; i++) {
+//            ans += arr[i];
+//        }
+//        for (int i = bl[rb]; i <= r; i++) {
+//            ans += arr[i];
+//        }
+//        for (int b = lb + 1; b <= rb - 1; b++) {
+//            ans += sum[b];
+//        }
+//    }
+//    return ans;
+//}
+//
+//long long query(int l, int r) {
+//    long long ans = querySum(l, r);
+//    for (int x = 1, lb, rb, num; x < blen; x++) {
+//        lb = (l - 1) / x + 1;
+//        rb = (r - 1) / x + 1;
+//        num = rb - lb - 1;
+//        if (lb == rb) {
+//            ans = ans - pre[x][(l - 1) % x] + pre[x][(r - 1) % x + 1];
+//        } else {
+//            ans = ans + pre[x][x] * num + pre[x][(r - 1) % x + 1] + suf[x][(l - 1) % x + 1];
+//        }
+//    }
+//    return ans;
+//}
+//
+//void prepare() {
+//    blen = 128;
+//    bnum = (n + blen - 1) / blen;
+//    for (int i = 1; i <= n; i++) {
+//        bi[i] = (i - 1) / blen + 1;
+//    }
+//    for (int b = 1; b <= bnum; b++) {
+//        bl[b] = (b - 1) * blen + 1;
+//        br[b] = min(b * blen, n);
+//        for (int i = bl[b]; i <= br[b]; i++) {
+//            sum[b] += arr[i];
+//        }
+//    }
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i];
+//    }
+//    prepare();
+//    for (int i = 1, op, x, y, z; i <= m; i++) {
+//        cin >> op >> x >> y;
+//        if (op == 1) {
+//            cin >> z;
+//            add(x, y, z);
+//        } else {
+//            cout << ((query(x, y) % MOD + MOD) % MOD) << '\n';
+//        }
+//    }
+//    return 0;
+//}
