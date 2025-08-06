67f3837a409a340650ac4bc254d71157ba5cf1c2
algorithmzuo <algorithmzuo@163.com>
2025-08-06 12:47:38 +0800

    modify code

--- src/class030/Code05_DoubleNumber1.java
+++ src/class030/Code05_DoubleNumber1.java
@@ -3,7 +3,7 @@ package class030;
 // 数组中有2种数出现了奇数次，其他的数都出现了偶数次
 // 返回这2种出现了奇数次的数
 // 测试链接 : https://leetcode.cn/problems/single-number-iii/
-// C++版本的实现，就是本节课Code05_DoubleNumber2文件
+// 这是java版本的实现，本节课Code05_DoubleNumber2文件是C++版本的实现
 public class Code05_DoubleNumber1 {
 
 	public static int[] singleNumber(int[] nums) {
