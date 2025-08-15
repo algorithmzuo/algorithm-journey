f50d6e93f57448ed8bbc9520c6b188d1203bd16b
algorithmzuo <algorithmzuo@163.com>
2025-08-15 20:06:51 +0800

    modify code

---
+++ src/class176/Code07_MachineLearning1.java
@@ -0,0 +1,239 @@
+package class176;
+
+// 机器学习，java版
+// 测试链接 : https://www.luogu.com.cn/problem/CF940F
+// 测试链接 : https://codeforces.com/problemset/problem/940/F
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Code07_MachineLearning1 {
+
+	public static class QueryCmp implements Comparator<int[]> {
+		@Override
+		public int compare(int[] a, int[] b) {
+			if (bi[a[0]] != bi[b[0]]) {
+				return bi[a[0]] - bi[b[0]];
+			}
+			if (bi[a[1]] != bi[b[1]]) {
+				return bi[a[1]] - bi[b[1]];
+			}
+			return a[2] - b[2];
+		}
+	}
+
+	public static int MAXN = 100001;
+	public static int n, m;
+	public static int[] arr = new int[MAXN];
+	public static int[] sorted = new int[MAXN << 1];
+
+	public static int[] bi = new int[MAXN];
+	public static int[][] query = new int[MAXN][4];
+	public static int[][] update = new int[MAXN][2];
+	public static int cntq, cntu;
+
+	public static int[] cnt1 = new int[MAXN << 1];
+	public static int[] cnt2 = new int[MAXN];
+	public static int[] ans = new int[MAXN];
+
+	public static int kth(int len, int num) {
+		int left = 1, right = len, mid, ret = 0;
+		while (left <= right) {
+			mid = (left + right) / 2;
+			if (sorted[mid] <= num) {
+				ret = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return ret;
+	}
+
+	public static void add(int num) {
+		--cnt2[cnt1[num]];
+		++cnt2[++cnt1[num]];
+	}
+
+	public static void del(int num) {
+		--cnt2[cnt1[num]];
+		++cnt2[--cnt1[num]];
+	}
+
+	public static void moveTime(int jobl, int jobr, int tim) {
+		int pos = update[tim][0];
+		int val = update[tim][1];
+		if (jobl <= pos && pos <= jobr) {
+			del(arr[pos]);
+			add(val);
+		}
+		int tmp = arr[pos];
+		arr[pos] = val;
+		update[tim][1] = tmp;
+	}
+
+	public static void compute() {
+		int winl = 1, winr = 0, wint = 0;
+		for (int i = 1; i <= cntq; i++) {
+			int jobl = query[i][0];
+			int jobr = query[i][1];
+			int jobt = query[i][2];
+			int id = query[i][3];
+			while (winl > jobl) {
+				add(arr[--winl]);
+			}
+			while (winr < jobr) {
+				add(arr[++winr]);
+			}
+			while (winl < jobl) {
+				del(arr[winl++]);
+			}
+			while (winr > jobr) {
+				del(arr[winr--]);
+			}
+			while (wint < jobt) {
+				moveTime(jobl, jobr, ++wint);
+			}
+			while (wint > jobt) {
+				moveTime(jobl, jobr, wint--);
+			}
+			int ret = 1;
+			while (ret <= n && cnt2[ret] > 0) {
+				ret++;
+			}
+			ans[id] = ret;
+		}
+	}
+
+	public static void prepare() {
+		int len = 0;
+		for (int i = 1; i <= n; i++) {
+			sorted[++len] = arr[i];
+		}
+		for (int i = 1; i <= cntu; i++) {
+			sorted[++len] = update[i][1];
+		}
+		Arrays.sort(sorted, 1, len + 1);
+		int tmp = 1;
+		for (int i = 2; i <= len; i++) {
+			if (sorted[tmp] != sorted[i]) {
+				sorted[++tmp] = sorted[i];
+			}
+		}
+		len = tmp;
+		for (int i = 1; i <= n; i++) {
+			arr[i] = kth(len, arr[i]);
+		}
+		for (int i = 1; i <= cntu; i++) {
+			update[i][1] = kth(len, update[i][1]);
+		}
+		int blen = Math.max(1, (int) Math.pow(n, 2.0 / 3));
+		for (int i = 1; i <= n; i++) {
+			bi[i] = (i - 1) / blen + 1;
+		}
+		Arrays.sort(query, 1, cntq + 1, new QueryCmp());
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader();
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			arr[i] = in.nextInt();
+		}
+		for (int i = 1, op, x, y; i <= m; i++) {
+			op = in.nextInt();
+			x = in.nextInt();
+			y = in.nextInt();
+			if (op == 1) {
+				cntq++;
+				query[cntq][0] = x;
+				query[cntq][1] = y;
+				query[cntq][2] = cntu;
+				query[cntq][3] = cntq;
+			} else {
+				cntu++;
+				update[cntu][0] = x;
+				update[cntu][1] = y;
+			}
+		}
+		prepare();
+		compute();
+		for (int i = 1; i <= cntq; i++) {
+			out.println(ans[i]);
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private final InputStream in;
+		private final byte[] buffer;
+		private int ptr, len;
+
+		public FastReader() {
+			in = System.in;
+			buffer = new byte[BUFFER_SIZE];
+			ptr = len = 0;
+		}
+
+		private boolean hasNextByte() throws IOException {
+			if (ptr < len)
+				return true;
+			ptr = 0;
+			len = in.read(buffer);
+			return len > 0;
+		}
+
+		private byte readByte() throws IOException {
+			if (!hasNextByte())
+				return -1;
+			return buffer[ptr++];
+		}
+
+		public char nextChar() throws IOException {
+			byte c;
+			do {
+				c = readByte();
+				if (c == -1)
+					return 0;
+			} while (c <= ' ');
+			char ans = 0;
+			while (c > ' ') {
+				ans = (char) c;
+				c = readByte();
+			}
+			return ans;
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+			byte b = readByte();
+			while (isWhitespace(b))
+				b = readByte();
+			boolean minus = false;
+			if (b == '-') {
+				minus = true;
+				b = readByte();
+			}
+			while (!isWhitespace(b) && b != -1) {
+				num = num * 10 + (b - '0');
+				b = readByte();
+			}
+			return minus ? -num : num;
+		}
+
+		private boolean isWhitespace(byte b) {
+			return b == ' ' || b == '\n' || b == '\r' || b == '\t';
+		}
+	}
+
+}
---
+++ src/class176/Code07_MachineLearning2.java
@@ -0,0 +1,170 @@
+package class176;
+
+// 机器学习，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/CF940F
+// 测试链接 : https://codeforces.com/problemset/problem/940/F
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct Query {
+//    int l, r, t, id;
+//};
+//
+//struct Update {
+//    int i, v;
+//};
+//
+//const int MAXN = 100001;
+//int n, m;
+//int arr[MAXN];
+//int sorted[MAXN << 1];
+//
+//int bi[MAXN];
+//Query query[MAXN];
+//Update update[MAXN];
+//int cntq, cntu;
+//
+//int cnt1[MAXN << 1];
+//int cnt2[MAXN];
+//int ans[MAXN];
+//
+//bool QueryCmp(Query &a, Query &b) {
+//    if (bi[a.l] != bi[b.l]) {
+//        return bi[a.l] < bi[b.l];
+//    }
+//    if (bi[a.r] != bi[b.r]) {
+//        return bi[a.r] < bi[b.r];
+//    }
+//    return a.t < b.t;
+//}
+//
+//int kth(int len, int num) {
+//    int left = 1, right = len, mid, ret = 0;
+//    while (left <= right) {
+//        mid = (left + right) >> 1;
+//        if (sorted[mid] <= num) {
+//            ret = mid;
+//            left = mid + 1;
+//        } else {
+//            right = mid - 1;
+//        }
+//    }
+//    return ret;
+//}
+//
+//void add(int num) {
+//    --cnt2[cnt1[num]];
+//    ++cnt2[++cnt1[num]];
+//}
+//
+//void del(int num) {
+//    --cnt2[cnt1[num]];
+//    ++cnt2[--cnt1[num]];
+//}
+//
+//void moveTime(int jobl, int jobr, int tim) {
+//    int pos = update[tim].i;
+//    int val = update[tim].v;
+//    if (jobl <= pos && pos <= jobr) {
+//        del(arr[pos]);
+//        add(val);
+//    }
+//    int tmp = arr[pos];
+//    arr[pos] = val;
+//    update[tim].v = tmp;
+//}
+//
+//void compute() {
+//    int winl = 1, winr = 0, wint = 0;
+//    for (int i = 1; i <= cntq; i++) {
+//        int jobl = query[i].l;
+//        int jobr = query[i].r;
+//        int jobt = query[i].t;
+//        int id = query[i].id;
+//        while (winl > jobl) {
+//            add(arr[--winl]);
+//        }
+//        while (winr < jobr) {
+//            add(arr[++winr]);
+//        }
+//        while (winl < jobl) {
+//            del(arr[winl++]);
+//        }
+//        while (winr > jobr) {
+//            del(arr[winr--]);
+//        }
+//        while (wint < jobt) {
+//            moveTime(jobl, jobr, ++wint);
+//        }
+//        while (wint > jobt) {
+//            moveTime(jobl, jobr,  wint--);
+//        }
+//        int ret = 1;
+//        while (ret <= n && cnt2[ret] > 0) {
+//            ret++;
+//        }
+//        ans[id] = ret;
+//    }
+//}
+//
+//void prepare() {
+//    int len = 0;
+//    for (int i = 1; i <= n; i++) {
+//        sorted[++len] = arr[i];
+//    }
+//    for (int i = 1; i <= cntu; i++) {
+//        sorted[++len] = update[i].v;
+//    }
+//    sort(sorted + 1, sorted + len + 1);
+//    int tmp = 1;
+//    for (int i = 2; i <= len; i++) {
+//        if (sorted[tmp] != sorted[i]) {
+//            sorted[++tmp] = sorted[i];
+//        }
+//    }
+//    len = tmp;
+//    for (int i = 1; i <= n; i++) {
+//        arr[i] = kth(len, arr[i]);
+//    }
+//    for (int i = 1; i <= cntu; i++) {
+//        update[i].v = kth(len, update[i].v);
+//    }
+//    int blen = max(1, (int)pow(n, 2.0 / 3));
+//    for (int i = 1; i <= n; i++) {
+//        bi[i] = (i - 1) / blen + 1;
+//    }
+//    sort(query + 1, query + cntq + 1, QueryCmp);
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i];
+//    }
+//    for (int i = 1, op, x, y; i <= m; i++) {
+//        cin >> op >> x >> y;
+//        if (op == 1) {
+//            ++cntq;
+//            query[cntq].l = x;
+//            query[cntq].r = y;
+//            query[cntq].t = cntu;
+//            query[cntq].id = cntq;
+//        } else {
+//            ++cntu;
+//            update[cntu].i = x;
+//            update[cntu].v = y;
+//        }
+//    }
+//    prepare();
+//    compute();
+//    for (int i = 1; i <= cntq; i++) {
+//        cout << ans[i] << '\n';
+//    }
+//    return 0;
+//}
