004f412b8f51d4e54f39a39dde312cd36afc2fd1
algorithmzuo <algorithmzuo@163.com>
2025-08-27 17:26:37 +0800

    modify code

--- src/class177/Code04_ZeroQuery2.java
+++ src/class177/Code04_ZeroQuery2.java
@@ -1,7 +1,8 @@
 package class177;
 
-// 相同数的最远距离，C++版
-// 测试链接 : https://www.luogu.com.cn/problem/P5906
+// 累加和为0的最长子数组，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/SP20644
+// 测试链接 : https://www.spoj.com/problems/ZQUERY/
 // 如下实现是C++的版本，C++版本和java版本逻辑完全一样
 // 提交如下代码，可以通过所有测试用例
 
@@ -13,21 +14,21 @@ package class177;
 //    int l, r, id;
 //};
 //
-//const int MAXN = 200001;
-//const int MAXB = 501;
+//const int MAXN = 50001;
+//const int MAXB = 301;
 //int n, m;
 //int arr[MAXN];
 //Query query[MAXN];
-//int sorted[MAXN];
+//int sorted[MAXN + 1];
 //int cntv;
 //
 //int blen, bnum;
 //int bi[MAXN];
 //int br[MAXB];
 //
-//int forceEd[MAXN];
-//int st[MAXN];
-//int ed[MAXN];
+//int forceEd[MAXN + 1];
+//int st[MAXN + 1];
+//int ed[MAXN + 1];
 //
 //int curAns = 0;
 //int ans[MAXN];
@@ -100,7 +101,7 @@ package class177;
 //        fill(ed + 1, ed + cntv + 1, 0);
 //        int winl = br[block] + 1, winr = br[block];
 //        for (; qi <= m && bi[query[qi].l] == block; qi++) {
-//            int jobl = query[qi].l;
+//            int jobl = query[qi].l - 1;
 //            int jobr = query[qi].r;
 //            int id = query[qi].id;
 //            if (jobr <= br[block]) {
@@ -125,16 +126,21 @@ package class177;
 //
 //void prepare() {
 //    for (int i = 1; i <= n; i++) {
-//        sorted[i] = arr[i];
+//        arr[i] += arr[i - 1];
 //    }
-//    sort(sorted + 1, sorted + n + 1);
+//    int len = 0;
+//    sorted[++len] = 0;
+//    for (int i = 1; i <= n; i++) {
+//        sorted[++len] = arr[i];
+//    }
+//    sort(sorted + 1, sorted + len + 1);
 //    cntv = 1;
-//    for (int i = 2; i <= n; i++) {
+//    for (int i = 2; i <= len; i++) {
 //        if (sorted[cntv] != sorted[i]) {
 //            sorted[++cntv] = sorted[i];
 //        }
 //    }
-//    for (int i = 1; i <= n; i++) {
+//    for (int i = 0; i <= n; i++) {
 //        arr[i] = kth(arr[i]);
 //    }
 //    blen = (int)sqrt(n);
@@ -151,11 +157,10 @@ package class177;
 //int main() {
 //    ios::sync_with_stdio(false);
 //    cin.tie(nullptr);
-//    cin >> n;
+//    cin >> n >> m;
 //    for (int i = 1; i <= n; i++) {
 //        cin >> arr[i];
 //    }
-//    cin >> m;
 //    for (int i = 1; i <= m; i++) {
 //        cin >> query[i].l >> query[i].r;
 //        query[i].id = i;
