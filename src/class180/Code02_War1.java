c4f4fb4a43419807e2bdfd5e65ba1555a1869d4c
algorithmzuo <algorithmzuo@163.com>
2025-10-06 09:48:15 +0800

    modify code

--- src/class180/Code02_War1.java
+++ src/class180/Code02_War1.java
@@ -54,7 +54,6 @@ public class Code02_War1 {
 		headv[u] = cntv;
 	}
 
-	// nums中的数，根据dfn的大小排序，手撸双指针快排
 	public static void sortByDfn(int[] nums, int l, int r) {
 		if (l >= r) return;
 		int i = l, j = r;
