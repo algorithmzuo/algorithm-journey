c6c191dcd0838a1240d38805c401295c06320b79
algorithmzuo <algorithmzuo@163.com>
2025-10-10 17:19:55 +0800

    modify code

---
+++ src/class180/Code06_TreelandAndViruses1.java
@@ -0,0 +1,280 @@
+package class180;
+
+// 树上病毒传播，java版
+// 测试链接 : https://www.luogu.com.cn/problem/CF1320E
+// 测试链接 : https://codeforces.com/problemset/problem/1320/E
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.PriorityQueue;
+
+public class Code06_TreelandAndViruses1 {
+
+	public static int MAXN = 200001;
+	public static int MAXP = 20;
+	public static int n, q, k, m;
+
+	public static int[] headg = new int[MAXN];
+	public static int[] nextg = new int[MAXN << 1];
+	public static int[] tog = new int[MAXN << 1];
+	public static int cntg;
+
+	public static int[] headv = new int[MAXN];
+	public static int[] nextv = new int[MAXN << 1];
+	public static int[] tov = new int[MAXN << 1];
+	public static int cntv;
+
+	public static int[] dep = new int[MAXN];
+	public static int[] dfn = new int[MAXN];
+	public static int[][] stjump = new int[MAXN][MAXP];
+	public static int cntd;
+
+	public static int[] start = new int[MAXN];
+	public static int[] speed = new int[MAXN];
+	public static int[] order = new int[MAXN];
+	public static int[] key = new int[MAXN];
+
+	public static int[] arr = new int[MAXN << 1];
+	public static int[] tmp = new int[MAXN << 2];
+	public static int unique;
+
+	// dijkstra算法需要
+	public static boolean[] vis = new boolean[MAXN];
+	public static int[] bestDist = new int[MAXN];
+	public static int[] bestTime = new int[MAXN];
+	public static int[] bestPre = new int[MAXN];
+
+	public static void addEdgeG(int u, int v) {
+		nextg[++cntg] = headg[u];
+		tog[cntg] = v;
+		headg[u] = cntg;
+	}
+
+	public static void addEdgeV(int u, int v) {
+		nextv[++cntv] = headv[u];
+		tov[cntv] = v;
+		headv[u] = cntv;
+	}
+
+	public static void sortByDfn(int[] nums, int l, int r) {
+		if (l >= r) return;
+		int i = l, j = r;
+		int pivot = nums[(l + r) >> 1];
+		while (i <= j) {
+			while (dfn[nums[i]] < dfn[pivot]) i++;
+			while (dfn[nums[j]] > dfn[pivot]) j--;
+			if (i <= j) {
+				int tmp = nums[i]; nums[i] = nums[j]; nums[j] = tmp;
+				i++; j--;
+			}
+		}
+		sortByDfn(nums, l, j);
+		sortByDfn(nums, i, r);
+	}
+
+	public static void dfs(int u, int fa) {
+		dep[u] = dep[fa] + 1;
+		dfn[u] = ++cntd;
+		stjump[u][0] = fa;
+		for (int p = 1; p < MAXP; p++) {
+			stjump[u][p] = stjump[stjump[u][p - 1]][p - 1];
+		}
+		for (int e = headg[u]; e > 0; e = nextg[e]) {
+			if (tog[e] != fa) {
+				dfs(tog[e], u);
+			}
+		}
+	}
+
+	public static int getLca(int a, int b) {
+		if (dep[a] < dep[b]) {
+			int tmp = a; a = b; b = tmp;
+		}
+		for (int p = MAXP - 1; p >= 0; p--) {
+			if (dep[stjump[a][p]] >= dep[b]) {
+				a = stjump[a][p];
+			}
+		}
+		if (a == b) {
+			return a;
+		}
+		for (int p = MAXP - 1; p >= 0; p--) {
+			if (stjump[a][p] != stjump[b][p]) {
+				a = stjump[a][p];
+				b = stjump[b][p];
+			}
+		}
+		return stjump[a][0];
+	}
+
+	public static int buildVirtualTree() {
+		int tot = 0;
+		for (int i = 1; i <= k; i++) {
+			arr[++tot] = start[i];
+		}
+		for (int i = 1; i <= m; i++) {
+			arr[++tot] = key[i];
+		}
+		sortByDfn(arr, 1, tot);
+		int len = 0;
+		for (int i = 1; i < tot; i++) {
+			tmp[++len] = arr[i];
+			tmp[++len] = getLca(arr[i], arr[i + 1]);
+		}
+		tmp[++len] = arr[tot];
+		sortByDfn(tmp, 1, len);
+		unique = 1;
+		for (int i = 2; i <= len; i++) {
+			if (tmp[unique] != tmp[i]) {
+				tmp[++unique] = tmp[i];
+			}
+		}
+		cntv = 0;
+		for (int i = 1; i <= unique; i++) {
+			headv[tmp[i]] = 0;
+		}
+		for (int i = 1; i < unique; i++) {
+			// 这里要两个方向加边
+			// 因为病毒感染既可以向上也可以向下
+			// 所以跑dijkstra算法时需要无向图
+			int lca = getLca(tmp[i], tmp[i + 1]);
+			addEdgeV(lca, tmp[i + 1]);
+			addEdgeV(tmp[i + 1], lca);
+		}
+		return tmp[1];
+	}
+
+	static class Node {
+		int id, dist, time, pre;
+
+		Node(int id_, int dist_, int time_, int pre_) {
+			id = id_;
+			dist = dist_;
+			time = time_;
+			pre = pre_;
+		}
+	}
+
+	public static int getTime(int dist, int pre) {
+		return (dist + speed[pre] - 1) / speed[pre];
+	}
+
+	public static void dijkstra() {
+		PriorityQueue<Node> heap = new PriorityQueue<>((a, b) -> {
+			if (a.time != b.time) {
+				return a.time - b.time;
+			}
+			return order[a.pre] - order[b.pre];
+		});
+		for (int i = 1; i <= unique; i++) {
+			int u = tmp[i];
+			bestDist[u] = n + 1;
+			bestTime[u] = n + 1;
+			bestPre[u] = 0;
+			vis[u] = false;
+		}
+		for (int i = 1; i <= k; i++) {
+			int s = start[i];
+			bestDist[s] = 0;
+			bestPre[s] = s;
+			bestTime[s] = 0;
+			heap.add(new Node(s, 0, 0, s));
+		}
+		while (!heap.isEmpty()) {
+			Node cur = heap.poll();
+			int u = cur.id;
+			if (!vis[u]) {
+				vis[u] = true;
+				for (int e = headv[u]; e > 0; e = nextv[e]) {
+					int v = tov[e];
+					int dist = cur.dist + Math.abs(dep[u] - dep[v]);
+					int pre = cur.pre;
+					int time = getTime(dist, pre);
+					if (!vis[v] && (time < bestTime[v] || (time == bestTime[v] && order[pre] < order[bestPre[v]]))) {
+						bestTime[v] = time;
+						bestDist[v] = dist;
+						bestPre[v] = pre;
+						heap.add(new Node(v, dist, time, pre));
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		for (int i = 1; i < n; i++) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+			addEdgeG(x, y);
+			addEdgeG(y, x);
+		}
+		dfs(1, 0);
+		q = in.nextInt();
+		for (int t = 1; t <= q; t++) {
+			k = in.nextInt();
+			m = in.nextInt();
+			for (int i = 1; i <= k; i++) {
+				start[i] = in.nextInt();
+				speed[start[i]] = in.nextInt();
+				order[start[i]] = i;
+			}
+			for (int i = 1; i <= m; i++) {
+				key[i] = in.nextInt();
+			}
+			buildVirtualTree();
+			dijkstra();
+			for (int i = 1; i <= m; i++) {
+				out.print(order[bestPre[key[i]]] + " ");
+			}
+			out.println();
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
+
+}
