277fb42afc926cc5ebbfd78c87ecb2bf09a5af32
algorithmzuo <algorithmzuo@163.com>
2025-11-18 13:29:44 +0800

    modify code

--- src/class183/Code05_CloseVertices1.java
+++ src/class183/Code05_CloseVertices1.java
@@ -3,12 +3,10 @@ package class183;
 // 相近点对的数量，java版
 // 一共有n个节点，所有节点组成一棵树，1号节点是树头
 // 从2号点开始，给定每个点的父节点编号、与父节点之间无向边的边权
-// 给定两个整数limitl、limitw，如下为(a, b)是相近点对的定义
-// 首先a < b，其次两者简单路径的边数不超过limitl、权值和不超过limitw
-// 打印树上有多少相近点对
-// 1 <= limitl <= n <= 10^5
-// 0 <= limitw <= 10^9
-// 0 <= 边权 <= 10^4
+// 给定两个整数limitl、limitw，要求两点之间的简单路径满足如下关系
+// 路径权值和 <= limitw、路径边数 <= limitl，求出这样的点对数量
+// 本题规定(x, x)不是点对，(x, y)和(y, x)认为是同一个点对
+// 1 <= limitl <= n <= 10^5    0 <= limitw <= 10^9    0 <= 边权 <= 10^4
 // 测试链接 : https://www.luogu.com.cn/problem/CF293E
 // 测试链接 : https://codeforces.com/problemset/problem/293/E
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
@@ -33,7 +31,7 @@ public class Code05_CloseVertices1 {
 	public static int[] siz = new int[MAXN];
 
 	public static int[] disArr = new int[MAXN];
-	public static int[] depArr = new int[MAXN];
+	public static int[] edgeArr = new int[MAXN];
 	public static int cnta;
 
 	public static int[] tree = new int[MAXN];
@@ -46,7 +44,7 @@ public class Code05_CloseVertices1 {
 			while (disArr[j] > pivot) j--;
 			if (i <= j) {
 				tmp = disArr[i]; disArr[i] = disArr[j]; disArr[j] = tmp;
-				tmp = depArr[i]; depArr[i] = depArr[j]; depArr[j] = tmp;
+				tmp = edgeArr[i]; edgeArr[i] = edgeArr[j]; edgeArr[j] = tmp;
 				i++; j--;
 			}
 		}
@@ -66,6 +64,9 @@ public class Code05_CloseVertices1 {
 	}
 
 	public static void add(int i, int v) {
+		// 边的数量最小是0，但是树状数组下标从1开始
+		// 所以i++，平移一下，然后使用树状数组
+		i++;
 		while (i <= limitl + 1) {
 			tree[i] += v;
 			i += lowbit(i);
@@ -73,6 +74,9 @@ public class Code05_CloseVertices1 {
 	}
 
 	public static int sum(int i) {
+		// 边的数量最小是0，但是树状数组下标从1开始
+		// 所以i++，平移一下，然后使用树状数组
+		i++;
 		int ret = 0;
 		while (i > 0) {
 			ret += tree[i];
@@ -111,38 +115,38 @@ public class Code05_CloseVertices1 {
 		return u;
 	}
 
-	public static void dfs(int u, int fa, int dis, int dep) {
-		if (dis > limitw || dep > limitl + 1) {
+	public static void dfs(int u, int fa, int dis, int edge) {
+		if (dis > limitw || edge > limitl) {
 			return;
 		}
 		disArr[++cnta] = dis;
-		depArr[cnta] = dep;
+		edgeArr[cnta] = edge;
 		for (int e = head[u]; e > 0; e = nxt[e]) {
 			int v = to[e];
 			if (v != fa && !vis[v]) {
-				dfs(v, u, dis + weight[e], dep + 1);
+				dfs(v, u, dis + weight[e], edge + 1);
 			}
 		}
 	}
 
-	// 从真正的头来到u时，路径权值和为dis，深度为dep
+	// 从真正的头来到u时，路径权值和为dis，边的数量为edge
 	// 子树u上的点对(x, y)，需要走过，x -> 真正的头 -> y
 	// 返回 距离<=limitw、边数<=limitwl 的点对数量
-	public static long calc(int u, int dis, int dep) {
+	public static long calc(int u, int dis, int edge) {
 		cnta = 0;
-		dfs(u, 0, dis, dep);
+		dfs(u, 0, dis, edge);
 		sort(1, cnta);
 		for (int i = 1; i <= cnta; i++) {
-			add(depArr[i], 1);
+			add(edgeArr[i], 1);
 		}
 		long ret = 0;
 		for (int l = 1, r = cnta; l <= r;) {
 			if (disArr[l] + disArr[r] <= limitw) {
-				add(depArr[l], -1);
-				ret += sum(limitl - depArr[l] + 2);
+				add(edgeArr[l], -1);
+				ret += sum(limitl - edgeArr[l]);
 				l++;
 			} else {
-				add(depArr[r], -1);
+				add(edgeArr[r], -1);
 				r--;
 			}
 		}
@@ -151,11 +155,11 @@ public class Code05_CloseVertices1 {
 
 	public static long solve(int u) {
 		vis[u] = true;
-		long ans = calc(u, 0, 1);
+		long ans = calc(u, 0, 0);
 		for (int e = head[u]; e > 0; e = nxt[e]) {
 			int v = to[e];
 			if (!vis[v]) {
-				ans -= calc(v, weight[e], 2);
+				ans -= calc(v, weight[e], 1);
 				ans += solve(getCentroid(v, u));
 			}
 		}
--- src/class183/Code05_CloseVertices2.java
+++ src/class183/Code05_CloseVertices2.java
@@ -3,12 +3,10 @@ package class183;
 // 相近点对的数量，C++版
 // 一共有n个节点，所有节点组成一棵树，1号节点是树头
 // 从2号点开始，给定每个点的父节点编号、与父节点之间无向边的边权
-// 给定两个整数limitl、limitw，如下为(a, b)是相近点对的定义
-// 首先a < b，其次两者简单路径的边数不超过limitl、权值和不超过limitw
-// 打印树上有多少相近点对
-// 1 <= limitl <= n <= 10^5
-// 0 <= limitw <= 10^9
-// 0 <= 边权 <= 10^4
+// 给定两个整数limitl、limitw，要求两点之间的简单路径满足如下关系
+// 路径权值和 <= limitw、路径边数 <= limitl，求出这样的点对数量
+// 本题规定(x, x)不是点对，(x, y)和(y, x)认为是同一个点对
+// 1 <= limitl <= n <= 10^5    0 <= limitw <= 10^9    0 <= 边权 <= 10^4
 // 测试链接 : https://www.luogu.com.cn/problem/CF293E
 // 测试链接 : https://codeforces.com/problemset/problem/293/E
 // 如下实现是C++的版本，C++版本和java版本逻辑完全一样
@@ -19,7 +17,7 @@ package class183;
 //using namespace std;
 //
 //struct Node {
-//    int dis, dep;
+//    int dis, edge;
 //};
 //
 //bool NodeCmp(Node a, Node b) {
@@ -55,6 +53,7 @@ package class183;
 //}
 //
 //void add(int i, int v) {
+//    i++;
 //    while (i <= limitl + 1) {
 //        tree[i] += v;
 //        i += lowbit(i);
@@ -62,6 +61,7 @@ package class183;
 //}
 //
 //int sum(int i) {
+//    i++;
 //    int ret = 0;
 //    while (i > 0) {
 //        ret += tree[i];
@@ -100,34 +100,34 @@ package class183;
 //    return u;
 //}
 //
-//void dfs(int u, int fa, int dis, int dep) {
-//    if (dis > limitw || dep > limitl + 1) {
+//void dfs(int u, int fa, int dis, int edge) {
+//    if (dis > limitw || edge > limitl) {
 //        return;
 //    }
-//    arr[++cnta] = { dis, dep };
+//    arr[++cnta] = { dis, edge };
 //    for (int e = head[u]; e; e = nxt[e]) {
 //        int v = to[e];
 //        if (v != fa && !vis[v]) {
-//            dfs(v, u, dis + weight[e], dep + 1);
+//            dfs(v, u, dis + weight[e], edge + 1);
 //        }
 //    }
 //}
 //
-//long long calc(int u, int dis, int dep) {
+//long long calc(int u, int dis, int edge) {
 //    cnta = 0;
-//    dfs(u, 0, dis, dep);
+//    dfs(u, 0, dis, edge);
 //    sort(arr + 1, arr + cnta + 1, NodeCmp);
 //    for (int i = 1; i <= cnta; i++) {
-//        add(arr[i].dep, 1);
+//        add(arr[i].edge, 1);
 //    }
 //    long long ret = 0;
 //    for (int l = 1, r = cnta; l <= r; ) {
 //        if (arr[l].dis + arr[r].dis <= limitw) {
-//            add(arr[l].dep, -1);
-//            ret += sum(limitl - arr[l].dep + 2);
+//            add(arr[l].edge, -1);
+//            ret += sum(limitl - arr[l].edge);
 //            l++;
 //        } else {
-//            add(arr[r].dep, -1);
+//            add(arr[r].edge, -1);
 //            r--;
 //        }
 //    }
@@ -136,11 +136,11 @@ package class183;
 //
 //long long solve(int u) {
 //    vis[u] = true;
-//    long long ans = calc(u, 0, 1);
+//    long long ans = calc(u, 0, 0);
 //    for (int e = head[u]; e; e = nxt[e]) {
 //        int v = to[e];
 //        if (!vis[v]) {
-//            ans -= calc(v, weight[e], 2);
+//            ans -= calc(v, weight[e], 1);
 //            ans += solve(getCentroid(v, u));
 //        }
 //    }
