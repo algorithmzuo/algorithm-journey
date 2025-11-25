da83d16d15f861ed321bd24b68c5e5a534144bea
algorithmzuo <algorithmzuo@163.com>
2025-11-25 11:03:20 +0800

    modify code

--- src/class184/Code03_Courier1.java
+++ src/class184/Code03_Courier1.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-public class Code04_Courier1 {
+public class Code03_Courier1 {
 
 	public static int MAXN = 100001;
 	public static int n, m;
