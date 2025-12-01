ff65e815db9c783900e2e1782a9a8830924dbaf0
algorithmzuo <algorithmzuo@163.com>
2025-12-01 16:33:02 +0800

    modify code

--- src/class185/Code01_Game1.java
+++ src/class185/Code01_Game1.java
@@ -4,6 +4,360 @@ package class185;
 // 测试链接 : https://www.luogu.com.cn/problem/P10603
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 public class Code01_Game1 {
+
+	public static int MAXN = 100001;
+	public static int MAXH = 18;
+	public static int MAXT = 20000001;
+	public static int n, m;
+
+	public static int[] head = new int[MAXN];
+	public static int[] nxt = new int[MAXN << 1];
+	public static int[] to = new int[MAXN << 1];
+	public static int cntg;
+
+	public static int[] dep = new int[MAXN];
+	public static int[][] stjump = new int[MAXN][MAXH];
+
+	public static boolean[] vis = new boolean[MAXN];
+	public static int[] siz = new int[MAXN];
+	public static int[] centfa = new int[MAXN];
+
+	public static int[] tree1 = new int[MAXN];
+	public static int[] tree2 = new int[MAXN];
+	public static int[] ls = new int[MAXT];
+	public static int[] rs = new int[MAXT];
+	public static int[] sum = new int[MAXT];
+	public static int cntt;
+
+	// 讲解118，递归函数改成迭代所需要的栈
+	public static int[][] stack = new int[MAXN][3];
+	public static int u, f, e;
+	public static int stacksize;
+
+	public static void push(int u, int f, int e) {
+		stack[stacksize][0] = u;
+		stack[stacksize][1] = f;
+		stack[stacksize][2] = e;
+		stacksize++;
+	}
+
+	public static void pop() {
+		--stacksize;
+		u = stack[stacksize][0];
+		f = stack[stacksize][1];
+		e = stack[stacksize][2];
+	}
+
+	public static void addEdge(int u, int v) {
+		nxt[++cntg] = head[u];
+		to[cntg] = v;
+		head[u] = cntg;
+	}
+
+	public static void dfs1(int u, int fa) {
+		dep[u] = dep[fa] + 1;
+		stjump[u][0] = fa;
+		for (int p = 1; p < MAXH; p++) {
+			stjump[u][p] = stjump[stjump[u][p - 1]][p - 1];
+		}
+		for (int e = head[u]; e > 0; e = nxt[e]) {
+			int v = to[e];
+			if (v != fa) {
+				dfs1(v, u);
+			}
+		}
+	}
+
+	// dfs1的迭代版
+	public static void dfs2(int cur, int fa) {
+		stacksize = 0;
+		push(cur, fa, -1);
+		while (stacksize > 0) {
+			pop();
+			if (e == -1) {
+				dep[u] = dep[f] + 1;
+				stjump[u][0] = f;
+				for (int p = 1; p < MAXH; p++) {
+					stjump[u][p] = stjump[stjump[u][p - 1]][p - 1];
+				}
+				e = head[u];
+			} else {
+				e = nxt[e];
+			}
+			if (e != 0) {
+				push(u, f, e);
+				int v = to[e];
+				if (v != f) {
+					push(v, u, -1);
+				}
+			}
+		}
+	}
+
+	// 找重心需要计算子树大小的递归版，java会爆栈，C++不会
+	public static void getSize1(int u, int fa) {
+		siz[u] = 1;
+		for (int e = head[u]; e > 0; e = nxt[e]) {
+			int v = to[e];
+			if (v != fa && !vis[v]) {
+				getSize1(v, u);
+				siz[u] += siz[v];
+			}
+		}
+	}
+
+	// getSize1的迭代版
+	public static void getSize2(int cur, int fa) {
+		stacksize = 0;
+		push(cur, fa, -1);
+		while (stacksize > 0) {
+			pop();
+			if (e == -1) {
+				siz[u] = 1;
+				e = head[u];
+			} else {
+				e = nxt[e];
+			}
+			if (e != 0) {
+				push(u, f, e);
+				int v = to[e];
+				if (v != f && !vis[v]) {
+					push(v, u, -1);
+				}
+			} else {
+				for (int ei = head[u]; ei > 0; ei = nxt[ei]) {
+					int v = to[ei];
+					if (v != f && !vis[v]) {
+						siz[u] += siz[v];
+					}
+				}
+			}
+		}
+	}
+
+	public static int getLca(int a, int b) {
+		if (dep[a] < dep[b]) {
+			int tmp = a;
+			a = b;
+			b = tmp;
+		}
+		for (int p = MAXH - 1; p >= 0; p--) {
+			if (dep[stjump[a][p]] >= dep[b]) {
+				a = stjump[a][p];
+			}
+		}
+		if (a == b) {
+			return a;
+		}
+		for (int p = MAXH - 1; p >= 0; p--) {
+			if (stjump[a][p] != stjump[b][p]) {
+				a = stjump[a][p];
+				b = stjump[b][p];
+			}
+		}
+		return stjump[a][0];
+	}
+
+	public static int getDist(int x, int y) {
+		return dep[x] + dep[y] - (dep[getLca(x, y)] << 1);
+	}
+
+	public static int getCentroid(int u, int fa) {
+		// getSize1(u, fa);
+		getSize2(u, fa);
+		int half = siz[u] >> 1;
+		boolean find = false;
+		while (!find) {
+			find = true;
+			for (int e = head[u]; e > 0; e = nxt[e]) {
+				int v = to[e];
+				if (v != fa && !vis[v] && siz[v] > half) {
+					fa = u;
+					u = v;
+					find = false;
+					break;
+				}
+			}
+		}
+		return u;
+	}
+
+	public static void centroidTree(int u, int fa) {
+		centfa[u] = fa;
+		vis[u] = true;
+		for (int e = head[u]; e > 0; e = nxt[e]) {
+			int v = to[e];
+			if (!vis[v]) {
+				centroidTree(getCentroid(v, u), u);
+			}
+		}
+	}
+
+	public static int update(int jobi, int jobv, int l, int r, int i) {
+		int rt = i;
+		if (rt == 0) {
+			rt = ++cntt;
+		}
+		if (l == r) {
+			sum[rt] += jobv;
+		} else {
+			int mid = (l + r) >> 1;
+			if (jobi <= mid) {
+				ls[rt] = update(jobi, jobv, l, mid, ls[rt]);
+			} else {
+				rs[rt] = update(jobi, jobv, mid + 1, r, rs[rt]);
+			}
+			sum[rt] = sum[ls[rt]] + sum[rs[rt]];
+		}
+		return rt;
+	}
+
+	public static int query(int jobl, int jobr, int l, int r, int i) {
+		if (i == 0) {
+			return 0;
+		}
+		if (jobl <= l && r <= jobr) {
+			return sum[i];
+		}
+		int mid = (l + r) >> 1;
+		int ans = 0;
+		if (jobl <= mid) {
+			ans += query(jobl, jobr, l, mid, ls[i]);
+		}
+		if (jobr > mid) {
+			ans += query(jobl, jobr, mid + 1, r, rs[i]);
+		}
+		return ans;
+	}
+
+	public static void update(int x, int k, int v) {
+		int cur = x, pre = 0, dist;
+		while (cur > 0) {
+			dist = getDist(cur, x);
+			if (k - dist >= 0) {
+				tree1[cur] = update(k - dist, v, 0, n - 1, tree1[cur]);
+				if (pre > 0) {
+					tree2[pre] = update(k - dist, v, 0, n - 1, tree2[pre]);
+				}
+			}
+			pre = cur;
+			cur = centfa[cur];
+		}
+	}
+
+	public static int query(int x) {
+		int ans = 0;
+		int cur = x;
+		while (cur > 0) {
+			ans += query(getDist(cur, x), n - 1, 0, n - 1, tree1[cur]);
+			if (centfa[cur] > 0) {
+				ans -= query(getDist(centfa[cur], x), n - 1, 0, n - 1, tree2[cur]);
+			}
+			cur = centfa[cur];
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader();
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1, u, v; i < n; i++) {
+			u = in.nextInt();
+			v = in.nextInt();
+			addEdge(u, v);
+			addEdge(v, u);
+		}
+		// dfs1(1, 0);
+		dfs2(1, 0);
+		centroidTree(getCentroid(1, 0), 0);
+		char op;
+		int x, k, v;
+		for (int i = 1; i <= m; i++) {
+			op = in.nextChar();
+			if (op == 'M') {
+				x = in.nextInt();
+				k = in.nextInt();
+				v = in.nextInt();
+				update(x, k, v);
+			} else {
+				x = in.nextInt();
+				out.println(query(x));
+			}
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private final InputStream in;
+		private final byte[] buffer;
+		private int ptr, len;
+
+		public FastReader() {
+			in = System.in;
+			buffer = new byte[BUFFER_SIZE];
+			ptr = len = 0;
+		}
+
+		private boolean hasNextByte() throws IOException {
+			if (ptr < len)
+				return true;
+			ptr = 0;
+			len = in.read(buffer);
+			return len > 0;
+		}
+
+		private byte readByte() throws IOException {
+			if (!hasNextByte())
+				return -1;
+			return buffer[ptr++];
+		}
+
+		public char nextChar() throws IOException {
+			byte c;
+			do {
+				c = readByte();
+				if (c == -1)
+					return 0;
+			} while (c <= ' ');
+			char ans = 0;
+			while (c > ' ') {
+				ans = (char) c;
+				c = readByte();
+			}
+			return ans;
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+			byte b = readByte();
+			while (isWhitespace(b))
+				b = readByte();
+			boolean minus = false;
+			if (b == '-') {
+				minus = true;
+				b = readByte();
+			}
+			while (!isWhitespace(b) && b != -1) {
+				num = num * 10 + (b - '0');
+				b = readByte();
+			}
+			return minus ? -num : num;
+		}
+
+		private boolean isWhitespace(byte b) {
+			return b == ' ' || b == '\n' || b == '\r' || b == '\t';
+		}
+	}
 
 }
---
+++ src/class185/Code01_Game2.java
@@ -0,0 +1,211 @@
+package class185;
+
+// 烁烁的游戏，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P10603
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//const int MAXN = 100001;
+//const int MAXH = 18;
+//const int MAXT = 20000001;
+//int n, m;
+//
+//int head[MAXN];
+//int nxt[MAXN << 1];
+//int to[MAXN << 1];
+//int cntg;
+//
+//int dep[MAXN];
+//int stjump[MAXN][MAXH];
+//
+//bool vis[MAXN];
+//int siz[MAXN];
+//int centfa[MAXN];
+//
+//int tree1[MAXN];
+//int tree2[MAXN];
+//int ls[MAXT];
+//int rs[MAXT];
+//int sum[MAXT];
+//int cntt;
+//
+//void addEdge(int u, int v) {
+//    nxt[++cntg] = head[u];
+//    to[cntg] = v;
+//    head[u] = cntg;
+//}
+//
+//void dfs(int u, int fa) {
+//    dep[u] = dep[fa] + 1;
+//    stjump[u][0] = fa;
+//    for (int p = 1; p < MAXH; p++) {
+//        stjump[u][p] = stjump[stjump[u][p - 1]][p - 1];
+//    }
+//    for (int e = head[u]; e; e = nxt[e]) {
+//        int v = to[e];
+//        if (v != fa) {
+//            dfs(v, u);
+//        }
+//    }
+//}
+//
+//void getSize(int u, int fa) {
+//    siz[u] = 1;
+//    for (int e = head[u]; e; e = nxt[e]) {
+//        int v = to[e];
+//        if (v != fa && !vis[v]) {
+//            getSize(v, u);
+//            siz[u] += siz[v];
+//        }
+//    }
+//}
+//
+//int getLca(int a, int b) {
+//    if (dep[a] < dep[b]) {
+//        swap(a, b);
+//    }
+//    for (int p = MAXH - 1; p >= 0; p--) {
+//        if (dep[stjump[a][p]] >= dep[b]) {
+//            a = stjump[a][p];
+//        }
+//    }
+//    if (a == b) {
+//        return a;
+//    }
+//    for (int p = MAXH - 1; p >= 0; p--) {
+//        if (stjump[a][p] != stjump[b][p]) {
+//            a = stjump[a][p];
+//            b = stjump[b][p];
+//        }
+//    }
+//    return stjump[a][0];
+//}
+//
+//int getDist(int x, int y) {
+//    return dep[x] + dep[y] - (dep[getLca(x, y)] << 1);
+//}
+//
+//int getCentroid(int u, int fa) {
+//    getSize(u, fa);
+//    int half = siz[u] >> 1;
+//    bool find = false;
+//    while (!find) {
+//        find = true;
+//        for (int e = head[u]; e; e = nxt[e]) {
+//            int v = to[e];
+//            if (v != fa && !vis[v] && siz[v] > half) {
+//                fa = u;
+//                u = v;
+//                find = false;
+//                break;
+//            }
+//        }
+//    }
+//    return u;
+//}
+//
+//void centroidTree(int u, int fa) {
+//    centfa[u] = fa;
+//    vis[u] = true;
+//    for (int e = head[u]; e; e = nxt[e]) {
+//        int v = to[e];
+//        if (!vis[v]) {
+//            centroidTree(getCentroid(v, u), u);
+//        }
+//    }
+//}
+//
+//int update(int jobi, int jobv, int l, int r, int i) {
+//    int rt = i;
+//    if (rt == 0) {
+//        rt = ++cntt;
+//    }
+//    if (l == r) {
+//        sum[rt] += jobv;
+//    } else {
+//        int mid = (l + r) >> 1;
+//        if (jobi <= mid) {
+//            ls[rt] = update(jobi, jobv, l, mid, ls[rt]);
+//        } else {
+//            rs[rt] = update(jobi, jobv, mid + 1, r, rs[rt]);
+//        }
+//        sum[rt] = sum[ls[rt]] + sum[rs[rt]];
+//    }
+//    return rt;
+//}
+//
+//int query(int jobl, int jobr, int l, int r, int i) {
+//    if (i == 0) {
+//        return 0;
+//    }
+//    if (jobl <= l && r <= jobr) {
+//        return sum[i];
+//    }
+//    int mid = (l + r) >> 1;
+//    int ans = 0;
+//    if (jobl <= mid) {
+//        ans += query(jobl, jobr, l, mid, ls[i]);
+//    }
+//    if (jobr > mid) {
+//        ans += query(jobl, jobr, mid + 1, r, rs[i]);
+//    }
+//    return ans;
+//}
+//
+//void update(int x, int k, int v) {
+//    int cur = x, pre = 0, dist;
+//    while (cur > 0) {
+//        dist = getDist(cur, x);
+//        if (k - dist >= 0) {
+//            tree1[cur] = update(k - dist, v, 0, n - 1, tree1[cur]);
+//            if (pre > 0) {
+//                tree2[pre] = update(k - dist, v, 0, n - 1, tree2[pre]);
+//            }
+//        }
+//        pre = cur;
+//        cur = centfa[cur];
+//    }
+//}
+//
+//int query(int x) {
+//    int ans = 0;
+//    int cur = x;
+//    while (cur > 0) {
+//        ans += query(getDist(cur, x), n - 1, 0, n - 1, tree1[cur]);
+//        if (centfa[cur] > 0) {
+//            ans -= query(getDist(centfa[cur], x), n - 1, 0, n - 1, tree2[cur]);
+//        }
+//        cur = centfa[cur];
+//    }
+//    return ans;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1, u, v; i < n; i++) {
+//        cin >> u >> v;
+//        addEdge(u, v);
+//        addEdge(v, u);
+//    }
+//    dfs(1, 0);
+//    centroidTree(getCentroid(1, 0), 0);
+//    char op;
+//    int x, k, v;
+//    for (int i = 1; i <= m; i++) {
+//        cin >> op;
+//        if (op == 'M') {
+//            cin >> x >> k >> v;
+//            update(x, k, v);
+//        } else {
+//            cin >> x;
+//            cout << query(x) << '\n';
+//        }
+//    }
+//    return 0;
+//}
