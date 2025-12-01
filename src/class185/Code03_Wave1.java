7c1cf8023ae20e3bd685b2e04d3f0c4ef6d6d881
algorithmzuo <algorithmzuo@163.com>
2025-12-01 15:42:18 +0800

    modify code

--- src/class185/Code03_Wave1.java
+++ src/class185/Code03_Wave1.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-public class Code01_Wave1 {
+public class Code03_Wave1 {
 
 	public static int MAXN = 100001;
 	public static int MAXH = 18;
