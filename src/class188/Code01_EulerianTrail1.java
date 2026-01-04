2361fd43dfafbbd863d393bcde49b57a56eb580f
algorithmzuo <algorithmzuo@163.com>
2026-01-04 14:39:16 +0800

    modify code

---
+++ src/class188/Code01_EulerianTrail1.java
@@ -0,0 +1,193 @@
+package class188;
+
+// 欧拉路径，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P7771
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Code01_EulerianTrail1 {
+
+	public static class EdgeCmp implements Comparator<int[]> {
+		@Override
+		public int compare(int[] e1, int[] e2) {
+			return e1[0] != e2[0] ? (e1[0] - e2[0]) : (e1[1] - e2[1]);
+		}
+	}
+
+	public static int MAXN = 100001;
+	public static int MAXM = 200002;
+	public static int n, m;
+	public static int[][] edgeArr = new int[MAXM][2];
+
+	public static int[] head = new int[MAXN];
+	public static int[] nxt = new int[MAXM];
+	public static int[] to = new int[MAXM];
+	public static int cnt;
+
+	public static int[] cur = new int[MAXN];
+	public static int[] outDeg = new int[MAXN];
+	public static int[] inDeg = new int[MAXN];
+
+	public static int[] trail = new int[MAXM];
+	public static int top;
+
+	public static int[] stack = new int[MAXM];
+	public static int u;
+	public static int stacksize;
+
+	public static void push(int u) {
+		stack[stacksize++] = u;
+	}
+
+	public static void pop() {
+		u = stack[--stacksize];
+	}
+
+	public static void addEdge(int u, int v) {
+		nxt[++cnt] = head[u];
+		to[cnt] = v;
+		head[u] = cnt;
+	}
+
+	public static void connect() {
+		Arrays.sort(edgeArr, 1, m + 1, new EdgeCmp());
+		for (int l = 1, r = 1; l <= m; l = ++r) {
+			while (r + 1 <= m && edgeArr[l][0] == edgeArr[r + 1][0]) {
+				r++;
+			}
+			for (int i = r, u, v; i >= l; i--) {
+				u = edgeArr[i][0];
+				v = edgeArr[i][1];
+				outDeg[u]++;
+				inDeg[v]++;
+				addEdge(u, v);
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			cur[i] = head[i];
+		}
+	}
+
+	public static int findStart() {
+		int s = 0;
+		for (int i = 1; i <= n; i++) {
+			if (Math.abs(outDeg[i] - inDeg[i]) > 1) {
+				return -1;
+			}
+			if (outDeg[i] > inDeg[i]) {
+				if (s != 0) {
+					return -1;
+				}
+				s = i;
+			}
+		}
+		if (s > 0) {
+			return s;
+		}
+		for (int i = 1; i <= n; i++) {
+			if (outDeg[i] > 0) {
+				return i;
+			}
+		}
+		return 1;
+	}
+
+	public static void dfs1(int u) {
+		for (int e = cur[u]; e > 0; e = cur[u]) {
+			cur[u] = nxt[e];
+			dfs1(to[e]);
+		}
+		trail[++top] = u;
+	}
+
+	public static void dfs2(int node) {
+		stacksize = 0;
+		push(node);
+		while (stacksize > 0) {
+			pop();
+			int e = cur[u];
+			if (e != 0) {
+				cur[u] = nxt[e];
+				push(u);
+				push(to[e]);
+			} else {
+				trail[++top] = u;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= m; i++) {
+			edgeArr[i][0] = in.nextInt();
+			edgeArr[i][1] = in.nextInt();
+		}
+		connect();
+		int start = findStart();
+		if (start == -1) {
+			out.println("No");
+		} else {
+			// dfs1(start);
+			dfs2(start);
+			if (top != m + 1) {
+				out.println("No");
+			} else {
+				for (int i = top; i >= 1; i--) {
+					out.print(trail[i] + " ");
+				}
+				out.println();
+			}
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
+
+}
---
+++ src/class188/Code01_EulerianTrail2.java
@@ -0,0 +1,120 @@
+package class188;
+
+// 欧拉路径，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P7771
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct Edge {
+//    int u, v;
+//};
+//
+//bool EdgeCmp(Edge e1, Edge e2) {
+//    if (e1.u != e2.u) {
+//        return e1.u < e2.u;
+//    }
+//    return e1.v < e2.v;
+//}
+//
+//const int MAXN = 100001;
+//const int MAXM = 200002;
+//int n, m;
+//Edge edgeArr[MAXM];
+//
+//int head[MAXN];
+//int nxt[MAXM];
+//int to[MAXM];
+//int cnt;
+//
+//int cur[MAXN];
+//int outDeg[MAXN];
+//int inDeg[MAXN];
+//
+//int trail[MAXM];
+//int top;
+//
+//void addEdge(int u, int v) {
+//    nxt[++cnt] = head[u];
+//    to[cnt] = v;
+//    head[u] = cnt;
+//}
+//
+//void connect() {
+//    sort(edgeArr + 1, edgeArr + m + 1, EdgeCmp);
+//    for (int l = 1, r = 1; l <= m; l = ++r) {
+//        while (r + 1 <= m && edgeArr[l].u == edgeArr[r + 1].u) {
+//            r++;
+//        }
+//        for (int i = r, u, v; i >= l; i--) {
+//            u = edgeArr[i].u;
+//            v = edgeArr[i].v;
+//            outDeg[u]++;
+//            inDeg[v]++;
+//            addEdge(u, v);
+//        }
+//    }
+//    for (int i = 1; i <= n; i++) {
+//        cur[i] = head[i];
+//    }
+//}
+//
+//int findStart() {
+//    int s = 0;
+//    for (int i = 1; i <= n; i++) {
+//        if (abs(outDeg[i] - inDeg[i]) > 1) {
+//            return -1;
+//        }
+//        if (outDeg[i] > inDeg[i]) {
+//            if (s != 0) {
+//                return -1;
+//            }
+//            s = i;
+//        }
+//    }
+//    if (s > 0) {
+//        return s;
+//    }
+//    for (int i = 1; i <= n; i++) {
+//        if (outDeg[i] > 0) {
+//            return i;
+//        }
+//    }
+//    return 1;
+//}
+//
+//void dfs(int u) {
+//    for (int e = cur[u]; e > 0; e = cur[u]) {
+//        cur[u] = nxt[e];
+//        dfs(to[e]);
+//    }
+//    trail[++top] = u;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    for (int i = 1; i <= m; i++) {
+//        cin >> edgeArr[i].u >> edgeArr[i].v;
+//    }
+//    connect();
+//    int start = findStart();
+//    if (start == -1) {
+//        cout << "No\n";
+//    } else {
+//        dfs(start);
+//        if (top != m + 1) {
+//            cout << "No\n";
+//        } else {
+//            for (int i = top; i >= 1; i--) {
+//                cout << trail[i] << " ";
+//            }
+//            cout << "\n";
+//        }
+//    }
+//    return 0;
+//}
