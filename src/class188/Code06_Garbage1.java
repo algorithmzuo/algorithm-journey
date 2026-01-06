8a430dfddaefa07a815dd525470c38ca5aa5108a
algorithmzuo <algorithmzuo@163.com>
2026-01-06 15:07:02 +0800

    modify code

---
+++ src/class188/Code06_Garbage1.java
@@ -0,0 +1,210 @@
+package class188;
+
+// 垃圾车，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P3520
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class Code06_Garbage1 {
+
+	public static int MAXN = 100001;
+	public static int MAXM = 2000001;
+	public static int n, m;
+
+	public static int[] head = new int[MAXN];
+	public static int[] nxt = new int[MAXM];
+	public static int[] to = new int[MAXM];
+	public static int[] eid = new int[MAXM];
+	public static int cntg;
+
+	public static int[] deg = new int[MAXN];
+	public static int[] cur = new int[MAXN];
+	public static boolean[] enter = new boolean[MAXN];
+	public static boolean[] visNode = new boolean[MAXN];
+	public static boolean[] visEdge = new boolean[MAXM];
+
+	public static int[] path = new int[MAXM];
+	public static int cntp;
+
+	public static int[] arr = new int[MAXM];
+	public static int idx;
+
+	public static int[] ansl = new int[MAXM];
+	public static int[] ansr = new int[MAXM];
+	public static int cnta;
+
+	// 讲解118，递归函数改成迭代所需要的栈
+	public static int[] stack = new int[MAXM];
+	public static int u;
+	public static int stacksize;
+
+	public static void push(int u) {
+		stack[stacksize++] = u;
+	}
+
+	public static void pop() {
+		u = stack[--stacksize];
+	}
+
+	public static void addEdge(int u, int v, int id) {
+		nxt[++cntg] = head[u];
+		to[cntg] = v;
+		eid[cntg] = id;
+		head[u] = cntg;
+	}
+
+	public static void euler1(int u) {
+		visNode[u] = true;
+		for (int e = cur[u]; e > 0; e = cur[u]) {
+			cur[u] = nxt[e];
+			if (!visEdge[eid[e]]) {
+				visEdge[eid[e]] = true;
+				euler1(to[e]);
+			}
+		}
+		if (!enter[u]) {
+			enter[u] = true;
+			path[++cntp] = u;
+		} else {
+			cnta++;
+			arr[++idx] = u;
+			ansl[cnta] = ansr[cnta] = idx;
+			while (cntp > 0 && path[cntp] != u) {
+				enter[path[cntp]] = false;
+				arr[++idx] = path[cntp--];
+				ansr[cnta] = idx;
+			}
+			arr[++idx] = u;
+			ansr[cnta] = idx;
+		}
+	}
+
+	public static void euler2(int node) {
+		stacksize = 0;
+		push(node);
+		while (stacksize > 0) {
+			pop();
+			visNode[u] = true;
+			int e = cur[u];
+			if (e != 0) {
+				cur[u] = nxt[e];
+				if (!visEdge[eid[e]]) {
+					visEdge[eid[e]] = true;
+					push(u);
+					push(to[e]);
+				} else {
+					push(u);
+				}
+			} else {
+				if (!enter[u]) {
+					enter[u] = true;
+					path[++cntp] = u;
+				} else {
+					cnta++;
+					arr[++idx] = u;
+					ansl[cnta] = ansr[cnta] = idx;
+					while (cntp > 0 && path[cntp] != u) {
+						enter[path[cntp]] = false;
+						arr[++idx] = path[cntp--];
+						ansr[cnta] = idx;
+					}
+					arr[++idx] = u;
+					ansr[cnta] = idx;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1, u, v, s, t; i <= m; i++) {
+			u = in.nextInt();
+			v = in.nextInt();
+			s = in.nextInt();
+			t = in.nextInt();
+			if (s != t) {
+				deg[u]++;
+				deg[v]++;
+				addEdge(u, v, i);
+				addEdge(v, u, i);
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			cur[i] = head[i];
+		}
+		boolean check = true;
+		for (int i = 1; i <= n; i++) {
+			if ((deg[i] & 1) == 1) {
+				check = false;
+				break;
+			}
+		}
+		if (!check) {
+			out.println("NIE");
+		} else {
+			for (int i = 1; i <= n; i++) {
+				if (!visNode[i]) {
+					// euler1(i);
+					euler2(i);
+				}
+			}
+			out.println(cnta);
+			for (int i = 1; i <= cnta; i++) {
+				out.print((ansr[i] - ansl[i]) + " ");
+				for (int j = ansl[i]; j <= ansr[i]; j++) {
+					out.print(arr[j] + " ");
+				}
+				out.println();
+			}
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
+
+}
