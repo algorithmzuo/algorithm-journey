fe2f6bcd400285ee023948605dc624e586bd32fb
algorithmzuo <algorithmzuo@163.com>
2026-01-10 00:26:18 +0800

    modify code

--- src/class188/Code09_Postman1.java
+++ src/class188/Code09_Postman1.java
@@ -4,6 +4,298 @@ package class188;
 // 测试链接 : https://www.luogu.com.cn/problem/P3443
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
 public class Code09_Postman1 {
+
+	static class PairMap {
+		public HashMap<Long, Integer> map = new HashMap<>();
+
+		public long key(int a, int b) {
+			return (((long) a) << 32) | (b & 0xffffffffL);
+		}
+
+		public boolean contains(int a, int b) {
+			return map.containsKey(key(a, b));
+		}
+
+		public boolean isEmpty() {
+			return map.isEmpty();
+		}
+
+		public void put(int a, int b, int v) {
+			map.put(key(a, b), v);
+		}
+
+		public int get(int a, int b) {
+			return map.get(key(a, b));
+		}
+
+		public void remove(int a, int b) {
+			map.remove(key(a, b));
+		}
+
+		public void clear() {
+			map.clear();
+		}
+	}
+
+	public static int MAXN = 50005;
+	public static int MAXM = 200005;
+	public static int MAXC = 2000005;
+	public static int n, m, t;
+	public static int[] u = new int[MAXM];
+	public static int[] v = new int[MAXM];
+	public static int[] chain = new int[MAXC];
+
+	public static int[] headg = new int[MAXN];
+	public static int[] nextg = new int[MAXM];
+	public static int[] tog = new int[MAXM];
+	public static int cntg;
+	public static int edgeCnt;
+
+	public static PairMap map1 = new PairMap();
+	public static PairMap map2 = new PairMap();
+	public static int[] lastChain = new int[MAXM];
+
+	public static int[] curv = new int[MAXN];
+	public static int[] inDeg = new int[MAXN];
+	public static int[] outDeg = new int[MAXN];
+
+	public static boolean[] startEdge = new boolean[MAXM];
+	public static int[] nextEdge = new int[MAXM];
+	public static boolean[] visEdge = new boolean[MAXM];
+
+	public static int[] sta = new int[MAXM];
+	public static int top;
+
+	public static int[] path = new int[MAXM];
+	public static int cntp;
+
+	public static int[] ans = new int[MAXM];
+	public static int cnta;
+
+	public static void addEdge(int x, int y) {
+		nextg[++cntg] = headg[x];
+		tog[cntg] = y;
+		headg[x] = cntg;
+	}
+
+	public static boolean prepare() {
+		for (int i = 1; i <= m; i++) {
+			map1.put(u[i], v[i], i);
+			startEdge[i] = true;
+		}
+		int siz = chain[1], l = 2, r = l + siz - 1;
+		int a, b, ledge, redge;
+		while (siz > 0) {
+			ledge = 0;
+			for (int i = l; i < r; i++) {
+				a = chain[i];
+				b = chain[i + 1];
+				if (!map1.contains(a, b)) {
+					return false;
+				}
+				redge = map1.get(a, b);
+				if (ledge != 0) {
+					if (nextEdge[ledge] != 0 && nextEdge[ledge] != redge) {
+						return false;
+					}
+					nextEdge[ledge] = redge;
+					startEdge[redge] = false;
+				}
+				ledge = redge;
+			}
+			siz = chain[r + 1];
+			l = r + 2;
+			r = l + siz - 1;
+		}
+		return true;
+	}
+
+	public static int getChainEnd(int start) {
+		int end = start;
+		while (!visEdge[end] && nextEdge[end] != 0) {
+			visEdge[end] = true;
+			end = nextEdge[end];
+		}
+		if (visEdge[end]) {
+			return -1;
+		}
+		visEdge[end] = true;
+		return end;
+	}
+
+	public static boolean compress() {
+		for (int i = 1; i <= m; i++) {
+			if (startEdge[i]) {
+				int x = u[i];
+				int y = v[i];
+				if (nextEdge[i] != 0) {
+					int end = getChainEnd(i);
+					if (end == -1) {
+						return false;
+					}
+					y = v[end];
+					if (map2.contains(x, y)) {
+						lastChain[i] = map2.get(x, y);
+					}
+					map2.put(x, y, i);
+				}
+				addEdge(x, y);
+				outDeg[x]++;
+				inDeg[y]++;
+				edgeCnt++;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			if (inDeg[i] != outDeg[i]) {
+				return false;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			curv[i] = headg[i];
+		}
+		return true;
+	}
+
+	public static void euler1(int u) {
+		for (int e = curv[u]; e > 0; e = curv[u]) {
+			curv[u] = nextg[e];
+			euler1(tog[e]);
+		}
+		path[++cntp] = u;
+	}
+
+	public static void euler2(int start) {
+		sta[++top] = start;
+		while (top > 0) {
+			int u = sta[top--];
+			int e = curv[u];
+			if (e > 0) {
+				curv[u] = nextg[e];
+				sta[++top] = u;
+				sta[++top] = tog[e];
+			} else {
+				path[++cntp] = u;
+			}
+		}
+	}
+
+	public static boolean decompress() {
+		for (int i = cntp; i >= 1; i--) {
+			int y = path[i];
+			if (cnta == 0) {
+				ans[++cnta] = y;
+			} else {
+				int x = ans[cnta];
+				if (!map2.contains(x, y)) {
+					ans[++cnta] = y;
+				} else {
+					int cur = map2.get(x, y);
+					if (lastChain[cur] != 0) {
+						map2.put(x, y, lastChain[cur]);
+					} else {
+						map2.remove(x, y);
+					}
+					for (int e = cur; e > 0; e = nextEdge[e]) {
+						ans[++cnta] = v[e];
+					}
+				}
+			}
+		}
+		if (!map2.isEmpty() || cnta != m + 1) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean compute() {
+		if (!prepare()) {
+			return false;
+		}
+		if (!compress()) {
+			return false;
+		}
+		// euler1(1);
+		euler2(1);
+		if (cntp != edgeCnt + 1) {
+			return false;
+		}
+		return decompress();
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		m = in.nextInt();
+		for (int i = 1; i <= m; i++) {
+			u[i] = in.nextInt();
+			v[i] = in.nextInt();
+		}
+		t = in.nextInt();
+		for (int i = 1, siz, idx = 0; i <= t; i++) {
+			siz = in.nextInt();
+			chain[++idx] = siz;
+			for (int j = 1; j <= siz; j++) {
+				chain[++idx] = in.nextInt();
+			}
+		}
+		boolean check = compute();
+		if (!check) {
+			out.println("NIE");
+		} else {
+			out.println("TAK");
+			for (int i = 1; i <= cnta; i++) {
+				out.println(ans[i]);
+			}
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
 
 }
