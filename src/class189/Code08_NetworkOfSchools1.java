817abd57b1ceede58b6347a90fac37736707012e
algorithmzuo <algorithmzuo@163.com>
2026-01-20 14:02:53 +0800

    modify code

--- src/class189/Code08_NetworkOfSchools1.java
+++ src/class189/Code08_NetworkOfSchools1.java
@@ -1,7 +1,7 @@
 package class189;
 
-// 消息扩散，java版
-// 测试链接 : https://www.luogu.com.cn/problem/P2002
+// 校园网络，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P2812
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
 import java.io.IOException;
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-public class Code07_NewsSpread1 {
+public class Code08_NetworkOfSchools1 {
 
 	public static int MAXN = 100001;
 	public static int MAXM = 500001;
@@ -33,26 +33,8 @@ public class Code07_NewsSpread1 {
 	public static int[] belong = new int[MAXN];
 	public static int sccCnt;
 
+	public static int[] outdegree = new int[MAXN];
 	public static int[] indegree = new int[MAXN];
-
-	// 迭代版需要的栈，讲解118讲了递归改迭代的技巧
-	public static int[][] stack = new int[MAXN][3];
-	public static int u, status, e;
-	public static int stacksize;
-
-	public static void push(int u, int status, int e) {
-		stack[stacksize][0] = u;
-		stack[stacksize][1] = status;
-		stack[stacksize][2] = e;
-		stacksize++;
-	}
-
-	public static void pop() {
-		stacksize--;
-		u = stack[stacksize][0];
-		status = stack[stacksize][1];
-		e = stack[stacksize][2];
-	}
 
 	public static void addEdge(int u, int v) {
 		nxt[++cntg] = head[u];
@@ -60,15 +42,14 @@ public class Code07_NewsSpread1 {
 		head[u] = cntg;
 	}
 
-	// 递归版，java会爆栈，C++可以通过
-	public static void tarjan1(int u) {
+	public static void tarjan(int u) {
 		dfn[u] = low[u] = ++cntd;
 		sta[++top] = u;
 		ins[u] = true;
 		for (int e = head[u]; e > 0; e = nxt[e]) {
 			int v = to[e];
 			if (dfn[v] == 0) {
-				tarjan1(v);
+				tarjan(v);
 				low[u] = Math.min(low[u], low[v]);
 			} else {
 				if (ins[v]) {
@@ -87,80 +68,51 @@ public class Code07_NewsSpread1 {
 		}
 	}
 
-	// 迭代版
-	public static void tarjan2(int node) {
-		stacksize = 0;
-		push(node, -1, -1);
-		int v;
-		while (stacksize > 0) {
-			pop();
-			if (status == -1) {
-				dfn[u] = low[u] = ++cntd;
-				sta[++top] = u;
-				ins[u] = true;
-				e = head[u];
-			} else {
-				v = to[e];
-				if (status == 0) {
-					low[u] = Math.min(low[u], low[v]);
-				}
-				if (status == 1 && ins[v]) {
-					low[u] = Math.min(low[u], dfn[v]);
-				}
-				e = nxt[e];
-			}
-			if (e != 0) {
-				v = to[e];
-				if (dfn[v] == 0) {
-					push(u, 0, e);
-					push(v, -1, -1);
-				} else {
-					push(u, 1, e);
-				}
-			} else {
-				if (dfn[u] == low[u]) {
-					sccCnt++;
-					int pop;
-					do {
-						pop = sta[top--];
-						belong[pop] = sccCnt;
-						ins[pop] = false;
-					} while (pop != u);
-				}
-			}
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
 		FastReader in = new FastReader(System.in);
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		n = in.nextInt();
-		m = in.nextInt();
-		for (int i = 1; i <= m; i++) {
-			a[i] = in.nextInt();
-			b[i] = in.nextInt();
-			addEdge(a[i], b[i]);
+		m = 0;
+		for (int i = 1, x; i <= n; i++) {
+			x = in.nextInt();
+			while (x != 0) {
+				m++;
+				a[m] = i;
+				b[m] = x;
+				addEdge(a[m], b[m]);
+				x = in.nextInt();
+			}
 		}
 		for (int i = 1; i <= n; i++) {
 			if (dfn[i] == 0) {
-				// tarjan1(i);
-				tarjan2(i);
+				tarjan(i);
 			}
 		}
-		for (int i = 1; i <= m; i++) {
-			int scc1 = belong[a[i]];
-			int scc2 = belong[b[i]];
-			if (scc1 != scc2) {
-				indegree[scc2]++;
+		if (sccCnt == 1) {
+			out.println("1");
+			out.println("0");
+		} else {
+			for (int i = 1; i <= m; i++) {
+				int scc1 = belong[a[i]];
+				int scc2 = belong[b[i]];
+				if (scc1 != scc2) {
+					outdegree[scc1]++;
+					indegree[scc2]++;
+				}
 			}
-		}
-		int ans = 0;
-		for (int i = 1; i <= sccCnt; i++) {
-			if (indegree[i] == 0) {
-				ans++;
+			int outZero = 0;
+			int inZero = 0;
+			for (int i = 1; i <= sccCnt; i++) {
+				if (outdegree[i] == 0) {
+					outZero++;
+				}
+				if (indegree[i] == 0) {
+					inZero++;
+				}
 			}
+			out.println(inZero);
+			out.println(Math.max(outZero, inZero));
 		}
-		out.println(ans);
 		out.flush();
 		out.close();
 	}
