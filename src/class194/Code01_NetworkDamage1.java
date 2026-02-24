bfc649add5c45cda25e0fc4baf4a444f30f11eb5
algorithmzuo <algorithmzuo@163.com>
2026-02-24 14:31:15 +0800

    modify code

---
+++ src/class194/Code01_NetworkDamage1.java
@@ -0,0 +1,8 @@
+package class194;
+
+// 通讯网破坏，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P3854
+
+public class Code01_NetworkDamage1 {
+
+}
--- src/class194/Code02_RoadsMeet1.java
+++ src/class194/Code02_RoadsMeet1.java
@@ -3,6 +3,6 @@ package class194;
 // 道路相遇，java版
 // 测试链接 : https://www.luogu.com.cn/problem/P4320
 
-public class Code01_Roads1 {
+public class Code02_RoadsMeet1 {
 
 }
