25c80ea0b751a50247b87bc462c0ad17ae76508f
algorithmzuo <algorithmzuo@163.com>
2026-02-26 16:13:42 +0800

    modify code

---
+++ src/class194/Code05_Tourists1.java
@@ -0,0 +1,9 @@
+package class194;
+
+// 游客，java版
+// 测试链接 : https://www.luogu.com.cn/problem/CF487E
+// 测试链接 : https://codeforces.com/problemset/problem/487/E
+
+public class Code05_Tourists1 {
+
+}
