1801b8c8705795cd222a31691fc235f06be0d8b0
algorithmzuo <algorithmzuo@163.com>
2026-03-24 17:57:15 +0800

    modify code

---
+++ src/class195/Code07_BeautifulTree2.java
@@ -0,0 +1,233 @@
+package class195;
+
+// 美丽的树，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/CF1904F
+// 测试链接 : https://codeforces.com/problemset/problem/1904/F
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//const int MAXN = 200001;
+//const int MAXT = MAXN * 41;
+//const int MAXE = MAXN * 205;
+//const int MAXP = 20;
+//int n, m;
+//
+//int head1[MAXN];
+//int next1[MAXN << 1];
+//int to1[MAXN << 1];
+//int cnt1;
+//
+//int indegree[MAXT];
+//int head2[MAXT];
+//int next2[MAXE];
+//int to2[MAXE];
+//int cnt2;
+//
+//int dep[MAXN];
+//int stfa[MAXN][MAXP];
+//int stout[MAXN][MAXP];
+//int stin[MAXN][MAXP];
+//int cntt;
+//
+//int que[MAXT];
+//int ans[MAXN];
+//
+//void addEdge1(int u, int v) {
+//    next1[++cnt1] = head1[u];
+//    to1[cnt1] = v;
+//    head1[u] = cnt1;
+//}
+//
+//void addEdge2(int u, int v) {
+//    indegree[v]++;
+//    next2[++cnt2] = head2[u];
+//    to2[cnt2] = v;
+//    head2[u] = cnt2;
+//}
+//
+//void dfs(int u, int fa) {
+//    dep[u] = dep[fa] + 1;
+//    stfa[u][0] = fa;
+//    stout[u][0] = ++cntt;
+//    addEdge2(u, cntt);
+//    addEdge2(fa, cntt);
+//    stin[u][0] = ++cntt;
+//    addEdge2(cntt, u);
+//    addEdge2(cntt, fa);
+//    for (int p = 1; p < MAXP; p++) {
+//        stfa[u][p] = stfa[stfa[u][p - 1]][p - 1];
+//        stout[u][p] = ++cntt;
+//        addEdge2(stout[u][p - 1], cntt);
+//        addEdge2(stout[stfa[u][p - 1]][p - 1], cntt);
+//        stin[u][p] = ++cntt;
+//        addEdge2(cntt, stin[u][p - 1]);
+//        addEdge2(cntt, stin[stfa[u][p - 1]][p - 1]);
+//    }
+//    for (int e = head1[u]; e > 0; e = next1[e]) {
+//        int v = to1[e];
+//        if (v != fa) {
+//            dfs(v, u);
+//        }
+//    }
+//}
+//
+//int getLca(int x, int y) {
+//    if (dep[x] < dep[y]) {
+//        swap(x, y);
+//    }
+//    for (int p = MAXP - 1; p >= 0; p--) {
+//        if (dep[stfa[x][p]] >= dep[y]) {
+//            x = stfa[x][p];
+//        }
+//    }
+//    if (x == y) {
+//        return x;
+//    }
+//    for (int p = MAXP - 1; p >= 0; p--) {
+//        if (stfa[x][p] != stfa[y][p]) {
+//            x = stfa[x][p];
+//            y = stfa[y][p];
+//        }
+//    }
+//    return stfa[x][0];
+//}
+//
+//int nearest(int x, int c) {
+//    int lca = getLca(x, c);
+//    if (lca == c) {
+//        int k = dep[x] - dep[c] - 1;
+//        for (int p = 0; p < MAXP; p++) {
+//            if (((k >> p) & 1) != 0) {
+//                x = stfa[x][p];
+//            }
+//        }
+//        return x;
+//    } else {
+//        return stfa[c][0];
+//    }
+//}
+//
+//void pathOut(int x, int y, int c) {
+//    if (dep[x] < dep[y]) {
+//        swap(x, y);
+//    }
+//    addEdge2(y, c);
+//    for (int p = MAXP - 1; p >= 0; p--) {
+//        if (dep[stfa[x][p]] >= dep[y]) {
+//            addEdge2(stout[x][p], c);
+//            x = stfa[x][p];
+//        }
+//    }
+//    if (x == y) {
+//        return;
+//    }
+//    for (int p = MAXP - 1; p >= 0; p--) {
+//        if (stfa[x][p] != stfa[y][p]) {
+//            addEdge2(stout[x][p], c);
+//            addEdge2(stout[y][p], c);
+//            x = stfa[x][p];
+//            y = stfa[y][p];
+//        }
+//    }
+//    addEdge2(stout[x][0], c);
+//}
+//
+//void pathIn(int x, int y, int c) {
+//    if (dep[x] < dep[y]) {
+//        swap(x, y);
+//    }
+//    addEdge2(c, y);
+//    for (int p = MAXP - 1; p >= 0; p--) {
+//        if (dep[stfa[x][p]] >= dep[y]) {
+//            addEdge2(c, stin[x][p]);
+//            x = stfa[x][p];
+//        }
+//    }
+//    if (x == y) {
+//        return;
+//    }
+//    for (int p = MAXP - 1; p >= 0; p--) {
+//        if (stfa[x][p] != stfa[y][p]) {
+//            addEdge2(c, stin[x][p]);
+//            addEdge2(c, stin[y][p]);
+//            x = stfa[x][p];
+//            y = stfa[y][p];
+//        }
+//    }
+//    addEdge2(c, stin[x][0]);
+//}
+//
+//void pathMin(int a, int b, int c) {
+//    if (a != c) {
+//        pathIn(a, nearest(a, c), c);
+//    }
+//    if (b != c) {
+//        pathIn(b, nearest(b, c), c);
+//    }
+//}
+//
+//void pathMax(int a, int b, int c) {
+//    if (a != c) {
+//        pathOut(a, nearest(a, c), c);
+//    }
+//    if (b != c) {
+//        pathOut(b, nearest(b, c), c);
+//    }
+//}
+//
+//bool topo() {
+//    int qi = 1, qsiz = 0;
+//    for (int i = 1; i <= cntt; i++) {
+//        if (indegree[i] == 0) {
+//            que[++qsiz] = i;
+//        }
+//    }
+//    int val = 0;
+//    while (qi <= qsiz) {
+//        int u = que[qi++];
+//        if (u >= 1 && u <= n) {
+//            ans[u] = ++val;
+//        }
+//        for (int e = head2[u]; e > 0; e = next2[e]) {
+//            int v = to2[e];
+//            if (--indegree[v] == 0) {
+//                que[++qsiz] = v;
+//            }
+//        }
+//    }
+//    return qsiz == cntt;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n >> m;
+//    cntt = n;
+//    for (int i = 1, u, v; i < n; i++) {
+//        cin >> u >> v;
+//        addEdge1(u, v);
+//        addEdge1(v, u);
+//    }
+//    dfs(1, 1);
+//    for (int i = 1, op, a, b, c; i <= m; i++) {
+//        cin >> op >> a >> b >> c;
+//        if (op == 1) {
+//            pathMin(a, b, c);
+//        } else {
+//            pathMax(a, b, c);
+//        }
+//    }
+//    bool check = topo();
+//    if (check) {
+//        for (int i = 1; i <= n; i++) {
+//            cout << ans[i] << " ";
+//        }
+//    } else {
+//        cout << -1 << "\n";
+//    }
+//    return 0;
+//}
