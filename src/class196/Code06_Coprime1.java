76d7786932bdb6ee2b10d4175861a4ecc727d841
algorithmzuo <algorithmzuo@163.com>
2026-04-08 13:16:13 +0800

    modify code

--- src/class196/Code06_Coprime1.java
+++ src/class196/Code06_Coprime1.java
@@ -5,6 +5,205 @@ package class196;
 // 测试链接 : https://atcoder.jp/contests/abc210/tasks/abc210_f
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 public class Code06_Coprime1 {
+
+	public static int MAXN = 1000001;
+	public static int MAXV = 2000001;
+	public static int MAXM = 3000001;
+	public static int n, maxv;
+	public static int[] ab = new int[MAXN];
+
+	public static int[] minp = new int[MAXV];
+	public static int[] pval = new int[MAXV];
+	public static int cntp;
+
+	public static int[][] arr = new int[MAXN][2];
+	public static int cnta;
+
+	public static int[] head = new int[MAXN];
+	public static int[] nxt = new int[MAXM];
+	public static int[] to = new int[MAXM];
+	public static int cntg;
+
+	public static int[] dfn = new int[MAXN];
+	public static int[] low = new int[MAXN];
+	public static int cntd;
+
+	public static int[] sta = new int[MAXN];
+	public static int top;
+
+	public static int[] belong = new int[MAXN];
+	public static int sccCnt;
+
+	public static int cntt;
+
+	public static void addEdge(int u, int v) {
+		nxt[++cntg] = head[u];
+		to[cntg] = v;
+		head[u] = cntg;
+	}
+
+	public static int other(int x) {
+		return x <= n ? (x + n) : (x - n);
+	}
+
+	// 讲解097，欧拉筛
+	public static void euler() {
+		for (int i = 2; i <= maxv; i++) {
+			if (minp[i] == 0) {
+				minp[i] = i;
+				pval[++cntp] = i;
+			}
+			for (int j = 1; j <= cntp; j++) {
+				if (i * pval[j] > maxv) {
+					break;
+				}
+				minp[i * pval[j]] = pval[j];
+				if (i % pval[j] == 0) {
+					break;
+				}
+			}
+		}
+	}
+
+	// 质因子分解
+	public static void decompose() {
+		for (int i = 1; i <= n << 1; i++) {
+			for (int v = ab[i], p = minp[v]; v > 1; p = minp[v]) {
+				arr[++cnta][0] = p;
+				arr[cnta][1] = i;
+				while (v % p == 0) {
+					v /= p;
+				}
+			}
+		}
+	}
+
+	// 优化建图
+	public static void link() {
+		Arrays.sort(arr, 1, cnta + 1, (a, b) -> a[0] != b[0] ? (a[0] - b[0]) : (a[1] - b[1]));
+		for (int l = 1, r = 1; l <= cnta; l = ++r) {
+			while (r + 1 <= cnta && arr[l][0] == arr[r + 1][0]) {
+				r++;
+			}
+			cntt++;
+			addEdge(cntt, other(arr[l][1]));
+			for (int i = l + 1; i <= r; i++) {
+				addEdge(arr[i][1], cntt);
+				cntt++;
+				addEdge(cntt, cntt - 1);
+				addEdge(cntt, other(arr[i][1]));
+			}
+			cntt++;
+			addEdge(cntt, other(arr[r][1]));
+			for (int i = r - 1; i >= l; i--) {
+				addEdge(arr[i][1], cntt);
+				cntt++;
+				addEdge(cntt, cntt - 1);
+				addEdge(cntt, other(arr[i][1]));
+			}
+		}
+	}
+
+	public static void tarjan(int u) {
+		dfn[u] = low[u] = ++cntd;
+		sta[++top] = u;
+		for (int e = head[u]; e > 0; e = nxt[e]) {
+			int v = to[e];
+			if (dfn[v] == 0) {
+				tarjan(v);
+				low[u] = Math.min(low[u], low[v]);
+			} else {
+				if (belong[v] == 0) {
+					low[u] = Math.min(low[u], dfn[v]);
+				}
+			}
+		}
+		if (dfn[u] == low[u]) {
+			sccCnt++;
+			int pop;
+			do {
+				pop = sta[top--];
+				belong[pop] = sccCnt;
+			} while (pop != u);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		cntt = n << 1;
+		for (int i = 1, a, b; i <= n; i++) {
+			a = in.nextInt();
+			b = in.nextInt();
+			ab[i] = a;
+			ab[i + n] = b;
+			maxv = Math.max(maxv, Math.max(a, b));
+		}
+		euler();
+		decompose();
+		link();
+		for (int i = 1; i <= cntt; i++) {
+			if (dfn[i] == 0) {
+				tarjan(i);
+			}
+		}
+		boolean check = true;
+		for (int i = 1; i <= n; i++) {
+			if (belong[i] == belong[i + n]) {
+				check = false;
+				break;
+			}
+		}
+		out.println(check ? "Yes" : "No");
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+	}
 
 }
