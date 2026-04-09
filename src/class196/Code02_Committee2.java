f87fedd74cd812c45cef274bce68ca9e21ce186b
algorithmzuo <algorithmzuo@163.com>
2026-04-09 15:15:44 +0800

    modify code

--- src/class196/Code02_Committee2.java
+++ src/class196/Code02_Committee2.java
@@ -1,13 +1,15 @@
 package class196;
 
-// 2-SAT模版题，C++版
-// 一共n个变量，变量的编号1~n，每个变量的取值不是0就是1
-// 给定m条限制，每条限制的格式 x1 v1 x2 v2，含义如下
-// 编号为x1的变量取值为v1 或者 编号为x2的变量取值为v2
-// 决定每个变量的值，来满足所有限制，如果无法做到，打印"IMPOSSIBLE"
-// 如果可以做到，先打印"POSSIBLE"，然后打印每个变量的值，任何一种方案都可以
-// 1 <= n、m <= 10^6
-// 测试链接 : https://www.luogu.com.cn/problem/P4782
+// 和平委员会，C++版
+// 一共n个团体，每个团体有两个代表，所以有2n个代表
+// 第i号团体两个代表的编号为2i−1和2i，然后给定m个讨厌关系
+// 格式 x y : 编号x和编号y这两个代表，相互讨厌，不可共事
+// 现在要选出n个人组成委员会，每个团体的两个代表，有且仅有1人入选
+// 同时要求选出的委员会中，没有相互讨厌的代表，如果做不到，打印"NIE"
+// 如果能做到，按升序打印组成委员会的n个人的编号，任何一种方案都可以
+// 1 <= n <= 8 * 10^3
+// 0 <= m <= 2 * 10^4
+// 测试链接 : https://www.luogu.com.cn/problem/P5782
 // 如下实现是C++的版本，C++版本和java版本逻辑完全一样
 // 提交如下代码，可以通过所有测试用例
 
@@ -15,12 +17,13 @@ package class196;
 //
 //using namespace std;
 //
-//const int MAXN = 2000001;
+//const int MAXN = 20001;
+//const int MAXM = 50001;
 //int n, m;
 //
 //int head[MAXN];
-//int nxt[MAXN];
-//int to[MAXN];
+//int nxt[MAXM];
+//int to[MAXM];
 //int cntg;
 //
 //int dfn[MAXN];
@@ -63,25 +66,18 @@ package class196;
 //    }
 //}
 //
+//int other(int x) {
+//    return (x & 1) == 0 ? (x - 1) : (x + 1);
+//}
+//
 //int main() {
 //    ios::sync_with_stdio(false);
 //    cin.tie(nullptr);
 //    cin >> n >> m;
-//    for (int i = 1, x1, v1, x2, v2; i <= m; i++) {
-//        cin >> x1 >> v1 >> x2 >> v2;
-//        if (v1 == 1 && v2 == 1) {
-//            addEdge(x1 + n, x2);
-//            addEdge(x2 + n, x1);
-//        } else if (v1 == 0 && v2 == 1) {
-//            addEdge(x1, x2);
-//            addEdge(x2 + n, x1 + n);
-//        } else if (v1 == 1 && v2 == 0) {
-//            addEdge(x2, x1);
-//            addEdge(x1 + n, x2 + n);
-//        } else {
-//            addEdge(x1, x2 + n);
-//            addEdge(x2, x1 + n);
-//        }
+//    for (int i = 1, u, v; i <= m; i++) {
+//        cin >> u >> v;
+//        addEdge(u, other(v));
+//        addEdge(v, other(u));
 //    }
 //    for (int i = 1; i <= (n << 1); i++) {
 //        if (dfn[i] == 0) {
@@ -89,23 +85,25 @@ package class196;
 //        }
 //    }
 //    bool check = true;
-//    for (int i = 1; i <= n; i++) {
-//        if (belong[i] == belong[i + n]) {
+//    for (int i = 1, a, b; i <= n; i++) {
+//        a = i << 1;
+//        b = a - 1;
+//        if (belong[a] == belong[b]) {
 //            check = false;
-//            break;
 //        }
 //    }
 //    if (check) {
-//        cout << "POSSIBLE" << "\n";
-//        for (int i = 1; i <= n; i++) {
-//            if (belong[i] < belong[i + n]) {
-//                cout << "1 ";
+//        for (int i = 1, a, b; i <= n; i++) {
+//            a = i << 1;
+//            b = a - 1;
+//            if (belong[a] < belong[b]) {
+//                cout << a << "\n";
 //            } else {
-//                cout << "0 ";
+//                cout << b << "\n";
 //            }
 //        }
 //    } else {
-//        cout << "IMPOSSIBLE" << "\n";
+//        cout << "NIE" << "\n";
 //    }
 //    return 0;
 //}
