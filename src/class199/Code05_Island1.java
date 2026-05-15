5055619246427698919f9cf7a99c060b079c64d8
algorithmzuo <algorithmzuo@163.com>
2026-05-15 21:46:20 +0800

    modify code

--- src/class199/Code05_Island1.java
+++ src/class199/Code05_Island1.java
@@ -21,18 +21,18 @@ public class Code05_Island1 {
 	public static int cntg;
 
 	public static boolean[] vis = new boolean[MAXN];
-	public static boolean[] inCycle = new boolean[MAXN];
-	public static int cycleStart;
+	public static boolean[] cycle = new boolean[MAXN];
+	public static int start;
 
-	public static int[] cycleArr = new int[MAXN];
-	public static int cntc;
+	public static int[] arr = new int[MAXN];
+	public static int cnta;
 
-	public static boolean[] computed = new boolean[MAXN];
+	public static boolean[] flag = new boolean[MAXN];
 	public static long[] dist = new long[MAXN];
 	public static long diameter;
 
-	public static long[] cycleDist = new long[MAXN << 1];
-	public static long[] treeHeight = new long[MAXN << 1];
+	public static long[] sum = new long[MAXN << 1];
+	public static long[] height = new long[MAXN << 1];
 	public static int[] que = new int[MAXN << 1];
 
 	public static void addEdge(int u, int v, int w) {
@@ -45,27 +45,24 @@ public class Code05_Island1 {
 	// 已知图上只有一个环，收集环上的节点，建立前缀和
 	public static boolean dfs(int u, int preEdge) {
 		if (vis[u] == true) {
-			cycleStart = u;
-			inCycle[u] = true;
-			cycleArr[++cntc] = u;
-			cycleDist[cntc] = 0;
+			start = u;
+			cycle[u] = true;
+			arr[++cnta] = u;
+			sum[cnta] = 0;
 			return true;
 		}
 		vis[u] = true;
 		for (int e = head[u]; e > 0; e = nxt[e]) {
-			if (e != (preEdge ^ 1)) {
-				int v = to[e];
-				int w = weight[e];
-				if (dfs(v, e)) {
-					if (u == cycleStart) {
-						cycleDist[cntc + 1] = w;
-						return false;
-					}
-					inCycle[u] = true;
-					cycleArr[++cntc] = u;
-					cycleDist[cntc] = w;
-					return true;
+			int v = to[e];
+			int w = weight[e];
+			if (e != (preEdge ^ 1) && dfs(v, e)) {
+				sum[cnta + 1] = w;
+				if (u == start) {
+					return false;
 				}
+				cycle[u] = true;
+				arr[++cnta] = u;
+				return true;
 			}
 		}
 		return false;
@@ -73,11 +70,11 @@ public class Code05_Island1 {
 
 	// 忽略环的情况下，得到子树的直径
 	public static void dp(int u) {
-		computed[u] = true;
+		flag[u] = true;
 		for (int e = head[u]; e > 0; e = nxt[e]) {
 			int v = to[e];
 			int w = weight[e];
-			if (!computed[v] && !inCycle[v]) {
+			if (!flag[v] && !cycle[v]) {
 				dp(v);
 				diameter = Math.max(diameter, dist[u] + dist[v] + w);
 				dist[u] = Math.max(dist[u], dist[v] + w);
@@ -86,37 +83,37 @@ public class Code05_Island1 {
 	}
 
 	public static long compute(int root) {
-		cntc = 0;
+		cnta = 0;
 		dfs(root, 0);
-		if (cntc == 0) {
+		if (cnta == 0) {
 			diameter = 0;
 			dp(root);
 			return diameter;
 		}
-		for (int i = cntc + 2; i <= cntc * 2; i++) {
-			cycleDist[i] = cycleDist[i - cntc];
+		for (int i = cnta + 2; i <= cnta * 2; i++) {
+			sum[i] = sum[i - cnta];
 		}
-		for (int i = 1; i <= cntc * 2; i++) {
-			cycleDist[i] += cycleDist[i - 1];
+		for (int i = 1; i <= cnta * 2; i++) {
+			sum[i] += sum[i - 1];
 		}
 		long ans1 = 0;
-		for (int i = 1; i <= cntc; i++) {
+		for (int i = 1; i <= cnta; i++) {
 			diameter = 0;
-			dp(cycleArr[i]);
+			dp(arr[i]);
 			ans1 = Math.max(ans1, diameter);
-			treeHeight[i] = dist[cycleArr[i]];
-			treeHeight[i + cntc] = treeHeight[i];
+			height[i] = dist[arr[i]];
+			height[i + cnta] = height[i];
 		}
 		long ans2 = 0;
 		int ql = 1, qr = 0;
-		for (int i = 1; i <= cntc * 2; i++) {
-			while (ql <= qr && que[ql] <= i - cntc) {
+		for (int i = 1; i <= cnta * 2; i++) {
+			while (ql <= qr && que[ql] <= i - cnta) {
 				ql++;
 			}
 			if (ql <= qr) {
-				ans2 = Math.max(ans2, treeHeight[i] + treeHeight[que[ql]] + cycleDist[i] - cycleDist[que[ql]]);
+				ans2 = Math.max(ans2, height[i] + height[que[ql]] + sum[i] - sum[que[ql]]);
 			}
-			while (ql <= qr && treeHeight[que[qr]] - cycleDist[que[qr]] <= treeHeight[i] - cycleDist[i]) {
+			while (ql <= qr && height[que[qr]] - sum[que[qr]] <= height[i] - sum[i]) {
 				qr--;
 			}
 			que[++qr] = i;
@@ -137,7 +134,7 @@ public class Code05_Island1 {
 		}
 		long ans = 0;
 		for (int i = 1; i <= n; i++) {
-			if (!computed[i]) {
+			if (!flag[i]) {
 				ans += compute(i);
 			}
 		}
