7a6558af37d17bc08cca1f31b00ece2264549955
algorithmzuo <algorithmzuo@163.com>
2026-05-21 18:59:30 +0800

    modify code

--- src/class199/Code05_ShortestDistance1.java
+++ src/class199/Code05_ShortestDistance1.java
@@ -11,6 +11,9 @@ package class199;
 // 测试链接 : https://www.luogu.com.cn/problem/P4949
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
-public class Code07_ShortestDistance1 {
+public class Code05_ShortestDistance1 {
+
+	public static int MAXN = 100007;
+	public static int n;
 
 }
--- src/class199/Code07_DiameterMinimize1.java
+++ src/class199/Code07_DiameterMinimize1.java
@@ -16,7 +16,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-public class Code05_DiameterMinimize1 {
+public class Code07_DiameterMinimize1 {
 
 	public static int MAXN = 200001;
 	public static int n;
