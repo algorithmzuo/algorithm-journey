d85e5c90d95a876d0b3c48f00ea884894134b5a8
algorithmzuo <algorithmzuo@163.com>
2026-06-01 15:38:43 +0800

    modify code

--- src/class200/Code04_AddEdgeWays1.java
+++ src/class200/Code04_AddEdgeWays1.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-public class Code04_CactusAddEdge1 {
+public class Code04_AddEdgeWays1 {
 
 	public static int MAXN = 500001;
 	public static int MAXM = 1000001;
---
+++ src/class200/Code05_LongestRoute1.java
@@ -0,0 +1,8 @@
+package class200;
+
+// 边不重复的最长路线，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P6335
+
+public class Code05_LongestRoute1 {
+
+}
