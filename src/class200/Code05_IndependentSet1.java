6f6b87456ed96c20f46b43129e6a250cfd1105a4
algorithmzuo <algorithmzuo@163.com>
2026-06-02 11:05:29 +0800

    modify code

---
+++ src/class200/Code05_IndependentSet1.java
@@ -0,0 +1,10 @@
+package class200;
+
+// 仙人掌独立集
+// 测试链接 : https://www.luogu.com.cn/problem/P10779
+// 测试链接 : https://www.luogu.com.cn/problem/P4410
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+
+public class Code05_IndependentSet1 {
+
+}
