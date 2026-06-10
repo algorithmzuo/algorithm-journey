aa73fb8e19ed7793c0d97905a7d824f34816c077
algorithmzuo <algorithmzuo@163.com>
2026-06-10 13:49:13 +0800

    modify code

--- src/class200/Code02_CactusAddEdge1.java
+++ src/class200/Code02_CactusAddEdge1.java
@@ -223,7 +223,8 @@ public class Code02_CactusAddEdge1 {
 
 	public static void prepare() {
 		f[0] = f[1] = 1;
-		for (int i = 2; i <= n; i++) {
+		f[2] = 2;
+		for (int i = 3; i <= n; i++) {
 			f[i] = (f[i - 1] + f[i - 2] * (i - 1)) % MOD;
 		}
 		for (int i = 1; i <= n; i++) {
--- src/class200/Code02_CactusAddEdge2.java
+++ src/class200/Code02_CactusAddEdge2.java
@@ -99,7 +99,8 @@ package class200;
 //
 //void prepare() {
 //    f[0] = f[1] = 1;
-//    for (int i = 2; i <= n; i++) {
+//    f[2] = 2;
+//    for (int i = 3; i <= n; i++) {
 //        f[i] = (f[i - 1] + f[i - 2] * (i - 1)) % MOD;
 //    }
 //    for (int i = 1; i <= n; i++) {
