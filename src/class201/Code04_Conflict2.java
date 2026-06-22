43dc8c1197bf755bb963f969b2612fb38607b980
algorithmzuo <algorithmzuo@163.com>
2026-06-22 15:55:50 +0800

    modify code

--- src/class201/Code04_Conflict2.java
+++ src/class201/Code04_Conflict2.java
@@ -1,13 +1,13 @@
 package class201;
 
-// 洞穴勘测，C++版
-// 一共n个点，初始时没有任何边，任意时刻图都是森林，一共m条操作，类型如下
-// 操作 Connect x y : 点x和点y之间增加直接边，保证原来不存在这条直接边
-// 操作 Destroy x y : 删除点x和点y之间的直接边，保证原来存在这条直接边
-// 操作 Query x y   : 查询点x和点y之间是否连通，打印 Yes 或 No
-// 1 <= n <= 10^4
-// 1 <= m <= 2 * 10^5
-// 测试链接 : https://www.luogu.com.cn/problem/P2147
+// 部落冲突，C++版
+// 一共n个点，给定n-1条边，所有点组成一棵树，一共m条操作，类型如下
+// 操作 C x y : x和y在树中相邻，之前未开战，现在开战，x和y的直接边变成不可通行
+//              每条C操作代表一次战争事件，战争事件从1开始编号，每次编号自增
+// 操作 U x   : 第x号战争事件结束，对应的直接边变为可通行，保证战争不会重复结束
+// 操作 Q x y : 查询点x和点y之间是否连通，打印 Yes 或 No
+// 1 <= n、m <= 3 * 10^5
+// 测试链接 : https://www.luogu.com.cn/problem/P3950
 // 如下实现是C++的版本，C++版本和java版本逻辑完全一样
 // 提交如下代码，可以通过所有测试用例
 
@@ -15,7 +15,7 @@ package class201;
 //
 //using namespace std;
 //
-//const int MAXN = 10001;
+//const int MAXN = 300001;
 //int n, m;
 //
 //int fa[MAXN];
@@ -23,6 +23,10 @@ package class201;
 //int rs[MAXN];
 //int sta[MAXN];
 //bool rev[MAXN];
+//
+//int cutx[MAXN];
+//int cuty[MAXN];
+//int cutc;
 //
 //bool isroot(int x) {
 //    return ls[fa[x]] != x && rs[fa[x]] != x;
@@ -138,14 +142,23 @@ package class201;
 //    ios::sync_with_stdio(false);
 //    cin.tie(nullptr);
 //    cin >> n >> m;
+//    for (int i = 1, x, y; i < n; i++) {
+//        cin >> x >> y;
+//        link(x, y);
+//    }
 //    for (int i = 1, x, y; i <= m; i++) {
 //        string op;
-//        cin >> op >> x >> y;
-//        if (op == "Connect") {
-//            link(x, y);
-//        } else if (op == "Destroy") {
+//        cin >> op;
+//        if (op == "C") {
+//            cin >> x >> y;
 //            cut(x, y);
+//            cutx[++cutc] = x;
+//            cuty[cutc] = y;
+//        } else if (op == "U") {
+//            cin >> x;
+//            link(cutx[x], cuty[x]);
 //        } else {
+//            cin >> x >> y;
 //            if (findroot(x) == findroot(y)) {
 //                cout << "Yes" << "\n";
 //            } else {
--- src/class201/Code05_OTOCI2.java
+++ src/class201/Code05_OTOCI2.java
@@ -1,13 +1,15 @@
 package class201;
 
-// 洞穴勘测，C++版
-// 一共n个点，初始时没有任何边，任意时刻图都是森林，一共m条操作，类型如下
-// 操作 Connect x y : 点x和点y之间增加直接边，保证原来不存在这条直接边
-// 操作 Destroy x y : 删除点x和点y之间的直接边，保证原来存在这条直接边
-// 操作 Query x y   : 查询点x和点y之间是否连通，打印 Yes 或 No
-// 1 <= n <= 10^4
-// 1 <= m <= 2 * 10^5
-// 测试链接 : https://www.luogu.com.cn/problem/P2147
+// 群岛，C++版
+// 一共n个点，每个点有点权，初始时没有边，一共m条操作，类型如下
+// 操作 bridge x y    : 如果x和y已经连通，打印no，表示不需要连边
+//                      如果x和y不连通，打印yes，然后增加无向边
+// 操作 penguins x y  : x的点权变成y
+// 操作 excursion x y : 如果x和y不连通，打印impossible
+//                      如果x和y连通，打印路径的点权累加和
+// 1 <= n <= 3 * 10^4
+// 1 <= m <= 3 * 10^5
+// 测试链接 : https://www.luogu.com.cn/problem/P4312
 // 如下实现是C++的版本，C++版本和java版本逻辑完全一样
 // 提交如下代码，可以通过所有测试用例
 
@@ -15,14 +17,21 @@ package class201;
 //
 //using namespace std;
 //
-//const int MAXN = 10001;
+//const int MAXN = 30001;
 //int n, m;
+//int arr[MAXN];
 //
 //int fa[MAXN];
 //int ls[MAXN];
 //int rs[MAXN];
 //int sta[MAXN];
 //bool rev[MAXN];
+//
+//int sum[MAXN];
+//
+//void up(int x) {
+//    sum[x] = sum[ls[x]] + sum[rs[x]] + arr[x];
+//}
 //
 //bool isroot(int x) {
 //    return ls[fa[x]] != x && rs[fa[x]] != x;
@@ -71,6 +80,8 @@ package class201;
 //    }
 //    fa[f] = x;
 //    fa[x] = g;
+//    up(f);
+//    up(x);
 //}
 //
 //void splay(int x) {
@@ -99,6 +110,7 @@ package class201;
 //    for (int y = 0; x != 0; y = x, x = fa[x]) {
 //        splay(x);
 //        rs[x] = y;
+//        up(x);
 //    }
 //}
 //
@@ -120,6 +132,12 @@ package class201;
 //    return x;
 //}
 //
+//void split(int x, int y) {
+//    makeroot(x);
+//    access(y);
+//    splay(y);
+//}
+//
 //void link(int x, int y) {
 //    makeroot(x);
 //    if (findroot(y) != x) {
@@ -131,25 +149,40 @@ package class201;
 //    makeroot(x);
 //    if (findroot(y) == x && fa[y] == x && ls[y] == 0 && rs[x] == y) {
 //        fa[y] = rs[x] = 0;
+//        up(x);
 //    }
 //}
 //
 //int main() {
 //    ios::sync_with_stdio(false);
 //    cin.tie(nullptr);
-//    cin >> n >> m;
-//    for (int i = 1, x, y; i <= m; i++) {
-//        string op;
+//    cin >> n;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i];
+//        sum[i] = arr[i];
+//    }
+//    cin >> m;
+//    string op;
+//    int x, y;
+//    for (int i = 1; i <= m; i++) {
 //        cin >> op >> x >> y;
-//        if (op == "Connect") {
-//            link(x, y);
-//        } else if (op == "Destroy") {
-//            cut(x, y);
-//        } else {
+//        if (op == "bridge") {
 //            if (findroot(x) == findroot(y)) {
-//                cout << "Yes" << "\n";
+//                cout << "no" << "\n";
 //            } else {
-//                cout << "No" << "\n";
+//                cout << "yes" << "\n";
+//                link(x, y);
+//            }
+//        } else if (op == "penguins") {
+//            splay(x);
+//            arr[x] = y;
+//            up(x);
+//        } else {
+//            if (findroot(x) != findroot(y)) {
+//                cout << "impossible" << "\n";
+//            } else {
+//                split(x, y);
+//                cout << sum[y] << "\n";
 //            }
 //        }
 //    }
