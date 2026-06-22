7b8c877b5a7494621a7f3d57cda4daf9b04097dd
algorithmzuo <algorithmzuo@163.com>
2026-06-22 16:16:59 +0800

    modify code

--- src/class202/Code01_DynamicAddMul1.java
+++ src/class202/Code01_DynamicAddMul1.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-public class Code01_PathAddMul1 {
+public class Code01_DynamicAddMul1 {
 
 	public static int MAXN = 100001;
 	public static int MOD = 51061;
