67e29280db90f3f9552b434f87df38911cac9839
algorithmzuo <algorithmzuo@163.com>
2026-07-21 15:19:04 +0800

    modify code

--- src/class203/Code04_MagicForest1.java
+++ src/class203/Code04_MagicForest1.java
@@ -1,11 +1,13 @@
 package class203;
 
-// 最小差值生成树，java版
-// 给定一个无向连通图，图中可能存在自环，选择一棵生成树
-// 希望生成树中最大边权减去最小边权的结果尽量小
-// 1 <= n <= 5 * 10^4
-// 1 <= m <= 2 * 10^5
-// 测试链接 : https://www.luogu.com.cn/problem/P4234
+// 魔法森林，java版
+// 给定一个无向图，每条边有a、b两种权值，可能存在重边和自环
+// 定义路径的代价为，路径上最大的a + 路径上最大的b
+// 选择1号点到n号点的路径，打印路径代价最小值，无法到达打印-1
+// 2 <= n <= 5 * 10^4
+// 0 <= m <= 10^5
+// 1 <= a、b <= 5 * 10^4
+// 测试链接 : https://www.luogu.com.cn/problem/P2387
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
 import java.io.IOException;
@@ -13,40 +15,46 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-public class Code02_MinimumDifference1 {
+public class Code04_MagicForest1 {
 
-	public static int MAXN = 300001;
+	public static int MAXN = 200001;
+	public static int INF = 1000000001;
 	public static int n, m;
 
 	public static int[] ex = new int[MAXN];
 	public static int[] ey = new int[MAXN];
-	public static int[] ew = new int[MAXN];
+	public static int[] ea = new int[MAXN];
+	public static int[] eb = new int[MAXN];
 
-	// removed[i]表示第i条边是否被淘汰
-	public static boolean[] removed = new boolean[MAXN];
-
-	// 辅助splay
+	public static int[] father = new int[MAXN];
 	public static int[] fa = new int[MAXN];
 	public static int[] ls = new int[MAXN];
 	public static int[] rs = new int[MAXN];
 	public static boolean[] rev = new boolean[MAXN];
 	public static int[] sta = new int[MAXN];
 
-	// minEdge[x]表示以x为根的辅助splay中，编号最小的边
-	// 按照权值从小到大排序，所以编号最小就是边权最小
-	public static int[] minEdge = new int[MAXN];
+	// maxbEdge[x]表示以x为根的辅助splay中，最大b权值的边的编号
+	public static int[] maxbEdge = new int[MAXN];
 
-	// 手撸双指针快排，根据边权从小到大排序
+	public static int find(int x) {
+		if (x != father[x]) {
+			father[x] = find(father[x]);
+		}
+		return father[x];
+	}
+
+	// 手撸双指针快排，根据a权值从小到大排序
 	public static void sort(int l, int r) {
 		if (l >= r) return;
-		int i = l, j = r, pivot = ew[(l + r) >> 1], tmp;
+		int i = l, j = r, pivot = ea[(l + r) >> 1], tmp;
 		while (i <= j) {
-			while (ew[i] < pivot) i++;
-			while (ew[j] > pivot) j--;
+			while (ea[i] < pivot) i++;
+			while (ea[j] > pivot) j--;
 			if (i <= j) {
 				tmp = ex[i]; ex[i] = ex[j]; ex[j] = tmp;
 				tmp = ey[i]; ey[i] = ey[j]; ey[j] = tmp;
-				tmp = ew[i]; ew[i] = ew[j]; ew[j] = tmp;
+				tmp = ea[i]; ea[i] = ea[j]; ea[j] = tmp;
+				tmp = eb[i]; eb[i] = eb[j]; eb[j] = tmp;
 				i++; j--;
 			}
 		}
@@ -55,14 +63,12 @@ public class Code02_MinimumDifference1 {
 	}
 
 	public static void up(int x) {
-		minEdge[x] = x <= n ? 0 : x - n;
-		int le = minEdge[ls[x]];
-		int re = minEdge[rs[x]];
-		if (le != 0 && (minEdge[x] == 0 || le < minEdge[x])) {
-			minEdge[x] = le;
+		maxbEdge[x] = x <= n ? 0 : x - n;
+		if (eb[maxbEdge[ls[x]]] > eb[maxbEdge[x]]) {
+			maxbEdge[x] = maxbEdge[ls[x]];
 		}
-		if (re != 0 && (minEdge[x] == 0 || re < minEdge[x])) {
-			minEdge[x] = re;
+		if (eb[maxbEdge[rs[x]]] > eb[maxbEdge[x]]) {
+			maxbEdge[x] = maxbEdge[rs[x]];
 		}
 	}
 
@@ -189,42 +195,37 @@ public class Code02_MinimumDifference1 {
 	}
 
 	public static int compute() {
-		if (n <= 1) {
-			return 0;
-		}
 		sort(1, m);
-		int edgeCnt = 0;
-		int first = 1;
-		int ans = Integer.MAX_VALUE;
+		for (int i = 1; i <= n; i++) {
+			father[i] = i;
+		}
+		int ans = INF;
 		for (int i = 1; i <= m; i++) {
 			int x = ex[i];
 			int y = ey[i];
-			if (x == y) {
-				removed[i] = true;
-			} else {
-				makeroot(x);
-				if (findroot(y) != x) {
+			if (x != y) {
+				up(n + i);
+				if (find(x) != find(y)) {
+					father[find(x)] = find(y);
 					link(x, n + i);
 					link(y, n + i);
-					edgeCnt++;
 				} else {
 					split(x, y);
-					int pre = minEdge[y];
-					cut(ex[pre], n + pre);
-					cut(ey[pre], n + pre);
-					link(x, n + i);
-					link(y, n + i);
-					removed[pre] = true;
+					int pre = maxbEdge[y];
+					if (eb[i] < eb[pre]) {
+						cut(ex[pre], n + pre);
+						cut(ey[pre], n + pre);
+						link(x, n + i);
+						link(y, n + i);
+					}
 				}
 			}
-			while (removed[first]) {
-				first++;
-			}
-			if (edgeCnt == n - 1) {
-				ans = Math.min(ans, ew[i] - ew[first]);
+			if (find(1) == find(n)) {
+				split(1, n);
+				ans = Math.min(ans, ea[i] + eb[maxbEdge[n]]);
 			}
 		}
-		return ans;
+		return ans == INF ? -1 : ans;
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -235,7 +236,8 @@ public class Code02_MinimumDifference1 {
 		for (int i = 1; i <= m; i++) {
 			ex[i] = in.nextInt();
 			ey[i] = in.nextInt();
-			ew[i] = in.nextInt();
+			ea[i] = in.nextInt();
+			eb[i] = in.nextInt();
 		}
 		int ans = compute();
 		out.println(ans);
