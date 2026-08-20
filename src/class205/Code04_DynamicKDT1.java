106d2c5f0762b09e5882e71630097214ed92f02c
algorithmzuo <algorithmzuo@163.com>
2026-08-20 15:36:37 +0800

    modify code

---
+++ src/class205/Code04_DynamicKDT1.java
@@ -0,0 +1,214 @@
+package class205;
+
+// 动态KDT模版题1，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P4148
+// 提交以下的code，提交时请把类名改成"Main"，本题卡空间，无法通过
+// 想通过用C++实现，本节课Code04_DynamicKDT2文件就是C++的实现
+// 两个版本的逻辑完全一样，C++版本可以通过所有测试
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class Code04_DynamicKDT1 {
+
+	public static int MAXN = 200001;
+	public static int MAXP = 19;
+	public static int INF = 1 << 30;
+	public static int n, cntn;
+
+	public static int[][] arr = new int[MAXN][3];
+
+	public static int[] ls = new int[MAXN];
+	public static int[] rs = new int[MAXN];
+	public static int[] sum = new int[MAXN];
+	public static int[] xmin = new int[MAXN];
+	public static int[] xmax = new int[MAXN];
+	public static int[] ymin = new int[MAXN];
+	public static int[] ymax = new int[MAXN];
+
+	// root[i]表示大小为2的i次方的KDT的根节点编号
+	public static int[] root = new int[MAXP];
+
+	public static int first, last;
+
+	public static void swap(int i, int j) {
+		int tmp = arr[i][0];
+		arr[i][0] = arr[j][0];
+		arr[j][0] = tmp;
+		tmp = arr[i][1];
+		arr[i][1] = arr[j][1];
+		arr[j][1] = tmp;
+		tmp = arr[i][2];
+		arr[i][2] = arr[j][2];
+		arr[j][2] = tmp;
+	}
+
+	public static void partition(int l, int r, int pivot, int dimension) {
+		first = l;
+		last = r;
+		int i = l;
+		while (i <= last) {
+			if (arr[i][dimension] == pivot) {
+				i++;
+			} else if (arr[i][dimension] < pivot) {
+				swap(first++, i++);
+			} else {
+				swap(i, last--);
+			}
+		}
+	}
+
+	public static void randSelect(int l, int r, int i, int dimension) {
+		while (l <= r) {
+			int pivot = arr[l + (int) (Math.random() * (r - l + 1))][dimension];
+			partition(l, r, pivot, dimension);
+			if (i < first) {
+				r = first - 1;
+			} else if (i > last) {
+				l = last + 1;
+			} else {
+				break;
+			}
+		}
+	}
+
+	public static void maintain(int i) {
+		sum[i] = arr[i][2] + sum[ls[i]] + sum[rs[i]];
+		xmin[i] = Math.min(arr[i][0], Math.min(xmin[ls[i]], xmin[rs[i]]));
+		xmax[i] = Math.max(arr[i][0], Math.max(xmax[ls[i]], xmax[rs[i]]));
+		ymin[i] = Math.min(arr[i][1], Math.min(ymin[ls[i]], ymin[rs[i]]));
+		ymax[i] = Math.max(arr[i][1], Math.max(ymax[ls[i]], ymax[rs[i]]));
+	}
+
+	public static int build(int l, int r, int dimension) {
+		if (l > r) {
+			return 0;
+		}
+		int mid = (l + r) >> 1;
+		if (l == r) {
+			ls[mid] = 0;
+			rs[mid] = 0;
+		} else {
+			randSelect(l, r, mid, dimension);
+			ls[mid] = build(l, mid - 1, dimension ^ 1);
+			rs[mid] = build(mid + 1, r, dimension ^ 1);
+		}
+		maintain(mid);
+		return mid;
+	}
+
+	public static void add(int x, int y, int v) {
+		cntn++;
+		arr[cntn][0] = x;
+		arr[cntn][1] = y;
+		arr[cntn][2] = v;
+		int p = 0, siz = 1;
+		while (root[p] != 0) {
+			root[p] = 0;
+			p++;
+			siz <<= 1;
+		}
+		root[p] = build(cntn - siz + 1, cntn, 0);
+	}
+
+	public static int query(int p, int x1, int y1, int x2, int y2) {
+		if (p == 0) {
+			return 0;
+		}
+		if (xmax[p] < x1 || x2 < xmin[p] || ymax[p] < y1 || y2 < ymin[p]) {
+			return 0;
+		}
+		if (x1 <= xmin[p] && xmax[p] <= x2 && y1 <= ymin[p] && ymax[p] <= y2) {
+			return sum[p];
+		}
+		int ans = 0;
+		if (x1 <= arr[p][0] && arr[p][0] <= x2 && y1 <= arr[p][1] && arr[p][1] <= y2) {
+			ans += arr[p][2];
+		}
+		ans += query(ls[p], x1, y1, x2, y2);
+		ans += query(rs[p], x1, y1, x2, y2);
+		return ans;
+	}
+
+	public static int query(int x1, int y1, int x2, int y2) {
+		int ans = 0;
+		for (int i = 0; i < MAXP; i++) {
+			ans += query(root[i], x1, y1, x2, y2);
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		xmin[0] = ymin[0] = INF;
+		xmax[0] = ymax[0] = -INF;
+		int op, a, b, c, d, lastAns;
+		op = in.nextInt();
+		lastAns = 0;
+		while (op != 3) {
+			a = in.nextInt();
+			b = in.nextInt();
+			c = in.nextInt();
+			a ^= lastAns;
+			b ^= lastAns;
+			c ^= lastAns;
+			if (op == 1) {
+				add(a, b, c);
+			} else {
+				d = in.nextInt();
+				d ^= lastAns;
+				lastAns = query(a, b, c, d);
+				out.println(lastAns);
+			}
+			op = in.nextInt();
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+
+	}
+
+}
---
+++ src/class205/Code04_DynamicKDT2.java
@@ -0,0 +1,159 @@
+package class205;
+
+// 动态KDT模版题1，C++版
+// 测试链接 : https://www.luogu.com.cn/problem/P4148
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct Node {
+//    int x;
+//    int y;
+//    int v;
+//};
+//
+//const int MAXN = 200001;
+//const int MAXP = 19;
+//const int INF = 1 << 30;
+//int n, cntn;
+//
+//Node arr[MAXN];
+//
+//int ls[MAXN];
+//int rs[MAXN];
+//int sum[MAXN];
+//int xmin[MAXN];
+//int xmax[MAXN];
+//int ymin[MAXN];
+//int ymax[MAXN];
+//
+//int root[MAXP];
+//
+//int first, last;
+//
+//void partition(int l, int r, int pivot, int dimension) {
+//    first = l;
+//    last = r;
+//    int i = l;
+//    while (i <= last) {
+//        int cur = dimension == 0 ? arr[i].x : arr[i].y;
+//        if (cur == pivot) {
+//            i++;
+//        } else if (cur < pivot) {
+//            swap(arr[first++], arr[i++]);
+//        } else {
+//            swap(arr[i], arr[last--]);
+//        }
+//    }
+//}
+//
+//void randSelect(int l, int r, int i, int dimension) {
+//    while (l <= r) {
+//        int idx = l + rand() % (r - l + 1);
+//        int pivot = dimension == 0 ? arr[idx].x : arr[idx].y;
+//        partition(l, r, pivot, dimension);
+//        if (i < first) {
+//            r = first - 1;
+//        } else if (i > last) {
+//            l = last + 1;
+//        } else {
+//            break;
+//        }
+//    }
+//}
+//
+//void maintain(int i) {
+//    sum[i] = arr[i].v + sum[ls[i]] + sum[rs[i]];
+//    xmin[i] = min(arr[i].x, min(xmin[ls[i]], xmin[rs[i]]));
+//    xmax[i] = max(arr[i].x, max(xmax[ls[i]], xmax[rs[i]]));
+//    ymin[i] = min(arr[i].y, min(ymin[ls[i]], ymin[rs[i]]));
+//    ymax[i] = max(arr[i].y, max(ymax[ls[i]], ymax[rs[i]]));
+//}
+//
+//int build(int l, int r, int dimension) {
+//    if (l > r) {
+//        return 0;
+//    }
+//    int mid = (l + r) >> 1;
+//    if (l == r) {
+//        ls[mid] = 0;
+//        rs[mid] = 0;
+//    } else {
+//        randSelect(l, r, mid, dimension);
+//        ls[mid] = build(l, mid - 1, dimension ^ 1);
+//        rs[mid] = build(mid + 1, r, dimension ^ 1);
+//    }
+//    maintain(mid);
+//    return mid;
+//}
+//
+//void add(int x, int y, int v) {
+//    cntn++;
+//    arr[cntn].x = x;
+//    arr[cntn].y = y;
+//    arr[cntn].v = v;
+//    int p = 0, siz = 1;
+//    while (root[p] != 0) {
+//        root[p] = 0;
+//        p++;
+//        siz <<= 1;
+//    }
+//    root[p] = build(cntn - siz + 1, cntn, 0);
+//}
+//
+//int query(int p, int x1, int y1, int x2, int y2) {
+//    if (p == 0) {
+//        return 0;
+//    }
+//    if (xmax[p] < x1 || x2 < xmin[p] || ymax[p] < y1 || y2 < ymin[p]) {
+//        return 0;
+//    }
+//    if (x1 <= xmin[p] && xmax[p] <= x2 && y1 <= ymin[p] && ymax[p] <= y2) {
+//        return sum[p];
+//    }
+//    int ans = 0;
+//    if (x1 <= arr[p].x && arr[p].x <= x2 && y1 <= arr[p].y && arr[p].y <= y2) {
+//        ans += arr[p].v;
+//    }
+//    ans += query(ls[p], x1, y1, x2, y2);
+//    ans += query(rs[p], x1, y1, x2, y2);
+//    return ans;
+//}
+//
+//int query(int x1, int y1, int x2, int y2) {
+//    int ans = 0;
+//    for (int i = 0; i < MAXP; i++) {
+//        ans += query(root[i], x1, y1, x2, y2);
+//    }
+//    return ans;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cin >> n;
+//    xmin[0] = ymin[0] = INF;
+//    xmax[0] = ymax[0] = -INF;
+//    int op, a, b, c, d, lastAns;
+//    cin >> op;
+//    lastAns = 0;
+//    while (op != 3) {
+//        cin >> a >> b >> c;
+//        a ^= lastAns;
+//        b ^= lastAns;
+//        c ^= lastAns;
+//        if (op == 1) {
+//            add(a, b, c);
+//        } else {
+//            cin >> d;
+//            d ^= lastAns;
+//            lastAns = query(a, b, c, d);
+//            cout << lastAns << "\n";
+//        }
+//        cin >> op;
+//    }
+//    return 0;
+//}
