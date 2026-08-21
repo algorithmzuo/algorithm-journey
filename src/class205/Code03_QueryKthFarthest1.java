5a9c5ac7a0258d1ebd846135c4d0a3ae731f7e9d
algorithmzuo <algorithmzuo@163.com>
2026-08-21 13:40:18 +0800

    modify code

--- src/class205/Code03_QueryKthFarthest1.java
+++ src/class205/Code03_QueryKthFarthest1.java
@@ -1,44 +1,49 @@
 package class205;
 
-// 巧克力王国，java版
-// 一共n个点，每个点有坐标(x, y)，还有点权v
-// 一共m条查询，格式 a b c，含义如下
-// 满足 a * x + b * y < c 的所有点，打印点权累加和
-// 1 <= n、m <= 5 * 10^4
-// -10^9 <= a、b、x、y <= +10^9
-// 测试链接 : https://www.luogu.com.cn/problem/P4475
+// 查询第k远，java版
+// 测试链接 : https://www.luogu.com.cn/problem/P2093
 // 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.PriorityQueue;
 
-public class Code03_ChocolateKingdom1 {
+public class Code03_QueryKthFarthest1 {
 
-	public static int MAXN = 50001;
+	public static int MAXN = 100001;
 	public static long INF = 1L << 60;
 	public static int n, m;
+	public static long qx, qy, qk;
 
-	// x y v
+	// x、y、id
 	public static long[][] arr = new long[MAXN][3];
 
 	public static int[] ls = new int[MAXN];
 	public static int[] rs = new int[MAXN];
 
-	// sum[i]表示以i为根的K-D Tree子树，所有点的权值和
-	public static long[] sum = new long[MAXN];
 	public static long[] xmin = new long[MAXN];
 	public static long[] xmax = new long[MAXN];
 	public static long[] ymin = new long[MAXN];
 	public static long[] ymax = new long[MAXN];
 
+	// dist、id
+	public static PriorityQueue<long[]> heap = new PriorityQueue<>(
+			(a, b) -> a[0] != b[0] ? Long.compare(a[0], b[0]) : Long.compare(b[1], a[1]));
+
 	public static int first, last;
 
 	public static void swap(int i, int j) {
-		long tmp = arr[i][0]; arr[i][0] = arr[j][0]; arr[j][0] = tmp;
-		tmp = arr[i][1]; arr[i][1] = arr[j][1]; arr[j][1] = tmp;
-		tmp = arr[i][2]; arr[i][2] = arr[j][2]; arr[j][2] = tmp;
+		long tmp = arr[i][0];
+		arr[i][0] = arr[j][0];
+		arr[j][0] = tmp;
+		tmp = arr[i][1];
+		arr[i][1] = arr[j][1];
+		arr[j][1] = tmp;
+		tmp = arr[i][2];
+		arr[i][2] = arr[j][2];
+		arr[j][2] = tmp;
 	}
 
 	public static void partition(int l, int r, long pivot, int dimension) {
@@ -71,7 +76,6 @@ public class Code03_ChocolateKingdom1 {
 	}
 
 	public static void maintain(int i) {
-		sum[i] = sum[ls[i]] + sum[rs[i]] + arr[i][2];
 		xmin[i] = Math.min(arr[i][0], Math.min(xmin[ls[i]], xmin[rs[i]]));
 		xmax[i] = Math.max(arr[i][0], Math.max(xmax[ls[i]], xmax[rs[i]]));
 		ymin[i] = Math.min(arr[i][1], Math.min(ymin[ls[i]], ymin[rs[i]]));
@@ -95,51 +99,82 @@ public class Code03_ChocolateKingdom1 {
 		return mid;
 	}
 
-	public static long query(int a, int b, int c, int l, int r) {
-		if (l > r) {
+	public static long dist(long x1, long y1, long x2, long y2) {
+		long dx = x1 - x2;
+		long dy = y1 - y2;
+		return dx * dx + dy * dy;
+	}
+
+	public static long guess(int rt) {
+		if (rt == 0) {
 			return 0;
+		}
+		long dx = Math.max(Math.abs(qx - xmin[rt]), Math.abs(qx - xmax[rt]));
+		long dy = Math.max(Math.abs(qy - ymin[rt]), Math.abs(qy - ymax[rt]));
+		return dx * dx + dy * dy;
+	}
+
+	public static void updateAns(int l, int r) {
+		if (l > r) {
+			return;
 		}
 		int mid = (l + r) >> 1;
-	    // a、b、x、y，可能是正或者负
-		long ax1 = xmin[mid] * a;
-		long ax2 = xmax[mid] * a;
-		long by1 = ymin[mid] * b;
-		long by2 = ymax[mid] * b;
-		long minv = Math.min(ax1, ax2) + Math.min(by1, by2);
-		long maxv = Math.max(ax1, ax2) + Math.max(by1, by2);
-		if (minv >= c) {
-			return 0;
-		} else if (maxv < c) {
-			return sum[mid];
-		} else {
-			long ans = 0;
-			if (a * arr[mid][0] + b * arr[mid][1] < c) {
-				ans += arr[mid][2];
-			}
-			ans += query(a, b, c, l, mid - 1);
-			ans += query(a, b, c, mid + 1, r);
-			return ans;
+		long d = dist(qx, qy, arr[mid][0], arr[mid][1]);
+		if (d > heap.peek()[0] || (d == heap.peek()[0] && arr[mid][2] < heap.peek()[1])) {
+			heap.poll();
+			heap.add(new long[] { d, arr[mid][2] });
 		}
+		if (l < r) {
+			long gl = guess(ls[mid]);
+			long gr = guess(rs[mid]);
+			if (gl > gr) {
+				// 注意判断，必须是 >=
+				// 因为本题距离相同时编号较小者更优
+				// 所以距离相同，依然可能存在编号更小的点
+				if (gl >= heap.peek()[0]) {
+					updateAns(l, mid - 1);
+				}
+				if (gr >= heap.peek()[0]) {
+					updateAns(mid + 1, r);
+				}
+			} else {
+				if (gr >= heap.peek()[0]) {
+					updateAns(mid + 1, r);
+				}
+				if (gl >= heap.peek()[0]) {
+					updateAns(l, mid - 1);
+				}
+			}
+		}
+	}
+
+	public static long query() {
+		heap.clear();
+		for (int i = 1; i <= qk; i++) {
+			heap.add(new long[] { -1, 0 });
+		}
+		updateAns(1, n);
+		return heap.peek()[1];
 	}
 
 	public static void main(String[] args) throws Exception {
 		FastReader in = new FastReader(System.in);
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
 		n = in.nextInt();
-		m = in.nextInt();
 		for (int i = 1; i <= n; i++) {
-			arr[i][0] = in.nextInt();
-			arr[i][1] = in.nextInt();
-			arr[i][2] = in.nextInt();
+			arr[i][0] = in.nextLong();
+			arr[i][1] = in.nextLong();
+			arr[i][2] = i;
 		}
 		xmin[0] = ymin[0] = INF;
 		xmax[0] = ymax[0] = -INF;
 		build(1, n, 0);
-		for (int i = 1, a, b, c; i <= m; i++) {
-			a = in.nextInt();
-			b = in.nextInt();
-			c = in.nextInt();
-			out.println(query(a, b, c, 1, n));
+		m = in.nextInt();
+		for (int i = 1; i <= m; i++) {
+			qx = in.nextLong();
+			qy = in.nextLong();
+			qk = in.nextLong();
+			out.println(query());
 		}
 		out.flush();
 		out.close();
@@ -177,6 +212,24 @@ public class Code03_ChocolateKingdom1 {
 				c = readByte();
 			}
 			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+
+		long nextLong() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			long val = 0;
 			while (c > ' ' && c != -1) {
 				val = val * 10 + (c - '0');
 				c = readByte();
