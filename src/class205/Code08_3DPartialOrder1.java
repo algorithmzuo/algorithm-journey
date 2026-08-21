87495a5c542fe6cc5452de58a4e778f0b2dc3ca8
algorithmzuo <algorithmzuo@163.com>
2026-08-21 16:00:56 +0800

    modify code

---
+++ src/class205/Code08_3DPartialOrder1.java
@@ -0,0 +1,226 @@
+package class205;
+
+// 三维偏序，java版
+// 本题就是讲解170，题目1，讲了CDQ分治的解法，这里用kdt的解法
+// 一共有n个对象，属性值范围[1, k]，每个对象有a属性、b属性、c属性
+// f(i)表示，aj <= ai 且 bj <= bi 且 cj <= ci 且 j != i 的j的数量
+// ans(d)表示，f(i) == d 的i的数量
+// 打印所有的ans[d]，d的范围[0, n)
+// 1 <= n <= 10^5
+// 1 <= k <= 2 * 10^5
+// 测试链接 : https://www.luogu.com.cn/problem/P3810
+// 提交以下的code，提交时请把类名改成"Main"，kdt不是最优解，java实现超时
+// 想通过用C++实现，本节课Code08_3DPartialOrder2文件就是C++的实现
+// 两个版本的逻辑完全一样，C++版本可以通过所有测试
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class Code08_3DPartialOrder1 {
+
+	public static int MAXN = 100001;
+	public static int MAXP = 18;
+	public static int INF = 1 << 30;
+	public static int n, k, cntn;
+
+	// a、b、c
+	public static int[][] abcArr = new int[MAXN][3];
+	// b、c
+	public static int[][] bcArr = new int[MAXN][2];
+
+	// siz[i]表示子树i的节点个数
+	public static int[] siz = new int[MAXN];
+	public static int[] ls = new int[MAXN];
+	public static int[] rs = new int[MAXN];
+
+	public static int[] bmin = new int[MAXN];
+	public static int[] bmax = new int[MAXN];
+	public static int[] cmin = new int[MAXN];
+	public static int[] cmax = new int[MAXN];
+
+	public static int[] root = new int[MAXP];
+	public static int[] ans = new int[MAXN];
+
+	public static int first, last;
+
+	public static void swap(int i, int j) {
+		int tmp = bcArr[i][0]; bcArr[i][0] = bcArr[j][0]; bcArr[j][0] = tmp;
+		tmp = bcArr[i][1]; bcArr[i][1] = bcArr[j][1]; bcArr[j][1] = tmp;
+	}
+
+	public static void partition(int l, int r, int pivot, int dimension) {
+		first = l;
+		last = r;
+		int i = l;
+		while (i <= last) {
+			if (bcArr[i][dimension] == pivot) {
+				i++;
+			} else if (bcArr[i][dimension] < pivot) {
+				swap(first++, i++);
+			} else {
+				swap(i, last--);
+			}
+		}
+	}
+
+	public static void randSelect(int l, int r, int i, int dimension) {
+		while (l <= r) {
+			int pivot = bcArr[l + (int) (Math.random() * (r - l + 1))][dimension];
+			partition(l, r, pivot, dimension);
+			if (i < first) {
+				r = first - 1;
+			} else if (i > last) {
+				l = last + 1;
+			} else {
+				break;
+			}
+		}
+	}
+
+	public static void maintain(int i) {
+		siz[i] = siz[ls[i]] + siz[rs[i]] + 1;
+		bmin[i] = Math.min(bcArr[i][0], Math.min(bmin[ls[i]], bmin[rs[i]]));
+		bmax[i] = Math.max(bcArr[i][0], Math.max(bmax[ls[i]], bmax[rs[i]]));
+		cmin[i] = Math.min(bcArr[i][1], Math.min(cmin[ls[i]], cmin[rs[i]]));
+		cmax[i] = Math.max(bcArr[i][1], Math.max(cmax[ls[i]], cmax[rs[i]]));
+	}
+
+	public static int build(int l, int r, int dimension) {
+		if (l > r) {
+			return 0;
+		}
+		int mid = (l + r) >> 1;
+		if (l == r) {
+			ls[mid] = 0;
+			rs[mid] = 0;
+		} else {
+			randSelect(l, r, mid, dimension);
+			ls[mid] = build(l, mid - 1, dimension ^ 1);
+			rs[mid] = build(mid + 1, r, dimension ^ 1);
+		}
+		maintain(mid);
+		return mid;
+	}
+
+	public static void add(int b, int c) {
+		cntn++;
+		bcArr[cntn][0] = b;
+		bcArr[cntn][1] = c;
+		int p = 0;
+		while (root[p] != 0) {
+			root[p++] = 0;
+		}
+		root[p] = build(cntn - (1 << p) + 1, cntn, 0);
+	}
+
+	// 查询i的子树中，有多少点满足 b' <= b && c' <= c
+	public static int query(int b, int c, int i) {
+		if (i == 0) {
+			return 0;
+		}
+		// 整棵子树在查询矩形外
+		if (bmin[i] > b || cmin[i] > c) {
+			return 0;
+		}
+		// 整棵子树在查询矩形内
+		if (bmax[i] <= b && cmax[i] <= c) {
+			return siz[i];
+		}
+		int ans = 0;
+		// 当前点满足要求
+		if (bcArr[i][0] <= b && bcArr[i][1] <= c) {
+			ans++;
+		}
+		ans += query(b, c, ls[i]);
+		ans += query(b, c, rs[i]);
+		return ans;
+	}
+
+	public static int query(int b, int c) {
+		int ans = 0;
+		for (int p = 0; p < MAXP; p++) {
+			ans += query(b, c, root[p]);
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		n = in.nextInt();
+		k = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			abcArr[i][0] = in.nextInt();
+			abcArr[i][1] = in.nextInt();
+			abcArr[i][2] = in.nextInt();
+		}
+		// 根据a排序，三维偏序变成二维偏序
+		Arrays.sort(abcArr, 1, n + 1, (x, y) -> x[0] - y[0]);
+		bmin[0] = cmin[0] = INF;
+		bmax[0] = cmax[0] = -INF;
+		for (int l = 1, r = 1; l <= n; l = ++r) {
+			while (r + 1 <= n && abcArr[r + 1][0] == abcArr[l][0]) {
+				r++;
+			}
+			// 同组都加入
+			for (int i = l; i <= r; i++) {
+				add(abcArr[i][1], abcArr[i][2]);
+			}
+			for (int i = l; i <= r; i++) {
+				int cur = query(abcArr[i][1], abcArr[i][2]);
+				// 不能把自己统计进去
+				ans[cur - 1]++;
+			}
+		}
+		for (int d = 0; d < n; d++) {
+			out.println(ans[d]);
+		}
+		out.flush();
+		out.close();
+	}
+
+	// 读写工具类
+	static class FastReader {
+
+		private final byte[] buffer = new byte[1 << 16];
+		private int ptr = 0, len = 0;
+		private final InputStream in;
+
+		FastReader(InputStream in) {
+			this.in = in;
+		}
+
+		private int readByte() throws IOException {
+			if (ptr >= len) {
+				len = in.read(buffer);
+				ptr = 0;
+				if (len <= 0)
+					return -1;
+			}
+			return buffer[ptr++];
+		}
+
+		int nextInt() throws IOException {
+			int c;
+			do {
+				c = readByte();
+			} while (c <= ' ' && c != -1);
+			boolean neg = false;
+			if (c == '-') {
+				neg = true;
+				c = readByte();
+			}
+			int val = 0;
+			while (c > ' ' && c != -1) {
+				val = val * 10 + (c - '0');
+				c = readByte();
+			}
+			return neg ? -val : val;
+		}
+
+	}
+
+}
---
+++ src/class205/Code08_3DPartialOrder2.java
@@ -0,0 +1,177 @@
+package class205;
+
+// 三维偏序，C++版
+// 本题就是讲解170，题目1，讲了CDQ分治的解法，这里用kdt的解法
+// 一共有n个对象，属性值范围[1, k]，每个对象有a属性、b属性、c属性
+// f(i)表示，aj <= ai 且 bj <= bi 且 cj <= ci 且 j != i 的j的数量
+// ans(d)表示，f(i) == d 的i的数量
+// 打印所有的ans[d]，d的范围[0, n)
+// 1 <= n <= 10^5
+// 1 <= k <= 2 * 10^5
+// 测试链接 : https://www.luogu.com.cn/problem/P3810
+// 如下实现是C++的版本，C++版本和java版本逻辑完全一样
+// 提交如下代码，可以通过所有测试用例
+
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//struct ABC {
+//    int a;
+//    int b;
+//    int c;
+//};
+//
+//bool ABCCmp(ABC x, ABC y) {
+//    return x.a < y.a;
+//}
+//
+//struct BC {
+//    int b;
+//    int c;
+//};
+//
+//const int MAXN = 100001;
+//const int MAXP = 18;
+//const int INF = 1 << 30;
+//int n, k, cntn;
+//
+//ABC abcArr[MAXN];
+//BC bcArr[MAXN];
+//
+//int siz[MAXN];
+//int ls[MAXN];
+//int rs[MAXN];
+//
+//int bmin[MAXN];
+//int bmax[MAXN];
+//int cmin[MAXN];
+//int cmax[MAXN];
+//
+//int root[MAXP];
+//int ans[MAXN];
+//
+//int first, last;
+//
+//void partition(int l, int r, int pivot, int dimension) {
+//    first = l;
+//    last = r;
+//    int i = l;
+//    while (i <= last) {
+//        int cur = dimension == 0 ? bcArr[i].b : bcArr[i].c;
+//        if (cur == pivot) {
+//            i++;
+//        } else if (cur < pivot) {
+//            swap(bcArr[first++], bcArr[i++]);
+//        } else {
+//            swap(bcArr[i], bcArr[last--]);
+//        }
+//    }
+//}
+//
+//void randSelect(int l, int r, int i, int dimension) {
+//    while (l <= r) {
+//        int idx = l + rand() % (r - l + 1);
+//        int pivot = dimension == 0 ? bcArr[idx].b : bcArr[idx].c;
+//        partition(l, r, pivot, dimension);
+//        if (i < first) {
+//            r = first - 1;
+//        } else if (i > last) {
+//            l = last + 1;
+//        } else {
+//            break;
+//        }
+//    }
+//}
+//
+//void maintain(int i) {
+//    siz[i] = siz[ls[i]] + siz[rs[i]] + 1;
+//    bmin[i] = min(bcArr[i].b, min(bmin[ls[i]], bmin[rs[i]]));
+//    bmax[i] = max(bcArr[i].b, max(bmax[ls[i]], bmax[rs[i]]));
+//    cmin[i] = min(bcArr[i].c, min(cmin[ls[i]], cmin[rs[i]]));
+//    cmax[i] = max(bcArr[i].c, max(cmax[ls[i]], cmax[rs[i]]));
+//}
+//
+//int build(int l, int r, int dimension) {
+//    if (l > r) {
+//        return 0;
+//    }
+//    int mid = (l + r) >> 1;
+//    if (l == r) {
+//        ls[mid] = 0;
+//        rs[mid] = 0;
+//    } else {
+//        randSelect(l, r, mid, dimension);
+//        ls[mid] = build(l, mid - 1, dimension ^ 1);
+//        rs[mid] = build(mid + 1, r, dimension ^ 1);
+//    }
+//    maintain(mid);
+//    return mid;
+//}
+//
+//void add(int b, int c) {
+//    cntn++;
+//    bcArr[cntn].b = b;
+//    bcArr[cntn].c = c;
+//    int p = 0;
+//    while (root[p] != 0) {
+//        root[p++] = 0;
+//    }
+//    root[p] = build(cntn - (1 << p) + 1, cntn, 0);
+//}
+//
+//int query(int b, int c, int i) {
+//    if (i == 0) {
+//        return 0;
+//    }
+//    if (bmin[i] > b || cmin[i] > c) {
+//        return 0;
+//    }
+//    if (bmax[i] <= b && cmax[i] <= c) {
+//        return siz[i];
+//    }
+//    int ans = 0;
+//    if (bcArr[i].b <= b && bcArr[i].c <= c) {
+//        ans++;
+//    }
+//    ans += query(b, c, ls[i]);
+//    ans += query(b, c, rs[i]);
+//    return ans;
+//}
+//
+//int query(int b, int c) {
+//    int ans = 0;
+//    for (int p = 0; p < MAXP; p++) {
+//        ans += query(b, c, root[p]);
+//    }
+//    return ans;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    srand((unsigned)time(nullptr));
+//    cin >> n >> k;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> abcArr[i].a >> abcArr[i].b >> abcArr[i].c;
+//    }
+//    sort(abcArr + 1, abcArr + n + 1, ABCCmp);
+//    bmin[0] = cmin[0] = INF;
+//    bmax[0] = cmax[0] = -INF;
+//    for (int l = 1, r = 1; l <= n; l = ++r) {
+//        while (r + 1 <= n && abcArr[r + 1].a == abcArr[l].a) {
+//            r++;
+//        }
+//        for (int i = l; i <= r; i++) {
+//            add(abcArr[i].b, abcArr[i].c);
+//        }
+//        for (int i = l; i <= r; i++) {
+//            int cur = query(abcArr[i].b, abcArr[i].c);
+//            ans[cur - 1]++;
+//        }
+//    }
+//    for (int d = 0; d < n; d++) {
+//        cout << ans[d] << "\n";
+//    }
+//    return 0;
+//}
