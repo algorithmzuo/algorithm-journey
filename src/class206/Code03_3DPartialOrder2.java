32097b9b71c9235bea53865928767490ed837b2f
algorithmzuo <algorithmzuo@163.com>
2026-08-30 19:47:28 +0800

    moidfy code

--- src/class206/Code03_3DPartialOrder2.java
+++ src/class206/Code03_3DPartialOrder2.java
@@ -1,6 +1,6 @@
 package class206;
 
-// 三维偏序，二进制分组的方式重构，C++版
+// 三维偏序，C++版
 // 本题就是讲解170，题目1，讲了CDQ分治的解法，这里用kdt的解法
 // 一共有n个对象，每个对象有a、b、c三个属性，每个属性值的范围都是[1, k]
 // f(i)表示，aj <= ai 且 bj <= bi 且 cj <= ci 且 j != i 的j的数量
@@ -17,55 +17,68 @@ package class206;
 //using namespace std;
 //
 //struct ABC {
-//    int a;
-//    int b;
-//    int c;
+//    int a, b, c;
 //};
 //
-//bool ABCCmp(ABC x, ABC y) {
+//bool ACmp(ABC x, ABC y) {
 //    return x.a < y.a;
 //}
 //
-//struct BC {
-//    int b;
-//    int c;
-//};
-//
-//bool BCmp(BC x, BC y) {
-//    return x.b < y.b;
-//}
-//
-//bool CCmp(BC x, BC y) {
-//    return x.c < y.c;
-//}
-//
 //const int MAXN = 100001;
-//const int MAXP = 18;
 //const int INF = 1 << 30;
 //int n, k;
 //
 //ABC abc[MAXN];
 //
+//int b[MAXN];
+//int c[MAXN];
+//
 //int cntkdt;
-//int root[MAXP];
-//BC arr[MAXN];
+//int root;
 //int siz[MAXN];
 //int ls[MAXN];
 //int rs[MAXN];
-//
 //int bmin[MAXN];
 //int bmax[MAXN];
 //int cmin[MAXN];
 //int cmax[MAXN];
 //
+//double ALPHA = 0.7;
+//int top;
+//int topFather;
+//int topSide;
+//int topDimension;
+//
+//int arr[MAXN];
+//int treeSiz;
+//
 //int ans[MAXN];
+//
+//int init(int qb, int qc) {
+//    cntkdt++;
+//    b[cntkdt] = qb;
+//    c[cntkdt] = qc;
+//    ls[cntkdt] = rs[cntkdt] = 0;
+//    siz[cntkdt] = 1;
+//    bmin[cntkdt] = bmax[cntkdt] = qb;
+//    cmin[cntkdt] = cmax[cntkdt] = qc;
+//    return cntkdt;
+//}
 //
 //void maintain(int i) {
 //    siz[i] = siz[ls[i]] + siz[rs[i]] + 1;
-//    bmin[i] = min(arr[i].b, min(bmin[ls[i]], bmin[rs[i]]));
-//    bmax[i] = max(arr[i].b, max(bmax[ls[i]], bmax[rs[i]]));
-//    cmin[i] = min(arr[i].c, min(cmin[ls[i]], cmin[rs[i]]));
-//    cmax[i] = max(arr[i].c, max(cmax[ls[i]], cmax[rs[i]]));
+//    bmin[i] = min(b[i], min(bmin[ls[i]], bmin[rs[i]]));
+//    bmax[i] = max(b[i], max(bmax[ls[i]], bmax[rs[i]]));
+//    cmin[i] = min(c[i], min(cmin[ls[i]], cmin[rs[i]]));
+//    cmax[i] = max(c[i], max(cmax[ls[i]], cmax[rs[i]]));
+//}
+//
+//bool BCmp(int x, int y) {
+//    return b[x] < b[y];
+//}
+//
+//bool CCmp(int x, int y) {
+//    return c[x] < c[y];
 //}
 //
 //int build(int l, int r, int dimension) {
@@ -78,21 +91,72 @@ package class206;
 //    } else {
 //        nth_element(arr + l, arr + mid, arr + r + 1, CCmp);
 //    }
-//    ls[mid] = build(l, mid - 1, dimension ^ 1);
-//    rs[mid] = build(mid + 1, r, dimension ^ 1);
-//    maintain(mid);
-//    return mid;
+//    int rt = arr[mid];
+//    ls[rt] = build(l, mid - 1, dimension ^ 1);
+//    rs[rt] = build(mid + 1, r, dimension ^ 1);
+//    maintain(rt);
+//    return rt;
+//}
+//
+//bool balance(int i) {
+//    return ALPHA * siz[i] >= max(siz[ls[i]], siz[rs[i]]);
+//}
+//
+//void dfs(int i) {
+//    if (i != 0) {
+//        arr[++treeSiz] = i;
+//        dfs(ls[i]);
+//        dfs(rs[i]);
+//    }
+//}
+//
+//void rebuild() {
+//    if (top != 0) {
+//        treeSiz = 0;
+//        dfs(top);
+//        int rt = build(1, treeSiz, topDimension);
+//        if (topFather == 0) {
+//            root = rt;
+//        } else if (topSide == 1) {
+//            ls[topFather] = rt;
+//        } else {
+//            rs[topFather] = rt;
+//        }
+//    }
+//}
+//
+//void add(int insertNode, int u, int fa, int side, int dimension) {
+//    if (u == 0) {
+//        if (fa == 0) {
+//            root = insertNode;
+//        } else if (side == 1) {
+//            ls[fa] = insertNode;
+//        } else {
+//            rs[fa] = insertNode;
+//        }
+//    } else {
+//        int insertd = dimension == 0 ? b[insertNode] : c[insertNode];
+//        int ud = dimension == 0 ? b[u] : c[u];
+//        if (insertd <= ud) {
+//            add(insertNode, ls[u], u, 1, dimension ^ 1);
+//        } else {
+//            add(insertNode, rs[u], u, 2, dimension ^ 1);
+//        }
+//        maintain(u);
+//        if (!balance(u)) {
+//            top = u;
+//            topFather = fa;
+//            topSide = side;
+//            topDimension = dimension;
+//        }
+//    }
 //}
 //
 //void add(int qb, int qc) {
-//    cntkdt++;
-//    arr[cntkdt].b = qb;
-//    arr[cntkdt].c = qc;
-//    int p = 0;
-//    while (root[p] != 0) {
-//        root[p++] = 0;
-//    }
-//    root[p] = build(cntkdt - (1 << p) + 1, cntkdt, 0);
+//    top = topFather = topSide = topDimension = 0;
+//    int insertNode = init(qb, qc);
+//    add(insertNode, root, 0, 0, 0);
+//    rebuild();
 //}
 //
 //int query(int qb, int qc, int i) {
@@ -106,19 +170,11 @@ package class206;
 //        return siz[i];
 //    }
 //    int ans = 0;
-//    if (arr[i].b <= qb && arr[i].c <= qc) {
+//    if (b[i] <= qb && c[i] <= qc) {
 //        ans++;
 //    }
 //    ans += query(qb, qc, ls[i]);
 //    ans += query(qb, qc, rs[i]);
-//    return ans;
-//}
-//
-//int query(int qb, int qc) {
-//    int ans = 0;
-//    for (int p = 0; p < MAXP; p++) {
-//        ans += query(qb, qc, root[p]);
-//    }
 //    return ans;
 //}
 //
@@ -129,7 +185,7 @@ package class206;
 //    for (int i = 1; i <= n; i++) {
 //        cin >> abc[i].a >> abc[i].b >> abc[i].c;
 //    }
-//    sort(abc + 1, abc + n + 1, ABCCmp);
+//    sort(abc + 1, abc + n + 1, ACmp);
 //    bmin[0] = cmin[0] = INF;
 //    bmax[0] = cmax[0] = -INF;
 //    for (int l = 1, r = 1; l <= n; l = ++r) {
@@ -140,7 +196,7 @@ package class206;
 //            add(abc[i].b, abc[i].c);
 //        }
 //        for (int i = l; i <= r; i++) {
-//            int cur = query(abc[i].b, abc[i].c);
+//            int cur = query(abc[i].b, abc[i].c, root);
 //            ans[cur - 1]++;
 //        }
 //    }
