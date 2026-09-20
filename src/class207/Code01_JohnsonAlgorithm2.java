39df76d3eb1a4d2e7b0291bd3c1d25658afbf2a7
algorithmzuo <algorithmzuo@163.com>
2026-09-20 17:28:10 +0800

    modify code

--- src/class207/Code01_JohnsonAlgorithm2.java
+++ src/class207/Code01_JohnsonAlgorithm2.java
@@ -2,7 +2,7 @@ package class207;
 
 // Johnson全源最短路，C++版
 // 测试链接 : https://www.luogu.com.cn/problem/P5905
-// 如下实现是C++的版本，C++版本使用正常的dijkstra算法
+// 如下实现是C++的版本，使用普通版本的dijkstra算法
 // 提交如下代码，可以通过所有测试用例
 
 //#include <bits/stdc++.h>
