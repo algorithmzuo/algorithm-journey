fe1e0f7792f5d11c57ce9e957579fbc507c7b059
algorithmzuo <algorithmzuo@163.com>
2026-09-23 14:44:26 +0800

    modify code

--- src/class207/Code03_Connectivity2.java
+++ src/class207/Code03_Connectivity2.java
@@ -1,13 +1,14 @@
 package class207;
 
-// 传递闭包，C++版
-// 一共n个点，给定有向图的邻接矩阵a，原图没有自环
+// 连通数，C++版
+// 一共n个点，给定有向图的邻接矩阵a
 // 如果 a[i][j] == 1，表示存在从i到j的有向边
 // 如果 a[i][j] == 0，表示不存在从i到j的有向边
-// 求有向图的传递闭包，输出关系矩阵b
-// 如果i经过至少一条有向边可以到达j，那么b[i][j]为1，否则为0
-// 1 <= n <= 100
-// 测试链接 : https://www.luogu.com.cn/problem/B3611
+// 如果i可以直接或间接到达j，则有序点对(i, j)计入答案
+// 每个点都认为可以到达自己，因此(i, i)也计入答案
+// 求所有可达有序点对的数量，打印这个数量
+// 1 <= n <= 2000
+// 测试链接 : https://www.luogu.com.cn/problem/P4306
 // 如下实现是C++的版本，C++版本和java版本逻辑完全一样
 // 提交如下代码，可以通过所有测试用例
 
@@ -15,7 +16,7 @@ package class207;
 //
 //using namespace std;
 //
-//const int MAXN = 101;
+//const int MAXN = 2001;
 //int n;
 //
 //bitset<MAXN> dp[MAXN];
@@ -34,23 +35,21 @@ package class207;
 //    ios::sync_with_stdio(false);
 //    cin.tie(nullptr);
 //    cin >> n;
-//    int s;
+//    char s;
 //    for (int i = 1; i <= n; i++) {
 //        for (int j = 1; j <= n; j++) {
 //            cin >> s;
-//            dp[i][j] = s;
+//            dp[i][j] = s == '1';
 //        }
+//    }
+//    for (int i = 1; i <= n; i++) {
+//        dp[i][i] = 1;
 //    }
 //    floyd();
+//    int ans = 0;
 //    for (int i = 1; i <= n; i++) {
-//        for (int j = 1; j <= n; j++) {
-//            if (dp[i][j]) {
-//                cout << "1 ";
-//            } else {
-//                cout << "0 ";
-//            }
-//        }
-//        cout << '\n';
+//        ans += dp[i].count();
 //    }
+//    cout << ans << '\n';
 //    return 0;
 //}
